9544269794a172a408509382ede7f300073073d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:54 +0000

    Add package mtpandas/1.11.202301150924/mtpandas-1.11.202301150924-py3-none-any.whl

---
+++ packages/mtpandas/mtpandas-1.11.202301150924-py3-none-any.whl/mt/pd/__init__.py
@@ -0,0 +1,7 @@
+"""Shorthand for loading pandas and mt.pandas into the same module."""
+
+from pandas import *
+import pandas.core as core
+import pandas.io as io
+import pandas.util as util
+from mt.pandas import *
