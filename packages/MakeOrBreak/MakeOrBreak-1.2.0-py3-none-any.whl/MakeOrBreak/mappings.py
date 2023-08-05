629dbad6238ad4be1af22c7394b560d5102ab213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:34 +0000

    Add package MakeOrBreak/1.2.0/MakeOrBreak-1.2.0-py3-none-any.whl

---
+++ packages/MakeOrBreak/MakeOrBreak-1.2.0-py3-none-any.whl/MakeOrBreak/mappings.py
@@ -0,0 +1,14 @@
+
+INTERVAL_TYPE_MAPPING = {
+   "Hourly": 0,
+   "Custom": 1,
+   "Random": 2
+}
+
+DIFFICULTY_MODE_MAPPING = {
+   "Hard": 0,
+   "Normal": 1,
+   "Easy": 2,
+}
+
+CONTENT_TYPE = ['compliments', 'insults']
