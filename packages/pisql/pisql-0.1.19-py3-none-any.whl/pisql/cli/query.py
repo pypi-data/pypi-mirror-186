76ea604629600049524a2d576e8061fbb9c46a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:14 +0000

    Add package pisql/0.1.19/pisql-0.1.19-py3-none-any.whl

---
+++ packages/pisql/pisql-0.1.19-py3-none-any.whl/pisql/cli/query.py
@@ -0,0 +1,20 @@
+import polars as pl
+import typer
+from pathlib import Path
+from typing import List, Tuple, Dict, Union, Optional, Any, Iterator, TypeVar
+from pisql.core.isql import iSQL
+from pisql.utils.paths import (
+    base_dir, tests_dir, package_dir
+)
+
+qapp = typer.Typer(
+    name="query",
+    help="Run a query against a Sybase ASE database",
+    no_args_is_help=True,
+)
+
+@qapp.command()
+def query(
+    query: typer.Argument(..., help="The query to run"),
+    ):
+    pass
