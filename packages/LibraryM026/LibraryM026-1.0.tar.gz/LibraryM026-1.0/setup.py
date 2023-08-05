d7eee19a13c67c9a27e76e96be98bdcec428ed48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:03 +0000

    Add package LibraryM026/1.0/LibraryM026-1.0.tar.gz

---
+++ packages/LibraryM026/LibraryM026-1.0.tar.gz/LibraryM026-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(name='LibraryM026',
+     version='1.0',
+    author='Aimen Ali',
+    description='Practising Basic Library',
+    long_description='A much longer explanation of the project and helpful resources',
+    url='',
+    keywords='development, setup, setuptools',
+    python_requires='>=3.7, <4',
+    packages=[],
+    install_requires=[]
+    )
