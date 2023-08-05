98d23898ce223e6809809f6b0cd6458f87d138e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:11 +0000

    Add package polars/0.15.15/polars-0.15.15.tar.gz

---
+++ packages/polars/polars-0.15.15.tar.gz/polars-0.15.15/local_dependencies/polars-ops/src/chunked_array/strings/mod.rs
@@ -0,0 +1,22 @@
+#[cfg(feature = "extract_jsonpath")]
+#[cfg_attr(docsrs, doc(cfg(feature = "extract_jsonpath")))]
+mod json_path;
+#[cfg(feature = "strings")]
+#[cfg_attr(docsrs, doc(cfg(feature = "strings")))]
+mod namespace;
+
+#[cfg(feature = "extract_jsonpath")]
+pub use json_path::*;
+#[cfg(feature = "strings")]
+pub use namespace::*;
+use polars_core::prelude::*;
+
+pub trait AsUtf8 {
+    fn as_utf8(&self) -> &Utf8Chunked;
+}
+
+impl AsUtf8 for Utf8Chunked {
+    fn as_utf8(&self) -> &Utf8Chunked {
+        self
+    }
+}
