fc50133c66cb0aa774a208be40f429019488b27a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:16 +0000

    Add package pisql/0.1.20/pisql-0.1.20.tar.gz

---
+++ packages/pisql/pisql-0.1.20.tar.gz/pisql-0.1.20/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pisql', 'pisql.cli', 'pisql.core', 'pisql.utils']
+
+package_data = \
+{'': ['*'], 'pisql': ['assets/*', 'assets/data/*', 'assets/sql/*']}
+
+install_requires = \
+['iotree==0.1.15', 'polars>=0.15.13,<0.16.0', 'typer[all]>=0.7.0,<0.8.0']
+
+entry_points = \
+{'console_scripts': ['pisql = pisql.main:app']}
+
+setup_kwargs = {
+    'name': 'pisql',
+    'version': '0.1.20',
+    'description': '',
+    'long_description': '',
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
