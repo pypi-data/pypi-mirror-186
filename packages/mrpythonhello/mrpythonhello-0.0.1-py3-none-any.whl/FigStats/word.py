7f845c468746eab0959bf336dc1775de78713dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:51 +0000

    Add package mrpythonhello/0.0.1/mrpythonhello-0.0.1-py3-none-any.whl

---
+++ packages/mrpythonhello/mrpythonhello-0.0.1-py3-none-any.whl/FigStats/word.py
@@ -0,0 +1,6 @@
+def Sqr(a):
+    ''' Function to get the area of square'''
+    s = a**2
+    p = a * 4
+    print(f"Area of Square is: {s} & perimeter is: {p}")
+    print("this is my first online package in python")
