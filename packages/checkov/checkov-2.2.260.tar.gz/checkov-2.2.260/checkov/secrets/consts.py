1094725a93a47f50f19a3980440203321d0ebda2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:04 +0000

    Add package checkov/2.2.260/checkov-2.2.260.tar.gz

---
+++ packages/checkov/checkov-2.2.260.tar.gz/checkov-2.2.260/checkov/secrets/consts.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+
+class ValidationStatus(Enum):
+    PRIVILEGED = 'Privileged'
+    VALID = 'Valid'
+    INVALID = 'Invalid'
+    UNKNOWN = 'Unknown'
+    UNAVAILABLE = 'UNAVAILABLE'
+
+    def __str__(self) -> str:
+        # needed, because of a Python 3.11 change
+        return self.value
+
+
+class VerifySecretsResult(Enum):
+    INSUFFICIENT_PARAMS = 'INSUFFICIENT_PARAMS'
+    FAILURE = 'FAILURE'
+    SUCCESS = 'SUCESS'
+
+    def __str__(self) -> str:
+        # needed, because of a Python 3.11 change
+        return self.value
