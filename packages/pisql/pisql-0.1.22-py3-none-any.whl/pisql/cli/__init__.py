7d798342d5e3dd5aaa97dbd255b30c942bc1a92d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:23 +0000

    Add package pisql/0.1.22/pisql-0.1.22-py3-none-any.whl

---
+++ packages/pisql/pisql-0.1.22-py3-none-any.whl/pisql/cli/__init__.py
@@ -0,0 +1,3 @@
+from .query import qapp
+from .run import run
+from .execute import exe
---
+++ packages/pisql/pisql-0.1.22-py3-none-any.whl/pisql/utils/__init__.py
@@ -0,0 +1,6 @@
+from .paths import (
+    package_dir, base_dir, tests_dir,
+    assets_dir, data_dir, config, pisql_home
+)
+
+from .render import console, errors
