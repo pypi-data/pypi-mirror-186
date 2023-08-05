964c72a33a67e19135cf345b5d369d9a3765b1c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:28 +0000

    Add package shabanapackage/0.1/shabanapackage-0.1-py3-none-any.whl

---
+++ packages/shabanapackage/shabanapackage-0.1-py3-none-any.whl/shabanapackage/__init__.py
@@ -0,0 +1,14 @@
+s=lambda arg:len(arg)
+mylist=['I','Love','Pakistan']
+rv=s(mylist)
+rv
+ 
+s=lambda s:s[0]
+res=s("shabana")
+res
+
+
+s=lambda arg:len(arg)
+mylist=['I','Love','Pakistan']
+rv=s(mylist)
+rv
