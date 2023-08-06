1bb421ee4d845d4915c5bb38e8ee9d979a549718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:11 +0000

    Add package scaleway-async/0.4.0/scaleway_async-0.4.0-py3-none-any.whl

---
+++ packages/scaleway-async/scaleway_async-0.4.0-py3-none-any.whl/scaleway_async/__init__.py
@@ -0,0 +1,51 @@
+"""Scaleway SDK for Python - Async"""
+
+import pkg_resources
+
+__version__: str = pkg_resources.get_distribution(__name__).version
+
+from scaleway_core.api import (
+    API,
+    ScalewayException,
+)
+
+from scaleway_core.client import Client
+
+from scaleway_core.profile import (
+    Profile,
+    ProfileConfig,
+    ProfileDefaults,
+)
+
+from scaleway_core.utils.waiter import (
+    WaitForOptions,
+    WaitForStopCondition,
+)
+
+from scaleway_core.bridge import (
+    Money,
+    Region,
+    Zone,
+    ScwFile,
+    ServiceInfo,
+    TimeSeriesPoint,
+    TimeSeries,
+)
+
+__all__ = [
+    "API",
+    "ScalewayException",
+    "Client",
+    "Profile",
+    "ProfileConfig",
+    "ProfileDefaults",
+    "WaitForOptions",
+    "WaitForStopCondition",
+    "Money",
+    "Region",
+    "Zone",
+    "ScwFile",
+    "ServiceInfo",
+    "TimeSeriesPoint",
+    "TimeSeries",
+]
