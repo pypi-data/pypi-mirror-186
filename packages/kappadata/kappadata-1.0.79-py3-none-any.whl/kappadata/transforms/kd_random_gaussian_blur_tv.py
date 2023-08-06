11073d70f4aaeade6e12b15b25b18a456f010d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:29 +0000

    Add package kappadata/1.0.79/kappadata-1.0.79-py3-none-any.whl

---
+++ packages/kappadata/kappadata-1.0.79-py3-none-any.whl/kappadata/transforms/kd_random_gaussian_blur_tv.py
@@ -0,0 +1,20 @@
+from .base.kd_random_apply_base import KDRandomApplyBase
+from .kd_gaussian_blur_tv import KDGaussianBlurTV
+
+
+class KDRandomGaussianBlurTV(KDRandomApplyBase):
+    def __init__(self, kernel_size, sigma, **kwargs):
+        super().__init__(**kwargs)
+        seed = self.seed + 1 if self.seed is not None else None
+        self.gaussian_blur = KDGaussianBlurTV(kernel_size=kernel_size, sigma=sigma, seed=seed)
+
+    # TODO reset_seed is deprecated
+    def reset_seed(self):
+        super().reset_seed()
+        self.gaussian_blur.reset_seed()
+
+    def _scale_strength(self, factor):
+        self.gaussian_blur.scale_strength(factor)
+
+    def forward(self, x, ctx):
+        return self.gaussian_blur(x, ctx)
