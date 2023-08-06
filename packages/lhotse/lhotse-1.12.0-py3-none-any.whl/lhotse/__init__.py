711548a47604c77af5ab473f1f60b875b7e331aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:04 +0000

    Add package lhotse/1.12.0/lhotse-1.12.0-py3-none-any.whl

---
+++ packages/lhotse/lhotse-1.12.0-py3-none-any.whl/lhotse/__init__.py
@@ -0,0 +1,41 @@
+from .audio import (
+    AudioSource,
+    Recording,
+    RecordingSet,
+    set_audio_duration_mismatch_tolerance,
+)
+from .caching import is_caching_enabled, set_caching_enabled
+from .cut import CutSet, MonoCut, MultiCut, create_cut_set_eager, create_cut_set_lazy
+from .features import *
+from .kaldi import load_kaldi_data_dir
+from .manipulation import combine, split_parallelize_combine, to_manifest
+from .qa import fix_manifests, validate, validate_recordings_and_supervisions
+from .serialization import load_manifest, load_manifest_lazy, store_manifest
+from .supervision import SupervisionSegment, SupervisionSet
+from .tools.env import add_tools_to_path as _add_tools_to_path
+from .utils import (
+    Decibels,
+    Seconds,
+    add_durations,
+    compute_num_frames,
+    compute_num_samples,
+    fastcopy,
+    fix_random_seed,
+    measure_overlap,
+    streaming_shuffle,
+)
+from .workflows import *
+
+try:
+    # Try to get Lhotse's version (should be created during running pip install / python setup.py ...)
+    from .version import __version__
+except:
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #
+    # NOTE: REMEMBER TO UPDATE THE ACTUAL VERSION IN setup.py WHEN RELEASING #
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #
+    # Use a default placeholder when the version is unavailable...
+    __version__ = "1.12.0+missing.version.file"
+
+from . import augmentation, dataset, features, recipes
+
+_add_tools_to_path()
