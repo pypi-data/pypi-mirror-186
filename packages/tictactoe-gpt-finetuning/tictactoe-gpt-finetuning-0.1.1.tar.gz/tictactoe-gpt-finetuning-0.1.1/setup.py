59d568638044c07f7236bf82f457e7299e297e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:50 +0000

    Add package tictactoe-gpt-finetuning/0.1.1/tictactoe-gpt-finetuning-0.1.1.tar.gz

---
+++ packages/tictactoe-gpt-finetuning/tictactoe-gpt-finetuning-0.1.1.tar.gz/tictactoe-gpt-finetuning-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.rst") as f:
+    readme = f.read()
+
+kwargs = {
+    "name": "tictactoe-gpt-finetuning",
+    "version": "0.1.1",
+    "description": "Python tic tac toe state generator and GPT fine tuning",
+    "author": "Nicky Pochinkov",
+    "author_email": "work@nicky.pro",
+    "url": "https://github.com/pesvut/tictactoe-gpt-finetuning",
+    "license": "MIT",
+    "keywords": ["tictactoe", "llm", "language-models"],
+    "install_requires": ["numpy", "torch", "transformers"],
+    "packages": ["tictactoe_gpt_finetuning"],
+    "long_description": readme,
+}
+
+setup(**kwargs)
