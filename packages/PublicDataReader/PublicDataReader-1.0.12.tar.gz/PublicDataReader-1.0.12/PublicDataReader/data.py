0536907741f31ed3b269885a34919060f9a97b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:01 +0000

    Add package PublicDataReader/1.0.12/PublicDataReader-1.0.12.tar.gz

---
+++ packages/PublicDataReader/PublicDataReader-1.0.12.tar.gz/PublicDataReader-1.0.12/PublicDataReader/data.py
@@ -0,0 +1,32 @@
+# 국토교통부(molit) Open API 통합
+from PublicDataReader.PublicDataPortal.molit import TransactionPrice, BuildingLedger
+# (Deprecated)
+from PublicDataReader.PublicDataPortal.molit import Transaction, Building
+
+# 소상공인 진흥공단(semas) Open API 통합
+from PublicDataReader.PublicDataPortal.semas import SmallShop
+# (Deprecated)
+from PublicDataReader.PublicDataPortal.semas import StoreInfo
+
+# 서울시 지하철호선별 역별 승하차 인원 정보 Open API
+from PublicDataReader.Seoul.transportation import Transportation
+
+# KOSIS Open API
+from PublicDataReader.kosis.kosis import Kosis
+# (Deprecated)
+from PublicDataReader.kosis.kosis import _Kosis
+
+# Vworld Open API
+from PublicDataReader.vworld.data import VworldData
+
+# 한국자산관리공사 Open API 통합
+from PublicDataReader.PublicDataPortal.kamco import Kamco
+
+# 국세청 Open API
+from PublicDataReader.PublicDataPortal.nts import Nts
+
+# KB부동산 API
+from PublicDataReader.kbland.kbland import Kbland
+
+# 코드 데이터 조회
+from PublicDataReader.utils.code import code_bdong, code_hdong, code_hdong_bdong, get_vworld_data_api_info_by_dataframe, get_vworld_data_api_info_by_dict
