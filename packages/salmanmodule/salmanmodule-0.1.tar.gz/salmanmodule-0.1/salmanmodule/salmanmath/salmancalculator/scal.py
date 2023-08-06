68a24b93b00e5a1edd003c78541b554fc8bb2bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:53 +0000

    Add package salmanmodule/0.1/salmanmodule-0.1.tar.gz

---
+++ packages/salmanmodule/salmanmodule-0.1.tar.gz/salmanmodule-0.1/salmanmodule/salmanmath/salmancalculator/scal.py
@@ -0,0 +1,14 @@
+#my module, it's simple calculator sir
+
+def ad(a,b):
+    return a+b
+
+def sb(a,b):
+    return a-b
+
+def ml(a,b):
+    return a*b
+
+def dv(a,b):
+    return a/b
+
---
+++ packages/salmanmodule/salmanmodule-0.1.tar.gz/salmanmodule-0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+ 
+
+setuptools.setup(
+  name='salmanmodule',
+  version='0.1',
+  description='It is my Module for Public Use', 
+  author='Salman Mumtaz',
+  author_email='msdsf22m007@pucit.edu.pk',
+  classifiers= [
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3' ],
+  packages=setuptools.find_packages()
+)
