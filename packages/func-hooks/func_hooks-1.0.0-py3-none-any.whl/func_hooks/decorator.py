11ce1286ed7e855b54e110ef252bfd3f9914dd78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:35 +0000

    Add package func-hooks/1.0.0/func_hooks-1.0.0-py3-none-any.whl

---
+++ packages/func-hooks/func_hooks-1.0.0-py3-none-any.whl/func_hooks/decorator.py
@@ -0,0 +1,28 @@
+"""The decorator module."""
+import typing
+
+import typing_extensions
+
+
+from .sync import Hooks
+
+_P = typing_extensions.ParamSpec("_P")
+_R_co = typing.TypeVar("_R_co", covariant=True)
+
+
+def hooks(func: typing.Callable[_P, _R_co]) -> Hooks[_P, _R_co]:
+    """Decorate a function to have hoks.
+
+    Args:
+        func (typing.Callable[_P, _R_co]): The function to decorate
+
+    Raises:
+        ValueError: If the provided function is not callable
+
+    Returns:
+        Hooks[_P, _R_co]: A callable object with some decorators to provide hooks
+    """
+    if not callable(func):
+        raise ValueError("Provided function is not callable.")
+
+    return Hooks(func)
