4738a209f21441da76b15f7a96dc2e02024c3326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:17 +0000

    Add package wikidata-bot-framework/5.0.0/wikidata-bot-framework-5.0.0.tar.gz

---
+++ packages/wikidata-bot-framework/wikidata-bot-framework-5.0.0.tar.gz/wikidata-bot-framework-5.0.0/wikidata_bot_framework/constants.py
@@ -0,0 +1,12 @@
+from typing import Union
+
+import pywikibot
+from requests import Session
+
+session = Session()
+
+url_prop = "P854"
+retrieved_prop = "P813"
+site = pywikibot.Site("wikidata", "wikidata")
+
+EntityPage = Union[pywikibot.ItemPage, pywikibot.PropertyPage, pywikibot.LexemePage]
