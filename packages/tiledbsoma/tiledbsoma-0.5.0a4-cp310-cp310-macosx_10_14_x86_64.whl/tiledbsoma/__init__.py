b355e2a5f15348fd4f09d7a9942afa0c202fe62a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:30 +0000

    Add package tiledbsoma/0.5.0a4/tiledbsoma-0.5.0a4-cp310-cp310-macosx_10_14_x86_64.whl

---
+++ packages/tiledbsoma/tiledbsoma-0.5.0a4-cp310-cp310-macosx_10_14_x86_64.whl/tiledbsoma/__init__.py
@@ -0,0 +1,42 @@
+from somacore import AxisQuery, ExperimentAxisQuery
+
+from .collection import Collection
+from .dataframe import DataFrame
+from .dense_nd_array import DenseNDArray
+from .exception import DoesNotExistError, SOMAError
+from .experiment import Experiment
+from .general_utilities import (
+    get_implementation,
+    get_implementation_version,
+    get_SOMA_version,
+    get_storage_engine,
+)
+from .measurement import Measurement
+from .metadata_mapping import MetadataMapping
+from .query_condition import QueryCondition
+from .sparse_nd_array import SparseNDArray
+from .tiledb_array import TileDBArray
+from .tiledb_object import TileDBObject
+
+__version__ = get_implementation_version()
+
+__all__ = [
+    "get_implementation",
+    "get_implementation_version",
+    "get_SOMA_version",
+    "get_storage_engine",
+    "TileDBObject",
+    "TileDBArray",
+    "AxisQuery",
+    "ExperimentAxisQuery",
+    "Collection",
+    "DenseNDArray",
+    "DoesNotExistError",
+    "Experiment",
+    "SOMAError",
+    "DataFrame",
+    "Measurement",
+    "MetadataMapping",
+    "SparseNDArray",
+    "QueryCondition",
+]
