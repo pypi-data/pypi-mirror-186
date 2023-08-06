3c124a7b828642733af8bbcf5e53c6b1e1ab611b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:44 +0000

    Add package internet-ml/3.0.0/internet_ml-3.0.0-py3-none-any.whl

---
+++ packages/internet-ml/internet_ml-3.0.0-py3-none-any.whl/internet_ml/tools/NLP/ml/dataset/long_QA.py
@@ -0,0 +1,7 @@
+# type: ignore
+from typing import Any
+
+import datasets
+
+CoQA: Any = datasets.load_dataset("coqa")
+DATASET: List[Any] = []
---
+++ packages/internet-ml/internet_ml-3.0.0-py3-none-any.whl/internet_ml/tools/NLP/ml/training/gpt-neox-20b-quac.py
@@ -0,0 +1,6 @@
+import multiprocessing as mp
+
+import accelerate
+import datasets
+import torch
+import transformers
