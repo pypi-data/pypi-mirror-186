ecabfc67597935e6f04cf222b1f2d0e3be62a715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:03 +0000

    Add package limburg-flood-impact/0.2.3/limburg_flood_impact-0.2.3.tar.gz

---
+++ packages/limburg-flood-impact/limburg_flood_impact-0.2.3.tar.gz/limburg_flood_impact-0.2.3/src/limburg_flood_impact/bin/classify_area_wide_rain.py
@@ -0,0 +1,40 @@
+import argparse
+from pathlib import Path
+import sys
+
+from .._functions import common_arguments_parser, print_percent
+from ..classify_area_wide_rain import classify_area_wide_rain
+
+
+def main():
+
+    parser = argparse.ArgumentParser(prog='ClassifyAreaWideRain',
+                                     description='Classify area wide rain.',
+                                     epilog='',
+                                     parents=[common_arguments_parser()])
+
+    args = parser.parse_args()
+
+    buildings_path: Path = args.buildings
+    t10_path: Path = args.t10
+    t25_path: Path = args.t25
+    t100_path: Path = args.t100
+
+    if not buildings_path.exists():
+        raise ValueError("File {} does not exist.".format(buildings_path.absolute().as_posix()))
+
+    if not t10_path.exists():
+        raise ValueError("File {} does not exist.".format(t10_path.absolute().as_posix()))
+
+    if not t25_path.exists():
+        raise ValueError("File {} does not exist.".format(t25_path.absolute().as_posix()))
+
+    if not t100_path.exists():
+        raise ValueError("File {} does not exist.".format(t100_path.absolute().as_posix()))
+
+    classify_area_wide_rain(buildings_path, t10_path, t25_path, t100_path, print_percent)
+
+
+if __name__ == "__main__":
+
+    sys.exit(main())
