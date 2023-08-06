7fa9a9ef7f9e1b62e6d76eb562fc344e27034585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:39 +0000

    Add package kdd/0.0.1/kdd-0.0.1-py3-none-any.whl

---
+++ packages/kdd/kdd-0.0.1-py3-none-any.whl/kdd/__init__.py
@@ -0,0 +1,2 @@
+from kdd.kdd import KDD, EntityTypeMapping, Coordinates, BoundingBox, TextLayerSource, TokenInformation, TextLayer, EntityAnnotation, EncodedPDF
+from kdd.kdd_manager import KDDManager
