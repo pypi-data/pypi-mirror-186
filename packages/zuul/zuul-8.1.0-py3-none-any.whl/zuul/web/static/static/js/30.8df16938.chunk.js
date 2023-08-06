c445436e2cd044c4ec1d065ba29c284c3a9c8a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:46 +0000

    Add package zuul/8.1.0/zuul-8.1.0-py3-none-any.whl

---
+++ packages/zuul/zuul-8.1.0-py3-none-any.whl/zuul/web/static/static/js/30.8df16938.chunk.js
@@ -0,0 +1,1 @@
+(this["webpackJsonp@zuul-ci/dashboard"]=this["webpackJsonp@zuul-ci/dashboard"]||[]).push([[30],{2687:function(s,u,a){}}]);
---
+++ packages/zuul/zuul-8.1.0-py3-none-any.whl/zuul/web/static/static/js/40.f1b56f9a.chunk.js
@@ -0,0 +1,1 @@
+(this["webpackJsonp@zuul-ci/dashboard"]=this["webpackJsonp@zuul-ci/dashboard"]||[]).push([[40],{2693:function(s,u,a){}}]);
