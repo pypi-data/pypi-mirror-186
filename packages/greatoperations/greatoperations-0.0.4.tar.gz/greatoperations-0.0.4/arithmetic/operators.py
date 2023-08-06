d83255b2d330f74ee77a2ecd4099c8b5a8cc1105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:17 +0000

    Add package greatoperations/0.0.4/greatoperations-0.0.4.tar.gz

---
+++ packages/greatoperations/greatoperations-0.0.4.tar.gz/greatoperations-0.0.4/arithmetic/operators.py
@@ -0,0 +1,30 @@
+
+import functools
+
+def myadd(*numbers):
+	"""This function take arbitrary number of arguments and return the sum of all"""
+	list_1 = list(numbers)
+	sum = functools.reduce(lambda a, b : a + b, list_1)
+	return sum
+
+
+def mysub(a, b):
+	"""This function take two numbers and return the difference of them"""
+	return a - b
+
+
+def mymul(*numbers):
+    """This function take arbitrary number of arguments and return the product of all"""
+    list_1 = list(numbers)
+    result = functools.reduce(lambda a, b : a * b, list_1)
+    return result
+
+
+def mydiv(a, b):
+	"""This function take two numbers and return the quotient of them"""
+	return a / b
+
+
+def mypower(a, b):
+    """This function is used for taking power"""
+    return a ** b
