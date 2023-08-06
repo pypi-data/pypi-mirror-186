e887747e8e99cd3ed8095764eea241e5305daaa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:22 +0000

    Add package fe-pycocotools/1.0/fe-pycocotools-1.0.tar.gz

---
+++ packages/fe-pycocotools/fe-pycocotools-1.0.tar.gz/fe-pycocotools-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+# To compile and install locally run "python setup.py build_ext --inplace"
+# To install library to Python site-packages run "python setup.py build_ext install"
+
+setup(name='fe-pycocotools',
+      packages=['pycocotools'],
+      package_dir={'pycocotools': 'pycocotools'},
+      install_requires=['setuptools>=18.0', 'matplotlib>=2.1.0'],
+      version='1.0')
