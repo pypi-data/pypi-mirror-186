988c41b10e1f51b6e02c86124189d448083eec0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:41 +0000

    Add package ampltools/0.4.0/ampltools-0.4.0-py3-none-any.whl

---
+++ packages/ampltools/ampltools-0.4.0-py3-none-any.whl/ampltools/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+__version__ = "0.4.0"
+from .notebooks import (
+    ampl_notebook,
+)
+
+from .amplpypi import (
+    install_modules,
+    load_modules,
+    list_modules,
+    list_modules_available,
+)
+
+from .licenses import (
+    activate_license,
+)
+
+from .utils import (
+    cloud_platform_name,
+)
+
+from .commands import (
+    ampltools_main,
+)
