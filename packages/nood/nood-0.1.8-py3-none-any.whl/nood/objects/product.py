1b0987ad4efcbac4719622e4275c31ca770f6ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:22 +0000

    Add package nood/0.1.8/nood-0.1.8-py3-none-any.whl

---
+++ packages/nood/nood-0.1.8-py3-none-any.whl/nood/objects/product.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class Variant(BaseModel):
+    id: str
+    value: str
+    stock: Optional[int]
+    atc_url: Optional[str]
+    direct_url: Optional[str]
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+class Product(BaseModel):
+    site_id: Optional[int]
+    url: str
+    name: str
+    brand: Optional[str]
+    price: Optional[float]
+    currency: Optional[str]
+    sku: Optional[str]
+    variants: Optional[List[Variant]] = []
+    thumbnail_url: Optional[str]
+
+    class Config:
+        allow_population_by_field_name = True
