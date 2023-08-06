66bbc6bf9c719133e1b56a95774af1b1664ea273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:28 +0000

    Add package ixai/0.1.3/ixai-0.1.3-py3-none-any.whl

---
+++ packages/ixai/ixai-0.1.3-py3-none-any.whl/ixai/__init__.py
@@ -0,0 +1,6 @@
+"""ixai is a library for Incremental Explainable Artificial Intelligence. It provides specific XAI explanation methods
+that naturally work with dynamic time-dependent models. Commonly such models are trained in the incremental learning
+paradigm. Such models can easily be explained with ixai.
+"""
+
+from .__version__ import __version__
