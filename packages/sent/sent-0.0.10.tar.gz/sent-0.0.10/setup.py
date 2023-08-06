4a26c79b03fa4f955f984752ad6fa92ad8e67ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:36 +0000

    Add package sent/0.0.10/sent-0.0.10.tar.gz

---
+++ packages/sent/sent-0.0.10.tar.gz/sent-0.0.10/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='sent',
+    version='0.0.10',
+    packages=find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    url='https://github.com/kalanakt/sent',
+    author='kalanakt',
+    author_email='e19198@eng.pdn.ac.lk',
+    description='A package for sending Telegram messages',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='telegram api',
+    license='MIT',
+)
+
+# python setup.py sdist
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
