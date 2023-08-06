12356804a0ae058f415c6fd7748130fa904d8ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:27 +0000

    Add package cc-py-commons/0.3.7/cc_py_commons-0.3.7.tar.gz

---
+++ packages/cc-py-commons/cc_py_commons-0.3.7.tar.gz/cc_py_commons-0.3.7/cc_py_commons/services/mercury/get_lane_price.py
@@ -0,0 +1,25 @@
+import os
+import requests
+
+from cc_py_commons.utils.logger import logger
+
+MERCURY_URL = os.environ.get("MERCURY_URL")
+MERCURY_TOKEN = os.environ.get("MERCURY_TOKEN")
+
+
+def execute(params, for_bulk=False):
+	url = f"{MERCURY_URL}/lanePricing"
+	if for_bulk:
+		url = f'{url}/bulk'
+	token = f"Bearer {MERCURY_TOKEN}"
+	headers = {
+		"Authorization": token
+	}
+	response = requests.get(url, params=params, headers=headers)
+
+	if response.status_code != 200:
+		logger.warning(
+			f"Lane Pricing lookup failed for params: {params} - {response.status_code}:{response.text}")
+		return None
+
+	return response.json()
