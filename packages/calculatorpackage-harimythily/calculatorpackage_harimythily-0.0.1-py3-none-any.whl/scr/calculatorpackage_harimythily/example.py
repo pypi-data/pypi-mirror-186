af8f1917e2e8147aa9ee65e97e713e27f36249f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:32 +0000

    Add package calculatorpackage-harimythily/0.0.1/calculatorpackage_harimythily-0.0.1-py3-none-any.whl

---
+++ packages/calculatorpackage-harimythily/calculatorpackage_harimythily-0.0.1-py3-none-any.whl/scr/calculatorpackage_harimythily/example.py
@@ -0,0 +1,24 @@
+def addition (a,b):
+    A=a+b
+    print("ADDITION")
+    print(A)
+    return a+b
+def subtraction (a,b):
+    A=a-b
+    print("SUBTRACTION")
+    print(A)
+    return a-b
+def multiplication (a,b):
+    A=a*b
+    print("MULTIPLICATION")
+    print(A)
+    return a*b
+def division (a,b):
+    A=a/b
+    print("DIVISION")
+    print(A)
+    return a/b
+def getinput ():
+    a=int(input("Enter the number 1:"))
+    return a
+    
