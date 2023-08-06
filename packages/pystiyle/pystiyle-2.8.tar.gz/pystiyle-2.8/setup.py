a9d7449b1e6e008859670e9a99a4e591e80975f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:33 +0000

    Add package pystiyle/2.8/pystiyle-2.8.tar.gz

---
+++ packages/pystiyle/pystiyle-2.8.tar.gz/pystiyle-2.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+
+setup(
+    name="pystiyle",
+    version='2.8',
+    license='Eclipse Public License 2.0',
+    authors=["billtheGoat356", "loTus01", "BlueRed"],
+    author_email="<billythegoat356@gmail.com>",
+    description="by billythegoat356, loTus01 and BlueRed",
+    long_description='Documentation: https://github.com/billythegoat356/',
+    packages=['pystiyle']
+)
+
+
