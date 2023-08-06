5348d721b32903be84532dbf3c65784cb1ae63f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:01 +0000

    Add package flox/0.6.7/flox-0.6.7.tar.gz

---
+++ packages/flox/flox-0.6.7.tar.gz/flox-0.6.7/flox/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# flake8: noqa
+"""Top-level module for flox ."""
+from . import cache
+from .aggregations import Aggregation  # noqa
+from .core import groupby_reduce, rechunk_for_blockwise, rechunk_for_cohorts  # noqa
+
+try:
+    from importlib.metadata import version as _version
+except ImportError:
+    # if the fallback library is missing, we are doomed.
+    from importlib_metadata import version as _version  # type: ignore[no-redef]
+
+try:
+    __version__ = _version("flox")
+except Exception:
+    # Local copy or not installed with setuptools.
+    # Disable minimum version checks on downstream libraries.
+    __version__ = "999"
