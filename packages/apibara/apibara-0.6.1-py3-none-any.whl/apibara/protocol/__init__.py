91649886c0b2fe1372bee8b58ef0fb52dc241272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:53 +0000

    Add package apibara/0.6.1/apibara-0.6.1-py3-none-any.whl

---
+++ packages/apibara/apibara-0.6.1-py3-none-any.whl/apibara/protocol/__init__.py
@@ -0,0 +1,3 @@
+from .client import StreamClient, StreamIter, StreamService
+from .proto.stream_pb2 import (Cursor, DataFinality, StreamDataRequest,
+                               StreamDataResponse)
