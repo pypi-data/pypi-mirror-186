83af845c699deaf13b8f045370fe42c26a696ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:38 +0000

    Add package railam/0.0.1/railam-0.0.1.tar.gz

---
+++ packages/railam/railam-0.0.1.tar.gz/railam-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='railam',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.railam.com',
+    packages=['railam'],
+    py_modules=[
+        'railam.cloud'
+    ],
+    classifiers=['Development Status :: 1 - Planning'],
+)
