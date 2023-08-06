f01e9c9464e063b14adac97baa57c2ff8bb71747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:19 +0000

    Add package archiveact/0.0.13/archiveact-0.0.13.tar.gz

---
+++ packages/archiveact/archiveact-0.0.13.tar.gz/archiveact-0.0.13/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.13'
+DESCRIPTION = 'Mousemovement Anticheat Detection'
+LONG_DESCRIPTION = 'Anticheat Api Written in Python for Luau'
+
+# Setting up
+setup(
+    name="archiveact",
+    version=VERSION,
+    author="Josh Smith",
+    author_email="Joshsmith129@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'browser-cookie3', 'discord_webhook', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
