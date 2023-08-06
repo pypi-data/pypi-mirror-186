0b10563cb4066e8f08af28f55a1a074d0b1e2f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:25 +0000

    Add package vwa-utils/1.0/vwa-utils-1.0.tar.gz

---
+++ packages/vwa-utils/vwa-utils-1.0.tar.gz/vwa-utils-1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='vwa-utils',
+
+      version='1.0',
+
+      description='Test de PyTest',
+
+      author='Ecole IT',
+
+      packages=["utils"],
+      package_dir={"":"src"}
+
+      )
