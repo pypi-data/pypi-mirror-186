1cc2f16c1d7f73e783afdef4a56f9c6178f04375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:26 +0000

    Add package youdotcom/1.0.25/youdotcom-1.0.25-py3-none-any.whl

---
+++ packages/youdotcom/youdotcom-1.0.25-py3-none-any.whl/youdotcom/youchat.py
@@ -0,0 +1,58 @@
+import asyncio
+import json
+import os
+import platform
+import re
+import time
+import cloudscraper,cfscrape
+
+import markdownify
+import undetected_chromedriver as uc
+import urllib3
+from pyvirtualdisplay import Display
+from selenium.common import exceptions as SeleniumExceptions
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+urllib3.disable_warnings()
+
+
+class Chat:
+    """
+    An unofficial Python wrapper for YOU.com YOUCHAT
+    """
+
+    # def __init__(
+    #     self,
+    #     verbose: bool = False,
+    #     window_size: tuple = (800, 600),
+    #     driver: object = None,
+    # ) -> None:
+
+    #     self.__verbose = verbose
+    #     self.__driver = driver
+
+    def send_message(message: str) -> dict:
+
+        """
+        Send a message to YouChat\n
+        Parameters:
+        - message: The message you want to send\n
+        - driver: pass the driver form the Init variable\n
+        Returns a `dict` with the following keys:
+        - message: The response from YouChat\n
+        - time: the time it took to complete your request
+        """
+        start = time.time()
+        scraper = cloudscraper.create_scraper(interpreter='nodejs')
+        
+        url = "https://you.com/api/youchatStreaming?question=" + str(message)
+        msg = scraper.post(url).text
+                
+                
+        timedate = time.time() - start
+        timedate = time.strftime("%S", time.gmtime(timedate))
+        return {"message": msg, "time": str(timedate)}
