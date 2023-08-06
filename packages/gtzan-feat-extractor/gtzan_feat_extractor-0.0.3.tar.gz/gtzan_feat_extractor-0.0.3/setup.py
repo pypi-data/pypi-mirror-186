89aad854d801b4634fe177cbdc19df66c1f5c82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:57 +0000

    Add package gtzan-feat-extractor/0.0.3/gtzan_feat_extractor-0.0.3.tar.gz

---
+++ packages/gtzan-feat-extractor/gtzan_feat_extractor-0.0.3.tar.gz/gtzan_feat_extractor-0.0.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(name='gtzan_feat_extractor',
+      version='0.0.3',
+      description='Feature Extraction for GTZAN Dataset',
+      long_description='Feature Extraction for GTZAN Dataset',
+      long_description_content_type="text/markdown",
+      classifiers=[
+          "Programming Language :: Python :: 3.8",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      python_requires=">=3.8",
+      package_dir = {"": "gtzan_feat_extractor"},
+      packages = setuptools.find_packages(where="gtzan_feat_extractor"),
+      author="ranierifr",
+      author_email='ranierifra@hotmail.it',
+      zip_safe=False)
