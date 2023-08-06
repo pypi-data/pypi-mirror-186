81c912c852e5dad0b2e935e69e17333ba5b0881b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:34 +0000

    Add package crossdeploy/0.1.7/crossdeploy-0.1.7-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/crossdeploy/crossdeploy-0.1.7-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/crossdeploy/imports/ibmcpd/record/__init__.py
@@ -0,0 +1,371 @@
+'''
+# `ibmcpd_record`
+
+Refer to the Terraform Registory for docs: [`ibmcpd_record`](https://www.terraform.io/docs/providers/ibmcpd/r/record).
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+from .._jsii import *
+
+import crossdeploy.cdktf as _cdktf_9a9027ec
+import constructs as _constructs_77d1e7e8
+
+
+class Record(
+    _cdktf_9a9027ec.TerraformResource,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="ibmcpd.record.Record",
+):
+    '''Represents a {@link https://www.terraform.io/docs/providers/ibmcpd/r/record ibmcpd_record}.'''
+
+    def __init__(
+        self,
+        scope: _constructs_77d1e7e8.Construct,
+        id: builtins.str,
+        *,
+        file_path: builtins.str,
+        subscription_id: builtins.str,
+        type: builtins.str,
+        connection: typing.Optional[typing.Union[typing.Union[_cdktf_9a9027ec.SSHProvisionerConnection, typing.Dict[builtins.str, typing.Any]], typing.Union[_cdktf_9a9027ec.WinrmProvisionerConnection, typing.Dict[builtins.str, typing.Any]]]] = None,
+        count: typing.Optional[jsii.Number] = None,
+        depends_on: typing.Optional[typing.Sequence[_cdktf_9a9027ec.ITerraformDependable]] = None,
+        for_each: typing.Optional[_cdktf_9a9027ec.ITerraformIterator] = None,
+        lifecycle: typing.Optional[typing.Union[_cdktf_9a9027ec.TerraformResourceLifecycle, typing.Dict[builtins.str, typing.Any]]] = None,
+        provider: typing.Optional[_cdktf_9a9027ec.TerraformProvider] = None,
+        provisioners: typing.Optional[typing.Sequence[typing.Union[typing.Union[_cdktf_9a9027ec.FileProvisioner, typing.Dict[builtins.str, typing.Any]], typing.Union[_cdktf_9a9027ec.LocalExecProvisioner, typing.Dict[builtins.str, typing.Any]], typing.Union[_cdktf_9a9027ec.RemoteExecProvisioner, typing.Dict[builtins.str, typing.Any]]]]] = None,
+    ) -> None:
+        '''Create a new {@link https://www.terraform.io/docs/providers/ibmcpd/r/record ibmcpd_record} Resource.
+
+        :param scope: The scope in which to define this construct.
+        :param id: The scoped construct ID. Must be unique amongst siblings in the same scope
+        :param file_path: Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/ibmcpd/r/record#file_path Record#file_path}.
+        :param subscription_id: Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/ibmcpd/r/record#subscription_id Record#subscription_id}.
+        :param type: Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/ibmcpd/r/record#type Record#type}.
+        :param connection: 
+        :param count: 
+        :param depends_on: 
+        :param for_each: 
+        :param lifecycle: 
+        :param provider: 
+        :param provisioners: 
+        '''
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__3640a8a8487ba9218be29372947dc4dea2ed8363098bb23f4fa5503706b7f8a6)
+            check_type(argname="argument scope", value=scope, expected_type=type_hints["scope"])
+            check_type(argname="argument id", value=id, expected_type=type_hints["id"])
+        config = RecordConfig(
+            file_path=file_path,
+            subscription_id=subscription_id,
+            type=type,
+            connection=connection,
+            count=count,
+            depends_on=depends_on,
+            for_each=for_each,
+            lifecycle=lifecycle,
+            provider=provider,
+            provisioners=provisioners,
+        )
+
+        jsii.create(self.__class__, self, [scope, id, config])
+
+    @jsii.member(jsii_name="synthesizeAttributes")
+    def _synthesize_attributes(self) -> typing.Mapping[builtins.str, typing.Any]:
+        return typing.cast(typing.Mapping[builtins.str, typing.Any], jsii.invoke(self, "synthesizeAttributes", []))
+
+    @jsii.python.classproperty
+    @jsii.member(jsii_name="tfResourceType")
+    def TF_RESOURCE_TYPE(cls) -> builtins.str:
+        return typing.cast(builtins.str, jsii.sget(cls, "tfResourceType"))
+
+    @builtins.property
+    @jsii.member(jsii_name="filePathInput")
+    def file_path_input(self) -> typing.Optional[builtins.str]:
+        return typing.cast(typing.Optional[builtins.str], jsii.get(self, "filePathInput"))
+
+    @builtins.property
+    @jsii.member(jsii_name="subscriptionIdInput")
+    def subscription_id_input(self) -> typing.Optional[builtins.str]:
+        return typing.cast(typing.Optional[builtins.str], jsii.get(self, "subscriptionIdInput"))
+
+    @builtins.property
+    @jsii.member(jsii_name="typeInput")
+    def type_input(self) -> typing.Optional[builtins.str]:
+        return typing.cast(typing.Optional[builtins.str], jsii.get(self, "typeInput"))
+
+    @builtins.property
+    @jsii.member(jsii_name="filePath")
+    def file_path(self) -> builtins.str:
+        return typing.cast(builtins.str, jsii.get(self, "filePath"))
+
+    @file_path.setter
+    def file_path(self, value: builtins.str) -> None:
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__8749ebcc18114367fd3a3cc8e92d1815f210b7f989835ac9a94b1a033e691d5f)
+            check_type(argname="argument value", value=value, expected_type=type_hints["value"])
+        jsii.set(self, "filePath", value)
+
+    @builtins.property
+    @jsii.member(jsii_name="subscriptionId")
+    def subscription_id(self) -> builtins.str:
+        return typing.cast(builtins.str, jsii.get(self, "subscriptionId"))
+
+    @subscription_id.setter
+    def subscription_id(self, value: builtins.str) -> None:
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__e79438fa8cca5809deba224d161363730a8db4c9a70b89ca61ff077753eceb23)
+            check_type(argname="argument value", value=value, expected_type=type_hints["value"])
+        jsii.set(self, "subscriptionId", value)
+
+    @builtins.property
+    @jsii.member(jsii_name="type")
+    def type(self) -> builtins.str:
+        return typing.cast(builtins.str, jsii.get(self, "type"))
+
+    @type.setter
+    def type(self, value: builtins.str) -> None:
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__382c999b6acbabf42538d482cd46ed559c7a74e3397419216a4a8a1fd8142be2)
+            check_type(argname="argument value", value=value, expected_type=type_hints["value"])
+        jsii.set(self, "type", value)
+
+
+@jsii.data_type(
+    jsii_type="ibmcpd.record.RecordConfig",
+    jsii_struct_bases=[_cdktf_9a9027ec.TerraformMetaArguments],
+    name_mapping={
+        "connection": "connection",
+        "count": "count",
+        "depends_on": "dependsOn",
+        "for_each": "forEach",
+        "lifecycle": "lifecycle",
+        "provider": "provider",
+        "provisioners": "provisioners",
+        "file_path": "filePath",
+        "subscription_id": "subscriptionId",
+        "type": "type",
+    },
+)
+class RecordConfig(_cdktf_9a9027ec.TerraformMetaArguments):
+    def __init__(
+        self,
+        *,
+        connection: typing.Optional[typing.Union[typing.Union[_cdktf_9a9027ec.SSHProvisionerConnection, typing.Dict[builtins.str, typing.Any]], typing.Union[_cdktf_9a9027ec.WinrmProvisionerConnection, typing.Dict[builtins.str, typing.Any]]]] = None,
+        count: typing.Optional[jsii.Number] = None,
+        depends_on: typing.Optional[typing.Sequence[_cdktf_9a9027ec.ITerraformDependable]] = None,
+        for_each: typing.Optional[_cdktf_9a9027ec.ITerraformIterator] = None,
+        lifecycle: typing.Optional[typing.Union[_cdktf_9a9027ec.TerraformResourceLifecycle, typing.Dict[builtins.str, typing.Any]]] = None,
+        provider: typing.Optional[_cdktf_9a9027ec.TerraformProvider] = None,
+        provisioners: typing.Optional[typing.Sequence[typing.Union[typing.Union[_cdktf_9a9027ec.FileProvisioner, typing.Dict[builtins.str, typing.Any]], typing.Union[_cdktf_9a9027ec.LocalExecProvisioner, typing.Dict[builtins.str, typing.Any]], typing.Union[_cdktf_9a9027ec.RemoteExecProvisioner, typing.Dict[builtins.str, typing.Any]]]]] = None,
+        file_path: builtins.str,
+        subscription_id: builtins.str,
+        type: builtins.str,
+    ) -> None:
+        '''
+        :param connection: 
+        :param count: 
+        :param depends_on: 
+        :param for_each: 
+        :param lifecycle: 
+        :param provider: 
+        :param provisioners: 
+        :param file_path: Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/ibmcpd/r/record#file_path Record#file_path}.
+        :param subscription_id: Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/ibmcpd/r/record#subscription_id Record#subscription_id}.
+        :param type: Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/ibmcpd/r/record#type Record#type}.
+        '''
+        if isinstance(lifecycle, dict):
+            lifecycle = _cdktf_9a9027ec.TerraformResourceLifecycle(**lifecycle)
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__d5e2a01551c3231ba7183c0af6bc324afa97e4c66588666cbeb3d284ebe3cd87)
+            check_type(argname="argument connection", value=connection, expected_type=type_hints["connection"])
+            check_type(argname="argument count", value=count, expected_type=type_hints["count"])
+            check_type(argname="argument depends_on", value=depends_on, expected_type=type_hints["depends_on"])
+            check_type(argname="argument for_each", value=for_each, expected_type=type_hints["for_each"])
+            check_type(argname="argument lifecycle", value=lifecycle, expected_type=type_hints["lifecycle"])
+            check_type(argname="argument provider", value=provider, expected_type=type_hints["provider"])
+            check_type(argname="argument provisioners", value=provisioners, expected_type=type_hints["provisioners"])
+            check_type(argname="argument file_path", value=file_path, expected_type=type_hints["file_path"])
+            check_type(argname="argument subscription_id", value=subscription_id, expected_type=type_hints["subscription_id"])
+            check_type(argname="argument type", value=type, expected_type=type_hints["type"])
+        self._values: typing.Dict[builtins.str, typing.Any] = {
+            "file_path": file_path,
+            "subscription_id": subscription_id,
+            "type": type,
+        }
+        if connection is not None:
+            self._values["connection"] = connection
+        if count is not None:
+            self._values["count"] = count
+        if depends_on is not None:
+            self._values["depends_on"] = depends_on
+        if for_each is not None:
+            self._values["for_each"] = for_each
+        if lifecycle is not None:
+            self._values["lifecycle"] = lifecycle
+        if provider is not None:
+            self._values["provider"] = provider
+        if provisioners is not None:
+            self._values["provisioners"] = provisioners
+
+    @builtins.property
+    def connection(
+        self,
+    ) -> typing.Optional[typing.Union[_cdktf_9a9027ec.SSHProvisionerConnection, _cdktf_9a9027ec.WinrmProvisionerConnection]]:
+        '''
+        :stability: experimental
+        '''
+        result = self._values.get("connection")
+        return typing.cast(typing.Optional[typing.Union[_cdktf_9a9027ec.SSHProvisionerConnection, _cdktf_9a9027ec.WinrmProvisionerConnection]], result)
+
+    @builtins.property
+    def count(self) -> typing.Optional[jsii.Number]:
+        '''
+        :stability: experimental
+        '''
+        result = self._values.get("count")
+        return typing.cast(typing.Optional[jsii.Number], result)
+
+    @builtins.property
+    def depends_on(
+        self,
+    ) -> typing.Optional[typing.List[_cdktf_9a9027ec.ITerraformDependable]]:
+        '''
+        :stability: experimental
+        '''
+        result = self._values.get("depends_on")
+        return typing.cast(typing.Optional[typing.List[_cdktf_9a9027ec.ITerraformDependable]], result)
+
+    @builtins.property
+    def for_each(self) -> typing.Optional[_cdktf_9a9027ec.ITerraformIterator]:
+        '''
+        :stability: experimental
+        '''
+        result = self._values.get("for_each")
+        return typing.cast(typing.Optional[_cdktf_9a9027ec.ITerraformIterator], result)
+
+    @builtins.property
+    def lifecycle(self) -> typing.Optional[_cdktf_9a9027ec.TerraformResourceLifecycle]:
+        '''
+        :stability: experimental
+        '''
+        result = self._values.get("lifecycle")
+        return typing.cast(typing.Optional[_cdktf_9a9027ec.TerraformResourceLifecycle], result)
+
+    @builtins.property
+    def provider(self) -> typing.Optional[_cdktf_9a9027ec.TerraformProvider]:
+        '''
+        :stability: experimental
+        '''
+        result = self._values.get("provider")
+        return typing.cast(typing.Optional[_cdktf_9a9027ec.TerraformProvider], result)
+
+    @builtins.property
+    def provisioners(
+        self,
+    ) -> typing.Optional[typing.List[typing.Union[_cdktf_9a9027ec.FileProvisioner, _cdktf_9a9027ec.LocalExecProvisioner, _cdktf_9a9027ec.RemoteExecProvisioner]]]:
+        '''
+        :stability: experimental
+        '''
+        result = self._values.get("provisioners")
+        return typing.cast(typing.Optional[typing.List[typing.Union[_cdktf_9a9027ec.FileProvisioner, _cdktf_9a9027ec.LocalExecProvisioner, _cdktf_9a9027ec.RemoteExecProvisioner]]], result)
+
+    @builtins.property
+    def file_path(self) -> builtins.str:
+        '''Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/ibmcpd/r/record#file_path Record#file_path}.'''
+        result = self._values.get("file_path")
+        assert result is not None, "Required property 'file_path' is missing"
+        return typing.cast(builtins.str, result)
+
+    @builtins.property
+    def subscription_id(self) -> builtins.str:
+        '''Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/ibmcpd/r/record#subscription_id Record#subscription_id}.'''
+        result = self._values.get("subscription_id")
+        assert result is not None, "Required property 'subscription_id' is missing"
+        return typing.cast(builtins.str, result)
+
+    @builtins.property
+    def type(self) -> builtins.str:
+        '''Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/ibmcpd/r/record#type Record#type}.'''
+        result = self._values.get("type")
+        assert result is not None, "Required property 'type' is missing"
+        return typing.cast(builtins.str, result)
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "RecordConfig(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+__all__ = [
+    "Record",
+    "RecordConfig",
+]
+
+publication.publish()
+
+def _typecheckingstub__3640a8a8487ba9218be29372947dc4dea2ed8363098bb23f4fa5503706b7f8a6(
+    scope: _constructs_77d1e7e8.Construct,
+    id: builtins.str,
+    *,
+    file_path: builtins.str,
+    subscription_id: builtins.str,
+    type: builtins.str,
+    connection: typing.Optional[typing.Union[typing.Union[_cdktf_9a9027ec.SSHProvisionerConnection, typing.Dict[builtins.str, typing.Any]], typing.Union[_cdktf_9a9027ec.WinrmProvisionerConnection, typing.Dict[builtins.str, typing.Any]]]] = None,
+    count: typing.Optional[jsii.Number] = None,
+    depends_on: typing.Optional[typing.Sequence[_cdktf_9a9027ec.ITerraformDependable]] = None,
+    for_each: typing.Optional[_cdktf_9a9027ec.ITerraformIterator] = None,
+    lifecycle: typing.Optional[typing.Union[_cdktf_9a9027ec.TerraformResourceLifecycle, typing.Dict[builtins.str, typing.Any]]] = None,
+    provider: typing.Optional[_cdktf_9a9027ec.TerraformProvider] = None,
+    provisioners: typing.Optional[typing.Sequence[typing.Union[typing.Union[_cdktf_9a9027ec.FileProvisioner, typing.Dict[builtins.str, typing.Any]], typing.Union[_cdktf_9a9027ec.LocalExecProvisioner, typing.Dict[builtins.str, typing.Any]], typing.Union[_cdktf_9a9027ec.RemoteExecProvisioner, typing.Dict[builtins.str, typing.Any]]]]] = None,
+) -> None:
+    """Type checking stubs"""
+    pass
+
+def _typecheckingstub__8749ebcc18114367fd3a3cc8e92d1815f210b7f989835ac9a94b1a033e691d5f(
+    value: builtins.str,
+) -> None:
+    """Type checking stubs"""
+    pass
+
+def _typecheckingstub__e79438fa8cca5809deba224d161363730a8db4c9a70b89ca61ff077753eceb23(
+    value: builtins.str,
+) -> None:
+    """Type checking stubs"""
+    pass
+
+def _typecheckingstub__382c999b6acbabf42538d482cd46ed559c7a74e3397419216a4a8a1fd8142be2(
+    value: builtins.str,
+) -> None:
+    """Type checking stubs"""
+    pass
+
+def _typecheckingstub__d5e2a01551c3231ba7183c0af6bc324afa97e4c66588666cbeb3d284ebe3cd87(
+    *,
+    connection: typing.Optional[typing.Union[typing.Union[_cdktf_9a9027ec.SSHProvisionerConnection, typing.Dict[builtins.str, typing.Any]], typing.Union[_cdktf_9a9027ec.WinrmProvisionerConnection, typing.Dict[builtins.str, typing.Any]]]] = None,
+    count: typing.Optional[jsii.Number] = None,
+    depends_on: typing.Optional[typing.Sequence[_cdktf_9a9027ec.ITerraformDependable]] = None,
+    for_each: typing.Optional[_cdktf_9a9027ec.ITerraformIterator] = None,
+    lifecycle: typing.Optional[typing.Union[_cdktf_9a9027ec.TerraformResourceLifecycle, typing.Dict[builtins.str, typing.Any]]] = None,
+    provider: typing.Optional[_cdktf_9a9027ec.TerraformProvider] = None,
+    provisioners: typing.Optional[typing.Sequence[typing.Union[typing.Union[_cdktf_9a9027ec.FileProvisioner, typing.Dict[builtins.str, typing.Any]], typing.Union[_cdktf_9a9027ec.LocalExecProvisioner, typing.Dict[builtins.str, typing.Any]], typing.Union[_cdktf_9a9027ec.RemoteExecProvisioner, typing.Dict[builtins.str, typing.Any]]]]] = None,
+    file_path: builtins.str,
+    subscription_id: builtins.str,
+    type: builtins.str,
+) -> None:
+    """Type checking stubs"""
+    pass
