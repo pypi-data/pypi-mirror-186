dd2804adeb318abb906ffbd7da91d305bb09db79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:51 +0000

    Add package pyfuncpatmatch/0.0.5/pyfuncpatmatch-0.0.5-py3-none-any.whl

---
+++ packages/pyfuncpatmatch/pyfuncpatmatch-0.0.5-py3-none-any.whl/pyfuncpatmatch/__init__.py
@@ -0,0 +1,5 @@
+from pyfuncpatmatch.pat_match_types import (PatEqMatch, PatListExtract,
+                                            PatMatchAll)
+from pyfuncpatmatch.pyfuncpatmatch import patfunc
+
+__all__ = ["patfunc", "PatListExtract", "PatEqMatch", "PatMatchAll"]
