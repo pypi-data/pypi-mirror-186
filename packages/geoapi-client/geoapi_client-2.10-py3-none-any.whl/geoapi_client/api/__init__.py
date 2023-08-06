d6a32a6ec1ef81d1b08b66cbb9bfcbe7809c3222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:01 +0000

    Add package geoapi-client/2.10/geoapi_client-2.10-py3-none-any.whl

---
+++ packages/geoapi-client/geoapi_client-2.10-py3-none-any.whl/geoapi_client/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from geoapi_client.api.notifications_api import NotificationsApi
+from geoapi_client.api.projects_api import ProjectsApi
+from geoapi_client.api.public_projects_api import PublicProjectsApi
+from geoapi_client.api.streetview_api import StreetviewApi
