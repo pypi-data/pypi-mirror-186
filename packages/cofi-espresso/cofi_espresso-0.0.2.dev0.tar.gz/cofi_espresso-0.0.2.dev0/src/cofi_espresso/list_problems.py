96a6855dbaba2fb0d15c336647aea4294d92f884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:12 +0000

    Add package cofi-espresso/0.0.2.dev0/cofi_espresso-0.0.2.dev0.tar.gz

---
+++ packages/cofi-espresso/cofi_espresso-0.0.2.dev0.tar.gz/cofi_espresso-0.0.2.dev0/src/cofi_espresso/list_problems.py
@@ -0,0 +1,34 @@
+_all_problem_names = []
+_all_problems = []
+
+def list_problem_names():
+    """Returns a list of all Espresso problem names"""
+    return _all_problem_names
+
+def list_problems():
+    """Returns a list of all Espresso problem classes"""
+    return _all_problems
+
+
+# from .example_name import ExampleName
+
+# __all_problem_names__ = [ "ExampleName", ]
+# __all_problems__ = [ ExampleName, ]
+
+from ._xray_tomography import XrayTomography
+from ._gravity_density import GravityDensity
+from ._fmm_tomography import FmmTomography
+from ._simple_regression import SimpleRegression
+
+_all_problem_names = [
+	'XrayTomography',
+	'GravityDensity',
+	'FmmTomography',
+	'SimpleRegression',
+]
+_all_problems = [
+	XrayTomography,
+	GravityDensity,
+	FmmTomography,
+	SimpleRegression,
+]
