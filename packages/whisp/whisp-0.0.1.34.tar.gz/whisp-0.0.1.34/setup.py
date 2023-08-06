612069a342b9ad409e42316df748eb33e630bda9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:47 +0000

    Add package whisp/0.0.1.34/whisp-0.0.1.34.tar.gz

---
+++ packages/whisp/whisp-0.0.1.34.tar.gz/whisp-0.0.1.34/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1.34'
+DESCRIPTION = 'Whisper package'
+LONG_DESCRIPTION = 'A package that allows easily to use the Whisper library'
+
+# Setting up
+setup(
+    name="whisp",
+    version=VERSION,
+    author="Peteris N.",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['setuptools-rust', 'torch', 'transformers', 'ffmpeg-python'],
+    keywords=[],
+    classifiers=[],
+    include_package_data=True,
+)
