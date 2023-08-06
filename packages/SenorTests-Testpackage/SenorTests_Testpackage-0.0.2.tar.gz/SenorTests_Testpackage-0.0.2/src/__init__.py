7a0ca97fe0c02fb48a274872d06e06ab60fe1b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:48 +0000

    Add package SenorTests-Testpackage/0.0.2/SenorTests_Testpackage-0.0.2.tar.gz

---
+++ packages/SenorTests-Testpackage/SenorTests_Testpackage-0.0.2.tar.gz/SenorTests_Testpackage-0.0.2/src/__init__.py
@@ -0,0 +1,11 @@
+import time
+
+def aFunction():
+    print("Commencing Evil")
+    time.sleep(1)
+    print("Collecting data...")
+    time.sleep(1)
+    print("Silently uploading data...")
+    time.sleep(1)
+    print("Evil has been done. Thank you sir")
+    return 1
