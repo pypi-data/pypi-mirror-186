f0870d1c7850d12a1f17542522d7594eaf49811c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:05 +0000

    Add package dicom-image-tools/23.1.2/dicom_image_tools-23.1.2.tar.gz

---
+++ packages/dicom-image-tools/dicom_image_tools-23.1.2.tar.gz/dicom_image_tools-23.1.2/src/dicom_image_tools/helpers/voxel_data.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class VoxelData:
+    """A class for managing voxel/pixel data for DICOM images
+
+    Attributes:
+        x: The voxel/pixel x-dimension in mm
+        y: The voxel/pixel y-dimension in mm
+        z: The voxel/pixel z-dimension in mm
+    """
+
+    x: float
+    y: float
+    z: Optional[float] = None
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.z == other.z
+
+    def pixel_area(self) -> float:
+        return self.x * self.y
+
+    def volume(self) -> float:
+        return self.x * self.y * self.z
