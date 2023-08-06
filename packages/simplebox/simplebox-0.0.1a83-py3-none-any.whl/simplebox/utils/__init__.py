78228ec1b12225e8cfb01f89fc67e02fa96ae002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:46 +0000

    Add package simplebox/0.0.1a83/simplebox-0.0.1a83-py3-none-any.whl

---
+++ packages/simplebox/simplebox-0.0.1a83-py3-none-any.whl/simplebox/utils/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from ._computer import network, cpu, memory, process, Disk
+from ._enums import EnumUtils
+from ._objects import ObjectsUtils
+from ._strings import StringUtils
