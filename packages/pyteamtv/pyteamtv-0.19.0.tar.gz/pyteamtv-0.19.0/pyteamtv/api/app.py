c0eb3f186e755491e000594f72f1dfea91251f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:06 +0000

    Add package pyteamtv/0.19.0/pyteamtv-0.19.0.tar.gz

---
+++ packages/pyteamtv/pyteamtv-0.19.0.tar.gz/pyteamtv-0.19.0/pyteamtv/api/app.py
@@ -0,0 +1,29 @@
+import jwt
+
+from pyteamtv.infra.requester import Requester
+from pyteamtv.models.resource_group.factory import factory as resource_group_factory
+from .endpoint import API_ENDPOINT
+
+from .token import TOKEN
+
+
+class TeamTVApp(object):
+    def __init__(self, jwt_token, app_id: str, use_cache: bool = False):
+        self.jwt_token = jwt_token
+
+        token = jwt.decode(
+            jwt_token, TOKEN, algorithms="RS256", verify=True, audience=f"app:{app_id}"
+        )
+
+        self._requester = Requester(
+            f"{API_ENDPOINT}/api", jwt_token, use_cache=use_cache
+        )
+        self.__token = token
+
+    def get_resource_group(self):
+        """
+        :rtype: :class:`pyteamtv.resource_group.team.Team`
+        """
+        data = self._requester.request("GET", "/resourceGroups/current")
+
+        return resource_group_factory(self._requester, data)
