8296ede2855059b5fc904fe4eb95d21d767f9791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:44 +0000

    Add package starlite-saqlalchemy/0.28.0/starlite_saqlalchemy-0.28.0.tar.gz

---
+++ packages/starlite-saqlalchemy/starlite_saqlalchemy-0.28.0.tar.gz/starlite_saqlalchemy-0.28.0/src/starlite_saqlalchemy/__init__.py
@@ -0,0 +1,67 @@
+"""
+starlite-saqlalchemy
+---
+
+An implementation of a `Starlite` application configuration plugin.
+
+Example:
+```python
+from starlite import Starlite, get
+
+from starlite_saqlalchemy import ConfigureApp
+
+
+@get("/example")
+def example_handler() -> dict:
+    return {"hello": "world"}
+
+
+app = Starlite(route_handlers=[example_handler], on_app_init=[ConfigureApp()])
+```
+"""
+# this is because pycharm wigs out when there is a module called `exceptions`:
+# noinspection PyCompatibility
+from . import (
+    cache,
+    compression,
+    db,
+    dependencies,
+    dto,
+    exceptions,
+    health,
+    http,
+    log,
+    openapi,
+    redis,
+    repository,
+    sentry,
+    service,
+    settings,
+    sqlalchemy_plugin,
+    worker,
+)
+from .init_plugin import ConfigureApp, PluginConfig
+
+__all__ = [
+    "ConfigureApp",
+    "PluginConfig",
+    "cache",
+    "compression",
+    "db",
+    "dependencies",
+    "dto",
+    "exceptions",
+    "health",
+    "http",
+    "log",
+    "openapi",
+    "redis",
+    "repository",
+    "sentry",
+    "service",
+    "settings",
+    "sqlalchemy_plugin",
+    "worker",
+]
+
+__version__ = "0.28.0"
