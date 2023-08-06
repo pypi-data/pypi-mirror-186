8aa1e6a9a57bd06988a7f5c83e4fe4180f06ec7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:16 +0000

    Add package pyrestsdk/0.2.4/pyrestsdk-0.2.4-py2.py3-none-any.whl

---
+++ packages/pyrestsdk/pyrestsdk-0.2.4-py2.py3-none-any.whl/pyrestsdk/request/__init__.py
@@ -0,0 +1,11 @@
+"""Requests"""
+from sys import version_info
+from pyrestsdk.request._invokeable_request import InvokableRequest
+from pyrestsdk.request._request import Request
+
+if version_info > (3,10):
+    from pyrestsdk.request._base_request39 import BaseRequest
+else:
+    from pyrestsdk.request._base_request310 import BaseRequest
+
+__all__ = ["BaseRequest", "InvokableRequest", "Request"]
