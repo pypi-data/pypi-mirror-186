10519ad40db2fdfa918a1c8f36ad21005fde4fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:31 +0000

    Add package AcodisApiHandler/0.2.6.post1/AcodisApiHandler-0.2.6.post1.tar.gz

---
+++ packages/AcodisApiHandler/AcodisApiHandler-0.2.6.post1.tar.gz/AcodisApiHandler-0.2.6.post1/test/test_acodis_handler.py
@@ -0,0 +1,18 @@
+from unittest import TestCase, mock
+
+
+class TestAcodisAPIHandler(TestCase):
+    def test_upload_pdf(self):
+        self.fail()
+
+    def test_get_status(self):
+        self.fail()
+
+    def test_wait_for_completion(self):
+        self.fail()
+
+    def test_download_prediction(self):
+        self.fail()
+
+    def test_workflow(self):
+        self.fail()
