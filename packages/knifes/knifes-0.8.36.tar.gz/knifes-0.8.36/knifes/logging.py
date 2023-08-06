62564d8bf080920a7bb8007eb4d5167b3a05dc16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:14:07 +0000

    Add package knifes/0.8.36/knifes-0.8.36.tar.gz

---
+++ packages/knifes/knifes-0.8.36.tar.gz/knifes-0.8.36/knifes/logging.py
@@ -0,0 +1,15 @@
+from logging.handlers import TimedRotatingFileHandler
+import logging
+from knifes import alarm
+from knifes.envconfig import config
+
+
+# critical级别日志报警
+class TimedRotatingFileWithCriticalAlarmHandler(TimedRotatingFileHandler):
+    def emit(self, record):
+        super(TimedRotatingFileWithCriticalAlarmHandler, self).emit(record)
+        if record.levelno == logging.CRITICAL:   # 前面添加NODE_ID
+            msg = f"[{record.module}]{record.getMessage()}"
+            if config('NODE_ID'):
+                msg = f"[{config('NODE_ID')}]" + msg
+            alarm.async_send_msg(msg)
