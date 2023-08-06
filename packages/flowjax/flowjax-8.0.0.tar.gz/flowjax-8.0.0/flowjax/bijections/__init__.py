acfedb3c74edf4dc292a8696edf6e60da7f2174d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:07 +0000

    Add package flowjax/8.0.0/flowjax-8.0.0.tar.gz

---
+++ packages/flowjax/flowjax-8.0.0.tar.gz/flowjax-8.0.0/flowjax/bijections/__init__.py
@@ -0,0 +1,33 @@
+"""Bijections from ``flowjax.bijections``"""
+
+from .bijection import Bijection
+from .affine import AdditiveLinearCondition, Affine, TriangularAffine
+from .block_autoregressive_network import BlockAutoregressiveNetwork
+from .chain import Chain
+from .coupling import Coupling
+from .masked_autoregressive import MaskedAutoregressive
+from .tanh import Tanh, TanhLinearTails
+from .utils import EmbedCondition, Flip, Invert, Partial, Permute
+from .rational_quadratic_spline import RationalQuadraticSpline
+from .jax_transforms import Scan, Vmap
+
+__all__ = [
+    "Bijection",
+    "Affine",
+    "TriangularAffine",
+    "BlockAutoregressiveNetwork",
+    "Coupling",
+    "MaskedAutoregressive",
+    "Tanh",
+    "TanhLinearTails",
+    "Chain",
+    "Scan",
+    "Vmap",
+    "Invert",
+    "Flip",
+    "Permute",
+    "AdditiveLinearCondition",
+    "Partial",
+    "EmbedCondition",
+    "RationalQuadraticSpline",
+]
