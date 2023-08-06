52166f7993be14c1043fe9a69508e47b44c84476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:56 +0000

    Add package wedoc/0.1.12/wedoc-0.1.12-py3-none-any.whl

---
+++ packages/wedoc/wedoc-0.1.12-py3-none-any.whl/wedoc/api/document.py
@@ -0,0 +1,36 @@
+from wedoc.api.base import WedocApiBase
+
+
+class Document(WedocApiBase):
+
+    def create_doc(self, doc_name, doc_type, spaceid="", fatherid="", admin_users=[]):
+        """创建文档"""
+        api = "/wedoc/create_doc"
+        pyload = {
+            "doc_type": doc_type,
+            "doc_name": doc_name,
+        }
+
+        res = self.request("post", api, pyload=pyload)
+        return res
+
+    def rename_doc(self, docid, new_name):
+        """重命名文档"""
+        api = "/wedoc/rename_doc"
+        pyload = {"docid": docid, "new_name": new_name}
+        res = self.request("post", api, pyload=pyload)
+        return res
+
+    def del_doc(self, docid):
+        """删除文档"""
+        api = "/wedoc/del_doc"
+        pyload = {"docid": docid}
+        res = self.request("post", api, pyload=pyload)
+        return res
+
+    def get_doc_base_info(self, docid):
+        """获取文档基础信息"""
+        api = "/wedoc/get_doc_base_info"
+        pyload = {"docid": docid}
+        res = self.request("post", api, pyload=pyload)
+        return res
---
+++ packages/wedoc/wedoc-0.1.12-py3-none-any.whl/wedoc/api/spreadsheet.py
@@ -0,0 +1,33 @@
+from wedoc.api.base import WedocApiBase
+
+
+class Spreadsheet(WedocApiBase):
+
+    def get_sheet_properties(self, docid):
+        """获取表格行列信息"""
+        api = "/wedoc/spreadsheet/get_sheet_properties"
+        pyload = {"docid": docid}
+        res = self.request("post", api, pyload=pyload)
+        return res
+
+    def get_sheet_range_data(self, docid, sheet_id, range):
+        """获取表格数据"""
+        api = "/wedoc/spreadsheet/get_sheet_range_data"
+        pyload = {"docid": docid, "sheet_id": sheet_id, "range": range}
+        res = self.request("post", api, pyload=pyload)
+        return res
+
+    def batch_update(self, docid):
+        """编辑表格内容"""
+        api = "/wedoc/spreadsheet/get_sheet_range_data"
+        pyload = {
+            "docid": docid,
+            "requests": [
+                {"add_sheet_request": {...}},
+                {"update_range_request": {...}},
+                {"delete_dimension_request": {...}},
+                {"delete_sheet_request": {...}},
+            ],
+        }
+        res = self.request("post", api, pyload=pyload)
+        return res
