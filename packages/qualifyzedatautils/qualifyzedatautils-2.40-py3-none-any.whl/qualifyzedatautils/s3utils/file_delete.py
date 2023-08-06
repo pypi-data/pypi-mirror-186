00e5d2294312e30d6a308646a28e7c014724bdb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:19 +0000

    Add package qualifyzedatautils/2.40/qualifyzedatautils-2.40-py3-none-any.whl

---
+++ packages/qualifyzedatautils/qualifyzedatautils-2.40-py3-none-any.whl/qualifyzedatautils/s3utils/file_delete.py
@@ -0,0 +1,17 @@
+from ..connections.dwh_awsservicer import initialize_dwhservicer_credentials, establish_dwhservicer_session
+
+def df_deletefile(filetodelete):
+
+    session = establish_dwhservicer_session()
+    s3 = session.resource('s3')
+
+    if filetodelete['use-subfolder']==True:
+        s3.Object(filetodelete['bucket-name'], filetodelete['file-name']).delete()
+        print(filetodelete['bucket-name'])
+        print(filetodelete['file-name'])
+        print('with subfile')
+    else:
+        s3.Object(filetodelete['bucket-name'], filetodelete['subfolder-full-path']+"/"+filetodelete['file-name']).delete()
+        print(filetodelete['bucket-name'])
+        print(filetodelete['subfolder-full-path']+"/"+filetodelete['file-name'])
+        print('without subfile')
