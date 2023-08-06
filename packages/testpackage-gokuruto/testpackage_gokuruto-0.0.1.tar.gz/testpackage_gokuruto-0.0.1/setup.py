996a32559fb40abb372d49f154e9a4dd785fa331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:16 +0000

    Add package testpackage-gokuruto/0.0.1/testpackage_gokuruto-0.0.1.tar.gz

---
+++ packages/testpackage-gokuruto/testpackage_gokuruto-0.0.1.tar.gz/testpackage_gokuruto-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = "test package."
+
+setup(
+    name="testpackage_gokuruto",
+    version=VERSION,
+    autor="MP",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=["python"]
+)
