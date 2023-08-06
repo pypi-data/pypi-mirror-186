34adf51885bae175f32f63d242687833f1ae9f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:35 +0000

    Add package forta-agent/0.1.9/forta_agent-0.1.9-py3-none-any.whl

---
+++ packages/forta-agent/forta_agent-0.1.9-py3-none-any.whl/forta_agent/label.py
@@ -0,0 +1,28 @@
+from enum import IntEnum
+from .utils import assert_enum_value_in_dict, assert_non_empty_string_in_dict
+
+
+class EntityType(IntEnum):
+    Unknown = 0
+    Address = 1
+    Transaction = 2
+    Block = 3
+    Url = 4
+
+
+class Label:
+    def __init__(self, dict):
+        assert_enum_value_in_dict(dict, 'entity_type', EntityType)
+        assert_non_empty_string_in_dict(dict, 'entity')
+        assert_non_empty_string_in_dict(dict, 'label')
+        self.entity = dict['entity']
+        self.confidence = dict['confidence']
+        self.entity_type = dict['entity_type']
+        self.label = dict['label']
+        self.remove = dict.get('remove', False)
+
+    def toDict(self):
+        d = dict(self.__dict__, **{
+            'entityType': self.entity_type,
+        })
+        return {k: v for k, v in d.items() if v is not None}
