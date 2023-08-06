3d26fcaa7e19ccf201041a03e58c69beffd9f553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:07 +0000

    Add package correctimport/0.3.0/correctimport-0.3.0.tar.gz

---
+++ packages/correctimport/correctimport-0.3.0.tar.gz/correctimport-0.3.0/correctimport/run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import directory_finder
+
+
+def main():
+    input_folder = directory_finder.LocateDirectory()
+    input_dir = input_folder.input_dir_pointer()
+    main_dir = input_folder.using_path_singleton()
+
+
+if __name__ == "__main__":
+    main()
