b807984fcc958cac9aad8d9a3da406e4ffd1fb7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:45 +0000

    Add package my-magento/2.0.0b4/my-magento-2.0.0b4.tar.gz

---
+++ packages/my-magento/my-magento-2.0.0b4.tar.gz/my-magento-2.0.0b4/magento/__init__.py
@@ -0,0 +1,13 @@
+from . import clients
+from . import search
+from . import models
+from . import utils
+from . import exceptions
+
+Client = clients.Client
+logger = utils.MagentoLogger(
+    name=utils.MagentoLogger.PACKAGE_LOG_NAME,
+    log_file=utils.MagentoLogger.PACKAGE_LOG_NAME + '.log',
+    stdout_level='WARNING'  # Clients will log to console
+)
+logger.debug('Initialized MyMagento')
