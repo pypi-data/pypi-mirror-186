a2081293b24c4465aaa01b4c9414162fb30c451d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:14 +0000

    Add package mitol-django-oauth-toolkit-extensions/1.3.0/mitol_django_oauth_toolkit_extensions-1.3.0-py3-none-any.whl

---
+++ packages/mitol-django-oauth-toolkit-extensions/mitol_django_oauth_toolkit_extensions-1.3.0-py3-none-any.whl/mitol/oauth_toolkit_extensions/__init__.py
@@ -0,0 +1,5 @@
+""" mitol.oauth_toolkit_extensions """
+default_app_config = "mitol.oauth_toolkit_extensions.apps.OAuthToolkitExtensionsApp"
+
+__version__ = "1.3.0"
+__distributionname__ = "mitol-django-oauth-toolkit-extensions"
