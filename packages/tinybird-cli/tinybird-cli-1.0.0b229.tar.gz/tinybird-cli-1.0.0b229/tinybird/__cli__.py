0e4a7fe21da847d71958c10eaa4502f012462fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:47 +0000

    Add package tinybird-cli/1.0.0b229/tinybird-cli-1.0.0b229.tar.gz

---
+++ packages/tinybird-cli/tinybird-cli-1.0.0b229.tar.gz/tinybird-cli-1.0.0b229/tinybird/__cli__.py
@@ -0,0 +1,8 @@
+
+__name__ = 'tinybird-cli'
+__description__ = 'Tinybird Command Line Tool'
+__url__ = 'https://docs.tinybird.co/cli.html'
+__author__ = 'Tinybird'
+__author_email__ = 'support@tinybird.co'
+__version__ = '1.0.0b229'
+__revision__ = '26dc1e2'
