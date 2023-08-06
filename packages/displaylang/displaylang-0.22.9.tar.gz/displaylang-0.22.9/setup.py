6ab0a41633043f0c6e585896c902331e5dc4d09e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:59 +0000

    Add package displaylang/0.22.9/displaylang-0.22.9.tar.gz

---
+++ packages/displaylang/displaylang-0.22.9.tar.gz/displaylang-0.22.9/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="displaylang",
+    version="0.22.9",
+    license="Apache 2.0",
+    url="https://github.com/proofscape/displaylang",
+    description="Just enough Python to write displays",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pfsc-util>=0.22.8',
+        'typeguard==2.13.3',
+    ],
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+)
