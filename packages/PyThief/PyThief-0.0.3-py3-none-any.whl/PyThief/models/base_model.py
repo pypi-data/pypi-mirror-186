a4436b7c20bd90a4df0cf148d8c33af6623aee7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:54 +0000

    Add package PyThief/0.0.3/PyThief-0.0.3-py3-none-any.whl

---
+++ packages/PyThief/PyThief-0.0.3-py3-none-any.whl/PyThief/models/base_model.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from abc import ABC, abstractmethod
+
+
+class BaseModel(ABC):
+    
+    @abstractmethod
+    def __init__(self):
+        pass
+    
+    @abstractmethod
+    def fit(self, y, **kwargs):
+        pass
+    
+    @abstractmethod
+    def predict(self, forecast_horizon):
+        pass
