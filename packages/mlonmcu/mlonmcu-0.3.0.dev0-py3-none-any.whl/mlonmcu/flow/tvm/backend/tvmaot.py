83a9826b37295032e0bd5d14e98ef81d90f8c4e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:01 +0000

    Add package mlonmcu/0.3.0.dev0/mlonmcu-0.3.0.dev0-py3-none-any.whl

---
+++ packages/mlonmcu/mlonmcu-0.3.0.dev0-py3-none-any.whl/mlonmcu/flow/tvm/backend/tvmaot.py
@@ -0,0 +1,162 @@
+#
+# Copyright (c) 2022 TUM Department of Electrical and Computer Engineering.
+#
+# This file is part of MLonMCU.
+# See https://github.com/tum-ei-eda/mlonmcu.git for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import sys
+import tempfile
+import json
+import tarfile
+from pathlib import Path
+
+from .backend import TVMBackend
+from mlonmcu.flow.backend import main
+from mlonmcu.config import str2bool
+from mlonmcu.artifact import Artifact, ArtifactFormat
+from .wrapper import generate_tvmaot_wrapper, generate_wrapper_header
+from .tvmc_utils import get_tvmaot_tvmc_args
+
+
+class TVMAOTBackend(TVMBackend):
+    FEATURES = [
+        *TVMBackend.FEATURES,
+        "debug_arena",
+        "unpacked_api",
+        "usmp",
+    ]
+
+    DEFAULTS = {
+        **TVMBackend.DEFAULTS,
+        "debug_arena": False,
+        "arena_size": None,  # Determined automatically
+        "unpacked_api": False,
+        "alignment_bytes": 4,
+    }
+
+    name = "tvmaot"
+
+    def __init__(self, runtime="crt", fmt="mlf", features=None, config=None):
+        super().__init__(executor="aot", runtime=runtime, fmt=fmt, features=features, config=config)
+
+    @property
+    def arena_size(self):
+        size = self.config["arena_size"]
+        return int(size) if size is not None else None
+
+    @property
+    def unpacked_api(self):
+        value = self.config["unpacked_api"]
+        return str2bool(value) if not isinstance(value, (bool, int)) else value
+
+    @property
+    def debug_arena(self):
+        value = self.config["debug_arena"]
+        return str2bool(value) if not isinstance(value, (bool, int)) else value
+
+    @property
+    def alignment_bytes(self):
+        return int(self.config["alignment_bytes"])
+
+    def get_tvmc_compile_args(self, out):
+        return super().get_tvmc_compile_args(out) + get_tvmaot_tvmc_args(self.alignment_bytes, self.unpacked_api)
+
+    def get_workspace_size_from_metadata(self, metadata):
+        if "modules" in metadata:
+            return metadata["modules"]["default"]["memory"]["functions"]["main"][0]["workspace_size_bytes"]
+        else:
+            # backwards compatibility
+            return metadata["memory"]["functions"]["main"][0]["workspace_size_bytes"]
+
+    def generate_code(self):
+        artifacts = []
+        assert self.model is not None
+        full = False  # Required due to bug in TVM
+        dump = ["c", "relay"] if full else []
+        with tempfile.TemporaryDirectory() as temp_dir:
+            out_path = Path(temp_dir) / f"{self.prefix}.tar"
+            out = self.invoke_tvmc_compile(out_path, dump=dump, cwd=temp_dir)
+            mlf_path = Path(temp_dir) / "mlf"
+            tarfile.open(out_path).extractall(mlf_path)
+            with open(mlf_path / "metadata.json") as handle:
+                metadata = json.load(handle)
+            # metadata_txt = json.dumps(metadata)
+            with open(out_path, "rb") as handle:
+                mlf_data = handle.read()
+                artifacts.append(
+                    Artifact(
+                        f"{self.prefix}.tar",
+                        raw=mlf_data,
+                        fmt=ArtifactFormat.MLF,
+                        archive=True,
+                    )
+                )
+            if full:  # FIXME: broken due to error in TVM
+                with open(str(out_path) + ".c", "r") as handle:
+                    mod_src = handle.read()
+                    artifacts.append(
+                        Artifact(
+                            f"{self.prefix}.c",
+                            content=mod_src,
+                            fmt=ArtifactFormat.SOURCE,
+                            optional=True,
+                        )
+                    )
+                with open(str(out_path) + ".relay", "r") as handle:
+                    mod_txt = handle.read()
+                    artifacts.append(
+                        Artifact(
+                            f"{self.prefix}.relay",
+                            content=mod_txt,
+                            fmt=ArtifactFormat.TEXT,
+                            optional=True,
+                        )
+                    )
+            generate_wrapper = True
+            if generate_wrapper:
+                if self.arena_size is not None:
+                    assert self.arena_size >= 0
+                    workspace_size = self.arena_size
+                else:
+                    workspace_size = self.get_workspace_size_from_metadata(metadata)
+                wrapper_src = generate_tvmaot_wrapper(
+                    self.model_info,
+                    workspace_size,
+                    self.prefix,
+                    api="c" if self.unpacked_api else "packed",
+                    debug_arena=self.debug_arena,
+                )
+                artifacts.append(Artifact("aot_wrapper.c", content=wrapper_src, fmt=ArtifactFormat.SOURCE))
+                header_src = generate_wrapper_header()
+                artifacts.append(Artifact("tvm_wrapper.h", content=header_src, fmt=ArtifactFormat.SOURCE))
+            workspace_size_artifact = Artifact(
+                "tvmaot_workspace_size.txt", content=f"{workspace_size}", fmt=ArtifactFormat.TEXT
+            )
+            artifacts.append(workspace_size_artifact)
+            stdout_artifact = Artifact(
+                "tvmc_compile_out.log", content=out, fmt=ArtifactFormat.TEXT
+            )  # TODO: rename to tvmaot_out.log?
+            artifacts.append(stdout_artifact)
+        # assert self.target
+        self.artifacts = artifacts
+
+
+if __name__ == "__main__":
+    sys.exit(
+        main(
+            TVMAOTBackend,
+            args=sys.argv[1:],
+        )
+    )  # pragma: no cover
---
+++ packages/mlonmcu/mlonmcu-0.3.0.dev0-py3-none-any.whl/mlonmcu/platform/platform.py
@@ -0,0 +1,244 @@
+#
+# Copyright (c) 2022 TUM Department of Electrical and Computer Engineering.
+#
+# This file is part of MLonMCU.
+# See https://github.com/tum-ei-eda/mlonmcu.git for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import tempfile
+import multiprocessing
+from pathlib import Path
+from filelock import FileLock
+
+from mlonmcu.config import filter_config
+from mlonmcu.feature.features import get_matching_features
+from mlonmcu.feature.type import FeatureType
+from mlonmcu.target.metrics import Metrics
+from mlonmcu.target.elf import get_results as get_static_mem_usage
+from mlonmcu.logging import get_logger
+from mlonmcu.config import str2bool
+
+logger = get_logger()
+
+
+class Platform:
+    """Abstract platform class."""
+
+    FEATURES = []
+
+    DEFAULTS = {
+        "print_outputs": False,
+    }
+
+    REQUIRED = []
+    OPTIONAL = []
+
+    def __init__(self, name, features=None, config=None):
+        self.name = name
+        self.config = config if config else {}
+        self.definitions = {}
+        self.features = self.process_features(features)
+        self.config = filter_config(self.config, self.name, self.DEFAULTS, self.OPTIONAL, self.REQUIRED)
+        self.artifacts = []
+
+    def init_directory(self, path=None, context=None):
+        raise NotImplementedError
+
+    @property
+    def supports_build(self):
+        return False
+
+    @property
+    def supports_tune(self):
+        return False
+
+    @property
+    def supports_compile(self):
+        return False
+
+    @property
+    def supports_flash(self):
+        return False
+
+    @property
+    def supports_monitor(self):
+        return False
+
+    @property
+    def print_outputs(self):
+        value = self.config["print_outputs"]
+        return str2bool(value) if not isinstance(value, (bool, int)) else value
+
+    def process_features(self, features):
+        if features is None:
+            return []
+        features = get_matching_features(features, FeatureType.PLATFORM)
+        for feature in features:
+            # assert feature.name in self.FEATURES, f"Incompatible feature: {feature.name}"
+            if feature.name in self.FEATURES:
+                feature.add_platform_config(self.name, self.config)
+                feature.add_platform_defs(self.name, self.definitions)
+        return features
+
+    def get_supported_backends(self):
+        return []
+
+    def get_supported_targets(self):
+        return []
+
+
+class BuildPlatform(Platform):
+    """Abstract backend platform class."""
+
+    FEATURES = Platform.FEATURES + []
+
+    DEFAULTS = {
+        **Platform.DEFAULTS,
+    }
+
+    REQUIRED = []
+
+    @property
+    def supports_build(self):
+        return True
+
+    def export_elf(self, path):
+        assert len(self.artifacts) > 0, "No artifacts found, please run generate_elf() first"
+
+        if not isinstance(path, Path):
+            path = Path(path)
+        assert (
+            path.is_dir()
+        ), "The supplied path does not exists."  # Make sure it actually exists (we do not create it by default)
+        for artifact in self.artifacts:
+            artifact.export(path)
+
+
+class TunePlatform(Platform):
+    """Abstract backend platform class."""
+
+    FEATURES = Platform.FEATURES + []
+
+    DEFAULTS = {
+        **Platform.DEFAULTS,
+    }
+
+    REQUIRED = []
+
+    @property
+    def supports_tune(self):
+        return True
+
+    def export_elf(self, path):
+        assert len(self.artifacts) > 0, "No artifacts found, please run generate_elf() first"
+
+        if not isinstance(path, Path):
+            path = Path(path)
+        assert (
+            path.is_dir()
+        ), "The supplied path does not exists."  # Make sure it actually exists (we do not create it by default)
+        for artifact in self.artifacts:
+            artifact.export(path)
+
+
+class CompilePlatform(Platform):
+    """Abstract compile platform class."""
+
+    FEATURES = Platform.FEATURES + ["debug"]
+
+    DEFAULTS = {
+        **Platform.DEFAULTS,
+        "debug": False,
+        "build_dir": None,
+        "num_threads": multiprocessing.cpu_count(),
+    }
+
+    REQUIRED = []
+
+    @property
+    def supports_compile(self):
+        return True
+
+    @property
+    def debug(self):
+        value = self.config["debug"]
+        return str2bool(value) if not isinstance(value, (bool, int)) else value
+
+    @property
+    def num_threads(self):
+        return int(self.config["num_threads"])
+
+    def get_metrics(self, elf):
+        static_mem = get_static_mem_usage(elf)
+        rom_ro, rom_code, rom_misc, ram_data, ram_zdata = (
+            static_mem["rom_rodata"],
+            static_mem["rom_code"],
+            static_mem["rom_misc"],
+            static_mem["ram_data"],
+            static_mem["ram_zdata"],
+        )
+        rom_total = rom_ro + rom_code + rom_misc
+        ram_total = ram_data + ram_zdata
+        metrics = Metrics()
+        metrics.add("Total ROM", rom_total)
+        metrics.add("Total RAM", ram_total)
+        metrics.add("ROM read-only", rom_ro)
+        metrics.add("ROM code", rom_code)
+        metrics.add("ROM misc", rom_misc)
+        metrics.add("RAM data", ram_data)
+        metrics.add("RAM zero-init data", ram_zdata)
+        return metrics
+
+    def generate_elf(self, src, target, model=None, data_file=None):
+        raise NotImplementedError
+
+    def tune_model(self, path):
+        raise NotImplementedError
+
+
+class TargetPlatform(Platform):
+    """Abstract target platform class."""
+
+    FEATURES = Platform.FEATURES + []
+
+    DEFAULTS = {
+        **Platform.DEFAULTS,
+    }
+
+    REQUIRED = []
+
+    def create_target(self, name):
+        raise NotImplementedError
+
+    @property
+    def supports_flash(self):
+        return True
+
+    @property
+    def supports_monitor(self):
+        return True
+
+    def flash(self, elf, target, timeout=120):
+        raise NotImplementedError
+
+    def monitor(self, target, timeout=60):
+        raise NotImplementedError
+
+    def run(self, elf, target, timeout=120):
+        # Only allow one serial communication at a time
+        with FileLock(Path(tempfile.gettempdir()) / "mlonmcu_serial.lock"):
+            self.flash(elf, target, timeout=timeout)
+            output = self.monitor(target, timeout=timeout)
+
+        return output
