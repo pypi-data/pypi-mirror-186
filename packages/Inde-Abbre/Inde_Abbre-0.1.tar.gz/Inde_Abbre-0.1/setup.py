aca99ff52b4159bac67cbbc797b7c7a5e9b760b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:48 +0000

    Add package Inde-Abbre/0.1/Inde_Abbre-0.1.tar.gz

---
+++ packages/Inde-Abbre/Inde_Abbre-0.1.tar.gz/Inde_Abbre-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='Inde_Abbre',
+ version='0.1',
+ author="Richard Alphonse",
+ author_email="richardriche96@gmail.com",
+ description="It will return the indian abbreviations",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
