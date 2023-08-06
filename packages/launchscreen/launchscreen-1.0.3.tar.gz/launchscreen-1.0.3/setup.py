98f04689d61f3c37554d59c04237b625607fdf08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:29 +0000

    Add package launchscreen/1.0.3/launchscreen-1.0.3.tar.gz

---
+++ packages/launchscreen/launchscreen-1.0.3.tar.gz/launchscreen-1.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="launchscreen",
+    version="1.0.3",
+    description="Launch Screen for Tkinter",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mahinbinhasan/launchscreen",
+    author="Mahin Bin Hasan (mahinbinhasan)",
+    author_email="<allmahin149@gmail.com>",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=[],
+    include_package_data=True,
+    install_requires=[],
+)
