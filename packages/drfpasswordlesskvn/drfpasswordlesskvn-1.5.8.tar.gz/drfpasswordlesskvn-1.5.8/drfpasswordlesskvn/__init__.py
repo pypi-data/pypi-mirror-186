37492ecb264216c73c4c93c4c7f58fe30af71f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:54 +0000

    Add package drfpasswordlesskvn/1.5.8/drfpasswordlesskvn-1.5.8.tar.gz

---
+++ packages/drfpasswordlesskvn/drfpasswordlesskvn-1.5.8.tar.gz/drfpasswordlesskvn-1.5.8/drfpasswordlesskvn/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'drfpasswordlesskvn'
+__version__ = '1.5.8'
+__author__ = 'Hedi'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022 Hedi'
+
+# Version synonym
+VERSION = __version__
+
+default_app_config = 'drfpasswordlesskvg.apps.DrfpasswordlessConfig'
