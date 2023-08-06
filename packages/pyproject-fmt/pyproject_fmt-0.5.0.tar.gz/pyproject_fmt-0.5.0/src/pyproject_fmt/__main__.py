f15cdf908dbffbaf5350ebe389724936f845cfdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:14:27 +0000

    Add package pyproject-fmt/0.5.0/pyproject_fmt-0.5.0.tar.gz

---
+++ packages/pyproject-fmt/pyproject_fmt-0.5.0.tar.gz/pyproject_fmt-0.5.0/src/pyproject_fmt/__main__.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import difflib
+import sys
+from pathlib import Path
+from typing import Iterable, Sequence
+
+from pyproject_fmt.cli import cli_args
+from pyproject_fmt.formatter import format_pyproject
+
+GREEN = "\u001b[32m"
+RED = "\u001b[31m"
+RESET = "\u001b[0m"
+
+
+def color_diff(diff: Iterable[str]) -> Iterable[str]:
+    for line in diff:
+        if line.startswith("+"):
+            yield GREEN + line + RESET
+        elif line.startswith("-"):
+            yield RED + line + RESET
+        else:
+            yield line
+
+
+def run(args: Sequence[str] | None = None) -> int:
+    opts = cli_args(sys.argv[1:] if args is None else args)
+    any_modified = False
+    for config in opts.as_configs:
+        formatted = format_pyproject(config)
+        before = config.toml
+        changed = before != formatted
+        any_modified = any_modified or changed
+        if opts.stdout:  # stdout just prints new format to stdout
+            print(formatted, end="")
+        else:
+            if before != formatted:
+                config.pyproject_toml.write_text(formatted, encoding="utf-8")
+            try:
+                name = str(config.pyproject_toml.relative_to(Path.cwd()))
+            except ValueError:
+                name = str(config.pyproject_toml)
+            diff: Iterable[str] = []
+            if changed:
+                diff = difflib.unified_diff(before.splitlines(), formatted.splitlines(), fromfile=name, tofile=name)
+
+            if diff:
+                diff = color_diff(diff)
+                print("\n".join(diff))  # print diff on change
+            else:
+                print(f"no change for {name}")
+    # exit with non success on change
+    return 1 if any_modified else 0
+
+
+if __name__ == "__main__":
+    sys.exit(run())
---
+++ packages/pyproject-fmt/pyproject_fmt-0.5.0.tar.gz/pyproject_fmt-0.5.0/src/pyproject_fmt/cli.py
@@ -0,0 +1,78 @@
+from __future__ import annotations
+
+import os
+from argparse import (
+    ArgumentDefaultsHelpFormatter,
+    ArgumentParser,
+    ArgumentTypeError,
+    Namespace,
+)
+from pathlib import Path
+from typing import Sequence
+
+from pyproject_fmt.formatter.config import DEFAULT_INDENT, Config
+
+
+class PyProjectFmtNamespace(Namespace):
+    """Options for pyproject-fmt tool"""
+
+    pyproject_tomls: list[Path]
+    stdout: bool
+    indent: int
+
+    @property
+    def as_configs(self) -> list[Config]:
+        return [
+            Config(
+                pyproject_toml=pyproject_toml,
+                toml=pyproject_toml.read_text(encoding="utf-8"),
+                indent=self.indent,
+            )
+            for pyproject_toml in self.pyproject_tomls
+        ]
+
+
+def pyproject_toml_path_creator(argument: str) -> Path:
+    """Validate that pyproject.toml can be formatted.
+
+    :param argument: the string argument passed in
+    :return: the pyproject.toml path
+    """
+    path = Path(argument).absolute()
+    if not path.exists():
+        raise ArgumentTypeError("path does not exist")
+    if not path.is_file():
+        raise ArgumentTypeError("path is not a file")
+    if not os.access(path, os.R_OK):
+        raise ArgumentTypeError("cannot read path")  # pragma: no cover
+    if not os.access(path, os.W_OK):
+        raise ArgumentTypeError("cannot write path")  # pragma: no cover
+    return path
+
+
+def _build_cli() -> ArgumentParser:
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    msg = "print the formatted text to the stdout (instead of update in-place)"
+    parser.add_argument("-s", "--stdout", action="store_true", help=msg)
+    parser.add_argument("--indent", type=int, default=DEFAULT_INDENT, help="number of spaces to indent")
+    parser.add_argument(
+        "pyproject_tomls", nargs="+", type=pyproject_toml_path_creator, help="pyproject.toml file(s) to format"
+    )
+    return parser
+
+
+def cli_args(args: Sequence[str]) -> PyProjectFmtNamespace:
+    """
+    Load the tools options.
+
+    :param args: CLI arguments
+    :return: the parsed options
+    """
+    parser = _build_cli()
+    return parser.parse_args(namespace=PyProjectFmtNamespace(), args=args)
+
+
+__all__ = [
+    "cli_args",
+    "PyProjectFmtNamespace",
+]
