48af89d4ccf393298efd29e3f59b993827ba14f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:22 +0000

    Add package square-learn/2.0.0/square_learn-2.0.0-py3-none-any.whl

---
+++ packages/square-learn/square_learn-2.0.0-py3-none-any.whl/sqlearn/model_interface.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+
+class Model:
+    """
+    An interface for machine learning models.
+
+    Functions:
+    ----------
+    train(self, X, y):
+        Trains the model on a dataset and the corresponding labels.
+    
+    predict(self, X):
+        Makes a prediction from a given observation.
+    """ 
+
+    def train(self, X, y):
+        """
+        Trains the model on a dataset and the corresponding labels.
+
+        Parameters:
+        ----------
+        X
+            A dataframe containing rows representing observations 
+            without a label column.
+        y
+            A series containing the labels for each observation 
+            in X.
+        """
+        pass
+
+    def predict(self, X):
+        """
+        Makes predictions from a dataframe of observations.
+
+        Parameters:
+        ----------
+        X
+            A dataframe containing observations.
+
+        Returns:
+        ----------
+        y
+            A series containing the predicted labels of the 
+            observations.
+        """
+        pass
+
+
+
