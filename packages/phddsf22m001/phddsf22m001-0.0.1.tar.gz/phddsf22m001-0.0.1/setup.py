5b52b9dc96bbe4020ad891a1f13d55aade2583ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:44 +0000

    Add package phddsf22m001/0.0.1/phddsf22m001-0.0.1.tar.gz

---
+++ packages/phddsf22m001/phddsf22m001-0.0.1.tar.gz/phddsf22m001-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='phddsf22m001',
+    version='0.0.1',
+    url='https://github.com/mhusnain8721/phddsf22m001',
+    author='Muhammad Husnain',
+    author_email='m.husnain8721@gmail.com',
+    description='A sample package',
+    packages=find_packages(),
+    install_requires=[],
+)
