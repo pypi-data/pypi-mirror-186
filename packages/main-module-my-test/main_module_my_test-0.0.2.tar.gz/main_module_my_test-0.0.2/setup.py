a93b0088e1b1bf7f32af16a7f4b03c47864cdd40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:33 +0000

    Add package main-module-my-test/0.0.2/main_module_my_test-0.0.2.tar.gz

---
+++ packages/main-module-my-test/main_module_my_test-0.0.2.tar.gz/main_module_my_test-0.0.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2' 
+DESCRIPTION = 'Mi primer paquete de Python'
+LONG_DESCRIPTION = 'Mi primer paquete de Python con una descripción ligeramente más larga'
+
+
+# Configurando
+setup(
+       # el nombre debe coincidir con el nombre de la carpeta 	  
+       #'modulomuysimple'
+        name="main_module_my_test", 
+        version=VERSION,
+        author="Hernan Hernandez",
+        author_email="<test@email.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # añade cualquier paquete adicional que debe ser
+        #instalado junto con tu paquete. Ej: 'caer'
+        
+        keywords=['python', 'primer paquete'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
