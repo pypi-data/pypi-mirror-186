6653fd519231bae460d94d82cc1320456bd71e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:56 +0000

    Add package ei-cli/0.0.14/ei_cli-0.0.14.tar.gz

---
+++ packages/ei-cli/ei_cli-0.0.14.tar.gz/ei_cli-0.0.14/ei/main.py
@@ -0,0 +1,20 @@
+import typing
+
+from ei.core.cli import create_application
+from ei.views.cli import ec2
+from ei.views.cli import elasticache
+from ei.views.cli import rds
+
+
+if typing.TYPE_CHECKING:
+    from ei.core.cli import Typeable  # noqa: F401
+
+
+APPS = [
+    ec2.group,
+    elasticache.group,
+    rds.group,
+]  # type: typing.List[Typeable]
+
+
+cli = create_application(APPS)
