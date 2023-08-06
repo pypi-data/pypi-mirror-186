d143ee6964fcd433370373674f135eea8bcc86a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:07 +0000

    Add package fileformats/0.3.0/fileformats-0.3.0-py3-none-any.whl

---
+++ packages/fileformats/fileformats-0.3.0-py3-none-any.whl/fileformats/numeric/__init__.py
@@ -0,0 +1,22 @@
+from ..core import __version__
+from fileformats.core import File
+
+
+class TextMatrix(File):
+    ext = ".mat"
+
+
+class RFile(File):
+    ext = ".rData"
+    binary = True
+
+
+class MatlabMatrix(File):
+    ext = ".mat"
+
+
+class DataFile(File):
+    """Generic binary data file"""
+
+    binary = True
+    ext = ".dat"
