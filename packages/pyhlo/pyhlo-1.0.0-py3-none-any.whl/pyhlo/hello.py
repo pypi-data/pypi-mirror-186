966ff312d246a60ae32495fe0d45ce0f9a94ae0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:30:55 +0000

    Add package pyhlo/1.0.0/pyhlo-1.0.0-py3-none-any.whl

---
+++ packages/pyhlo/pyhlo-1.0.0-py3-none-any.whl/pyhlo/hello.py
@@ -0,0 +1,14 @@
+def sayhello():
+    print('Hello World!')
+
+
+def reply_hello():
+    print('Hello, How are you!')
+
+
+def main():
+    sayhello()
+
+
+if __name__ == '__main__':
+    sayhello()
