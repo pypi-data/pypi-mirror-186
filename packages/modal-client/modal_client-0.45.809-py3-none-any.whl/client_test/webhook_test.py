089b94fc37bf69c44661268c30fa164e8e13de5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:35 +0000

    Add package modal-client/0.45.809/modal_client-0.45.809-py3-none-any.whl

---
+++ packages/modal-client/modal_client-0.45.809-py3-none-any.whl/client_test/webhook_test.py
@@ -0,0 +1,40 @@
+# Copyright Modal Labs 2022
+import pytest
+
+from modal.aio import AioStub
+
+stub = AioStub()
+
+
+@stub.webhook(method="POST")
+async def f(x):
+    return {"square": x**2}
+
+
+@pytest.mark.asyncio
+async def test_webhook(servicer, aio_client):
+    async with stub.run(client=aio_client):
+        assert f.web_url
+
+
+def test_webhook_cors():
+    from fastapi.testclient import TestClient
+
+    from modal._asgi import webhook_asgi_app
+
+    def handler():
+        return {"message": "Hello, World!"}
+
+    app = webhook_asgi_app(handler, method="GET")
+    client = TestClient(app)
+    resp = client.options(
+        "/",
+        headers={
+            "Origin": "http://example.com",
+            "Access-Control-Request-Method": "POST",
+        },
+    )
+    assert resp.headers["Access-Control-Allow-Origin"] == "http://example.com"
+
+    assert client.get("/").json() == {"message": "Hello, World!"}
+    assert client.post("/").status_code == 405  # Method Not Allowed
