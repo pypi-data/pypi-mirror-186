1ff9aa90eae0f3f3ed7696654e445bcecaefba69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:44 +0000

    Add package BanglaSpeech2Text/0.0.16/BanglaSpeech2Text-0.0.16-py3-none-any.whl

---
+++ packages/BanglaSpeech2Text/BanglaSpeech2Text-0.0.16-py3-none-any.whl/banglaspeech2text/utils/__init__.py
@@ -0,0 +1,20 @@
+import logging
+from banglaspeech2text.utils.download_path import get_app_path
+
+app_name = "BanglaSpeech2Text"
+
+FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(format=FORMAT)
+
+# create a logger
+logger = logging.getLogger(app_name)
+# logger.setLevel(logging.CRITICAL)
+
+
+# app_path = get_app_path(app_name)
+
+models_download_repo = "https://raw.githubusercontent.com/shhossain/whisper_bangla_models/"
+
+__all__ = [
+    'get_app_path', 'logger', 'models_download_repo', 'app_name'
+]
