38356842d6107284d72c9e8f565c779fddc2f220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:33 +0000

    Add package geoapis/0.3.2/geoapis-0.3.2.tar.gz

---
+++ packages/geoapis/geoapis-0.3.2.tar.gz/geoapis-0.3.2/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 30 11:22:08 2021
+
+@author: pearsonra
+"""
+import setuptools
+
+setuptools.setup(
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
