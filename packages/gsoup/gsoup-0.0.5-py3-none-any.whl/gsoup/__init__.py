8b33e152a5e7f17455a19049dbf17d46cda1c8ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:25:40 +0000

    Add package gsoup/0.0.5/gsoup-0.0.5-py3-none-any.whl

---
+++ packages/gsoup/gsoup-0.0.5-py3-none-any.whl/gsoup/__init__.py
@@ -0,0 +1,78 @@
+__version__ = "0.0.5"
+
+from .core import (
+    broadcast_batch,
+    to_hom,
+    homogenize,
+    compose_rt,
+    to_44,
+    to_34,
+    look_at_np,
+    to_np,
+    to_numpy,
+    to_torch,
+    to_8b,
+    to_float,
+    rotx,
+    roty,
+    rotz,
+    create_random_cameras_on_unit_sphere,
+    opengl_c2w_to_opencv_c2w,
+    opencv_c2w_to_opengl_c2w,
+    opengl_project_from_opencv_intrinsics,
+)
+
+from .geometry_basic import (
+    duplicate_faces,
+    remove_duplicate_faces,
+    get_aspect_ratio,
+    normalize_vertices,
+    clean_infinite_vertices,
+    calc_edges,
+    calc_face_normals,
+    calc_vertex_normals,
+    calc_edge_length,
+    get_center_of_attention,
+    scale_poses,
+    find_princple_componenets,
+    rotmat2qvec,
+    qvec2rotmat,
+    qslerp
+)
+
+from .geometry_advanced import (
+    distribute_field,
+    distribute_scalar_field,
+    distribute_vector_field,
+    qem
+)
+
+from .gsoup_io import (
+    save_image,
+    save_images,
+    save_animation,
+    save_mesh,
+    save_meshes,
+    save_obj,
+    load_image,
+    load_images,
+    load_obj,
+    load_mesh,
+)
+
+from .image import (
+    alpha_compose,
+    draw_text_on_image,
+    draw_gizmo_on_image,
+    merge_figures_with_line,
+    generate_voronoi_diagram,
+    interpolate_single_channel,
+    interpolate_multi_channel,
+)
+
+from .sphere_trace import (
+    generate_rays,
+    render
+)
+
+from . import structures
