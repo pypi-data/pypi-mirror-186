b4f9cfad25cea7011d8ae7c25b4be23fd1b83ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:06 +0000

    Add package scNodes/1.0.40/scNodes-1.0.40.tar.gz

---
+++ packages/scNodes/scNodes-1.0.40.tar.gz/scNodes-1.0.40/scNodes/install.py
@@ -0,0 +1,18 @@
+import subprocess
+import sys
+import os
+import tkinter as tk
+from tkinter import filedialog
+tkroot = tk.Tk()
+tkroot.withdraw()
+
+def install():
+    root = os.path.join(os.path.dirname(__file__)).replace("\\", "/") + "/"
+    #directory = filedialog.askdirectory() + "/"
+    #executable = sys.executable
+    #with open(directory + "scNodes.bat", "w") as f:
+    #    f.write('@echo off\n"' + str(executable) + '" "' + root + '__main__.py' + '"\npause')
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--no-index", "--find-links", root+"wheels/pyGpufit-1.2.0-py2.py3-none-any.whl", "pyGpufit"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--no-index", "--find-links", root+"wheels/dill-0.3.6-py3-none-any.whl", "pyGpufit"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", root+"wheels/pyimgui_wheels-2.0.1-cp38-cp38-manylinux1_x86_64.whl", "pyimgui-wheels"])
+
