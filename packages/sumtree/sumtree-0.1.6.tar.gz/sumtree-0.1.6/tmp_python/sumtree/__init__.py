a9c7559d96239256d03e340fdc6785f846a98629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:14:23 +0000

    Add package sumtree/0.1.6/sumtree-0.1.6.tar.gz

---
+++ packages/sumtree/sumtree-0.1.6.tar.gz/sumtree-0.1.6/tmp_python/sumtree/__init__.py
@@ -0,0 +1,8 @@
+from . import sumtree
+
+__doc__ = sumtree.__doc__
+if hasattr(sumtree, "__all__"):
+    __all__ = sumtree.__all__
+
+
+from sumtree import SumTree
---
+++ packages/sumtree/sumtree-0.1.6.tar.gz/sumtree-0.1.6/truc.py
@@ -0,0 +1,5 @@
+import sumtree
+
+print(sumtree.__version__)
+st = sumtree.SumTree(50)
+print(st.capacity)
