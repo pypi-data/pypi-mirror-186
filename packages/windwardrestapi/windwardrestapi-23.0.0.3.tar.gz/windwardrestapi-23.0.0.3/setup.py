242c064a2d2466a228e54de16a2b8ef7d9396634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:44 +0000

    Add package windwardrestapi/23.0.0.3/windwardrestapi-23.0.0.3.tar.gz

---
+++ packages/windwardrestapi/windwardrestapi-23.0.0.3.tar.gz/windwardrestapi-23.0.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+import distutils.sysconfig
+
+from setuptools import Distribution
+
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(foo):
+        return True
+
+setuptools.setup(
+    name= 'windwardrestapi',
+    version= "23.0.0.3",
+    description = 'Python client for the Windward RESTful Engine',
+    long_description = '',
+    url = 'http://www.windward.net/products/restful/',
+    author = 'Windward Studios',
+    author_email ='support@windward.net',
+    install_requires = ['requests', 'six'],
+    package_dir={'': 'src'},
+    packages = setuptools.find_packages(where='src'),
+    distclass=BinaryDistribution
+
+)
---
+++ packages/windwardrestapi/windwardrestapi-23.0.0.3.tar.gz/windwardrestapi-23.0.0.3/src/windwardrestapi/Model/VersionNumbers.py
@@ -0,0 +1,5 @@
+
+class VersionNumbers:
+    CLIENT_VERSION = "23.0.0.3"
+
+    REST_API_VERSION = "23.0.0"
