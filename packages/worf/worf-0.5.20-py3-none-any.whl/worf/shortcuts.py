a506c92e758fcd64776446cd069e6a57a16ecd2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:51 +0000

    Add package worf/0.5.20/worf-0.5.20-py3-none-any.whl

---
+++ packages/worf/worf-0.5.20-py3-none-any.whl/worf/shortcuts.py
@@ -0,0 +1,27 @@
+import subprocess
+
+from worf import __version__
+from worf.casing import camel_to_snake
+
+
+def field_list(value, delimiter="."):
+    return [
+        delimiter.join(map(camel_to_snake, field.split(".")))
+        for field in string_list(value)
+    ]
+
+
+def get_version():
+    try:
+        hash = (
+            subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
+            .strip()
+            .decode()
+        )
+        return f"{__version__}@{hash}"
+    except:  # pragma: no cover # noqa E722 Dont crash for any reason whatsoever
+        return __version__
+
+
+def string_list(value):
+    return value.split(",") if isinstance(value, str) else value
---
+++ packages/worf/worf-0.5.20-py3-none-any.whl/worf/views/__init__.py
@@ -0,0 +1,7 @@
+from worf.views.action import ActionAPI  # noqa
+from worf.views.base import AbstractBaseAPI, APIResponse  # noqa
+from worf.views.create import CreateAPI  # noqa
+from worf.views.delete import DeleteAPI  # noqa
+from worf.views.detail import DetailAPI, DetailUpdateAPI  # noqa
+from worf.views.list import ListAPI, ListCreateAPI  # noqa
+from worf.views.update import UpdateAPI  # noqa
