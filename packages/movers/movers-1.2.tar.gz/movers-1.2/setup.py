c98f8e5de6b01e92e8099d2b444145cc20f5c990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:02 +0000

    Add package movers/1.2/movers-1.2.tar.gz

---
+++ packages/movers/movers-1.2.tar.gz/movers-1.2/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+  name='movers',
+  version='1.2',
+  author="Lauren Jackson",
+  author_email="laurendevyork@gmail.com",
+  description="A great package for sound effects treatment.",
+  packages=setuptools.find_packages(),
+  classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
