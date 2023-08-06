b489d04c7f8ba2697309da90108d2c6d5aac9f8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:37 +0000

    Add package fermy/0.4.5/fermy-0.4.5.tar.gz

---
+++ packages/fermy/fermy-0.4.5.tar.gz/fermy-0.4.5/src/fermy/CINAC/__init__.py
@@ -0,0 +1,24 @@
+"""
+Created the 2021/10/08
+v0.0 First version
+
+@author: Nicolas Hardy
+
+This file is part of Fermy.
+
+    Fermy is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Fermy is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Fermy.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+print("CINAC loaded.")
+from . import CINAC
