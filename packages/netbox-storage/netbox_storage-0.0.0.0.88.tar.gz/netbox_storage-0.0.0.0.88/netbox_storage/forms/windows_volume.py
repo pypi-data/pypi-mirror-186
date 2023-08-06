8f08b4c41ea9db212c432bd68ca5c136e07d10d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:57 +0000

    Add package netbox-storage/0.0.0.0.88/netbox_storage-0.0.0.0.88.tar.gz

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.88.tar.gz/netbox_storage-0.0.0.0.88/netbox_storage/forms/windows_volume.py
@@ -0,0 +1,125 @@
+from django.forms import (
+    CharField,
+)
+
+from netbox.forms import (
+    NetBoxModelBulkEditForm,
+    NetBoxModelFilterSetForm,
+    NetBoxModelImportForm,
+    NetBoxModelForm,
+)
+from utilities.forms import (
+    DynamicModelChoiceField, DynamicModelMultipleChoiceField,
+)
+
+from netbox_storage.models import Filesystem, WindowsVolume, Drive
+from virtualization.models import VirtualMachine
+
+
+class WindowsVolumeForm(NetBoxModelForm):
+    """Form for creating a new LinuxVolume object."""
+
+    drive_name = CharField(
+        label="Drive Name",
+        help_text="The drive name of the windows volume e.g. D",
+    )
+    fs = DynamicModelChoiceField(
+        queryset=Filesystem.objects.all(),
+        label="Filesystem Name",
+        help_text="The Filesystem of the Volume e.g. NTFS",
+    )
+    drives = DynamicModelMultipleChoiceField(
+        queryset=Drive.objects.all(),
+        label="Drives",
+        required=False
+    )
+    virtual_machine = DynamicModelChoiceField(
+        queryset=VirtualMachine.objects.all(),
+        required=False
+    )
+
+    fieldsets = (
+        (
+            "Windows Volume",
+            (
+                "drive_name",
+                "fs",
+                "virtual_machine"
+            ),
+        ),
+        (
+            "Drive Configuration",
+            (
+                "drives",
+            ),
+        ),
+    )
+
+    class Meta:
+        model = WindowsVolume
+
+        fields = (
+            "drive_name",
+            "fs",
+            "drives",
+            "virtual_machine",
+            "description",
+        )
+
+
+class WindowsVolumeFilterForm(NetBoxModelFilterSetForm):
+    """Form for filtering LinuxVolume instances."""
+
+    model = WindowsVolume
+
+    drive_name = CharField(
+        required=False,
+        label="VG Name",
+    )
+    fs = DynamicModelChoiceField(
+        queryset=Filesystem.objects.all(),
+        required=False,
+        label="Filesystem Name",
+    )
+
+
+class WindowsVolumeImportForm(NetBoxModelImportForm):
+    fs = DynamicModelChoiceField(
+        queryset=Filesystem.objects.all(),
+        required=False
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = WindowsVolume
+
+        fields = (
+            "drive_name",
+            "fs",
+            "description",
+        )
+
+
+class WindowsVolumeBulkEditForm(NetBoxModelBulkEditForm):
+    model = WindowsVolume
+
+    drive_name = CharField(
+        required=False,
+        label="Drive Name",
+    )
+    fs = DynamicModelChoiceField(
+        queryset=Filesystem.objects.all(),
+        required=False
+    )
+    description = CharField(max_length=255, required=False)
+
+    fieldsets = (
+        (
+            None,
+            ("drive_name", "fs", "description"),
+        ),
+    )
+    nullable_fields = ["description"]
+
