37f565680087e47114e927a5f961192197205b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:53 +0000

    Add package fake-bpy-module-2.83/20230117/fake_bpy_module_2.83-20230117-py3-none-any.whl

---
+++ packages/fake-bpy-module-2.83/fake_bpy_module_2.83-20230117-py3-none-any.whl/bl_keymap_utils/__init__.py
@@ -0,0 +1,8 @@
+import sys
+import typing
+from . import keymap_hierarchy
+from . import keymap_from_toolbar
+from . import platform_helpers
+from . import io
+
+GenericType = typing.TypeVar("GenericType")
