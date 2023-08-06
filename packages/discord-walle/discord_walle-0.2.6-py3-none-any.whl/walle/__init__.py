08c5637b5b76338e7be631d17579078139a765b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:33 +0000

    Add package discord-walle/0.2.6/discord_walle-0.2.6-py3-none-any.whl

---
+++ packages/discord-walle/discord_walle-0.2.6-py3-none-any.whl/walle/__init__.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from .core import *
+from .configs import *
+
+try:
+    version_path = Path(__file__).parent / "VERSION"
+    version = version_path.read_text().strip()
+except FileNotFoundError:
+    version = "0.0.0"
+
+__version__ = version
+del version
