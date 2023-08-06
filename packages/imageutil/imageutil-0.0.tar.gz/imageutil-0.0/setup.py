5db7651f92e8ae6d1074994388f27cb00704cd2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:51 +0000

    Add package imageutil/0.0/imageutil-0.0.tar.gz

---
+++ packages/imageutil/imageutil-0.0.tar.gz/imageutil-0.0/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Fabio Caccamo',
+    author_email='fabio.caccamo@gmail.com',
+    description='high-level image operations.',
+    install_requires=['python-imageutil'],
+    long_description='Use `python-imageutil <https://pypi.python.org/pypi/python-imageutil/>`_ instead.',
+    maintainer='Fabio Caccamo',
+    maintainer_email='fabio.caccamo@gmail.com',
+    name='imageutil',
+    platforms=['all'],
+    url='https://pypi.python.org/pypi/python-imageutil/',
+    version='0.0',
+    zip_safe=False,
+)
