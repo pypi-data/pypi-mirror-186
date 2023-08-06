99d86abeb6892b3a5e7c05865745abc891f5f75f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:28 +0000

    Add package percheron/0.0.4/percheron-0.0.4.tar.gz

---
+++ packages/percheron/percheron-0.0.4.tar.gz/percheron-0.0.4/percheron/config.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CACHE_FOLDER = "cache"
+REPORT_FOLDER = "reports"
+DATA_FOLDER = "data"
+
+
+DJANGO_TRAC = "https://code.djangoproject.com/jsonrpc"
+DJANGO_PROJECT = "django"
+TRANSLATIONS_PROJECT = "django-docs-translations"
+
+DJANGO_REPO = Path(CACHE_FOLDER) / DJANGO_PROJECT
+TRANSLATIONS_REPO = Path(CACHE_FOLDER) / TRANSLATIONS_PROJECT
+
+
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", None)
+
+
+def validate_configuration():
+    """Confirm settings before things start."""
+    if "GITHUB_TOKEN" not in os.environ.keys():
+        return "GITHUB_TOKEN is not defined. You will have problems later."
