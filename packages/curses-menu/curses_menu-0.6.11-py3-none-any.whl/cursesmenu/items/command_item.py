1a70595e3856e82c8a620189167feda3d24f6e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:50 +0000

    Add package curses-menu/0.6.11/curses_menu-0.6.11-py3-none-any.whl

---
+++ packages/curses-menu/curses_menu-0.6.11-py3-none-any.whl/cursesmenu/items/command_item.py
@@ -0,0 +1,87 @@
+"""A  menu item that runs a shell command."""
+
+import os
+import subprocess
+import sys
+from typing import TYPE_CHECKING, Any, List, Optional
+
+from cursesmenu.items.external_item import ExternalItem
+
+if TYPE_CHECKING:
+    from cursesmenu.curses_menu import CursesMenu
+
+    PathType = os.PathLike[Any]
+else:
+    CursesMenu = Any
+    PathType = Any
+
+
+class CommandItem(ExternalItem):
+    """
+    A  menu item that runs a shell command using subprocess.run.
+
+    :param text: The text for the menu item.
+    :param command: The shell command to run when the item is selected.
+    :param arguments: Additional arguments passed to the command.
+    :param menu: The menu that this item belongs to
+    :param should_exit: Whether the menu will exit when this item is selected
+    :param stdout_filepath: A filepath that the stdout for the command will be written \
+    to
+    :param kwargs: A list of kwargs to be passed to subprocess.run
+    """
+
+    def __init__(
+        self,
+        text: str,
+        command: str,
+        arguments: Optional[List[str]] = None,
+        menu: Optional[CursesMenu] = None,
+        *,
+        should_exit: bool = False,
+        override_index: Optional[str] = None,
+        stdout_filepath: Optional[PathType] = None,
+        **kwargs: Any,  # noqa: ANN401
+    ) -> None:
+        """Initialize the menu."""
+        super().__init__(
+            text=text,
+            menu=menu,
+            should_exit=should_exit,
+            override_index=override_index,
+        )
+        self.command = command
+        self.arguments: List[str]
+        if arguments:
+            self.arguments = arguments
+        else:
+            self.arguments = []
+        self.kwargs = kwargs
+        self.stdout_filepath = stdout_filepath
+        self.exit_status: Optional[int] = None
+
+    def _get_args_list(self) -> List[str]:
+        args = [self.command] + self.arguments
+        if not sys.platform.startswith("win"):  # pragma: no-cover-windows
+            return [" ".join(args)]
+        else:  # pragma: no-cover-nonwindows
+            return args
+
+    def action(self) -> None:
+        """Run the command using subprocess.run."""
+        args = self._get_args_list()
+
+        if self.stdout_filepath:
+            with open(self.stdout_filepath, "w") as stdout:
+                completed_process = subprocess.run(
+                    args,
+                    shell=True,
+                    stdout=stdout,
+                    **self.kwargs,
+                )
+        else:
+            completed_process = subprocess.run(args, shell=True, **self.kwargs)
+        self.exit_status = completed_process.returncode
+
+    def get_return(self) -> Optional[int]:
+        """Get the exit status of the command or None if it hasn't been run."""
+        return self.exit_status
---
+++ packages/curses-menu/curses_menu-0.6.11-py3-none-any.whl/cursesmenu/items/submenu_item.py
@@ -0,0 +1,91 @@
+"""A menu item that opens a submenu."""
+
+from typing import TYPE_CHECKING, Any, Optional
+
+from cursesmenu.items.menu_item import MenuItem
+
+if TYPE_CHECKING:
+    from cursesmenu.curses_menu import CursesMenu
+else:
+    CursesMenu = Any
+
+
+class SubmenuItem(MenuItem):
+    """
+    A menu item that opens a submenu.
+
+    :param text: The text of the item
+    :param submenu: A CursesMenu to be displayed when the item is selected
+    :param menu: The menu that this item belongs to
+    :param should_exit: Whether the menu will exit when this item is selected
+    """
+
+    def __init__(
+        self,
+        text: str,
+        submenu: Optional[CursesMenu] = None,
+        menu: Optional[CursesMenu] = None,
+        *,
+        should_exit: bool = False,
+        override_index: Optional[str] = None,
+    ) -> None:
+        """Initialize the item."""
+        self._submenu: Optional[CursesMenu] = submenu
+        self._menu: Optional[CursesMenu] = menu
+        if self._submenu:
+            self._submenu.parent = menu
+        super().__init__(
+            text=text,
+            menu=menu,
+            should_exit=should_exit,
+            override_index=override_index,
+        )
+
+    @property
+    def submenu(self) -> Optional[CursesMenu]:
+        """Get the submenu associated with this item."""
+        return self._submenu
+
+    @submenu.setter
+    def submenu(self, submenu: Optional[CursesMenu]) -> None:
+        """Set the submenu and update its parent."""
+        self._submenu = submenu
+        if self._submenu is not None:
+            self._submenu.parent = self._menu
+
+    @property  # type: ignore[override]
+    def menu(self) -> Optional[CursesMenu]:  # type: ignore[override]
+        """Get the menu that this item belongs to."""
+        return self._menu
+
+    @menu.setter
+    def menu(self, menu: Optional[CursesMenu]) -> None:
+        """Set the menu for the item and update the submenu's parent."""
+        self._menu = menu
+        if self._submenu is not None:
+            self._submenu.parent = menu
+
+    def set_up(self) -> None:
+        """Set the screen up for the submenu."""
+        assert self.menu is not None
+        self.menu.pause()
+        self.menu.clear_screen()
+
+    def action(self) -> None:
+        """Start the submenu."""
+        assert self.submenu is not None
+        self.submenu.start()
+
+    def clean_up(self) -> None:
+        """Block until the submenu is done and then return to the parent."""
+        assert self.menu is not None
+        assert self.submenu is not None
+        self.submenu.join()
+        self.submenu.clear_screen()
+        self.menu.resume()
+
+    def get_return(self) -> Any:  # noqa: ANN401
+        """Get the returned value from the submenu."""
+        if self.submenu is not None:
+            return self.submenu.returned_value
+        return None
