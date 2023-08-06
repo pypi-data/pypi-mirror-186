e3ac8e313ddb3e528e76b62995b080e5209694fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:28 +0000

    Add package hamid/1.0.0/hamid-1.0.0.tar.gz

---
+++ packages/hamid/hamid-1.0.0.tar.gz/hamid-1.0.0/README.md
@@ -0,0 +1,9 @@
+How to import hamid's library is as follows:
+
+<< from hamid import hamid >>
+
+An example:
+
+from hamid import hamid
+
+bot = hamid("Your Auth Account")
---
+++ packages/hamid/hamid-1.0.0.tar.gz/hamid-1.0.0/hamid/How.py
@@ -0,0 +1,27 @@
+#Copyright (C) 2022 X CODER
+from time import sleep
+
+class color:
+      red = '\033[91m'
+      green = '\033[92m'
+      blue = '\033[94m'
+      yellow = '\033[93m'
+      magenta = '\033[95m'
+      cyan = '\033[96m'
+      white = '\033[97m'
+      bold = '\033[1m'
+      underline = '\033[4m'
+      black='\033[30m'
+
+class chup:
+      x_coder = f"""{color.bold}
+{color.cyan} [彡★] {color.white} ༻   {color.red} 𝙷𝙰𝙼𝙸𝙳 {color.yellow} 𝐕𝐄𝐑𝐒𝐈𝐎𝐍  {color.green}𝟷.0.𝟷
+{color.bold}
+{color.cyan} [彡★] {color.white} ༻   {color.red} 𝙷𝙰𝙼𝙸𝙳   {color.yellow} 𝙲𝙾𝙿𝚈𝚁𝙸𝙶𝙷𝚃 (𝑪) {color.green} 𝙷𝙰𝙼𝙸𝙳
+
+{color.cyan} [彡★]  {color.white}༻   {color.red} 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁  {color.yellow} 𝐑𝐔𝐁𝐈𝐊𝐀 {color.green} 𝙸𝙳 :  @MR_EzaTian_313
+
+{color.white}╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾
+"""
+      print(x_coder)
+      
