131c835ae8004a8b923a0b79c20610c3f9983065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:09 +0000

    Add package moms-apriltag/2023.1.15/moms_apriltag-2023.1.15-py3-none-any.whl

---
+++ packages/moms-apriltag/moms_apriltag-2023.1.15-py3-none-any.whl/moms_apriltag/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*
+##############################################
+# The MIT License (MIT)
+# Copyright (c) 2020 Kevin Walchko
+# see LICENSE for full details
+##############################################
+from importlib.metadata import version # type: ignore
+
+from .target import ApriltagBoard
+from .target import arucoTags
+from .apriltag_drawer import ApriltagDrawer
+from .generator2 import TagGenerator2, apriltags_v2
+from .generator3 import TagGenerator3, tag2RGBA, apriltags_v3
+from .color_space import *
+from .aruco_tags.calibrate import ApriltagCameraCalibration
+from .aruco_tags.calibrate import ApriltagStereoCalibration
+
+__license__ = "MIT"
+__author__ = "Kevin J. Walchko"
+__version__ = version("moms_apriltag")
