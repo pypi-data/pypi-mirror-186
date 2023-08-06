5244da120e6248beb6d731b63958c0f692ab9e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:05 +0000

    Add package nonebot-plugin-orangedice/0.2.2/nonebot_plugin_orangedice-0.2.2-py3-none-any.whl

---
+++ packages/nonebot-plugin-orangedice/nonebot_plugin_orangedice-0.2.2-py3-none-any.whl/nonebot_plugin_orangedice/config.py
@@ -0,0 +1,7 @@
+from typing import Literal
+from pydantic import BaseModel, Extra
+
+class Config(BaseModel, extra=Extra.ignore):
+    card_file: str = 'card.json'
+    log_file: str = 'log.json'
+    cache_file: str = 'cache.json'
