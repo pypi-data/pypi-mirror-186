31522fd9d2a6b11044a4fde50b2da5b1d1a69031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:58 +0000

    Add package azdsdr/1.230118.1/azdsdr-1.230118.1.tar.gz

---
+++ packages/azdsdr/azdsdr-1.230118.1.tar.gz/azdsdr-1.230118.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='azdsdr',
+    version='1.230118.1',
+    license='Apache License',
+    author="Andrew Zhu",
+    author_email='xhinker@hotmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/xhinker/azdsdr',
+    keywords='DS Data Reader',
+    install_requires=[
+        'numpy'
+        ,'pandas'
+        ,'pyodbc'
+        ,'azure-cli'
+        ,'azure-kusto-data'
+        ,'azure-kusto-ingest'
+        ,'azure-storage-blob'
+        ,'matplotlib'
+        ,'jupyterlab'
+        ,'ipython'
+        ,'ipykernel'
+    ],
+)
