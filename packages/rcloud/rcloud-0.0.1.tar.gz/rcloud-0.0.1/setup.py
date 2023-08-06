c3915ac6c1d988e6c2555aedcd9988b0b02e0f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:39 +0000

    Add package rcloud/0.0.1/rcloud-0.0.1.tar.gz

---
+++ packages/rcloud/rcloud-0.0.1.tar.gz/rcloud-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='rcloud',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://console.railam.cloud',
+    packages=['rcloud'],
+    py_modules=[
+        'rcloud.deploy'
+    ],
+    classifiers=['Development Status :: 1 - Planning'],
+)
