fba17f9f6358141c7595dbc90dd62e2068fad6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:01 +0000

    Add package deepsparse-ent/1.3.2/deepsparse_ent-1.3.2-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/deepsparse-ent/deepsparse_ent-1.3.2-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/deepsparse/generated_version.py
@@ -0,0 +1,17 @@
+# neuralmagic: no copyright
+# flake8: noqa
+__all__ = ["__version__", "version", "version_major", "version_minor", "version_bug", "version_build", "version_major_minor", "optimized", "is_release", "revision", "splash", "is_nightly", "build_date"]
+__version__ = '1.3.2'
+
+version = __version__
+version_major, version_minor, version_bug, version_build = version.split(".") + (
+    [None] if len(version.split(".")) < 4 else []
+) # handle conditional for version being 3 parts or 4 
+version_major_minor = f"{version_major}.{version_minor}"
+optimized = 1
+is_release = 1
+is_nightly = 0
+is_enterprise = 1
+revision = '783530f7'
+splash = 'DeepSparse, Copyright 2021-present / Neuralmagic, Inc. version: 1.3.2 (783530f7) (release) (optimized) (system=avx2, binary=avx2)'
+build_date = '20230116'
