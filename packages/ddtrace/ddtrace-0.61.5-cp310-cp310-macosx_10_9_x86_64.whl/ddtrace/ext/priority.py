d56147e9b951b38043e984a4fbd7ab646173e107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:57 +0000

    Add package ddtrace/0.61.5/ddtrace-0.61.5-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/ddtrace/ddtrace-0.61.5-cp310-cp310-macosx_10_9_x86_64.whl/ddtrace/ext/priority.py
@@ -0,0 +1,32 @@
+"""
+Priority is a hint given to the backend so that it knows which traces to reject or kept.
+In a distributed context, it should be set before any context propagation (fork, RPC calls) to be effective.
+
+For example:
+
+from ddtrace.ext.priority import USER_REJECT, USER_KEEP
+
+context = tracer.context_provider.active()
+# Indicate to not keep the trace
+context.sampling_priority = USER_REJECT
+
+# Indicate to keep the trace
+span.context.sampling_priority = USER_KEEP
+"""
+from ddtrace.constants import AUTO_KEEP
+from ddtrace.constants import AUTO_REJECT
+from ddtrace.constants import USER_KEEP
+from ddtrace.constants import USER_REJECT
+from ddtrace.internal.utils.deprecations import DDTraceDeprecationWarning
+
+from ..vendor.debtcollector.removals import removed_module
+
+
+removed_module(
+    module="ddtrace.ext.priority",
+    replacement="ddtrace.constants",
+    category=DDTraceDeprecationWarning,
+    removal_version="1.0.0",
+)
+
+__all__ = ["USER_REJECT", "AUTO_REJECT", "AUTO_KEEP", "USER_KEEP"]
