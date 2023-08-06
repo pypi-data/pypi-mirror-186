e6bcaebed1a2e7b9e43aac06f1f523514c31d17a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:38 +0000

    Add package VKLong/0.1/VKLong-0.1.tar.gz

---
+++ packages/VKLong/VKLong-0.1.tar.gz/VKLong-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="VKLong",
+    version="0.1",
+    description="Simple wrapper to make VK-Bot on Longpoll API",
+    install_requires=[
+        'requests',
+        'json',
+        'random'
+     ],
+     author="LeetCrash",
+     author_email="leetcrash.official@gmail.com",
+     keywords=[
+        "vk api",
+        "vk api library",
+        "vk longpoll",
+        "vk bot",
+        "vklong",
+        "vkapi",
+        "vk long poll bot"
+     ],
+     zip_safe=False
+)
