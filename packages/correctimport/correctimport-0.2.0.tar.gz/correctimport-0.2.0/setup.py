2b9982c6cbc440b7766cb22ef8f4b5e9d89ce0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:35 +0000

    Add package correctimport/0.2.0/correctimport-0.2.0.tar.gz

---
+++ packages/correctimport/correctimport-0.2.0.tar.gz/correctimport-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['correctimport']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pathsingleton==0.1.0']
+
+setup_kwargs = {
+    'name': 'correctimport',
+    'version': '0.2.0',
+    'description': 'Fixing the imports for packaging',
+    'long_description': '',
+    'author': 'Yashesh Dasari',
+    'author_email': 'yasheshdasari@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
