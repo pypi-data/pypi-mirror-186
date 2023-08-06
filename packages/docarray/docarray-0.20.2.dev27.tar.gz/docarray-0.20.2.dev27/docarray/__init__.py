bbb7f9c73017ff87eb29e78e961ece703aa37a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:23 +0000

    Add package docarray/0.20.2.dev27/docarray-0.20.2.dev27.tar.gz

---
+++ packages/docarray/docarray-0.20.2.dev27.tar.gz/docarray-0.20.2.dev27/docarray/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '0.20.2.dev27'
+
+import os
+
+from docarray.document import Document
+from docarray.array import DocumentArray
+from docarray.dataclasses import dataclass, field
+from docarray.helper import login, logout
+
+if 'DA_RICH_HANDLER' in os.environ:
+    from rich.traceback import install
+
+    install()
