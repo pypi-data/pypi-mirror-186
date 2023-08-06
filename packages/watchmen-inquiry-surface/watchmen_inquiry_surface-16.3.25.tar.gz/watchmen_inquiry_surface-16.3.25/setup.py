02d81ffa2d111dc23e52b59216a3f876088654c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:26 +0000

    Add package watchmen-inquiry-surface/16.3.25/watchmen_inquiry_surface-16.3.25.tar.gz

---
+++ packages/watchmen-inquiry-surface/watchmen_inquiry_surface-16.3.25.tar.gz/watchmen_inquiry_surface-16.3.25/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['watchmen_inquiry_surface', 'watchmen_inquiry_surface.data']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['watchmen-inquiry-kernel==16.3.25', 'watchmen-rest==16.3.25']
+
+extras_require = \
+{'mongodb': ['watchmen-storage-mongodb==16.3.25'],
+ 'mssql': ['watchmen-storage-mssql==16.3.25'],
+ 'mysql': ['watchmen-storage-mysql==16.3.25'],
+ 'oracle': ['watchmen-storage-oracle==16.3.25'],
+ 'oss': ['watchmen-storage-oss==16.3.25'],
+ 'postgresql': ['watchmen-storage-postgresql==16.3.25'],
+ 's3': ['watchmen-storage-s3==16.3.25'],
+ 'trino': ['watchmen-inquiry-trino==16.3.25']}
+
+setup_kwargs = {
+    'name': 'watchmen-inquiry-surface',
+    'version': '16.3.25',
+    'description': '',
+    'long_description': 'None',
+    'author': 'botlikes',
+    'author_email': '75356972+botlikes456@users.noreply.github.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
