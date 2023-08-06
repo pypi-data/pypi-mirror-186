1a908c5c074171db949c9b2e83ae6770c05ddb7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:56 +0000

    Add package ayaka/0.0.0.5/ayaka-0.0.0.5.tar.gz

---
+++ packages/ayaka/ayaka-0.0.0.5.tar.gz/ayaka-0.0.0.5/ayaka/adapters/__init__.py
@@ -0,0 +1,42 @@
+import sys
+from ..logger import ayaka_clog
+
+
+def is_hoshino():
+    if "hoshino" in sys.modules:
+        ayaka_clog("识别到 <y>hoshino</y>，加载为hoshino插件")
+        return True
+
+
+def is_nb1():
+    if "nonebot" in sys.modules:
+        if hasattr(sys.modules["nonebot"], "NoneBot"):
+            ayaka_clog("识别到 <y>nonebot1</y>，加载为nonebot1插件")
+            return True
+
+
+def is_nb2ob11():
+    if "nonebot.adapters.onebot.v11" in sys.modules:
+        ayaka_clog("识别到 <y>nonebot2 onebot11</y>，加载为nonebot2插件")
+        return True
+
+
+# hoshino
+if is_hoshino():
+    from .hoshino import regist
+    regist()
+
+# nonebot1
+elif is_nb1():
+    from .nb1 import regist
+    regist()
+
+# nonebot2 onebot11
+elif is_nb2ob11():
+    from .nb2ob11 import regist
+    regist()
+
+# console
+else:
+    ayaka_clog("未检测到环境信息，加载为<y>console</y>程序")
+    from .console import run
