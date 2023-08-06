e6a883ee968dfec82c4f95170b6d42f40bf3009b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:54 +0000

    Add package signalsnap/0.1.4/signalsnap-0.1.4.tar.gz

---
+++ packages/signalsnap/signalsnap-0.1.4.tar.gz/signalsnap-0.1.4/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+
+setup(name='signalsnap',
+      version='0.1.4',
+      description="Spectra at your fingertips",
+      url='https://github.com/MarkusSifft/SignalSnap',
+      author='Markus Sifft',
+      author_email='markus.sifft@rub.de',
+      license='BSD 3 - Clause License',
+      packages=['signalsnap'],
+      zip_safe=False)
+
+
+
+
+
+
+
+# [project]
+# name = "signalsnap"
+# version = "0.1.3"
+# authors = [
+#   { name="Markus Sifft", email="markus.sifft@rub.de" },
+# ]
+# description = "Spectra at your fingertips"
+# readme = "README.md"
+# requires-python = ">=3.7"
+# classifiers = [
+#     "Development Status :: 2 - Pre-Alpha",
+#     "Intended Audience :: Science/Research",
+#     "License :: OSI Approved :: BSD License",
+#     "Programming Language :: Python",
+#     "Programming Language :: Python :: 3",
+#     "Topic :: Scientific/Engineering",
+#     "Operating System :: OS Independent",
+# ]
+# dependencies = [
+#     "numpy",
+#     "matplotlib",
+#     "h5py",
+#     "arrayfire",
+#     "numba",
+#     "scipy",
+#     "tqdm"
+# ]
+#
+# [project.urls]
+# "Homepage" = "https://github.com/MarkusSifft/SignalSnap"
+# "Bug Tracker" = "https://github.com/MarkusSifft/SignalSnap/issues"
