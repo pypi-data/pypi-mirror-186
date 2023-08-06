5ece81e0f50696beff306492b86d520d297bbe27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:29 +0000

    Add package opensafely/1.33.2/opensafely-1.33.2.tar.gz

---
+++ packages/opensafely/opensafely-1.33.2.tar.gz/opensafely-1.33.2/opensafely/_vendor/jobrunner/lib/docker_stats.py
@@ -0,0 +1,57 @@
+import json
+
+from opensafely._vendor.jobrunner.lib.subprocess_utils import subprocess_run
+
+
+DEFAULT_TIMEOUT = 10
+
+
+def get_volume_and_container_sizes(timeout=DEFAULT_TIMEOUT):
+    response = subprocess_run(
+        ["docker", "system", "df", "--verbose", "--format", "{{json .}}"],
+        capture_output=True,
+        check=True,
+        timeout=timeout,
+    )
+    data = json.loads(response.stdout)
+    volumes = {row["Name"]: _parse_size(row["Size"]) for row in data["Volumes"]}
+    containers = {row["Names"]: _parse_size(row["Size"]) for row in data["Containers"]}
+    return volumes, containers
+
+
+def get_container_stats(timeout=DEFAULT_TIMEOUT):
+    response = subprocess_run(
+        ["docker", "stats", "--no-stream", "--format", "{{json .}}"],
+        capture_output=True,
+        check=True,
+        timeout=timeout,
+    )
+    data = [json.loads(line) for line in response.stdout.splitlines()]
+    return {
+        row["Name"]: {
+            "cpu_percentage": float(row["CPUPerc"].rstrip("%")),
+            "memory_used": _parse_size(row["MemUsage"].split()[0]),
+        }
+        for row in data
+    }
+
+
+CONVERSIONS = {
+    "B": 1,
+    "KB": 10**3,
+    # Is this correct?
+    "kB": 10**3,
+    "KiB": 2**10,
+    "MB": 10**6,
+    "MiB": 2**20,
+    "GB": 10**9,
+    "GiB": 2**30,
+    "TB": 10**12,
+    "TiB": 2**40,
+}
+
+
+def _parse_size(size):
+    units = size.lstrip("0123456789.-")
+    value = float(size[: -len(units)])
+    return int(value * CONVERSIONS[units])
