8f3c0eda6a391adb33da9cda616ed917d02b022a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:16:44 +0000

    Add package aegis-engine/1.0.2/aegis_engine-1.0.2.tar.gz

---
+++ packages/aegis-engine/aegis_engine-1.0.2.tar.gz/aegis_engine-1.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="aegis_engine",
+  version="1.0.2",
+  license="MIT",
+  author="Bailey de Villiers",
+  author_email="bailey.devilliers@gmail.com",
+  description = "Capital market asset valuation engine.",
+  long_description=open("README.md", 'r').read(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/itchysnake/aegis',
+  keywords="",
+  # ["featherbot.analysts", "featherbot.explorers"]
+  packages = find_packages("scripts"),
+  package_dir={"aegis" : "scripts"},
+  install_requires=[
+    "numpy>=1.2",
+    "pandas>=1.5",
+    "python-dateutil>=2.8",
+    "pytz>=2022.7",
+    "six>=1.16",
+    "webencodings>=0.5"
+  ],
+)
