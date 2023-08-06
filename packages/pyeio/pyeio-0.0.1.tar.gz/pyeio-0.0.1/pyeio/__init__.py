eff334036452b243fe0950b79bd9eae9c2d10a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:54 +0000

    Add package pyeio/0.0.1/pyeio-0.0.1.tar.gz

---
+++ packages/pyeio/pyeio-0.0.1.tar.gz/pyeio-0.0.1/pyeio/__init__.py
@@ -0,0 +1,1 @@
+from pyeio.director import EIO
---
+++ packages/pyeio/pyeio-0.0.1.tar.gz/pyeio-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyeio",
+    description="Python library for easy input/output file operations.",
+    version="0.0.1",
+    author="Hart Traveller",
+    author_email="hart@cephalon.io",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["pathlib"],
+)
