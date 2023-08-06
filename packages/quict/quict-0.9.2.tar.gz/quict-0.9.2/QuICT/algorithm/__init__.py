7ab5f1ef1f919cfac2f1cc595651b0f718cc91fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:16 +0000

    Add package quict/0.9.2/quict-0.9.2.tar.gz

---
+++ packages/quict/quict-0.9.2.tar.gz/quict-0.9.2/QuICT/algorithm/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+# @TIME    : 2020/2/11 10:30
+# @Author  : Han Yu
+# @File    : __init__.py.py
+
+from ._algorithm import Algorithm
+from .quantum_algorithm import *
+from .quantum_machine_learning import *
+from .weight_decision import WeightDecision
