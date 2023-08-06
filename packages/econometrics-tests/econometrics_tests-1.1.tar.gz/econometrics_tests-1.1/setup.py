c4be10ba0839ace47455847971d7cc8dcb93b273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:42 +0000

    Add package econometrics-tests/1.1/econometrics_tests-1.1.tar.gz

---
+++ packages/econometrics-tests/econometrics_tests-1.1.tar.gz/econometrics_tests-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(name='econometrics_tests',
+      version='1.1',
+      description='-',
+      author_email='dmitriyalmazof1@gmail.com',
+      packages=find_packages(),
+      install_requires = ['pandas'],
+      zip_safe=False,
+      )
