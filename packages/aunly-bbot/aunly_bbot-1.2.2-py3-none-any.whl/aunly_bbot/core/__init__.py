9a494cca88a60bcf6e11801edc322005d17de95a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:48 +0000

    Add package aunly-bbot/1.2.2/aunly_bbot-1.2.2-py3-none-any.whl

---
+++ packages/aunly-bbot/aunly_bbot-1.2.2-py3-none-any.whl/aunly_bbot/core/__init__.py
@@ -0,0 +1,12 @@
+from bilireq.auth import Auth
+
+BOT_Status = {
+    "started": False,
+    "living": {},
+    "offset": None,
+    "init": False,
+    "dynamic_updating": False,
+    "live_updating": False,
+}
+
+Bili_Auth = Auth()
