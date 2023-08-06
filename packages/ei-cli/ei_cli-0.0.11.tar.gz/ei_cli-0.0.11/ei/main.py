6262d57ebce09ecb6ae1642119f94e51eaea7a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:17 +0000

    Add package ei-cli/0.0.11/ei_cli-0.0.11.tar.gz

---
+++ packages/ei-cli/ei_cli-0.0.11.tar.gz/ei_cli-0.0.11/ei/main.py
@@ -0,0 +1,4 @@
+from ei.core.applications import create_application
+
+
+cli = create_application()
---
+++ packages/ei-cli/ei_cli-0.0.11.tar.gz/ei_cli-0.0.11/tests/test_cli.py
@@ -0,0 +1,5 @@
+from ei.core.applications import create_application
+
+
+def test_create_application() -> None:
+    create_application()
