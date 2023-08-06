0404822e6587fc2efbcb596dd774a8b54beb5bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:41 +0000

    Add package dallinger/9.3.1/dallinger-9.3.1-py3-none-any.whl

---
+++ packages/dallinger/dallinger-9.3.1-py3-none-any.whl/dallinger/heroku/__init__.py
@@ -0,0 +1,5 @@
+"""This is Dallinger, a platform for simulating evolution with people."""
+
+from .tools import app_name, auth_token, log_in, sanity_check, container_log_in
+
+__all__ = ("app_name", "auth_token", "log_in", "container_log_in", "sanity_check")
