c012bdf571e90d2fa5ba144c0a9c1d306c7d6e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:27 +0000

    Add package geomapi/0.1.6/geomapi-0.1.6-py3-none-any.whl

---
+++ packages/geomapi/geomapi-0.1.6-py3-none-any.whl/geomapi/tools/alignmenttools/pose3d.py
@@ -0,0 +1,17 @@
+from alignmenttools.match import Match3d
+import geomapi
+from geomapi.nodes import Node
+from geomapi.nodes import GeometryNode
+from geomapi.nodes import SessionNode
+from typing import List
+
+
+def get_transformation(testSession : SessionNode, refSessions : list[SessionNode]):
+    pass
+
+
+def match_fgr(testGeometry: GeometryNode, refSession : SessionNode):
+    pass
+
+def match_super4pcs(testGeometry: GeometryNode, refSession : SessionNode):
+    pass
