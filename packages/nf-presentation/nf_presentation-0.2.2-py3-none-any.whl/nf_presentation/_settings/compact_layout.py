10e5d46fd8fb35a9ab9a6d74be9da36c5b6aa0eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:19 +0000

    Add package nf-presentation/0.2.2/nf_presentation-0.2.2-py3-none-any.whl

---
+++ packages/nf-presentation/nf_presentation-0.2.2-py3-none-any.whl/nf_presentation/_settings/compact_layout.py
@@ -0,0 +1,18 @@
+"""A module contataining constants for ComractRenderer class
+
+Thic type of layout renders a single exercise to a single slide list"""
+
+TITLE_POSITION=(0.5,0.5)
+TITLE_SIZE=(33,1)
+TITLE_BACKGROUND=(46,117,182)
+TITLE_FOREGROUND=(255,255,255)
+
+SCHEME_POSITION=(12.0,2.0)
+SCHEME_HEIGHT=8.5 # not used atm
+SCHEME_WIDTH=12.5
+
+LEFT_TABLE_POSITION=(0.5,2.0)
+LEFT_TABLE_WIDTH=10.5
+
+RIGHT_TABLE_POSITION=(12.0,10.5)
+RIGHT_TABLE_WIDTH=21.5
