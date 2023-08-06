092cdc48734ebea24e026f79757bbcc797c49e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:12 +0000

    Add package netbox-storage/0.0.0.0.0.14/netbox_storage-0.0.0.0.0.14.tar.gz

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.0.14.tar.gz/netbox_storage-0.0.0.0.0.14/netbox_storage/forms/windows_volume.py
@@ -0,0 +1,90 @@
+from django.forms import (
+    CharField,
+)
+
+from netbox.forms import (
+    NetBoxModelBulkEditForm,
+    NetBoxModelFilterSetForm,
+    NetBoxModelImportForm,
+    NetBoxModelForm,
+)
+from utilities.forms import (
+    DynamicModelChoiceField,
+)
+
+from netbox_storage.models import Filesystem, WindowsVolume
+
+
+class WindowsVolumeForm(NetBoxModelForm):
+    """Form for creating a new LinuxVolume object."""
+
+    fs = DynamicModelChoiceField(
+        queryset=Filesystem.objects.all(),
+        required=False
+    )
+
+    class Meta:
+        model = WindowsVolume
+
+        fields = (
+            "drive_name",
+            "fs",
+            "description",
+        )
+
+
+class WindowsVolumeFilterForm(NetBoxModelFilterSetForm):
+    """Form for filtering LinuxVolume instances."""
+
+    model = WindowsVolume
+
+    drive_name = CharField(
+        required=False,
+        label="VG Name",
+    )
+    fs = DynamicModelChoiceField(
+        queryset=Filesystem.objects.all(),
+        required=False
+    )
+
+
+class WindowsVolumeImportForm(NetBoxModelImportForm):
+    fs = DynamicModelChoiceField(
+        queryset=Filesystem.objects.all(),
+        required=False
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = WindowsVolume
+
+        fields = (
+            "drive_name",
+            "fs",
+            "description",
+        )
+
+
+class WindowsVolumeBulkEditForm(NetBoxModelBulkEditForm):
+    model = WindowsVolume
+
+    drive_name = CharField(
+        required=False,
+        label="Drive Name",
+    )
+    fs = DynamicModelChoiceField(
+        queryset=Filesystem.objects.all(),
+        required=False
+    )
+    description = CharField(max_length=255, required=False)
+
+    fieldsets = (
+        (
+            None,
+            ("drive_name", "fs", "description"),
+        ),
+    )
+    nullable_fields = ["description"]
+
