c35204e128718e7bc986026f9f35780c166c0d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:54 +0000

    Add package drfpasswordlesskvn/1.5.8/drfpasswordlesskvn-1.5.8-py3-none-any.whl

---
+++ packages/drfpasswordlesskvn/drfpasswordlesskvn-1.5.8-py3-none-any.whl/drfpasswordlesskvn/urls.py
@@ -0,0 +1,21 @@
+from drfpasswordlesskvn.settings import api_settings
+from django.urls import path
+from drfpasswordlesskvn.views import (
+     ObtainEmailCallbackToken,
+     ObtainMobileCallbackToken,
+     ObtainAuthTokenFromCallbackToken,
+     VerifyAliasFromCallbackToken,
+     ObtainEmailVerificationCallbackToken,
+     ObtainMobileVerificationCallbackToken,
+)
+
+app_name = 'drfpasswordlesskvn'
+
+urlpatterns = [
+     path(api_settings.PASSWORDLESS_AUTH_PREFIX + 'email/', ObtainEmailCallbackToken.as_view(), name='auth_email'),
+     path(api_settings.PASSWORDLESS_AUTH_PREFIX + 'mobile/', ObtainMobileCallbackToken.as_view(), name='auth_mobile'),
+     path(api_settings.PASSWORDLESS_AUTH_PREFIX + 'token/', ObtainAuthTokenFromCallbackToken.as_view(), name='auth_token'),
+     path(api_settings.PASSWORDLESS_VERIFY_PREFIX + 'email/', ObtainEmailVerificationCallbackToken.as_view(), name='verify_email'),
+     path(api_settings.PASSWORDLESS_VERIFY_PREFIX + 'mobile/', ObtainMobileVerificationCallbackToken.as_view(), name='verify_mobile'),
+     path(api_settings.PASSWORDLESS_VERIFY_PREFIX, VerifyAliasFromCallbackToken.as_view(), name='verify_token'),
+]
