72c08629b9d6878c33994b7b82b8fbd81ecb02bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:46 +0000

    Add package magnumnp/0.9.15/magnumnp-0.9.15.tar.gz

---
+++ packages/magnumnp/magnumnp-0.9.15.tar.gz/magnumnp-0.9.15/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='magnumnp',
+      version='v0.9.15',
+      description='magnum.np finite-difference package for the solution of micromagnetic problems',
+      author='Florian Bruckner',
+      author_email='florian.bruckner@univie.ac.at',
+      url='http://micromagnetics.org/magnum.np/',
+      packages=['magnumnp', 'magnumnp.common', 'magnumnp.field_terms', 'magnumnp.loggers', 'magnumnp.solvers', 'magnumnp.utils'],
+      install_requires = [
+            'torch',
+            'numpy',
+            'scipy',
+            'setproctitle',
+            'pyvista',
+            'xitorch'
+            ]
+     )
