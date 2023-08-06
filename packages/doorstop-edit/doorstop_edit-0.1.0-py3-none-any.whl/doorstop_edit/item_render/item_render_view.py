de2e0ac9762f601fc42a11469e2f29b88ff75c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:13 +0000

    Add package doorstop-edit/0.1.0/doorstop_edit-0.1.0-py3-none-any.whl

---
+++ packages/doorstop-edit/doorstop_edit-0.1.0-py3-none-any.whl/doorstop_edit/item_render/item_render_view.py
@@ -0,0 +1,127 @@
+import logging
+import re
+from typing import Callable, List, Optional, Union
+
+import doorstop
+from PySide6.QtCore import QObject, QUrl, Signal
+from PySide6.QtWebEngineCore import QWebEnginePage
+from PySide6.QtWebEngineWidgets import QWebEngineView
+
+from doorstop_edit.doorstop_data import DoorstopData
+from doorstop_edit.item_render.render_worker import RenderWorker
+from doorstop_edit.utils.debug_timer import time_function
+from doorstop_edit.utils.item_matcher import match_level
+
+logger = logging.getLogger("gui")
+
+
+class RedirectOverridePage(QWebEnginePage):
+    """Class to enable Child and Parent items link navigation in HTML."""
+
+    def __init__(self, parent: QObject, on_navigration_request: Callable[[str], None]):
+        super().__init__(parent)
+        self._on_navigration_request = on_navigration_request
+
+    def acceptNavigationRequest(self, url: Union[QUrl, str], _2: QWebEnginePage.NavigationType, _3: bool) -> bool:
+        if isinstance(url, QUrl):
+            url = url.url()
+
+        is_nav_ok = url.startswith("data:text/html")
+        if not is_nav_ok:
+            self._on_navigration_request(url)
+
+        return is_nav_ok
+
+
+class ItemRenderView(QObject):
+    BASE_URL = "file://"
+
+    signal_render_html = Signal((list, doorstop.Item))  # type: ignore
+
+    def __init__(self, web_view: QWebEngineView, doorstop_data: DoorstopData) -> None:
+        super().__init__(web_view)
+        self.web_view = web_view
+        self.doorstop_data = doorstop_data
+
+        self.web_view.loadFinished.connect(self._on_load_finished)  # type: ignore
+
+        page = RedirectOverridePage(web_view, self._on_navigration_request)
+        web_view.setPage(page)
+
+        self._clear_history_on_load = True
+        self._section_mode_on = False
+        self._viewed_item: Optional[doorstop.Item] = None
+
+        self._render_worker = RenderWorker()
+        self._render_worker.result_ready.connect(self._on_render_finished)
+        self.signal_render_html.connect(self._render_worker.render)
+
+        web_view.show()  # Render empty view to set backround color.
+
+    @time_function("Changing content in HTML view")
+    def show(self, item: Optional[doorstop.Item]) -> None:
+        self._viewed_item = item
+        self._clear_history_on_load = True
+        self._show(item)
+
+    def destroy(self) -> None:
+        self.web_view.page().deleteLater()
+        self._render_worker.destroy()
+
+    def set_section_mode(self, on: bool) -> None:
+        self._section_mode_on = on
+        self.show(self._viewed_item)
+
+    def _show(self, item: Optional[doorstop.Item]) -> None:
+        def sort_func(val: doorstop.Item) -> List[int]:
+            return [int(p) for p in str(val.level).split(".")]
+
+        items_to_render: List[doorstop.Item] = []
+        if item is not None:
+            if self._section_mode_on:
+                for doc_item in self.doorstop_data.iter_items(item.document):
+                    if not doc_item.active and doc_item.uid != item.uid:
+                        # Hide inactive item unless it is the selected one.
+                        continue
+                    if match_level(doc_item, item.level, include_parent=True):
+                        items_to_render.append(doc_item)
+                items_to_render.sort(key=sort_func)
+            else:
+                items_to_render.append(item)
+
+        self.signal_render_html.emit(items_to_render, item)
+
+    def _on_render_finished(self, html: str, base_url: str) -> None:
+        self.web_view.setHtml(html, base_url)
+
+    def _on_load_finished(self, ok: bool) -> None:
+        if not ok:
+            logger.warning("Web page failed to load.")
+            return
+
+        if self._clear_history_on_load:
+            self.web_view.history().clear()
+            self._clear_history_on_load = False
+
+    def _on_navigration_request(self, url: str) -> None:
+        if not url.startswith(self.BASE_URL):
+            return
+
+        item_uid: Optional[str] = None
+        for doc_name, doc in self.doorstop_data.get_documents().items():
+            if doc_name in url:
+                escaped_doc_name = re.escape(doc_name + doc.sep)
+                m = re.match(rf".*({escaped_doc_name}\d+).*", url)
+                if m is not None:
+                    item_uid = m.group(1)
+                    break
+        if item_uid is None:
+            logger.warning("Could not find item uid in url: %s", url)
+            return
+
+        item = self.doorstop_data.find_item(item_uid)
+        if item is None:
+            logger.warning("Could not find item of uid %s", item_uid)
+            return
+
+        self._show(item)
---
+++ packages/doorstop-edit/doorstop_edit-0.1.0-py3-none-any.whl/doorstop_edit/main.py
@@ -0,0 +1,105 @@
+import argparse
+import logging
+import os
+import re
+import sys
+from pathlib import Path
+from typing import List, Optional
+
+import doorstop
+from doorstop import settings as doorstop_settings
+from PySide6.QtCore import QLoggingCategory
+from PySide6.QtGui import QFontDatabase
+from PySide6.QtWidgets import QApplication
+from qt_material import apply_stylesheet
+
+from doorstop_edit.application import DoorstopEdit
+from doorstop_edit.theme import setup_colors
+from doorstop_edit.utils.version_summary import create_version_summary
+
+logger = logging.getLogger("gui")
+
+# Do not use version control system since it will slow down the UI by calling git/svn etc. on
+# every item save.
+doorstop_settings.ADDREMOVE_FILES = False
+doorstop.Item.auto = False  # Disable automatic save.
+
+
+def load_custom_css() -> str:
+    with open(Path(__file__).parent / "custom.css", "r", encoding="utf-8") as file:
+        custom_css = file.read()
+
+    result = re.findall("__(.*)__", custom_css)
+    for replace_val in result:
+        if replace_val in os.environ:
+            custom_css = custom_css.replace("__" + replace_val + "__", os.environ[replace_val])
+        else:
+            raise RuntimeError(f"Failed to expand {replace_val} in css")
+
+    return custom_css
+
+
+def setup(app: QApplication, argv: List[str]) -> Optional[DoorstopEdit]:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", action="store_true", help="turn on verbose logging")
+    parser.add_argument("--version", action="store_true", help="turn on verbose logging")
+    parser.add_argument("--font-size", default=13, type=int, help="set custom font-size")
+    parser.add_argument("--density", default=-1, type=int, help="set density scale (make thing smaller or bigger)")
+    parser.add_argument("directory", default=".", nargs="?", help="Doorstop root directory")
+    args = parser.parse_args(argv[1:])
+
+    if args.version:
+        print(create_version_summary())
+        return None
+
+    logging.basicConfig(
+        level=logging.WARNING,
+        format="%(asctime)s [%(name)s] %(message)s",
+        datefmt="%H:%M:%S",
+    )
+    logger.setLevel(logging.DEBUG if args.verbose else logging.INFO)
+
+    root_directory = Path(args.directory)
+    if not root_directory.is_dir():
+        logger.error("Invalid argument: '%s' is not a directory.", root_directory)
+        return None
+
+    # Setup custom theme
+    extra = {
+        # Button colors
+        "danger": "#dc3545",
+        "warning": "#ffc107",
+        "success": "#17a2b8",
+        # Font
+        "font_family": "Roboto",
+        "font_size": args.font_size,
+        "line_height": args.font_size,
+        # Density Scale
+        "density_scale": args.density,
+    }
+    apply_stylesheet(app, theme="dark_teal.xml", extra=extra)
+    app.setStyleSheet(app.styleSheet() + load_custom_css())
+
+    setup_colors(extra)
+
+    # Add custom font
+    QFontDatabase.addApplicationFont(":/font/DroidSansMono.ttf")
+
+    # Disable info logs from WebEngine
+    web_engine_context_log = QLoggingCategory("qt.webenginecontext")  # type: ignore
+    web_engine_context_log.setFilterRules("*.info=false")
+
+    return DoorstopEdit(root_directory)
+
+
+def main() -> int:
+    # As minimalistic as possible since it wont be tested.
+    app = QApplication([])
+
+    editor = setup(app, sys.argv)
+    app.aboutToQuit.connect(editor.quit)  # type: ignore
+    if editor is not None:
+        editor.show()
+        return app.exec()
+
+    return 0
