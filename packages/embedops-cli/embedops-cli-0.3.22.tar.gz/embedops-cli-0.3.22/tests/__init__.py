6cb25404df1dc0340b0e06c4c904da9c66e2bef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:48 +0000

    Add package embedops-cli/0.3.22/embedops-cli-0.3.22.tar.gz

---
+++ packages/embedops-cli/embedops-cli-0.3.22.tar.gz/embedops-cli-0.3.22/tests/__init__.py
@@ -0,0 +1,7 @@
+"""
+Group of tests for the embedops_cli and eo_tools
+"""
+
+GLYML_FILENAME = "tests/test-.gitlab-ci.yml"
+BBYML_FILENAME = "tests/test-bitbucket-pipelines.yml"
+GHYML_FILENAME = "tests/test-.github-ci.yml"
