fa202f95ec56d16ba3f3b42cd27f3d889281599b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:23 +0000

    Add package easy-dp/0.0.3/easy_dp-0.0.3.tar.gz

---
+++ packages/easy-dp/easy_dp-0.0.3.tar.gz/easy_dp-0.0.3/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="easy-dp",
+    version="0.0.1",
+    author="Tarasco Pier Paolo",
+    author_email="pier.tarasco@gmail.com",
+    description="A not-so-efficient library for easy implementation of multi-gpu differential privacy",
+)
