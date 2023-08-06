710a19bd677d11fdfd9b5286a324fe82cd981ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:39 +0000

    Add package dhlab/2.17.0/dhlab-2.17.0.tar.gz

---
+++ packages/dhlab/dhlab-2.17.0.tar.gz/dhlab-2.17.0/README.md
@@ -0,0 +1,56 @@
+# DHLAB
+<!-- start dhlab-intro -->
+
+ [`dhlab`](https://pypi.org/project/dhlab/) is a python library for accessing reduced representations of text and pictures at
+the National Library of Norway (NLN), *Nasjonalbiblioteket* (*NB*) in Norwegian. 
+ It is developed and maintained by [The Digital Humanities lab group](https://www.nb.no/dh-lab/).
+
+The python package includes wrapper functions for the [DHLAB API](https://api.nb.no/dhlab) (Application 
+Programming Interface) that can be used to query the texts in [NB Digital](https://www.nb.no/search), the NLN's digital collection of books and newspapers.
+
+The API allows for textual qualitative and quantitative analyses of the digital texts by generating 
+e.g. word frequency lists, concordances, collocations, n-grams, as well as
+extracting names and narrative graphs.
+
+Analyses can be performed on both a single document, and on a larger corpus.
+It is also possible to build one's own corpora based on bibliographic metadata.
+<!-- end dhlab-intro -->
+
+The Jupyter Notebooks in the [digital_tekstanalyse](https://github.com/NationalLibraryOfNorway/digital_tekstanalyse) repo show examples on
+how to use the library, and can be used
+[directly in your browser](https://mybinder.org/v2/gh/DH-LAB-NB/DHLAB/master)
+without prior programming experience.
+
+
+
+## Installation
+
+<!-- start installation -->
+
+Install `dhlab` in your terminal with pip: 
+
+```
+pip install dhlab
+```
+
+<!-- end installation -->
+
+
+## Example use
+<!-- start example-use -->
+
+You could start by building your own [corpus](https://en.wikipedia.org/wiki/Text_corpus), e.g. of 
+books published between 1814 and 1905: 
+
+```python
+from dhlab.text import Corpus
+
+book_corpus = Corpus(doctype="digibok", from_year=1814, to_year=1905)
+```
+
+<!-- end example-use -->
+
+## Contact
+If you have any questions, or run into any problems with the code, please log them in our [issue 
+tracker](https://github.com/NationalLibraryOfNorway/DHLAB/issues) in the DHLAB repo. 
+
---
+++ packages/dhlab/dhlab-2.17.0.tar.gz/dhlab-2.17.0/dhlab/text/corpus.py
@@ -0,0 +1,174 @@
+from pandas import DataFrame
+import pandas as pd
+from dhlab.text.dhlab_object import DhlabObj
+from dhlab.api.dhlab_api import document_corpus, get_metadata, evaluate_documents
+import dhlab as dh
+from dhlab.text.utils import urnlist
+# from dhlab.text.conc_coll import Concordance, Collocations, Counts
+class Corpus(DhlabObj):
+    """Class representing as DHLAB Corpus"""
+    def __init__(
+            self,
+            doctype=None,
+            author=None,
+            freetext=None,
+            fulltext=None,
+            from_year=None,
+            to_year=None,
+            from_timestamp=None,
+            to_timestamp=None,
+            title=None,
+            ddk=None,
+            subject=None,
+            lang=None,
+            limit=10,
+            ):
+
+        if (doctype
+            or author
+            or freetext
+            or fulltext
+            or from_year
+            or to_year
+            or from_timestamp
+            or to_timestamp
+            or title
+            or ddk
+            or lang):
+
+            self.corpus = document_corpus(
+                doctype,
+                author,
+                freetext,
+                fulltext,
+                from_year,
+                to_year,
+                from_timestamp,
+                to_timestamp,
+                title,
+                ddk,
+                subject,
+                lang,
+                limit
+            )
+
+        else:
+            self.corpus = pd.DataFrame(columns=["urn"])
+
+        super().__init__(self.corpus)
+        self.size = len(self.corpus)
+        
+    @classmethod
+    def from_identifiers(cls, identifiers):
+        """Construct Corpus from list of identifiers"""
+        corpus = Corpus()
+        corpus.extend_from_identifiers(identifiers=identifiers)
+        return corpus
+        
+
+    @classmethod
+    def from_df(cls, df, check_for_urn=False):
+        """Typecast Pandas DataFrame to Corpus class
+
+        DataFrame most contain URN column"""
+        corpus = Corpus()
+        if check_for_urn:
+            corpus.corpus = cls._urn_id_in_dataframe_cols(df)
+        else:
+            corpus.corpus = df
+        corpus.frame = corpus.corpus
+        corpus.size = len(corpus.corpus)
+        return corpus
+
+    @classmethod
+    def from_csv(cls, path):
+        """Import corpus from csv"""
+        df = pd.read_csv(path)
+        return cls.from_df(df)
+
+    @staticmethod
+    def _urn_id_in_dataframe_cols(dataframe):
+        """Checks if dataframe contains URN column"""
+        if "urn" in dataframe.columns:
+            if dataframe.urn.str.contains("^URN:NBN:no-nb_.+").all():
+                return dataframe
+        raise ValueError("No'urn'-column in dataframe.")
+
+    def extend_from_identifiers(self, identifiers=None):
+        new_corpus = get_metadata(urnlist(identifiers))
+        self.add(new_corpus)
+
+    def evaluate_words(self, wordbags = None):
+        df = evaluate_documents(wordbags = wordbags, urns = list(self.corpus.urn))
+        df.index = df.index.astype(int)
+        cols = df.columns
+        df = pd.concat([df, self.corpus[['dhlabid','urn']].set_index('dhlabid')], axis = 1)
+        df = df.set_index('urn')
+        return df[cols].fillna(0)
+
+    def add(self, new_corpus):
+        """Utility for appending Corpus or DataFrame to self"""
+        if self._is_Corpus(new_corpus):
+            new_corpus = new_corpus.frame
+        self.frame = pd.concat([self.frame, new_corpus]).drop_duplicates().reset_index(drop=True)
+        self.corpus = self.frame
+        self.size = len(self.frame)
+
+    def sample(self, n=5):
+        "Create random subkorpus with `n` entries"
+        n = min(n, self.size)
+        sample = self.corpus.sample(n).copy()
+        return self.from_df(sample)
+
+    def conc(self, words, window=20, limit=500):
+        "Get concodances of `words` in corpus"
+        return dh.Concordance(corpus=self.frame, query=words, window=window, limit=limit)
+
+    def coll(
+        self,
+        words=None,
+        before=10,
+        after=10,
+        reference=None,
+        samplesize=20000,
+        alpha=False,
+        ignore_caps=False):
+        "Get collocations of `words` in corpus"
+        return dh.Collocations(
+            corpus=self.frame,
+            words=words,
+            before=before,
+            after=after,
+            reference=reference,
+            samplesize=samplesize,
+            alpha=alpha,
+            ignore_caps=ignore_caps
+            )
+
+    def count(self, words=None):
+        return dh.Counts(self.frame, words)
+
+    @staticmethod
+    def _is_Corpus(corpus) -> bool:
+        """Check if `input` is Corpus or DataFrame"""
+        if type(corpus) not in [DataFrame, Corpus]:
+            raise TypeError("Input is not Corpus or DataFrame")
+        return isinstance(corpus, Corpus)
+
+class EmptyCorpus(Corpus):
+    """DEPRECATED: call Corpus without parameters to represent an empty corpus"""
+    def __init__(self):
+        print("DEPRECATED: use Corpus without parameters to create empty coprus")
+        self.corpus = pd.DataFrame()
+        self.size = 0
+        self.frame = self.corpus
+
+class Corpus_from_identifiers(Corpus):
+    """DEPRECATED: build Corpus from list of identifiers. Use Corpus.extend_from_identifiers() instead"""
+    def __init__(self, identifiers=None):
+        print("DEPRECATED: use Corpus.from_identifiers()")
+        self.corpus = get_metadata(urnlist(identifiers))
+        self.size = len(self.corpus)
+        self.frame = self.corpus
+
+
