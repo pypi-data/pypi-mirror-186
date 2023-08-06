2db206cf72ab71c1cac4e7dc58bb435ff3b468f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:33 +0000

    Add package protobuf-grpc-distutils/0.1.5/protobuf_grpc_distutils-0.1.5-py3-none-any.whl

---
+++ packages/protobuf-grpc-distutils/protobuf_grpc_distutils-0.1.5-py3-none-any.whl/protobuf_grpc_distutils/__init__.py
@@ -0,0 +1,8 @@
+# Copyright IDEX Biometrics
+# Licensed under the MIT License, see LICENSE
+# SPDX-License-Identifier: MIT
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "unknown"
