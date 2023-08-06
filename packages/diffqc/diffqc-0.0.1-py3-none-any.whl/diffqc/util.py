e31dee6edd62a95894e7049f6a9831b9334d3d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:36 +0000

    Add package diffqc/0.0.1/diffqc-0.0.1-py3-none-any.whl

---
+++ packages/diffqc/diffqc-0.0.1-py3-none-any.whl/diffqc/util.py
@@ -0,0 +1,63 @@
+from typing import Callable
+
+import jax
+import jax.numpy as jnp
+
+
+def CreatePossibleState(op, nqubits: int, dtype: jnp.dtype) -> jnp.ndarray:
+    """
+    Create all possible state
+
+    Parameters
+    ----------
+    op
+        `dense` or `sparse` module
+    nqubits : int
+        Number of qubits
+    dtype : jnp.dtype
+        dtype
+
+    Returns
+    -------
+    jnp.ndarray
+        [|00...0>, |00...1>, ..., |11...1>]
+    """
+    cs = jnp.expand_dims(op.zeros(nqubits, dtype), 0)
+
+    for i in range(nqubits):
+        ith = nqubits - 1 - i
+        @jax.vmap
+        def flip(c):
+            return op.PauliX(c, (ith,))
+        cs = jnp.concatenate((cs, flip(cs)), axis=0)
+
+    return cs
+
+def CreateMatrix(op, nqubits: int, dtype: jnp.dtype,
+                 f: Callable[[jnp.ndarray], jnp.ndarray]) -> jnp.ndarray:
+    """
+    Create Transition Matrix from function
+
+    Parameters
+    ----------
+    op
+        `dense` or `sparse`
+    nqubits : int
+        Number of qubits
+    dtype : jnp.dtype
+        dtype
+    f : Callable[[jnp.ndarray], jnp.ndarray]
+
+    Returns
+    -------
+    jnp.ndarray
+        Transition Matrix
+    """
+    cs = CreatePossibleState(op, nqubits, dtype)
+
+    @jax.vmap
+    def F(c):
+        return op.to_state(f(c))
+
+    return jnp.moveaxis(F(cs), (0,), (1,))
+
