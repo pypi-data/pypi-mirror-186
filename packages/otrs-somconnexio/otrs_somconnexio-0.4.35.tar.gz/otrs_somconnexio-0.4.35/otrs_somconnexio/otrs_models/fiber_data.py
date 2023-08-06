614cbd04224b50694778748de5044fbd9c9e3d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:53 +0000

    Add package otrs-somconnexio/0.4.35/otrs_somconnexio-0.4.35.tar.gz

---
+++ packages/otrs-somconnexio/otrs_somconnexio-0.4.35.tar.gz/otrs_somconnexio-0.4.35/otrs_somconnexio/otrs_models/fiber_data.py
@@ -0,0 +1,15 @@
+from otrs_somconnexio.otrs_models.internet_data import InternetData
+
+
+class FiberData(InternetData):
+    service_type = 'fiber'
+
+    def __init__(self, **args):
+        self.previous_contract_pon = args.get("previous_contract_pon", "")
+        self.previous_contract_fiber_speed = args.get("previous_contract_fiber_speed", "")
+        self.activation_notes = args.get("activation_notes", "")
+        self.all_grouped_SIMS_recieved = args.get("all_grouped_SIMS_recieved")
+        for key in ["previous_contract_pon", "previous_contract_fiber_speed",
+                    "activation_notes", "all_grouped_SIMS_recieved"]:
+            args.pop(key, None)
+        super().__init__(**args)
