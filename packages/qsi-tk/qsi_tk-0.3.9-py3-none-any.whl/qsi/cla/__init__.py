6eafc693d5d048a3d3736079cf9defd71719eff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:49 +0000

    Add package qsi-tk/0.3.9/qsi_tk-0.3.9-py3-none-any.whl

---
+++ packages/qsi-tk/qsi_tk-0.3.9-py3-none-any.whl/qsi/cla/__init__.py
@@ -0,0 +1,6 @@
+try:
+    from cla.metrics import grid_search_svm_hyperparams, plot_svm_boundary, \
+        plot_lr_boundary
+except Exception as e:
+    print(e)
+    print('Please try: pip install cla==1.0.2 or above')
