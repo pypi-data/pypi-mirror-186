c3385e2421563f7442691a0a8985ecd248b805b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:35 +0000

    Add package systemy/0.1.2/systemy-0.1.2.tar.gz

---
+++ packages/systemy/systemy-0.1.2.tar.gz/systemy-0.1.2/systemy/__init__.py
@@ -0,0 +1,20 @@
+from .system import (
+        BaseFactory, 
+        BaseConfig,
+        BaseSystem, 
+        systemclass, 
+        SystemList, 
+        SystemDict, 
+        FactoryDict, 
+        FactoryList
+    )
+from .loaders import (
+        SystemLoader,
+        SystemIo, 
+        register_factory,
+        get_system_class,
+        get_factory_class, 
+        iter_factory,
+        iter_system_class
+    )
+
