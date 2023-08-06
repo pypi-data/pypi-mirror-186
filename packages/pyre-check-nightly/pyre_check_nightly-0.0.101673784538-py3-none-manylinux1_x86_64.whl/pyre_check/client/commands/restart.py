c451d92d180a4077e9e4051e08212a7b364b673c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:12 +0000

    Add package pyre-check-nightly/0.0.101673784538/pyre_check_nightly-0.0.101673784538-py3-none-manylinux1_x86_64.whl

---
+++ packages/pyre-check-nightly/pyre_check_nightly-0.0.101673784538-py3-none-manylinux1_x86_64.whl/pyre_check/client/commands/restart.py
@@ -0,0 +1,28 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""
+TODO(T132414938) Add a module-level docstring
+"""
+
+
+import logging
+
+from .. import command_arguments, configuration as configuration_module, identifiers
+from . import commands, frontend_configuration, incremental, stop
+
+
+LOG: logging.Logger = logging.getLogger(__name__)
+
+
+def run(
+    configuration: configuration_module.Configuration,
+    incremental_arguments: command_arguments.IncrementalArguments,
+) -> commands.ExitCode:
+    restart_configuration = frontend_configuration.OpenSource(configuration)
+    stop.run_stop(restart_configuration, flavor=identifiers.PyreFlavor.CLASSIC)
+    return incremental.run_incremental(
+        restart_configuration, incremental_arguments
+    ).exit_code
