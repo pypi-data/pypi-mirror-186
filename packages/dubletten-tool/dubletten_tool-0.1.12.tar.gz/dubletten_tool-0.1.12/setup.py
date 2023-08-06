4279de5db76e82d9df104540742f98f69b39f0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:21 +0000

    Add package dubletten-tool/0.1.12/dubletten_tool-0.1.12.tar.gz

---
+++ packages/dubletten-tool/dubletten_tool-0.1.12.tar.gz/dubletten_tool-0.1.12/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dubletten_tool',
+ 'dubletten_tool.management',
+ 'dubletten_tool.management.commands',
+ 'dubletten_tool.migrations']
+
+package_data = \
+{'': ['*'],
+ 'dubletten_tool': ['static/dubletten_tool/css/*',
+                    'templates/*',
+                    'templates/del/*',
+                    'templates/merge/*',
+                    'templates/merge/detail_views/*']}
+
+install_requires = \
+['python-Levenshtein>=0.12.2,<0.13.0']
+
+setup_kwargs = {
+    'name': 'dubletten-tool',
+    'version': '0.1.12',
+    'description': 'APIS module for the VieCPro instance to perform manual deduplication of person instances',
+    'long_description': 'None',
+    'author': 'Gregor Pirgie',
+    'author_email': 'gregor.pirgie@oeaw.ac.at',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
