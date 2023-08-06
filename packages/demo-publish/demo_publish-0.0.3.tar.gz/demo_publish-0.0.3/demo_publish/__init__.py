60f5d87f8e5dc6080c6b9823c8dd9c984748686c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:43 +0000

    Add package demo-publish/0.0.3/demo_publish-0.0.3.tar.gz

---
+++ packages/demo-publish/demo_publish-0.0.3.tar.gz/demo_publish-0.0.3/demo_publish/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2023-present flexfalk <sofu0284@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from .hejverden import friend
---
+++ packages/demo-publish/demo_publish-0.0.3.tar.gz/demo_publish-0.0.3/demo_publish/hejverden.py
@@ -0,0 +1,8 @@
+class friend:
+        def __init__(self):
+            pass
+
+        def speak():
+            print("Hej Verden")
+
+        
