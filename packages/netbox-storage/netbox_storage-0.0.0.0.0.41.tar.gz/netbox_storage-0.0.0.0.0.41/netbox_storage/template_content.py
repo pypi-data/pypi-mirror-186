ac7b0c3646814cba749a560f2d33f8dcabc83c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:58 +0000

    Add package netbox-storage/0.0.0.0.0.41/netbox_storage-0.0.0.0.0.41.tar.gz

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.0.41.tar.gz/netbox_storage-0.0.0.0.0.41/netbox_storage/template_content.py
@@ -0,0 +1,42 @@
+from extras.plugins import PluginTemplateExtension
+
+from netbox_storage.models import Drive, WindowsVolume
+from netbox_storage.tables import RelatedDriveTable
+from dcim.models import Platform
+
+
+class RelatedDrives(PluginTemplateExtension):
+    model = "virtualization.virtualmachine"
+
+    def full_width_page(self):
+        obj = self.context.get("object")
+
+        drives = Drive.objects.filter(
+            virtual_machine=obj
+        )
+        drive_table = RelatedDriveTable(
+            data=drives
+        )
+        volumes = WindowsVolume.objects.filter(
+            virtual_machine=obj
+        )
+
+        platform = obj.platform
+        if platform.name.__contains__('Windows'):
+            return self.render(
+                "netbox_storage/volume/windowsvolume_box.html",
+                extra_context={
+                    "drives": drives,
+                    "volumes": volumes
+                },
+            )
+        else:
+            return self.render(
+                "netbox_storage/volume/linuxvolume_box.html",
+                extra_context={
+                    "related_drives": drive_table,
+                },
+            )
+
+
+template_extensions = [RelatedDrives]
