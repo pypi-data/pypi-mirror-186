feddf46bc7682b2f213c94cfb5ec703acc167298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:37 +0000

    Add package netbox-storage/0.0.0.0.0.18/netbox_storage-0.0.0.0.0.18.tar.gz

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.0.18.tar.gz/netbox_storage-0.0.0.0.0.18/netbox_storage/api/nested_serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from netbox.api.serializers import WritableNestedSerializer
+from netbox_storage.models import Filesystem
+
+
+#
+# Filesystem
+#
+
+class NestedFilesystemSerializer(WritableNestedSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name="plugins-api:netbox_storage-api:filesystem-detail"
+    )
+
+    class Meta:
+        model = Filesystem
+        fields = ["id", "url", "display", "filesystem"]
