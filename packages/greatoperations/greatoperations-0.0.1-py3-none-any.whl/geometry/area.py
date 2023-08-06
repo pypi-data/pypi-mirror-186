397dff76878e4c3763c8a3916803e2044c058f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:41 +0000

    Add package greatoperations/0.0.1/greatoperations-0.0.1-py3-none-any.whl

---
+++ packages/greatoperations/greatoperations-0.0.1-py3-none-any.whl/geometry/area.py
@@ -0,0 +1,11 @@
+def square(length = 1, width = 1)
+    """" This function calculate the geometry of a square"""
+    return (legth * width)
+
+def rectangle(length = 1, width = 1)
+    """" This function calculate the geometry of a square"""
+    return (legth * width)
+
+def circle(radius)
+    """" This function calculate the geometry of a square"""
+    return (3.14 * radius*radius)
