77253f740e9d7dd6bbaa1ee76b0a04e16af054c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:03 +0000

    Add package cobyqa/1.0.dev1/cobyqa-1.0.dev1-py3-none-any.whl

---
+++ packages/cobyqa/cobyqa-1.0.dev1-py3-none-any.whl/cobyqa/utils/__init__.py
@@ -0,0 +1,5 @@
+from ._show_versions import show_versions
+from .maths import huge, max_abs_arrays
+from .structs import OptimizeResult
+
+__all__ = ["OptimizeResult", "huge", "max_abs_arrays", "show_versions"]
