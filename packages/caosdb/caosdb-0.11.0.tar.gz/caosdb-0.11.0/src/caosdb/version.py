e57cdb453bd5c48b8fe29c8b593c156726561817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:27 +0000

    Add package caosdb/0.11.0/caosdb-0.11.0.tar.gz

---
+++ packages/caosdb/caosdb-0.11.0.tar.gz/caosdb-0.11.0/src/caosdb/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM caosdb SETUP.PY
+#
+short_version = '0.11.0'
+version = '0.11.0'
+full_version = '0.11.0'
+git_revision = '1cf4248f95a5d247735d01927a63d5d0ba4f74ff'
+release = True
+
+if not release:
+    version = full_version
