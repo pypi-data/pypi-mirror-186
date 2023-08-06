49eca1febce20b99e1f7c6ee5251f7cdb6eb062c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:35 +0000

    Add package coba/6.2.4/coba-6.2.4.tar.gz

---
+++ packages/coba/coba-6.2.4.tar.gz/coba-6.2.4/coba/pipes/primitives.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+from typing import Any, TypeVar, Generic, Mapping
+
+_T_out = TypeVar("_T_out", bound=Any, covariant    =True)
+_T_in  = TypeVar("_T_in" , bound=Any, contravariant=True)
+
+class Pipe:
+
+    @property
+    def params(self) -> Mapping[str,Any]:
+        """Parameters describing the pipe."""
+        return { }
+
+    def __str__(self) -> str:
+        return str(self.params)
+
+class Source(ABC, Pipe, Generic[_T_out]):
+    """A pipe that can be read."""
+
+    @abstractmethod
+    def read(self) -> _T_out:
+        """Read the item."""
+        ...
+
+class Filter(ABC, Pipe, Generic[_T_in, _T_out]):
+    """A pipe that can modify an item."""
+
+    @abstractmethod
+    def filter(self, item: _T_in) -> _T_out:
+        """Filter the item."""
+        ...
+
+class Sink(ABC, Pipe, Generic[_T_in]):
+    """A pipe that writes item."""
+
+    @abstractmethod
+    def write(self, item: _T_in) -> None:
+        """Write the item."""
+        ...
