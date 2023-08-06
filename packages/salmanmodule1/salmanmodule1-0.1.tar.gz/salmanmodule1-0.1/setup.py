e67314d176f82ede7326c2fc136124d4d60c8535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:39 +0000

    Add package salmanmodule1/0.1/salmanmodule1-0.1.tar.gz

---
+++ packages/salmanmodule1/salmanmodule1-0.1.tar.gz/salmanmodule1-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+
+
+setup(
+  name='salmanmodule1',
+  version='0.1',
+  description='It is my Module for Public Use', 
+  author='Salman Mumtaz',
+  author_email='msdsf22m007@pucit.edu.pk',
+  classifiers= [
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3' ],
+  packages=['salmanmodule1'],
+  install_reqires=[]
+)
