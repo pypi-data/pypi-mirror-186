cdf5daad75f4645a90d2414c6fe3253b1b2b7466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:48 +0000

    Add package zuul/8.1.0/zuul-8.1.0.tar.gz

---
+++ packages/zuul/zuul-8.1.0.tar.gz/zuul-8.1.0/zuul/web/static/static/js/34.d1466432.chunk.js
@@ -0,0 +1,1 @@
+(this["webpackJsonp@zuul-ci/dashboard"]=this["webpackJsonp@zuul-ci/dashboard"]||[]).push([[34],{2708:function(s,u,a){}}]);
---
+++ packages/zuul/zuul-8.1.0.tar.gz/zuul-8.1.0/zuul/web/static/static/js/7.38e2089a.chunk.js
@@ -0,0 +1,1 @@
+(this["webpackJsonp@zuul-ci/dashboard"]=this["webpackJsonp@zuul-ci/dashboard"]||[]).push([[7],{2496:function(s,u,a){}}]);
