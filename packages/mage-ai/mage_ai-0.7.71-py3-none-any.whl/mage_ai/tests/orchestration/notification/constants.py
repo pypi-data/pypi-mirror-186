aa6bdd2d3f254a9b963b2aaea2a16e1548670bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:55 +0000

    Add package mage-ai/0.7.71/mage_ai-0.7.71-py3-none-any.whl

---
+++ packages/mage-ai/mage_ai-0.7.71-py3-none-any.whl/mage_ai/tests/orchestration/notification/constants.py
@@ -0,0 +1,22 @@
+EMAIL_NOTIFICATION_CONFIG = dict(
+    email_config=dict(
+        smtp_host='test_host',
+        smtp_mail_from='test_from@abc.com',
+        smtp_user='test_user',
+        smtp_password='test_password',
+        to_emails=['test_to@xyz.com'],
+    )
+)
+
+SLACK_NOTIFICATION_CONFIG = dict(
+    slack_config=dict(
+        webhook_url='test_webhook_url',
+    )
+)
+
+
+TEAMS_NOTIFICATION_CONFIG = dict(
+    teams_config=dict(
+        webhook_url='test_webhook_url',
+    )
+)
