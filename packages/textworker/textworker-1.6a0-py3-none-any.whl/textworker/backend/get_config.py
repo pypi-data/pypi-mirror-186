347a325bbd160359ad7ee2f427bc6a3308525bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:36 +0000

    Add package textworker/1.6a0/textworker-1.6a0-py3-none-any.whl

---
+++ packages/textworker/textworker-1.6a0-py3-none-any.whl/textworker/backend/get_config.py
@@ -0,0 +1,265 @@
+import configparser
+import darkdetect
+import os
+import os.path
+import platform
+import textworker.backend
+import wx
+
+from PIL import ImageColor
+from . import constants
+
+textworker.backend.require_version("1.6a", ">=")
+
+# Configuration file
+if platform.system() == "Windows":
+    dir_to_use = os.environ["USERPROFILE"] + "\\.config\\texteditor\\"
+    defconsole = "cmd"
+else:
+    dir_to_use = os.environ["HOME"] + "/.config/texteditor/"
+    defconsole = "xterm"
+
+if textworker.backend.is_development_build():
+    file = dir_to_use + "configs_dev.ini"
+else:
+    file = dir_to_use + "configs.ini"
+
+# Default configs
+cfg = {}
+
+cfg["interface"] = {"color": "light", "autocolor": "no", "textcolor": "default"}
+
+cfg["interface.font"] = {
+    "style": "normal",
+    "weight": "normal",
+    "family": "default",
+    "size": "normal",
+}
+
+cfg["extensions.cmd"] = {"enable": "yes", "console": defconsole}
+
+cfg["extensions.autosave"] = {"enable": "no", "time": "120"}
+
+
+class ConfigurationError(Exception):
+    def __init__(
+        self, section: str = "", option: str = "", msg: str = "", *args: object
+    ):
+        prefix = "Error in the configuration file: "
+        if not msg:
+            msg = "*UNKNOW ERROR*"
+        elif section and option != None:
+            msg = "[{}->{}] : {}".format(section, option, msg)
+        full = prefix + msg
+        super().__init__(full, *args)
+
+
+class GetConfig(configparser.ConfigParser):
+    # Values
+    yes_value = "yes"
+    no_value = "no"
+    returnbool = True
+    aliases = {}
+
+    # Functions
+    setfontfn = {}
+    setcolorfn = {}
+
+    def __init__(self, config: dict, file: str, *args):
+        """Customized configuration parser.
+        :param config : Default configurations, used to reset the file or do some comparisions
+        :param file : Configuration file
+        :param *args : To pass to configparser.ConfigParser (base class)
+
+        When initialized, GetConfig loads all default configs (from config param) and store it in
+        a dictionary for further actions (backup/restore file).
+
+        How to use:
+        * Just call it once for your project, pass everything needed to the contrucstor and do things
+        your self!
+        * Always set default_section to one of your section (in str format)
+        * Configure function is used to config wxPython widgets - so override it if needed
+
+        For the configure function (must do):
+        * Use setcolorfunc function to set the color (background)-set function
+        * Use setfontcfunc function to set the font color(foreground)-set function
+
+        Both of them will be used in configure function.
+        """
+        super().__init__(*args)
+        
+        self.cfg = {}
+        
+        for key in config:
+            self[key] = config[key]
+            self.cfg[key] = config[key]
+
+        self.readf(file)
+        self.file = file
+
+    # File tasks
+    def readf(self, file, encoding: str | None = None):
+        if os.path.isfile(file):
+            self.read(file, encoding)
+        else:
+            with open(file, mode="w") as f:
+                try:
+                    self.write(f)
+                except OSError:
+                    raise Exception("Unable to access to the file name %s" % file)
+                else:
+                    self.read(file, encoding)
+            del f
+
+            # raise Exception("Unable to read configuration file")
+
+    def reset(self, evt=None) -> bool:
+        try:
+            os.remove(self.file)
+        except:
+            return False
+        else:
+            for key in self.cfg:
+                self[key] = self.cfg[key]
+            with open(self.file, mode="w") as f:
+                self.write(f)
+            return True
+
+    # Options
+    def getkey(self, section, option):
+        value = self.get(section, option)
+        if value == self.yes_value:
+            if self.returnbool == True:
+                return True
+        elif value == self.no_value:
+            if self.returnbool == True:
+                return False
+        elif value in self.aliases:
+            return self.aliases[value]
+        else:
+            return value
+
+    def aliasyesno(self, yesvalue, novalue, enable: bool = True):
+        """Use a custom yes/no value, for example:
+        There is an option under [section], and GetConfig will return\
+        True if the options is 'yes', False if the options is 'no'.
+        You can change 'yes' and 'no' value for your use.
+        If you dont want the parser return a boolean, set enable to false."""
+        self.yes_value = yesvalue
+        self.no_value = novalue
+        self.returnbool = enable
+        return
+
+    def alias(self, value, value2):
+        self.aliases[value] = value2
+
+    # Configure widgets
+    def _get_font(self):
+        family = self.get("interface.font", "family")
+        size = self.get("interface.font", "size")
+        weight = self.get("interface.font", "weight")
+        style = self.get("interface.font", "style")
+
+        weight_ = constants.FONTWT[weight]
+        style_ = constants.FONTST[style]
+
+        if family == "default":
+            family = ""
+
+        try:
+            int(size)
+        except ValueError:
+            size_ = constants.FONTSZ[size]
+        else:
+            size_ = int(size)
+
+        return wx.Font(size_, wx.FONTFAMILY_DEFAULT, style_, weight_, 0, family)
+
+    def _get_color(self):
+
+        def _get_sys_mode():
+            return darkdetect.theme().lower()
+
+        # Get values
+        color = self.get("interface", "color")
+        fontcolor = self.get("interface", "textcolor")
+        autocolor = self.getkey("interface", "autocolor")
+
+        # Interface color
+        ## Default color modes
+        colors = {
+            "light": "#ffffff",
+            "dark": "#1c1c1c",
+        }
+
+        ## Check
+        if autocolor == True:
+            color_ = colors[_get_sys_mode()]
+        elif color in colors:
+            color_ = colors[color]
+
+        # Text color
+        colors["green"] = "#00ff00"
+        colors["red"] = "#ff0000"
+        if fontcolor == "default":
+            if autocolor == False:
+                if color == "light":
+                    fontcolor_ = colors["dark"]
+                elif color == "dark":
+                    fontcolor_ = colors["light"]
+            else:
+                if _get_sys_mode() == "dark":
+                    fontcolor_ = colors["light"]
+                else:
+                    fontcolor_ = colors["dark"]
+        else:
+            if fontcolor in colors:
+                fontcolor_ = colors[fontcolor]
+            elif fontcolor.startswith("#") and len(fontcolor) == 7:
+                try:
+                    ImageColor.getrgb(fontcolor)
+                except ValueError:
+                    raise ConfigurationError(
+                        "interface", "textcolor", "Invalid color name/code"
+                    )
+                else:
+                    fontcolor_ = fontcolor
+            else:
+                raise ConfigurationError(
+                    "interface", "textcolor", "Invalid color name/code"
+                )
+
+        return ImageColor.getrgb(color_), ImageColor.getrgb(fontcolor_)
+
+    def setcolorfunc(self, objname: str, obj, func: str, params):
+        self.setcolorfn[objname] = {}
+        self.setcolorfn[objname]["obj"] = obj
+        self.setcolorfn[objname]["fn"] = func
+        self.setcolorfn[objname]["params"] = params
+
+    def setfontcfunc(self, objname: str, obj, func: str, params):
+        self.setfontfn[objname] = {}
+        self.setfontfn[objname]["obj"] = obj
+        self.setfontfn[objname]["fn"] = func
+        self.setfontfn[objname]["params"] = params
+
+    def configure(self, widget):
+        """Configures a wxPython widget."""
+        widget.SetFont(self._get_font())
+        color, fontcolor = self._get_color()
+
+        for item in self.setfontfn:
+            if self.setfontfn[item]["obj"] == widget:
+                fn = getattr(self.setfontfn[item]["obj"], self.setfontfn[item]["fn"])
+                if not self.setfontfn[item]["params"]:
+                    fn(fontcolor)
+                else:
+                    fn(self.setfontfn[item]["params"], fontcolor)
+
+        for item in self.setcolorfn:
+            if self.setcolorfn[item]["obj"] == widget:
+                fn = getattr(self.setcolorfn[item]["obj"], self.setcolorfn[item]["fn"])
+                if not self.setcolorfn[item]["params"]:
+                    fn(color)
+                else:
+                    fn(self.setcolorfn[item]["params"], color)
---
+++ packages/textworker/textworker-1.6a0-py3-none-any.whl/textworker/extensions/autosave.py
@@ -0,0 +1,157 @@
+import textworker
+import tkinter.messagebox as msb
+from tkinter import BooleanVar, Label, StringVar, Toplevel
+from tkinter.ttk import Button, Checkbutton, Combobox
+
+from ..backend import get_config, logger
+
+# Minutes to seconds
+MIN_05 = 30  # 30 secs
+MIN_1 = MIN_05 * 2  # 60 secs
+MIN_15 = MIN_1 * 15  # 900 secs
+MIN_20 = MIN_15 + MIN_1 * 5  # 1200 secs
+MIN_30 = MIN_15 * 2  # 1800 secs
+
+log = logger.Logger("texteditor.extensions.autosave")
+
+
+class AutoSave:
+    """Contructs the autosaving files function on texteditor.\n
+    Configurations:
+    * forceEnable : bool : Force enable this
+    * useTime : float : Force auto save after a time
+
+    Functions:
+    * openpopup : Make a popup window which asks the user to change the auto save time.
+    It will modify useTime!
+    * start : Start the timing loop
+    """
+
+    forceEnable: bool = False
+    useTime: float = float(get_config.GetConfig.getvalue("filemgr", "autosave-time"))
+
+    def __init__(self, master, savefile_fn, _=None):
+        super().__init__()
+        if _ is None:
+            self._ = textworker._
+        else:
+            self._ = _
+        self.autosave = get_config.GetConfig.getvalue("filemgr", "autosave")
+        self._do_check()
+        self.parent = master
+        self.savecommand = savefile_fn
+
+    def _do_check(self):
+        if self.forceEnable == False:
+            if self.autosave == "yes":
+                self.__converter(self.useTime)
+                pass
+            else:
+                return "break"
+
+    def openpopup(self):
+        askwin = Toplevel(self.parent)
+        self.askwin = askwin
+        # askwin.geometry("400x230")
+        askwin.title(self._("Autosave configuration"))
+        askwin.resizable(False, False)
+
+        selected_time = StringVar()
+        updatest = BooleanVar()
+
+        label = Label(
+            askwin,
+            text=self._(
+                "Select autosave time (minutes)\nAutosave function will be launched after a time."
+            ),
+        )
+        label2 = Label(
+            askwin,
+            text=self._(
+                "Please note that Autosave only saves the current selected tab."
+            ),
+        )
+        cb = Combobox(askwin, textvariable=selected_time)
+        cb["values"] = [0.5, 1, 2, 5, 10, 15, 20, 30]
+        cb["state"] = "readonly"
+        if self.useTime in cb["values"]:
+            cb.current(self.__converter(self.useTime))
+
+        checkbtn = Checkbutton(
+            askwin,
+            text=self._("Save this value"),
+            variable=updatest,
+            onvalue=True,
+            offvalue=False,
+        )
+        okbtn = Button(
+            askwin,
+            text="OK",
+            command=lambda: self.__okbtn_clicked(selected_time, updatest),
+        )
+        cancelbtn = Button(
+            askwin, text=self._("Cancel"), command=lambda: askwin.destroy()
+        )
+
+        label.pack(fill="x")
+        cb.pack(fill="x", padx=15, pady=15)
+        checkbtn.pack(fill="x")
+        okbtn.pack(padx=30)
+        cancelbtn.pack(padx=40)
+        label2.pack(fill="x")
+
+        get_config.GetConfig(askwin, "config")
+        get_config.GetConfig(label, "config")
+        get_config.GetConfig(label2, "config")
+        get_config.GetConfig(cb, "config")
+
+    def __converter(self, time: float):
+        switch = {
+            0.5: MIN_05,
+            1: MIN_1,
+            2: MIN_1 * 2,
+            5: MIN_1 * 5,
+            10: MIN_1 * 10,
+            15: MIN_15,
+            20: MIN_20,
+            30: MIN_30,
+        }
+        return switch.get(time)
+
+    def __okbtn_clicked(self, selected_time, up_st, event=None):
+        tm = selected_time.get()
+        st = up_st.get()
+        timetouse = self.__converter(float(tm))
+
+        # This depends on the __converter function.
+        # No problem if you don't rewrite it.
+        if not isinstance(timetouse, int):
+            log.throwerr(self._("Wrong autosave time specified."), noexp=True)
+            return
+        else:
+            self.config(timetouse)
+
+        if st is True:
+            get_config.GetConfig.change_config(
+                "filemgr", "autosave-time", str(self.useTime)
+            )
+        self.askwin.destroy()
+        self.start()
+
+    def config(self, useTime, event=None):
+        print("Trying to use autosave time: ", useTime)
+        self.useTime = float(useTime)
+        self._do_check()
+
+    def start(self):
+        result = msb.askquestion(message="Do you want to start autosave loop now?")
+        if result != "yes":
+            return
+        else:
+            try:
+                self.parent.after(
+                    int(self.useTime * 1000),  # Tk().after() uses miliseconds
+                    lambda: self.savecommand(),
+                )
+            except:
+                return
