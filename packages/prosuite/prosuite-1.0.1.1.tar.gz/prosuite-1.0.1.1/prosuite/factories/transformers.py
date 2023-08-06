26aa2342cbde1f9abc65a28c3ab24320fe67e28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:54 +0000

    Add package prosuite/1.0.1.1/prosuite-1.0.1.1.tar.gz

---
+++ packages/prosuite/prosuite-1.0.1.1.tar.gz/prosuite-1.0.1.1/prosuite/factories/transformers.py
@@ -0,0 +1,17 @@
+﻿__author__ = "The ProSuite Authors"
+__copyright__ = "Copyright 2021-2023, The ProSuite Authors"
+__license__ = "MIT"
+__version__ = "1.0.1.0"
+__maintainer__ = "Dira GeoSystems"
+__email__ = "programmers@dirageosystems.ch"
+__date__  = "16.01.2023"
+__status__ = "Production"
+
+
+from datetime import datetime
+from typing import List
+from prosuite.transformer import Transformer
+from prosuite.parameter import Parameter
+from prosuite.dataset import BaseDataset
+from prosuite.factories.enums import *
+
