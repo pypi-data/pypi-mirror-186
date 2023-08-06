a4ef0a59d2356863ad31f9c2310800eb1011d527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:56 +0000

    Add package odin/2.1/odin-2.1.tar.gz

---
+++ packages/odin/odin-2.1.tar.gz/odin-2.1/src/odin/bases.py
@@ -0,0 +1,23 @@
+import abc
+from typing import Protocol
+
+
+class ResourceIterable(abc.ABC):
+    """
+    Iterable object that yields resources.
+    """
+
+    @abc.abstractmethod
+    def __iter__(self):
+        """
+        Iterate resources
+        """
+
+
+class TypedResourceIterable(ResourceIterable, abc.ABC):
+    """
+    Iterable object that yields a specific resource.
+    """
+
+    def __init__(self, resource_type):
+        self.resource_type = resource_type
