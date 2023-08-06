53c0038eb2f506884fbbc7ce6f17de89dc29fa37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:30 +0000

    Add package anova-wifi/0.1.0/anova_wifi-0.1.0.tar.gz

---
+++ packages/anova-wifi/anova_wifi-0.1.0.tar.gz/anova_wifi-0.1.0/src/anova_wifi/__init__.py
@@ -0,0 +1,13 @@
+from .parser import (
+    AnovaPrecisionCooker,
+    AnovaPrecisionCookerBinarySensor,
+    AnovaPrecisionCookerSensor,
+)
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "AnovaPrecisionCooker",
+    "AnovaPrecisionCookerBinarySensor",
+    "AnovaPrecisionCookerSensor",
+]
