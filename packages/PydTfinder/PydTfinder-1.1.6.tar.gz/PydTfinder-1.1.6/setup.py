cf31f459f51e7ee0adb5546c05a45628e166e3b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:33 +0000

    Add package PydTfinder/1.1.6/PydTfinder-1.1.6.tar.gz

---
+++ packages/PydTfinder/PydTfinder-1.1.6.tar.gz/PydTfinder-1.1.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+	  name='PydTfinder', 
+	  version='1.1.6',
+	  description='::A tool to find the phase eq temperature at a given pressure with a given ΔT value::',
+	  long_description= 'Added useful features for the user.', 
+	  author='wjgoarxiv',
+	  author_email='woo_go@yahoo.com',
+	  url='https://pypi.org/project/PydTfinder/',
+	  license='MIT',
+	  py_modules=['PydTfinder'],
+		readme = "README.md",
+		python_requires = ">=3.7",
+	  install_requires = [
+	  'matplotlib',
+	  'numpy',
+	  'scikit-learn',
+	  'scipy',
+	  'pandas',
+	  'seaborn',
+	  'argparse',
+    'pyfiglet',
+    'tabulate',
+	  ],
+	  packages=['PydTfinder'],
+		entry_points={
+			'console_scripts': [
+				'pydtfinder = PydTfinder.__main__:main'
+			]
+		}
+	)
