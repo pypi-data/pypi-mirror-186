4254579ee2a6a63449a8e6022a09918478022124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:23 +0000

    Add package swibots/1.0.5/swibots-1.0.5-py3-none-any.whl

---
+++ packages/swibots/swibots-1.0.5-py3-none-any.whl/swibots/bots/register_command.py
@@ -0,0 +1,10 @@
+class RegisterCommand:
+    def __init__(
+        self,
+        command: str,
+        description: str,
+        channel: bool = False,
+    ):
+        self.command = command
+        self.description = description
+        self.channel = channel
