98289430dbc5b2a4fb070392c01e622de4ffb7ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:14 +0000

    Add package execdata/2.0.0/execdata-2.0.0.tar.gz

---
+++ packages/execdata/execdata-2.0.0.tar.gz/execdata-2.0.0/execdata/data_conv.py
@@ -0,0 +1,19 @@
+'''
+Date         : 2023-01-17 10:33:06
+Author       : BDFD,bdfd2005@gmail.com
+Github       : https://github.com/bdfd
+LastEditTime : 2023-01-17 10:37:14
+LastEditors  : BDFD
+Description  : 
+FilePath     : \execdata\data_conv.py
+Copyright (c) 2023 by BDFD, All Rights Reserved. 
+'''
+def convint(str):
+    result = ''
+    result = int(str)
+    return result
+
+def convfloat(str):
+    result = ''
+    result = float(str)
+    return result
