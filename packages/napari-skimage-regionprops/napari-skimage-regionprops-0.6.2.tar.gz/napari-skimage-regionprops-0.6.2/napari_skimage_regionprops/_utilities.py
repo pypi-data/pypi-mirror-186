37a679b3d9e1497b14a48b892abb0720b9d4caa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:32 +0000

    Add package napari-skimage-regionprops/0.6.2/napari-skimage-regionprops-0.6.2.tar.gz

---
+++ packages/napari-skimage-regionprops/napari-skimage-regionprops-0.6.2.tar.gz/napari-skimage-regionprops-0.6.2/napari_skimage_regionprops/_utilities.py
@@ -0,0 +1,31 @@
+import numpy as np
+from napari_plugin_engine import napari_hook_implementation
+from napari.types import ImageData
+from napari import Viewer
+from napari_tools_menu import register_function
+from typing_extensions import Annotated
+from napari.layers import Image, Labels, Layer
+LayerInput = Annotated[Layer, {"label": "Image"}]
+
+@napari_hook_implementation
+def napari_experimental_provide_function():
+    return [duplicate_current_frame]
+
+@register_function(menu="Utilities > Duplicate current timepoint (nsr)")
+def duplicate_current_frame(layer : LayerInput, napari_viewer : Viewer, axis : int = 0) -> Layer:
+    image = layer.data
+    current_dim_value = napari_viewer.dims.current_step[axis]
+    new_image = np.take(image, current_dim_value, axis)
+    new_name = layer.name + "[t=" + str(current_dim_value) + "]"
+
+    if isinstance(layer, Labels):
+        result = Labels(new_image, name=new_name)
+    else:
+        result = Image(new_image, name=new_name)
+
+    return result
+
+def isimage(value):
+    return isinstance(value, np.ndarray) or str(type(value)) in ["<class 'cupy._core.core.ndarray'>",
+                                                          "<class 'dask.array.core.Array'>",
+                                                          "<class 'pyclesperanto_prototype._tier0._pycl.OCLArray'>"]
