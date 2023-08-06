c153254629f1bdc46afc4ab3e4bf36beaec4dcdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:49 +0000

    Add package frictionless/5.4.0/frictionless-5.4.0.tar.gz

---
+++ packages/frictionless/frictionless-5.4.0.tar.gz/frictionless-5.4.0/frictionless/program/__init__.py
@@ -0,0 +1,10 @@
+from .program import program
+
+# Register modules
+from . import api
+from . import convert
+from . import describe
+from . import extract
+from . import summary
+from . import transform
+from . import validate
---
+++ packages/frictionless/frictionless-5.4.0.tar.gz/frictionless-5.4.0/frictionless/server/__init__.py
@@ -0,0 +1,6 @@
+from .config import Config
+from .server import Server
+
+# Register modules
+from . import projects
+from . import resource
