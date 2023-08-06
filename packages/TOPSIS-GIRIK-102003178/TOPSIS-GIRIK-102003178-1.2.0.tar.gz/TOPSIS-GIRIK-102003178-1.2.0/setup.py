b35f7ccdc5bea31abeec0eee16ce6a375f023cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:52 +0000

    Add package TOPSIS-GIRIK-102003178/1.2.0/TOPSIS-GIRIK-102003178-1.2.0.tar.gz

---
+++ packages/TOPSIS-GIRIK-102003178/TOPSIS-GIRIK-102003178-1.2.0.tar.gz/TOPSIS-GIRIK-102003178-1.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "TOPSIS-GIRIK-102003178",
+    version = "1.2.0",
+    license = "MIT",
+    description = "A Python package to find TOPSIS for multi-criteria decision analysis method",
+    long_description = open('readme.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type = "text/markdown",
+    author = "Girik Garg",
+    author_email = "girikgarg8@gmail.com",
+    url = "https://www.github.com/girikgarg8",
+    keywords = ['topsis', 'UCS654', 'TIET'],
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = ['pandas', 'tabulate'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3' 
+    ]
+)
