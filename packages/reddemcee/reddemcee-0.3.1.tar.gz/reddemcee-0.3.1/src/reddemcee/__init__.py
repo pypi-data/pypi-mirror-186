7116fa7ec09b826f47718e2dad97c1b912bea50e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:51 +0000

    Add package reddemcee/0.3.1/reddemcee-0.3.1.tar.gz

---
+++ packages/reddemcee/reddemcee-0.3.1.tar.gz/reddemcee-0.3.1/src/reddemcee/__init__.py
@@ -0,0 +1,10 @@
+# @auto-fold regex /^\s*if/ /^\s*else/ /^\s*def/
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# version 0.3.1
+# date 17 jan 2023
+
+__version__ = '0.3.1'
+__name__ = 'reddemcee'
+
+__all__ = ['reddemcee', 'reddwrappers']
