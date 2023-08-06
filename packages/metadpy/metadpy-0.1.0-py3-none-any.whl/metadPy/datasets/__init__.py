1b27dbce01ab3cd07fee4451ebc9d97ff3169318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:58 +0000

    Add package metadpy/0.1.0/metadpy-0.1.0-py3-none-any.whl

---
+++ packages/metadpy/metadpy-0.1.0-py3-none-any.whl/metadPy/datasets/__init__.py
@@ -0,0 +1,22 @@
+# Author: Nicolas Legrand <nicolas.legrand@cfin.au.dk>
+
+import os.path as op
+
+import pandas as pd
+
+
+def load_dataset(dataset):
+    """Load simulated dataset
+
+    Parameters
+    ----------
+    dataset : str
+        Which dataset. Defalut is `'rm'`
+
+    Returns
+    -------
+    df : :py:class:`pandas.DataFrame`
+        Pandas dataframe.
+    """
+    path = "https://github.com/LegrandNico/metadpy/raw/" "master/metadpy/datasets/"
+    return pd.read_csv(op.join(path, "rm.txt"))
