78e2d4f0671e35183f584dbeb21e896aed482210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:49 +0000

    Add package bleepy-profanity-check/0.1.9/bleepy-profanity-check-0.1.9.tar.gz

---
+++ packages/bleepy-profanity-check/bleepy-profanity-check-0.1.9.tar.gz/bleepy-profanity-check-0.1.9/setup.py
@@ -0,0 +1,46 @@
+"""
+Setup
+"""
+from pathlib import Path
+
+import setuptools
+
+long_description = Path("README.md").read_text(encoding="utf8")
+
+authors = [
+  "Victor Zhou (original author)",
+  "Menelaos Kotoglou",
+  "Dimitrios Mistriotis",
+  "Reyniel Mark T. Escamillas"
+]
+
+setuptools.setup(
+    name="alt-profanity-check",
+    version="0.1.9",
+    author= ", ".join(authors),
+    author_email="rtescamillas@ccc.edu.ph",
+    description=(
+        "A fast, robust library to check for offensive language in strings. "
+        'This version of "profanity-check" is used by Bleepy'
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/reyniel26/bleepy-profanity-check",
+    packages=setuptools.find_packages(),
+    install_requires=["scikit-learn==1.1.3", "joblib>=1.2.0"],
+    python_requires=">=3.7",
+    package_data={"profanity_check": [
+      "data/model.joblib", "data/vectorizer.joblib",
+      "data/tagalog_vectorizer.joblib","data/tagalog_model.joblib"
+      ]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # entry_points={
+    #     "console_scripts": ["profanity_check=profanity_check.command_line:main"],
+    # },
+)
---
+++ packages/bleepy-profanity-check/bleepy-profanity-check-0.1.9.tar.gz/bleepy-profanity-check-0.1.9/tests/test_profanity_check.py
@@ -0,0 +1,42 @@
+"""Test Cases"""
+from profanity_check import predict, predict_prob
+
+
+def test_accuracy():
+    """Test Accuracy"""
+    texts = [
+      'Hello there, how are you',
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      '!!!! Click this now!!! -> https://example.com',
+      'fuck you',
+      'fUcK u',
+      'GO TO hElL, you dirty scum',
+    ]
+
+    assert list(predict(texts)) == [0, 1, 0, 1, 1, 1]
+
+    probs = list(predict_prob(texts))
+    for i, prob in enumerate(probs):
+        if i in (0,2):
+            assert prob <= 0.5
+        else:
+            assert prob >= 0.5
+
+
+def test_edge_cases():
+    """Test Edge cases"""
+    texts = [
+      '',
+      '                    ',
+      'this is but a test string, there is no offensive language to be found here! :) ' * 25,
+      'aaaaaaa' * 100,
+    ]
+    assert list(predict(texts)) == [0, 0, 0, 0]
+
+def test_tagalog_words():
+    """Test Tagalog Words"""
+
+    words = [
+      "tanga","mahal","bobo","ikaw","talaga"
+    ]
+    assert list(predict(words,lang="tagalog")==[1,0,1,0,0])
