43d1e5f9fbb7981c8f088c08796eab292aea40e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:47 +0000

    Add package visiongraph/0.1.33/visiongraph-0.1.33-py3-none-any.whl

---
+++ packages/visiongraph/visiongraph-0.1.33-py3-none-any.whl/visiongraph/util/VectorUtils.py
@@ -0,0 +1,48 @@
+from typing import List, Tuple, Sequence
+
+import numpy as np
+import vector
+from numpy.lib.recfunctions import structured_to_unstructured, unstructured_to_structured
+
+
+def list_of_vector4D(data: List[Tuple[float, float, float, float]]) -> vector.VectorNumpy4D:
+    return vector.array(
+        data, dtype=[("x", float), ("y", float), ("z", float), ("t", float)]
+    ).view(vector.VectorNumpy4D)
+
+
+def vector_to_array(vectors: vector.VectorNumpy) -> np.ndarray:
+    return structured_to_unstructured(np.asarray(vectors))
+
+
+def array_to_vector(data: np.ndarray) -> vector.VectorNumpy:
+    h, w = data.shape[:2]
+
+    data = unstructured_to_structured(data)
+
+    if w == 2:
+        return vector.array(data, dtype=[("x", float), ("y", float)]).view(vector.VectorNumpy2D)
+    elif w == 3:
+        return vector.array(data, dtype=[("x", float), ("y", float), ("z", float)]).view(vector.VectorNumpy3D)
+    elif w == 4:
+        return vector.array(data, dtype=[("x", float), ("y", float),
+                                         ("z", float), ("t", float)]).view(vector.VectorNumpy4D)
+    else:
+        raise Exception(f"Shape ({h}, {w}) is not a valid vector numpy shape.")
+
+
+def lerp4d(a: vector.VectorNumpy4D, b: vector.VectorNumpy4D, amt: float) -> vector.VectorNumpy4D:
+    return vector.obj(
+        x=(a.x * (1.0 - amt)) + (b.x * amt),
+        y=(a.y * (1.0 - amt)) + (b.y * amt),
+        z=(a.z * (1.0 - amt)) + (b.z * amt),
+        t=(a.t * (1.0 - amt)) + (b.t * amt),
+    )
+
+
+def landmarks_center_by_indices(landmarks: vector.VectorNumpy4D, indices: Sequence[int]) -> vector.Vector4D:
+    x = np.average(landmarks.x[indices])
+    y = np.average(landmarks.y[indices])
+    z = np.average(landmarks.z[indices])
+    t = np.average(landmarks.t[indices])
+    return vector.obj(x=x, y=y, z=z, t=t)
