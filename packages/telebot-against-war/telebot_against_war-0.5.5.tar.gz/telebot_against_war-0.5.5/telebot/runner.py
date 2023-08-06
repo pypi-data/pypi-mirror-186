bc5ec2779e13e5e76b7b63615db00d34131d0f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:43 +0000

    Add package telebot-against-war/0.5.5/telebot_against_war-0.5.5.tar.gz

---
+++ packages/telebot-against-war/telebot_against_war-0.5.5.tar.gz/telebot_against_war-0.5.5/telebot/runner.py
@@ -0,0 +1,48 @@
+import asyncio
+import urllib.parse
+from dataclasses import dataclass, field
+from hashlib import sha256
+from typing import Coroutine
+
+from aiohttp import hdrs
+from aiohttp.typedefs import Handler as AiohttpHandler
+
+from telebot import AsyncTeleBot
+
+
+@dataclass
+class AuxBotEndpoint:
+    method: str
+    route: str  # with leading slash
+    handler: AiohttpHandler
+
+    def __post_init__(self):
+        if self.method not in hdrs.METH_ALL:
+            raise ValueError(f"Unknown method {self.method!r}")
+
+
+@dataclass
+class BotRunner:
+    bot_prefix: str
+    bot: AsyncTeleBot
+    background_jobs: list[Coroutine[None, None, None]] = field(default_factory=list)
+    aux_endpoints: list[AuxBotEndpoint] = field(default_factory=list)
+
+    def __post_init__(self):
+        if self.bot.log_marker is None:
+            self.bot.log_marker = self.bot_prefix
+
+    async def run_polling(self):
+        """For local run / testing only"""
+        await asyncio.gather(
+            self.bot.infinity_polling(interval=1),
+            *self.background_jobs,
+        )
+
+    @property
+    def bot_prefix_urlsafe(self) -> str:
+        return urllib.parse.quote("-".join(self.bot_prefix.split()))
+
+    def webhook_subroute(self) -> str:
+        token_hash = sha256(self.bot.token.encode("utf-8")).hexdigest()
+        return f"{self.bot_prefix_urlsafe}-{token_hash}"
