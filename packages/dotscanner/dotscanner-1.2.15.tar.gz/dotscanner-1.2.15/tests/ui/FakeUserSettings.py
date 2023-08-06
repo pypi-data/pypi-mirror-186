2598f0ce9db290e5e593597e422f9b208ad7242c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:59 +0000

    Add package dotscanner/1.2.15/dotscanner-1.2.15.tar.gz

---
+++ packages/dotscanner/dotscanner-1.2.15.tar.gz/dotscanner-1.2.15/tests/ui/FakeUserSettings.py
@@ -0,0 +1,11 @@
+class FakeUserSettings:
+    def __init__(self, filepath="test/directory/", dotSize=2, blobSize=5, saveFigures=False, 
+        startImage="", skipsAllowed=0, removeEdgeFrames=True, thresholds=(1.5, 5.0, 2.0)):
+        self.filepath = filepath
+        self.dotSize = dotSize
+        self.blobSize = blobSize
+        self.saveFigures = saveFigures
+        self.startImage = startImage
+        self.skipsAllowed = skipsAllowed
+        self.removeEdgeFrames = removeEdgeFrames
+        self.thresholds = thresholds
