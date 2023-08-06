e1eb1251fe94ae824b5ef3b9ff1c2fdb29553633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:16 +0000

    Add package whisp/0.0.1.21/whisp-0.0.1.21.tar.gz

---
+++ packages/whisp/whisp-0.0.1.21.tar.gz/whisp-0.0.1.21/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1.21'
+DESCRIPTION = 'Whisper package'
+LONG_DESCRIPTION = 'A package that allows easily to use the Whisper library'
+
+# Setting up
+setup(
+    name="whisp",
+    version=VERSION,
+    author="Peteris N.",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['setuptools-rust'],
+    dependencies=[
+        'whisper @ git+https://github.com/openai/whisper.git'
+    ],
+    keywords=[],
+    classifiers=[]
+)
