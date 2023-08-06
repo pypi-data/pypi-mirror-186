db570991d936c5fa734ae6959609f78a8d1008fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:45 +0000

    Add package coiled/0.2.61/coiled-0.2.61-py3-none-any.whl

---
+++ packages/coiled/coiled-0.2.61-py3-none-any.whl/coiled/cli/tests/test_core.py
@@ -0,0 +1,14 @@
+from coiled.cli.core import cli
+
+
+def test_available_commands():
+    assert set(cli.commands) == {
+        "cluster",
+        "login",
+        "setup",
+        "install",
+        "upload",
+        "env",
+        "diagnostics",
+        "package-sync",
+    }
