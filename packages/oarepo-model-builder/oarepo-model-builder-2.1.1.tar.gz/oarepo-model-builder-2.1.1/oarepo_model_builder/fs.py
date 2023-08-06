0ebfd874000f871b09920b858ea8c95571e989d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:49 +0000

    Add package oarepo-model-builder/2.1.1/oarepo-model-builder-2.1.1.tar.gz

---
+++ packages/oarepo-model-builder/oarepo-model-builder-2.1.1.tar.gz/oarepo-model-builder-2.1.1/oarepo_model_builder/fs.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+
+class AbstractFileSystem:
+    def open(self, path, *args, **kwargs):
+        raise NotImplementedError("Not implemented")
+
+    def exists(self, path):
+        raise NotImplementedError("Not implemented")
+
+    def mkdir(self, path):
+        raise NotImplementedError("Not implemented")
+
+    def make_executable(self, path):
+        pass
+
+
+class FileSystem(AbstractFileSystem):
+    def __init__(self):
+        self.opened_files = set()
+        self.overwrite = False
+
+    def open(self, path, mode="r", **kwargs):
+        if self.overwrite:
+            if "r" in mode and path not in self.opened_files:
+                raise FileNotFoundError(f"File {path} not found")
+            if "w" or "a" in mode:
+                self.opened_files.add(path)
+        return open(path, mode=mode, **kwargs)
+
+    def exists(self, path):
+        # if overwrite and the file has not been saved, return False
+        if self.overwrite:
+            return path in self.opened_files
+        return Path(path).exists()
+
+    def mkdir(self, path):
+        Path(path).mkdir(exist_ok=True, parents=True)
+
+    def make_executable(self, path):
+        Path(path).chmod(0o777)
---
+++ packages/oarepo-model-builder/oarepo-model-builder-2.1.1.tar.gz/oarepo-model-builder-2.1.1/oarepo_model_builder/invenio/invenio_base.py
@@ -0,0 +1,31 @@
+from oarepo_model_builder.builders.python import PythonBuilder
+from oarepo_model_builder.outputs.python import PythonOutput
+from oarepo_model_builder.utils.hyphen_munch import HyphenMunch
+from oarepo_model_builder.utils.jinja import package_name
+
+
+class InvenioBaseClassPythonBuilder(PythonBuilder):
+    class_config = None
+    template = None
+    parent_modules = True
+
+    def finish(self, **extra_kwargs):
+        python_path = self.class_to_path(self.settings.python[self.class_config])
+        self.process_template(
+            python_path,
+            self.template,
+            current_package_name=package_name(self.settings.python[self.class_config]),
+            **extra_kwargs,
+        )
+
+    def process_template(self, python_path, template, **extra_kwargs):
+        if self.parent_modules:
+            self.create_parent_modules(python_path)
+        output: PythonOutput = self.builder.get_output("python", python_path)
+        context = HyphenMunch(
+            settings=self.settings, python=self.settings.python, **extra_kwargs
+        )
+        template = self.call_components(
+            "invenio_before_python_template", template, context=context
+        )
+        output.merge(template, context)
