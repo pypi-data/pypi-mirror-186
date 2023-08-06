20d2eb00cda917f43e52e596ec9d38a1017d8787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:25 +0000

    Add package pruebaEmer/0.0.2/pruebaEmer-0.0.2.tar.gz

---
+++ packages/pruebaEmer/pruebaEmer-0.0.2.tar.gz/pruebaEmer-0.0.2/README.md
@@ -0,0 +1,22 @@
+# pruebaEmer
+
+pruebaEmer es una libreria de prueba.
+
+## Instalación
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install pruebaEmer.
+
+```bash
+pip install pruebaEmer
+
+## Usage
+
+```python
+from pruebaEmer import *
+
+# Funcion
+a = pruebaEmer1()
+
+## Contribuciones
+
+No son necesarias.
---
+++ packages/pruebaEmer/pruebaEmer-0.0.2.tar.gz/pruebaEmer-0.0.2/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.0.2' 
+PACKAGE_NAME = 'pruebaEmer' 
+AUTHOR = 'Agustina'
+AUTHOR_EMAIL = 'MGuillen@emergencias.com.ar'
+URL = 'https://github.com/magui394'
+
+LICENSE = 'MIT'
+DESCRIPTION = 'Aquí debes incluir una descripción corta de la librería' 
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding='utf-8')
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      ]
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    install_requires=INSTALL_REQUIRES,
+    license=LICENSE,
+    packages=find_packages(),
+    include_package_data=True
+)
