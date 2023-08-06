baf01c831215d52d2057ebdeb57dfcc1acfd043d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:23 +0000

    Add package larry-utils/0.1.3/larry_utils-0.1.3.tar.gz

---
+++ packages/larry-utils/larry_utils-0.1.3.tar.gz/larry_utils-0.1.3/larry_utils/__init__.py
@@ -0,0 +1,35 @@
+from PIL import Image
+import io
+
+
+def multiprocess_pool(process_func: callable,
+                      args: list,
+                      num_parallel: int = 20):
+    from multiprocessing import Process
+    pool = {i: None for i in range(num_parallel)}
+
+    for arg in args:
+        while True:
+            search = False
+            for i in range(num_parallel):
+                if (pool[i] is None) or (not pool[i].is_alive()):
+                    pool[i] = Process(target=process_func, args=arg)
+                    pool[i].start()
+                    search = True
+                    break
+            if search:
+                break
+
+    for i in range(num_parallel):
+        if pool[i] is not None:
+            pool[i].join()
+
+
+def read_bytes(file_name, start_num, size, is_image=True):
+    with open(file_name, 'rb') as f:
+        f.seek(start_num)
+        image = f.read(size)
+    if is_image:
+        image = Image.open(io.BytesIO(image))
+    return image
+
