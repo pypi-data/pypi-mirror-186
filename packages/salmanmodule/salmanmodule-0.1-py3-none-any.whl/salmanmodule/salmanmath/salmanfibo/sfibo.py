40355aad9b735f32d12f77de51604ea04df3fce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:53 +0000

    Add package salmanmodule/0.1/salmanmodule-0.1-py3-none-any.whl

---
+++ packages/salmanmodule/salmanmodule-0.1-py3-none-any.whl/salmanmodule/salmanmath/salmanfibo/sfibo.py
@@ -0,0 +1,20 @@
+#salman fibo
+def s_fibo(n):
+    a=1
+    b=1
+    print("Fibonacci Series: ", end=' ')
+    if n<1:
+        print("Incorrect input")
+    elif n==1:
+        print('0', end=' ')
+    elif n==2:
+        print('0','1', end=' ')
+    else:
+        print('0',a,b,end=' ')
+        for i in range(n-3):
+            c = a + b
+            print(c, end=' ')
+            a = b  
+            b = c
+        print()       
+         
---
+++ packages/salmanmodule/salmanmodule-0.1-py3-none-any.whl/salmanmodule/simplecode/simpleintro.py
@@ -0,0 +1,2 @@
+def yourname(nam):
+    print("Hello "+nam+" , I'm Salman Mumtaz having Reg No. MSDSF22M007")
