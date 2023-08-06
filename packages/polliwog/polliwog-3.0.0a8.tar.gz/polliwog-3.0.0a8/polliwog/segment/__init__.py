a6410baf2c50efdcb6fcf575326c5f4faf9391d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:01 +0000

    Add package polliwog/3.0.0a8/polliwog-3.0.0a8.tar.gz

---
+++ packages/polliwog/polliwog-3.0.0a8.tar.gz/polliwog-3.0.0a8/polliwog/segment/__init__.py
@@ -0,0 +1,13 @@
+from ._segment_functions import (
+    closest_point_of_line_segment,
+    path_centroid,
+    subdivide_segment,
+    subdivide_segments,
+)
+
+__all__ = [
+    "closest_point_of_line_segment",
+    "path_centroid",
+    "subdivide_segment",
+    "subdivide_segments",
+]
