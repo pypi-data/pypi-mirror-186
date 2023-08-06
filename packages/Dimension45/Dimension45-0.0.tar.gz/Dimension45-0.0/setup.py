8a174c9f5fdf762b932251f5f22f3d39e00c326c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:57 +0000

    Add package Dimension45/0.0/Dimension45-0.0.tar.gz

---
+++ packages/Dimension45/Dimension45-0.0.tar.gz/Dimension45-0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Dimension45',
+    version='0.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'sklearn'
+    ],
+    author='Hanzlah Shah',
+    author_email='shahhanzlah90@gmail.com',
+    description='Package for Dimension Reduction',
+    
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        
+    ],
+    python_requires='>=3.6,<4.0', 
+)
