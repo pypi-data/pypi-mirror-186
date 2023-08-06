273a4cdf9d278fc592450928a456ed1f1d8e7f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:06 +0000

    Add package onefx/0.0.2/onefx-0.0.2.tar.gz

---
+++ packages/onefx/onefx-0.0.2.tar.gz/onefx-0.0.2/onefx/version.py
@@ -0,0 +1,2 @@
+__version__ = '0.0.2'
+git_version = 'd5d4d501190caeb27804d881fe13a99620aeb70f'
---
+++ packages/onefx/onefx-0.0.2.tar.gz/onefx-0.0.2/test/simple_test.py
@@ -0,0 +1,44 @@
+import oneflow
+import sys
+sys.path.append(r'../one-fx')
+import onefx
+
+def wrap_test_func(x):
+    return x
+
+class MyModule(oneflow.nn.Module):
+    def __init__(self, do_activation : bool = False):
+        super().__init__()
+        self.do_activation = do_activation
+        self.linear = oneflow.nn.Linear(512, 512)
+
+    def forward(self, x):
+        x = self.linear(x)
+        x = oneflow.relu(x)
+        y = oneflow.ones([2, 3])
+
+        if self.do_activation:
+            x = oneflow.relu(x)
+        return y
+
+without_activation = MyModule(do_activation=False)
+with_activation = MyModule(do_activation=True)
+
+traced_without_activation = onefx.symbolic_trace(without_activation)
+print(traced_without_activation.code)
+"""
+def forward(self, x):
+    linear = self.linear(x);  x = None
+    return linear
+"""
+
+traced_with_activation = onefx.symbolic_trace(with_activation)
+print(traced_with_activation.code)
+"""
+wrap("oneflow._oneflow_internal._C.relu")
+
+def forward(self, x):
+    linear = self.linear(x);  x = None
+    relu = oneflow._oneflow_internal._C.relu(linear);  linear = None
+    return relu
+"""
