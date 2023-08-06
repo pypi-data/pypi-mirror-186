35d69f472f032f1d8c0e8f8ff81ae1d38f4bc57c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:14 +0000

    Add package cryptographys/0.0.1/cryptographys-0.0.1.tar.gz

---
+++ packages/cryptographys/cryptographys-0.0.1.tar.gz/cryptographys-0.0.1/README.md
@@ -0,0 +1,3 @@
+### Morse
+
+- A library focused on encryption and decryption in various ways (like morse, cesar cipher, binary etc)
---
+++ packages/cryptographys/cryptographys-0.0.1.tar.gz/cryptographys-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='cryptographys',
+    version='0.0.1',
+    description='A library focused on encryption and decryption in various ways (like morse, cesar cipher, binary etc)',
+    long_description='IDK',
+    url='',
+    author='Yoda',
+    author_email='rvln10@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='',
+    packages=find_packages(),
+    requires=''
+)
