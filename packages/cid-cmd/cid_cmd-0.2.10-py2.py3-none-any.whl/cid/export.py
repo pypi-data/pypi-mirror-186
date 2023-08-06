93938c945bbac19870af262ab9e0ebb9041693f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:35 +0000

    Add package cid-cmd/0.2.10/cid_cmd-0.2.10-py2.py3-none-any.whl

---
+++ packages/cid-cmd/cid_cmd-0.2.10-py2.py3-none-any.whl/cid/export.py
@@ -0,0 +1,199 @@
+'''
+Source Account                    Destination Account
+┌─────────────────────────┐       ┌──────────────────┐
+│  Analysis    Template   │       │  Master Template │
+│  ┌─┬──┬─┐    ┌──────┐   │       │    ┌──────┐      │
+│  │ │┼┼│ ├────►      ├───┼───────┼────►      │      │
+│  └─┴──┴─┘    └──────┘   │       │    └──────┘      │
+│                         │       │                  │
+└─────────────────────────┘       └──────────────────┘
+'''
+import time
+import logging
+
+import yaml
+import boto3
+
+from cid.helpers import quicksight
+from cid.utils import get_parameter, get_parameters
+from cid.exceptions import CidCritical
+
+logger = logging.getLogger(__name__)
+
+def export_analysis(qs):
+
+    # Choose Analysis to share
+    analysis_id = None
+    if get_parameters().get('analysis-id'):
+        analysis_id = get_parameters().get('analysis-id')
+
+    if not analysis_id:
+        try:
+            analyzes = []
+            logger.info("Discovering analyses")
+            paginator = qs.client.get_paginator('list_analyses')
+            response_iterator = paginator.paginate(
+                AwsAccountId=qs.account_id,
+                PaginationConfig={'MaxItems': 100}
+            )
+            for page in response_iterator:
+                analyzes.extend(page.get('AnalysisSummaryList'))
+            if len(analyzes) == 100:
+                logger.info('Too many analyses. Will show first 100')
+        except qs.client.exceptions.AccessDeniedException:
+            logger.info("AccessDeniedException while discovering analyses")
+        else:
+            analyzes = list(filter(lambda a: a['Status']=='CREATION_SUCCESSFUL', analyzes ))
+            if not analyzes:
+                raise CidCritical("No analyses was found, please save your dashboard as an analyse first")
+            choices = {a['Name']:a for a in sorted(analyzes, key=lambda a: a['LastUpdatedTime'])[::-1]}
+            choice = get_parameter(
+                'analysis-name',
+                message='Select Analysis you want to share.',
+                choices=choices.keys(),
+            )
+            analysis_id = choices[choice]['AnalysisId']
+
+    if not analysis_id:
+        analysis_id = get_parameter(
+            'analysis-id',
+            message='Enter ID of analysis you want to share (open analysis in browser and copy id from url)',
+        )
+    if not analysis_id:
+        raise CidCritical("Need a parameter --analysis-id or --analysis-name")
+
+    analysis = qs.client.describe_analysis(
+        AwsAccountId=qs.account_id,
+        AnalysisId=analysis_id
+    )['Analysis']
+
+    logger.info("analysing datasets")
+    dataset_references = []
+    datasets = {}
+    for dataset_arn in analysis['DataSetArns']:
+        dataset_id = dataset_arn.split('/')[-1]
+        dataset = qs.describe_dataset(dataset_id)
+
+        if not isinstance(dataset, quicksight.Dataset):
+            raise CidCritical(f'dataset {dataset_id} not found. '
+                'We need all datasets to be preset for template generation')
+
+        dataset_references.append({
+            "DataSetPlaceholder": dataset.raw['Name'],
+            "DataSetArn": dataset_arn
+        })
+
+        dataset_data = {
+            "DataSetId": dataset.raw['DataSetId'],
+            "Name": dataset.raw['Name'],
+            "PhysicalTableMap": dataset.raw['PhysicalTableMap'],
+            "LogicalTableMap": dataset.raw['LogicalTableMap'],
+            "ImportMode": dataset.raw['ImportMode'],
+        }
+
+        for key, value in dataset_data['PhysicalTableMap'].items():
+            if 'RelationalTable' not in value \
+                or 'DataSourceArn' not in value['RelationalTable'] \
+                or 'Schema' not in value['RelationalTable']:
+                raise CidCritical(f'Dataset {key} does not seems to be Antena dataset. Only Athena datasets are supported.' )
+            value['RelationalTable']['DataSourceArn'] = '${athena_datasource_arn}'
+            value['RelationalTable']['Schema'] = '${athena_database_name}'
+
+        datasets[dataset_arn] = dataset_data
+
+    template_id = get_parameter(
+        'template-id',
+        message='Enter template id',
+        default=analysis.get('Name').replace(' ', '-')
+    )
+
+    template_version_description = get_parameter(
+        'template-version-description',
+        message='Enter version description',
+        default='vX.X.X' # FIXME: get version from analysis / template
+    )
+
+    logger.info('Updating template')
+    params = {
+        "AwsAccountId": qs.account_id,
+        "TemplateId": template_id,
+        "Name": template_id,
+        "SourceEntity": {
+            "SourceAnalysis": {
+                "Arn": analysis.get("Arn"),
+                "DataSetReferences": dataset_references
+            }
+        },
+        "VersionDescription": template_version_description,
+    }
+    logger.debug(f'Template params = {params}')
+    try:
+        res = qs.client.update_template(**params)
+        logger.info(f'Template {template_id} updated from Analysis {analysis.get("Arn")}')
+    except qs.client.exceptions.ResourceNotFoundException:
+        res = qs.client.create_template(**params)
+        logger.info(f'Template {template_id} created from Analysis {analysis.get("Arn")}')
+
+    if res['CreationStatus'] not in ['CREATION_IN_PROGRESS', 'UPDATE_IN_PROGRESS']:
+        raise CidCritical(f'failed template operation {res}')
+
+    template_arn = res['Arn']
+    logger.info(f'Template arn = {template_arn}')
+
+    time.sleep(5)
+
+    reader_account_id = get_parameter(
+        'reader-account',
+        message='Enter account id to share the template with or *',
+        default='*'
+    )
+    res = qs.update_template_permissions(
+        TemplateId=template_id,
+        GrantPermissions=[
+            {
+                "Principal": f'arn:aws:iam::{reader_account_id}:root' if reader_account_id != '*' else '*',
+                'Actions': [
+                    "quicksight:DescribeTemplate",
+                ]
+            },
+        ],
+    )
+
+    dashboard_id = get_parameter(
+        'dashboard-id',
+        message='dashboard id (will be used in url of dashboard)',
+        default=analysis['Name'].replace(' ', '-').lower()
+    )
+
+    resources = {}
+    resources['dashboards'] = {}
+    resources['datasets'] = {}
+    resources['views'] = {}
+
+    resources['dashboards'][analysis['Name'].upper()] = {
+        'name': analysis['Name'],
+        'templateId': template_id,
+        'sourceAccountId': qs.account_id,
+        'region': qs.session.region_name,
+        'dashboardId': dashboard_id,
+        'dependsOn':{
+            'datasets': [dataset['Name'].replace(' ', '-') for dataset in datasets.values()]
+        }
+    }
+
+    for dataset in datasets.values():
+        resources['datasets'][dataset.get("Name").replace(' ', '-')] = {'Data': dataset}
+
+    output = get_parameter(
+        'output',
+        message='Enter a filename (.yaml)',
+        default=f"{analysis['Name'].replace(' ', '-')}-{template_version_description}.yaml"
+    )
+    with open(output, "w") as output_file:
+        output_file.write(yaml.safe_dump(resources, sort_keys=False))
+
+
+if __name__ == "__main__": # for testing
+    qs = quicksight.QuickSight(boto3.session.Session(), boto3.client('sts').get_caller_identity())
+    export_analysis(qs)
+
---
+++ packages/cid-cmd/cid_cmd-0.2.10-py2.py3-none-any.whl/cid/helpers/athena.py
@@ -0,0 +1,436 @@
+from io import StringIO
+import json
+import logging
+from string import Template
+import time, csv
+
+from pkg_resources import resource_string
+
+from cid.base import CidBase
+from cid.utils import get_parameter, get_parameters
+from cid.exceptions import CidCritical
+
+logger = logging.getLogger(__name__)
+
+class Athena(CidBase):
+    # Define defaults
+    defaults = {
+        'CatalogName': 'AwsDataCatalog',
+        'DatabaseName': 'customer_cur_data',
+        'WorkGroup': 'primary'
+    }
+    _CatalogName = None
+    _DatabaseName = None
+    _WorkGroup = None
+    ahq_queries = None
+    _metadata = dict()
+    _resources = dict()
+    _client = None
+
+    def __init__(self, session, resources: dict=None) -> None:
+        super().__init__(session)
+        self._resources = resources
+
+    @property
+    def client(self):
+        if not self._client:
+            self._client = self.session.client('athena', region_name=self.region)
+        return self._client
+
+    @property
+    def CatalogName(self) -> str:
+        """ Check if AWS Datacalog and Athena database exist """
+        if not self._CatalogName:
+            # Get AWS Glue DataCatalogs
+            glue_data_catalogs = [d for d in self.list_data_catalogs() if d['Type'] == 'GLUE']
+            if not len(glue_data_catalogs):
+                logger.error('AWS DataCatog of type GLUE not found!')
+                self._status = 'AWS DataCatog of type GLUE not found'
+            if len(glue_data_catalogs) == 1:
+                self._CatalogName = glue_data_catalogs.pop().get('CatalogName')
+            elif len(glue_data_catalogs) > 1:
+                # Ask user
+                self._CatalogName = get_parameter(
+                    param_name='glue-data-catalog',
+                    message="Select AWS DataCatalog to use",
+                    choices=[catalog.get('CatalogName') for catalog in glue_data_catalogs],
+                )
+            logger.info(f'Using datacatalog: {self._CatalogName}')
+        return self._CatalogName
+
+    @CatalogName.setter
+    def set_catalog_name(self, catalog):
+        self._CatalogName = catalog
+
+    @property
+    def DatabaseName(self) -> str:
+        """ Check if Athena database exist """
+
+        if not self._DatabaseName:
+            if get_parameters().get('athena-database'):
+                self._DatabaseName = get_parameters().get('athena-database')
+                try:
+                    if not self.get_database(self._DatabaseName):
+                        raise CidCritical(f'Database {self._DatabaseName} not found in Athena catalog {self.CatalogName}')
+                except Exception as exc:
+                    if 'AccessDeniedException' in str(exc):
+                        logger.warning(f'{type(exc)} - Missing athena:GetDatabase permission. Cannot verify existance of {self._DatabaseName} in {self.CatalogName}. Hope you have it there.')
+                        return self._DatabaseName
+                    raise
+            # Get AWS Athena databases
+            athena_databases = self.list_databases()
+            if not len(athena_databases):
+                self._status = 'AWS Athena databases not found'
+                raise CidCritical(self._status)
+            if len(athena_databases) == 1:
+                self._DatabaseName = athena_databases.pop().get('Name')
+            elif len(athena_databases) > 1:
+                # Remove empty databases from the list
+                for d in athena_databases:
+                    tables = self.list_table_metadata(
+                        DatabaseName=d.get('Name'),
+                        max_items=1000, # This is an impiric limit. User can have up to 200k tables in one DB we need to draw a line somewhere
+                    )
+                    if not len(tables):
+                        athena_databases.remove(d)
+                # Select default database if present
+                default_database = [d for d in athena_databases if d['Name'] == self.defaults.get('DatabaseName')]
+                if len(default_database):
+                    self._DatabaseName = default_database.pop().get('Name')
+                else:
+                    # Ask user
+                    self._DatabaseName = get_parameter(
+                        param_name='athena-database',
+                        message="Select AWS Athena database to use",
+                        choices=[d['Name'] for d in athena_databases],
+                    )
+            logger.info(f'Using Athena database: {self._DatabaseName}')
+        return self._DatabaseName
+
+    @DatabaseName.setter
+    def DatabaseName(self, database):
+        self._DatabaseName = database
+
+    @property
+    def WorkGroup(self) -> str:
+        """ Select AWS Athena workgroup """
+        if not self._WorkGroup:
+            logger.info('Selecting Athena workgroup...')
+            workgroups = self.list_work_groups()
+            logger.info(f'Found {len(workgroups)} workgroups: {", ".join([wg.get("Name") for wg in workgroups])}')
+            if len(workgroups) == 1:
+                self.WorkGroup = workgroups.pop().get('Name')
+            elif len(workgroups) > 1:
+                # Select default workgroup if present
+                default_workgroup = next(iter([wg.get('Name') for wg in workgroups if wg['Name'] == self.defaults.get('WorkGroup')]), None)
+                if default_workgroup: logger.info(f'Found "{default_workgroup}" as a default workgroup')
+                # Ask user
+                self.WorkGroup = get_parameter(
+                    param_name='athena-workgroup',
+                    message="Select AWS Athena workgroup to use",
+                    choices=[d['Name'] for d in workgroups],
+                    default=default_workgroup
+                )
+            logger.info(f'Selected workgroup: "{self._WorkGroup}"')
+        return self._WorkGroup
+
+    @WorkGroup.setter
+    def WorkGroup(self, name: str):
+        try:
+            _workgroup = self.client.get_work_group(WorkGroup=name).get('WorkGroup')
+        except self.client.exceptions.InvalidRequestException as e:
+            raise CidCritical(e)
+        if _workgroup.get('State') == 'DISABLED':
+            raise CidCritical(f'Athena Workgroup "{name}" is disabled.')
+        if not _workgroup.get('Configuration', {}).get('ResultConfiguration', {}).get('OutputLocation'):
+            raise CidCritical(f'Athena Workgroup "{name}" must have an output location s3 bucket configured in the region {self.region}.')
+        self._WorkGroup = name
+        logger.info(f'Selected Athena WorkGroup: "{self._WorkGroup}"')
+
+    def list_data_catalogs(self) -> list:
+        return self.client.list_data_catalogs().get('DataCatalogsSummary')
+    
+    def list_databases(self) -> list:
+        return self.client.list_databases(CatalogName=self.CatalogName).get('DatabaseList')
+    
+    def get_database(self, DatabaseName: str=None) -> bool:
+        """ Check if AWS Datacalog and Athena database exist """
+        if not DatabaseName:
+            DatabaseName=self.DatabaseName
+        try:
+            self.client.get_database(CatalogName=self.CatalogName, DatabaseName=DatabaseName).get('Database')
+            return True
+        except Exception as exc:
+            if 'AccessDeniedException' in str(exc):
+                raise
+            else:
+                logger.debug(e, exc_info=True)
+                return False
+
+    def list_table_metadata(self, DatabaseName: str=None, max_items: int=None) -> dict:
+        params = {
+            'CatalogName': self.CatalogName,
+            'DatabaseName': DatabaseName or self.DatabaseName,
+            'PaginationConfig':{
+                'MaxItems': max_items,
+            },
+        }
+        table_metadata = list()
+        try:
+            paginator = self.client.get_paginator('list_table_metadata')
+            response_iterator = paginator.paginate(**params)
+            for page in response_iterator:
+                table_metadata.extend(page.get('TableMetadataList'))
+            logger.debug(f'Table metadata: {table_metadata}')
+            logger.info(f'Found {len(table_metadata)} tables in {DatabaseName if DatabaseName else self.DatabaseName}')
+        except Exception as e:
+            logger.error(f'Failed to list tables in {DatabaseName if DatabaseName else self.DatabaseName}')
+            logger.error(e)
+            
+        return table_metadata
+
+    def list_work_groups(self) -> list:
+        """ List AWS Athena workgroups """
+        result = self.client.list_work_groups()
+        logger.debug(f'Workgroups: {result.get("WorkGroups")}')
+        return result.get('WorkGroups')
+
+    def get_table_metadata(self, TableName: str) -> dict:
+        table_metadata = self._metadata.get(TableName)
+        params = {
+            'CatalogName': self.CatalogName,
+            'DatabaseName': self.DatabaseName,
+            'TableName': TableName
+        }
+        if not table_metadata:
+            table_metadata = self.client.get_table_metadata(**params).get('TableMetadata')
+            self._metadata.update({TableName: table_metadata})
+
+        return table_metadata
+
+
+    def execute_query(self, sql_query, sleep_duration=1, database: str=None, catalog: str=None, fail: bool=True) -> str:
+        """ Executes an AWS Athena Query """
+
+        # Set execution context
+        execution_context = {
+            'Database': database or self.DatabaseName,
+            'Catalog': catalog or self.CatalogName,
+        }
+
+        try:
+            # Start Athena query
+            response = self.client.start_query_execution(
+                QueryString=sql_query, 
+                QueryExecutionContext=execution_context, 
+                WorkGroup=self.WorkGroup
+            )
+
+            # Get Query ID
+            query_id = response.get('QueryExecutionId')
+
+            # Get Query Status
+            query_status = self.client.get_query_execution(QueryExecutionId=query_id)
+        except self.client.exceptions.InvalidRequestException as e:
+            logger.debug(f'Full query: {sql_query}')
+            raise CidCritical(f'InvalidRequestException: {e}')
+        except Exception as e:
+            logger.debug(f'Full query: {sql_query}')
+            raise CidCritical(f'Athena query failed: {e}')
+
+
+        current_status = query_status['QueryExecution']['Status']['State']
+
+        # Poll for the current status of query as long as its not finished
+        while current_status in ['SUBMITTED', 'RUNNING', 'QUEUED']:
+            response = self.client.get_query_execution(QueryExecutionId=query_id)
+            current_status = response['QueryExecution']['Status']['State']
+
+            # Sleep before polling again
+            time.sleep(sleep_duration)
+
+        # Return result, either positive or negative
+        if (current_status == "SUCCEEDED"):
+            return query_id
+        elif not fail:
+            return False
+        else:
+            failure_reason = response['QueryExecution']['Status']['StateChangeReason']
+            logger.error(f'Athena query failed: {failure_reason}')
+            logger.info(f'Full query: {sql_query}')
+            raise CidCritical(f'Athena query failed: {failure_reason}')
+
+    def get_query_results(self, query_id):
+        return self.client.get_query_results(QueryExecutionId=query_id)
+    
+    def get_query_execution(self, query_id):
+        return self.client.get_query_execution(QueryExecutionId=query_id)
+
+    def parse_response_as_list(self, response, include_header=False):
+        data = list()
+
+        # Get results rows, either with or without the header row
+        rows = response['ResultSet']['Rows'] if include_header else response['ResultSet']['Rows'][1:]
+
+        for row in rows:
+            for r in row['Data']:
+                data.append(r['VarCharValue'] if 'VarCharValue' in r else '')
+
+        return data
+
+    def query_results_to_csv(self, query_id, return_header=False):
+        # Get query results
+        response = self.client.get_query_results(QueryExecutionId=query_id)
+
+        # Get results rows, either with or without the header row
+        rows = response['ResultSet']['Rows'] if return_header else response['ResultSet']['Rows'][1:]
+
+        if rows:
+            # Write rows to StringIO in CSV format
+            buf = StringIO()
+            csv_writer = csv.writer(buf, delimiter=',')
+
+            for row in rows:
+                csv_writer.writerow([x['VarCharValue'] if 'VarCharValue' in x else None for x in row['Data']])
+
+            # Strip whitespaces from CSVified string and return it
+            return buf.getvalue().rstrip('\n')
+        else:
+            return None
+
+    def show_columns(self, table_name):
+        sql_query = f'SHOW COLUMNS in {table_name}'
+        query_id = self.execute_query(sql_query=sql_query)
+
+        describe = self.query_results_to_csv(query_id).split('\n')
+
+        # Athena is weird.. Remove whitespaces.
+        result = [elem.rstrip() for elem in describe]
+
+        return result
+
+    def parse_selected_tables(self, month_list):
+        d = {}
+
+        for month in month_list:
+            split = month.split('_')
+
+        payer = split[1]
+        year = split[2][:4]
+        month = split[2][4:]
+
+        if payer in d:
+            d[payer].append((year, month))
+        else:
+            d[payer] = list()
+            d[payer].append((year, month))
+        
+        return d
+
+    # AHQ functions
+    def execute_ahq(self, query_id, **kwargs) -> list:
+        """ Execute Athena Query by name """
+        # Load query
+        query = self.get_ahq(query_id, **kwargs)
+        # Execute query
+        execution_id = self.execute_query(query)
+        results = self.get_query_results(execution_id)
+        # Return results as list
+        return self.parse_response_as_list(results)
+
+
+    def get_ahq(self, query_id, **kwargs) -> str:
+        """ Returns a fully compiled AHQ """
+        # Query path
+        query_file = self.get_ahqs().get(query_id).get('File')
+
+        template = Template(resource_string(__name__, f'../queries/{query_file}').decode('utf-8'))
+
+        # Fill in TPLs
+        columns_tpl = dict()
+        columns_tpl.update(**kwargs)
+        compiled_query = template.safe_substitute(columns_tpl)
+
+        return compiled_query
+
+
+    def get_ahqs(self) -> dict:
+        """ Return a list of all availiable AHQs """
+        
+        if not self.ahq_queries:            
+            # Load queries
+            queries_files = resource_string(__name__, '../queries/ahq-queries.json')
+            self.ahq_queries = json.loads(queries_files).get('query_templates')
+
+        return self.ahq_queries
+
+
+    def discover_views(self, views: dict={}) -> None:
+        for view_name in views:
+            try:
+                self.get_table_metadata(TableName=view_name)
+            except self.client.exceptions.MetadataException:
+                pass
+
+
+    def wait_for_view(self, view_name: str, poll_interval=1, timeout=60) -> None:
+        deadline = time.time() + timeout
+        while time.time() <= deadline:
+            self.discover_views([view_name])
+            if view_name in self._metadata.keys():
+                logger.info(f'view {view_name} exists')
+                return True
+            else:
+                time.sleep(poll_interval)
+        else:
+            logger.info(f'View {view_name} does not exist, or not discoverable')
+            return False
+
+
+    def delete_table(self, name: str, catalog: str=None, database: str=None):
+        if get_parameter(
+                param_name=f'confirm-{name}',
+                message=f'Delete Athena table {name}?',
+                choices=['yes', 'no'],
+                default='no') != 'yes':
+            return False
+
+        try:
+            res = self.execute_query(
+                f'DROP TABLE IF EXISTS {name};',
+                catalog=catalog,
+                database=database,
+                fail=False
+            )
+        except Exception as exc:
+            logger.debug(exc, exc_info=True)
+            logger.info(f'Table {name} cannot be deleted: {exc}')
+            return False
+        else:
+            if name in self._metadata: del self._metadata[name]
+            logger.info(f'Table {name} deleted')
+        return True
+
+    def delete_view(self, name: str, catalog: str=None, database: str=None):
+        if get_parameter(
+                param_name=f'confirm-{name}',
+                message=f'Delete Athena view {name}?',
+                choices=['yes', 'no'],
+                default='no') != 'yes':
+            return False
+
+        try:
+            res = self.execute_query(
+                f'DROP VIEW IF EXISTS {name};',
+                catalog=catalog,
+                database=database,
+                fail=False
+            )
+        except Exception as exc:
+            logger.debug(exc, exc_info=True)
+            logger.info(f'View {name} cannot be deleted: {exc}')
+            return False
+        else:
+            if name in self._metadata: del self._metadata[name]
+            logger.info(f'View {name} deleted')
+        return True
