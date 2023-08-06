e56da5f61617064683d1e881c9be0b92fd32a534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:24 +0000

    Add package cpg-utils-ms/1.3.1/cpg-utils-ms-1.3.1.tar.gz

---
+++ packages/cpg-utils-ms/cpg-utils-ms-1.3.1.tar.gz/cpg-utils-ms-1.3.1/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name='cpg-utils-ms',
+    # This tag is automatically updated by bumpversion
+    version='1.3.1',
+    description='Library of convenience functions specific to the CPG (MS version)',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url=f'https://github.com/gregsmi/cpg-utils',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'google-auth>=1.27.0',
+        'google-cloud-secret-manager',
+        'azure-identity',
+        'azure-keyvault-secrets',
+        'azure-storage-blob',
+        'cloudpathlib[all]',
+        'hail',
+        'toml',
+        'frozendict',
+    ],
+    extras_require={
+        'test': [
+            'pytest',
+            'pytest-mock',
+            'pytest-cov'
+        ],
+    },
+    keywords='bioinformatics',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+)
