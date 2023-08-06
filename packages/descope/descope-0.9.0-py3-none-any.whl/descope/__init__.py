57decb2c3e338e8cb7af69b9b866f6df2f123dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:35 +0000

    Add package descope/0.9.0/descope-0.9.0-py3-none-any.whl

---
+++ packages/descope/descope-0.9.0-py3-none-any.whl/descope/__init__.py
@@ -0,0 +1,12 @@
+from descope.common import (
+    COOKIE_DATA_NAME,
+    REFRESH_SESSION_COOKIE_NAME,
+    REFRESH_SESSION_TOKEN_NAME,
+    SESSION_COOKIE_NAME,
+    SESSION_TOKEN_NAME,
+    DeliveryMethod,
+)
+from descope.descope_client import DescopeClient
+from descope.exceptions import AuthException
+from descope.management.common import AssociatedTenant
+from descope.management.sso_settings import AttributeMapping, RoleMapping
