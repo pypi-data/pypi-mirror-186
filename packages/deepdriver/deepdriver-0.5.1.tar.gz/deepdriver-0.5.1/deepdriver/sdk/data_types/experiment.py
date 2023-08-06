2e210753de6ed0037e3f83c17ebb5facd5942201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:14 +0000

    Add package deepdriver/0.5.1/deepdriver-0.5.1.tar.gz

---
+++ packages/deepdriver/deepdriver-0.5.1.tar.gz/deepdriver-0.5.1/deepdriver/sdk/data_types/experiment.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+experiment: Experiment = None
+
+
+def set_experiment(experiment_: Experiment) -> None:
+    global experiment
+    experiment = experiment_
+
+
+def get_experiment() -> Experiment:
+    global experiment
+    return experiment
+
+
+class Experiment:
+    def __init__(self, exp_name, team_name):
+        self.__exp_name = exp_name
+        self.__team_name = team_name
+
+    @property
+    def exp_name(self):
+        return self.__exp_name
+
+    @property
+    def team_name(self):
+        return self.__team_name
