7fbb923685155b9e2425e74cd7800e7f167e057c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:06 +0000

    Add package ecmanalysis/0.0.7/ecmanalysis-0.0.7.tar.gz

---
+++ packages/ecmanalysis/ecmanalysis-0.0.7.tar.gz/ecmanalysis-0.0.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.7'
+DESCRIPTION = 'Tools for analysis of the extracellular matrix'
+
+# Setting up
+setup(
+    name="ecmanalysis",
+    version=VERSION,
+    author="Mohamed Ghafoor",
+    author_email="<moeghaf@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['scanpy', 'anndata', 'pandas', 'numpy', 'matplotlib', 'seaborn', 'scikit-learn', 'yellowbrick', 'requests'],
+    keywords=['extracellular matrix', 'matrisome', 'spatial transcriptomics'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
+
