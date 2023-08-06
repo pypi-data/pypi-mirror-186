bda4c33e8674162f3f6032922d5050b28681da51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:30:34 +0000

    Add package mage-ai/0.7.72/mage-ai-0.7.72.tar.gz

---
+++ packages/mage-ai/mage-ai-0.7.72.tar.gz/mage-ai-0.7.72/mage_ai/orchestration/notification/config.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+from mage_ai.services.email.config import EmailConfig
+from mage_ai.services.slack.config import SlackConfig
+from mage_ai.services.teams.config import TeamsConfig
+from mage_ai.shared.config import BaseConfig
+from typing import Dict
+import traceback
+
+
+@dataclass
+class NotificationConfig(BaseConfig):
+    email_config: EmailConfig = None
+    slack_config: SlackConfig = None
+    teams_config: TeamsConfig = None
+
+    @classmethod
+    def load(self, config_path: str = None, config: Dict = None):
+        notification_config = super().load(config_path=config_path, config=config)
+        if notification_config.slack_config is not None and \
+                type(notification_config.slack_config) is dict:
+            try:
+                notification_config.slack_config = SlackConfig.load(
+                    config=notification_config.slack_config,
+                )
+            except Exception:
+                traceback.print_exc()
+                notification_config.slack_config = None
+        if notification_config.teams_config is not None and \
+                type(notification_config.teams_config) is dict:
+            try:
+                notification_config.teams_config = TeamsConfig.load(
+                    config=notification_config.teams_config,
+                )
+            except Exception:
+                traceback.print_exc()
+                notification_config.teams_config = None
+        if notification_config.email_config is not None and \
+                type(notification_config.email_config) is dict:
+            try:
+                notification_config.email_config = EmailConfig.load(
+                    config=notification_config.email_config,
+                )
+            except Exception:
+                traceback.print_exc()
+                notification_config.email_config = None
+        return notification_config
