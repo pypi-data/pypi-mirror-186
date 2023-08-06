fbbbe3fa8ab40fa78709d6141af2317a48f390f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:00 +0000

    Add package pgx/0.0.10/pgx-0.0.10.tar.gz

---
+++ packages/pgx/pgx-0.0.10.tar.gz/pgx-0.0.10/pgx/tic_tac_toe.py
@@ -0,0 +1,85 @@
+from typing import Tuple
+
+import jax
+import jax.numpy as jnp
+from flax import struct
+
+FALSE = jnp.bool_(False)
+TRUE = jnp.bool_(True)
+
+
+@struct.dataclass
+class State:
+    curr_player: jnp.ndarray = jnp.int8(0)
+    legal_action_mask: jnp.ndarray = jnp.ones(9, jnp.bool_)
+    terminated: jnp.ndarray = jnp.bool_(False)
+    # 0: 先手, 1: 後手
+    turn: jnp.ndarray = jnp.int8(0)
+    # 0 1 2
+    # 3 4 5
+    # 6 7 8
+    # -1: empty, 0: 先手, 1: 後手
+    board: jnp.ndarray = -jnp.ones(9, jnp.int8)
+
+
+def init(rng: jax.random.KeyArray) -> Tuple[jnp.ndarray, State]:
+    curr_player = jnp.int8(jax.random.bernoulli(rng))
+    return curr_player, State(curr_player=curr_player)  # type:ignore
+
+
+def step(
+    state: State, action: jnp.ndarray
+) -> Tuple[jnp.ndarray, State, jnp.ndarray]:
+    # TODO(sotetsuk): illegal action check
+    # if state.legal_action_mask.at[action]:
+    #     ...
+    board = state.board.at[action].set(state.turn)
+    won = _win_check(board, state.turn)
+    rewards = jax.lax.cond(
+        won,
+        lambda: jnp.int16([-1, -1]).at[state.curr_player].set(1),
+        lambda: jnp.zeros(2, jnp.int16),
+    )
+    terminated = won | jnp.all(board != -1)
+    curr_player = (state.curr_player + 1) % 2
+    state = State(
+        curr_player=curr_player,
+        legal_action_mask=board < 0,
+        terminated=terminated,
+        turn=(state.turn + 1) % 2,
+        board=board,
+    )  # type: ignore
+    return curr_player, state, rewards
+
+
+def _win_check(board, turn) -> jnp.ndarray:
+    # board:
+    #   0 1 2
+    #   3 4 5
+    #   6 7 8
+    won = FALSE
+    for i in range(0, 9, 3):
+        # e.g., [0, 1, 2]
+        won = jax.lax.cond(
+            jnp.all(board[i : i + 3] == turn), lambda: TRUE, lambda: won
+        )
+    for i in range(3):
+        # e.g., [0, 3, 6]
+        won = jax.lax.cond(
+            jnp.all(board[i:9:3] == turn), lambda: TRUE, lambda: won
+        )
+    won = jax.lax.cond(
+        jnp.all(board[jnp.int8([0, 4, 8])] == turn),
+        lambda: TRUE,
+        lambda: won,
+    )
+    won = jax.lax.cond(
+        jnp.all(board[jnp.int8([2, 4, 6])] == turn),
+        lambda: TRUE,
+        lambda: won,
+    )
+    return won
+
+
+def observe(state: State) -> jnp.ndarray:
+    return jnp.int8(0)
---
+++ packages/pgx/pgx-0.0.10.tar.gz/pgx-0.0.10/tests/test_tic_tac_toe.py
@@ -0,0 +1,193 @@
+import jax
+import jax.numpy as jnp
+
+from pgx.tic_tac_toe import init, step, _win_check
+
+init = jax.jit(init)
+step = jax.jit(step)
+
+
+def test_init():
+    rng = jax.random.PRNGKey(0)
+    curr_player, state = init(rng=rng)
+    assert curr_player == 1
+
+
+def test_step():
+    rng = jax.random.PRNGKey(0)
+    curr_player, state = init(rng=rng)
+    assert curr_player == 1
+    assert state.turn == 0
+    assert jnp.all(state.legal_action_mask == jnp.array([1, 1, 1, 1, 1, 1, 1, 1, 1], jnp.bool_))  # fmt: ignore
+    assert jnp.all(state.board == jnp.int8([-1, -1, -1, -1, -1, -1, -1, -1, -1]))
+    assert not state.terminated
+    # -1 -1 -1
+    # -1 -1 -1
+    # -1 -1 -1
+
+    action = jnp.int8(4)
+    curr_player, state, rewards = step(state, action)
+    assert curr_player == 0
+    assert state.turn == 1
+    assert jnp.all(state.legal_action_mask == jnp.array([1, 1, 1, 1, 0, 1, 1, 1, 1], jnp.bool_))  # fmt: ignore
+    assert jnp.all(state.board == jnp.int8([-1, -1, -1, -1, 0, -1, -1, -1, -1]))
+    assert jnp.all(rewards == 0)  # fmt: ignore
+    assert not state.terminated
+    # -1 -1 -1
+    # -1  0 -1
+    # -1 -1 -1
+
+    action = jnp.int8(0)
+    curr_player, state, rewards = step(state, action)
+    assert curr_player == 1
+    assert state.turn == 0
+    assert jnp.all(state.legal_action_mask == jnp.array([0, 1, 1, 1, 0, 1, 1, 1, 1], jnp.bool_))  # fmt: ignore
+    assert jnp.all(state.board == jnp.int8([1, -1, -1, -1, 0, -1, -1, -1, -1]))
+    assert jnp.all(rewards == 0)  # fmt: ignore
+    assert not state.terminated
+    #  1 -1 -1
+    # -1  0 -1
+    # -1 -1 -1
+
+    action = jnp.int8(1)
+    curr_player, state, rewards = step(state, action)
+    assert curr_player == 0
+    assert state.turn == 1
+    assert jnp.all(state.legal_action_mask == jnp.array([0, 0, 1, 1, 0, 1, 1, 1, 1], jnp.bool_))  # fmt: ignore
+    assert jnp.all(state.board == jnp.int8([1, 0, -1, -1, 0, -1, -1, -1, -1]))
+    assert jnp.all(rewards == 0)  # fmt: ignore
+    assert not state.terminated
+    #  1  0 -1
+    # -1  0 -1
+    # -1 -1 -1
+
+    action = jnp.int8(8)
+    curr_player, state, rewards = step(state, action)
+    assert curr_player == 1
+    assert state.turn == 0
+    assert jnp.all(state.legal_action_mask == jnp.array([0, 0, 1, 1, 0, 1, 1, 1, 0], jnp.bool_))  # fmt: ignore
+    assert jnp.all(state.board == jnp.int8([1, 0, -1, -1, 0, -1, -1, -1, 1]))
+    assert jnp.all(rewards == 0)  # fmt: ignore
+    assert not state.terminated
+    #  1  0 -1
+    # -1  0 -1
+    # -1 -1  1
+
+    action = jnp.int8(7)
+    curr_player, state, rewards = step(state, action)
+    assert curr_player == 0
+    assert state.turn == 1
+    assert jnp.all(state.legal_action_mask == jnp.array([0, 0, 1, 1, 0, 1, 1, 0, 0], jnp.bool_))  # fmt: ignore
+    assert jnp.all(state.board == jnp.int8([1, 0, -1, -1, 0, -1, -1, 0, 1]))
+    assert jnp.all(rewards == jnp.int16([-1, 1]))  # fmt: ignore
+    assert state.terminated
+    #  1  0 -1
+    # -1  0 -1
+    # -1  0  1
+
+
+def test_random_play():
+    N = 1000
+    key = jax.random.PRNGKey(0)
+    for i in range(N):
+        done = jnp.bool_(False)
+        key, sub_key = jax.random.split(key)
+        curr_player, state = init(sub_key)
+        rewards = jnp.int16([0., 0.])
+        while not done:
+            assert jnp.all(rewards == 0), state.board
+            legal_actions = jnp.where(state.legal_action_mask)[0]
+            key, sub_key = jax.random.split(key)
+            action = jax.random.choice(sub_key, legal_actions)
+            curr_player, state, rewards = step(state, action)
+            done = state.terminated
+
+
+def test_win_check():
+    board = jnp.int8(
+        [-1, -1, -1,
+         -1, -1, -1,
+         -1, -1, -1]
+    )
+    turn = jnp.int8(1)
+    assert not _win_check(board, turn)
+
+    board = jnp.int8(
+        [1, -1, -1,
+         -1, 1, -1,
+         0, -1, 0]
+    )
+    turn = jnp.int8(1)
+    assert not _win_check(board, turn)
+
+    board = jnp.int8(
+        [1, -1, -1,
+         -1, 1, -1,
+         -1, -1, 1]
+    )
+    turn = jnp.int8(1)
+    assert _win_check(board, turn)
+
+    board = jnp.int8(
+        [-1, -1, 1,
+         -1, 1, -1,
+         1, -1, -1]
+    )
+    turn = jnp.int8(1)
+    assert _win_check(board, turn)
+
+    board = jnp.int8(
+        [1, 1, 1,
+         -1, -1, -1,
+         -1, -1, -1]
+    )
+    turn = jnp.int8(1)
+    assert _win_check(board, turn)
+
+    board = jnp.int8(
+        [-1, -1, -1,
+          1, 1, 1,
+         -1, -1, -1]
+    )
+    turn = jnp.int8(1)
+    assert _win_check(board, turn)
+
+    board = jnp.int8(
+        [-1, -1, -1,
+         -1, -1, -1,
+         1, 1, 1]
+    )
+    turn = jnp.int8(1)
+    assert _win_check(board, turn)
+
+    board = jnp.int8(
+        [1, -1, -1,
+         1, -1, -1,
+         1, -1, -1]
+    )
+    turn = jnp.int8(1)
+    assert _win_check(board, turn)
+
+    board = jnp.int8(
+        [-1, 1, -1,
+         -1, 1, -1,
+         -1, 1, -1]
+    )
+    turn = jnp.int8(1)
+    assert _win_check(board, turn)
+
+    board = jnp.int8(
+        [-1, -1, 1,
+         -1, -1, 1,
+         -1, -1, 1]
+    )
+    turn = jnp.int8(1)
+    assert _win_check(board, turn)
+
+    board = jnp.int8(
+        [-1, 0, -1,
+         -1, 0, -1,
+         -1, 0, -1]
+    )
+    turn = jnp.int8(0)
+    assert _win_check(board, turn)
