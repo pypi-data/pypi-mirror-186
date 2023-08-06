f23c5e407bc34716a6f321f89516e0a9faee95e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:16 +0000

    Add package gwpopulation/0.9.0/gwpopulation-0.9.0-py3-none-any.whl

---
+++ packages/gwpopulation/gwpopulation-0.9.0-py3-none-any.whl/gwpopulation/__init__.py
@@ -0,0 +1,52 @@
+"""
+GWPopulation
+============
+
+A collection of code for doing population inference.
+
+All of this code will run on either CPUs or GPUs using cupy for GPU
+acceleration.
+
+This includes:
+  - commonly used likelihood functions in the Bilby framework.
+  - population models for gravitational-wave sources.
+  - selection functions for gravitational-wave sources.
+
+The code is hosted at `<www.github.com/ColmTalbot/gwpopulation>`_.
+"""
+from . import conversions, cupy_utils, hyperpe, models, utils, vt
+from .hyperpe import RateLikelihood
+
+try:
+    from ._version import version as __version__
+except ModuleNotFoundError:  # development mode
+    __version__ = "unknown"
+
+
+__all_with_xp = [
+    models.mass,
+    models.redshift,
+    models.spin,
+    cupy_utils,
+    hyperpe,
+    utils,
+    vt,
+]
+
+
+def disable_cupy():
+    import numpy as np
+
+    for module in __all_with_xp:
+        module.xp = np
+
+
+def enable_cupy():
+    try:
+        import cupy as cp
+    except ImportError:
+        import numpy as cp
+
+        print("Cannot import cupy, falling back to numpy.")
+    for module in __all_with_xp:
+        module.xp = cp
