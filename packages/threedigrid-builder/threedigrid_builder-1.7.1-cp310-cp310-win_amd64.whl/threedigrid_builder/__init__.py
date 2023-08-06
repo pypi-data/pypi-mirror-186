157c4b5f4be959df0ad9e6dfb4ac141c5159fad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:02 +0000

    Add package threedigrid-builder/1.7.1/threedigrid_builder-1.7.1-cp310-cp310-win_amd64.whl

---
+++ packages/threedigrid-builder/threedigrid_builder-1.7.1-cp310-cp310-win_amd64.whl/threedigrid_builder/__init__.py
@@ -0,0 +1,22 @@
+
+
+""""""# start delvewheel patch
+def _delvewheel_init_patch_1_2_0():
+    import os
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'threedigrid_builder.libs'))
+    is_pyinstaller = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
+    if not is_pyinstaller or os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_init_patch_1_2_0()
+del _delvewheel_init_patch_1_2_0
+# end delvewheel patch
+
+from .application import *  # NOQA
+from .exceptions import *  # NOQA
+
+# fmt: off
+__version__ = '1.7.1'
+# fmt: on
