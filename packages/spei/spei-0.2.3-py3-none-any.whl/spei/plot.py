c835cb4de1864cbe33ace71025a93aa6fd5db962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:44 +0000

    Add package spei/0.2.3/spei-0.2.3-py3-none-any.whl

---
+++ packages/spei/spei-0.2.3-py3-none-any.whl/spei/plot.py
@@ -0,0 +1,217 @@
+import matplotlib.pyplot as plt
+
+from itertools import cycle
+from calendar import month_name, month_abbr
+from pandas import Series
+from numpy import meshgrid, linspace, array, reshape
+from scipy.stats import gaussian_kde
+from .utils import check_series, dist_test
+
+# Type Hinting
+from typing import List, Optional
+from .typing import Axes, ContinuousDist
+
+
+def si(
+    si: Series, bound: float = 3.0, figsize: tuple = (8, 4), ax: Axes = None
+) -> Axes:
+    """Plot the standardized index values as a time series.
+
+    Parameters
+    ----------
+    si : pandas.Series
+        Series of the standardized index
+    bound : int, optional
+        Maximum and minimum ylim of plot
+    figsize : tuple, optional
+        Figure size, by default (8, 4)
+    ax : matplotlib.Axes, optional
+        Axes handle, by default None which create a new axes
+
+    Returns
+    -------
+    matplotlib.Axes
+        Axes handle
+    """
+    if ax is None:
+        _, ax = plt.subplots(figsize=figsize)
+
+    ax.plot(si, color="k", label="SGI")
+    ax.axhline(0, linestyle="--", color="k")
+
+    nmin = -bound
+    nmax = bound
+    droughts = si.to_numpy(copy=True)
+    droughts[droughts > 0] = 0
+    nodroughts = si.to_numpy(copy=True)
+    nodroughts[nodroughts < 0] = 0
+
+    x, y = meshgrid(si.index, linspace(nmin, nmax, 100))
+    ax.contourf(x, y, y, cmap=plt.cm.seismic_r, levels=linspace(nmin, nmax, 100))
+    ax.fill_between(x=si.index, y1=droughts, y2=nmin, color="w")
+    ax.fill_between(x=si.index, y1=nodroughts, y2=nmax, color="w")
+    ax.set_ylim(nmin, nmax)
+
+    return ax
+
+
+def dist(
+    series: Series,
+    dist: ContinuousDist,
+    cumulative: bool = False,
+    test_dist: bool = True,
+    cmap: Optional[str] = None,
+    figsize: tuple = (8, 10),
+    legend: bool = True,
+) -> Axes:
+    """Plot the (cumulative) histogram and scipy fitted distribution
+    for the time series on a monthly basis.
+
+    Parameters
+    ----------
+    series : pandas.Series
+        Time series of the precipitation (excess) or head
+    dist : scipy.stats._continuous_distns
+        Continuous distribution of the scipy stats library
+        to fit on series using maximum likelihood estimation
+    cumulative : bool, optional
+        If True plots cumulative histogram instead of probability
+        density histogram, by default False
+    test_dist : bool, optional
+        If True, fit the distribution with the two-sided
+        Kolmogorov-Smirnov test for goodness of fit.
+    cmap : str, optional
+        Matplotlib colormap name to use in subplots, by default None
+        which uses black for all subplots.
+    figsize : tuple, optional
+        Figure size, by default (8, 10)
+    legend : bool, optional
+        Add legend, by default True
+
+    Returns
+    -------
+    matplotlib.Axes
+        Axes handle
+    """
+
+    check_series(series)
+
+    _, axs = plt.subplots(4, 3, figsize=figsize, sharey=True, sharex=True)
+    ax = axs.ravel()
+    if cmap is not None:
+        cm = plt.get_cmap(cmap, 12)
+        c = [cm(i) for i in range(12)]
+    else:
+        c = ["k" for _ in range(12)]
+
+    for i, month in enumerate(range(1, 13)):
+        data = series[series.index.month == month].sort_values()
+        ax[i].hist(
+            data,
+            color=c[i],
+            alpha=0.2,
+            density=True,
+            cumulative=cumulative,
+            label="Density",
+        )
+        if test_dist:
+            _, p_value, _, *pars, loc, scale = dist_test(data, dist)
+            label = f"{dist.name.capitalize()} KS:\n{p_value=:0.2f}"
+        else:
+            *pars, loc, scale = dist.fit(data, scale=data.std())
+            label = f"{dist.name.capitalize()} fit:\n{loc=:0.1f}\n{scale=:0.1f}"
+        if cumulative:
+            cdf = dist.cdf(data, pars, loc=loc, scale=scale)
+            ax[i].plot(
+                data,
+                cdf,
+                color=c[i],
+                label=label,
+            )
+            if i in range(0, 12, 3):
+                ax[i].set_ylabel("Cumulative Probability")
+        else:
+            x = linspace(min(data), max(data))
+            pdf = dist.pdf(x, pars, loc=loc, scale=scale)
+            ax[i].plot(
+                x,
+                pdf,
+                color=c[i],
+                label=label,
+            )
+            if i in range(0, 12, 3):
+                ax[i].set_ylabel("Probability Density")
+        ax[i].set_title(month_name[month])
+        if legend:
+            ax[i].legend()
+
+    return axs
+
+
+def monthly_density(
+    si: Series,
+    years: Optional[List[int]] = None,
+    months: Optional[List[int]] = None,
+    cmap: str = "tab20c",
+    ax: Optional[Axes] = None,
+) -> Axes:
+    """Plot the monthly kernel-density estimate for a specific year.
+
+    Parameters
+    ----------
+    si : pandas.Series
+        Series of the standardized index
+    year : list, optional
+        List of years as int
+    months : list, optional
+        List of months as int, by default all months
+    cmap : str, optional
+        matlotlib colormap, by default 'tab10'
+    ax : matplotlib.Axes, optional
+        Axes handle, by default None which create a new axes
+
+    Returns
+    -------
+    matplotlib.Axes
+        Axes handle
+    """
+    if ax is None:
+        _, ax = plt.subplots(figsize=(6, 4))
+
+    if years is None:
+        years = []
+
+    if months is None:
+        months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+    cm = plt.get_cmap(cmap, 20)
+    colors = reshape(array([cm(x) for x in range(20)], dtype="f,f,f,f"), (5, 4))
+    lsts = cycle(["--", "-.", ":"])
+
+    ind = linspace(-3.3, 3.3, 1000)
+    for i, month in enumerate(months):
+        gkde_all = gaussian_kde(si[(si.index.month == month)])
+        ax.plot(
+            ind,
+            gkde_all.evaluate(ind),
+            c=colors[i, 0],
+            label=f"{month_abbr[month]} all",
+        )
+        for j, year in enumerate(years, start=1):
+            gkde_spec = gaussian_kde(
+                si[(si.index.month == month) & (si.index.year == year)]
+            )
+            ax.plot(
+                ind,
+                gkde_spec.evaluate(ind),
+                c=colors[i, j],
+                label=f"{month_abbr[month]} {year}",
+                linestyle=next(lsts),
+            )
+    ax.set_ylabel("Kernel-Density Estimate")
+    ax.set_xlim(ind[0], ind[-1])
+    ax.set_ylim(bottom=0)
+    ax.legend()
+    ax.grid(True)
+
+    return ax
---
+++ packages/spei/spei-0.2.3-py3-none-any.whl/spei/utils.py
@@ -0,0 +1,144 @@
+from pandas import Series, DataFrame
+from scipy.stats import (
+    norm,
+    gamma,
+    genextreme,
+    pearson3,
+    fisk,
+    lognorm,
+    logistic,
+    genlogistic,
+    kstest,
+)
+
+# Type Hinting
+from typing import List, Optional, Tuple
+from .typing import ArrayLike, ContinuousDist
+
+
+def check_series(series: Series) -> None:
+    """Check if provided time series is of type pandas.Series
+
+    Parameters
+    ----------
+    series : any
+        Time series of any kind
+
+    Raises
+    ------
+    TypeError
+        If series is not a pandas.Series
+
+    """
+    if not isinstance(series, Series):
+        if isinstance(series, DataFrame):
+            raise TypeError(
+                "Please convert pandas.DataFrame to a"
+                "pandas.Series using DataFrame.squeeze()"
+            )
+        else:
+            raise TypeError(f"Please provide a Pandas Series instead of {type(series)}")
+
+
+def dist_test(
+    data: Series, dist: List[ContinuousDist], N: int = 100, alpha: float = 0.05
+) -> Tuple[str, float, bool, ArrayLike]:
+    """Fit a distribution and perform the two-sided
+    Kolmogorov-Smirnov test for goodness of fit. The
+    null hypothesis is that the data and distributions
+    are identical, the alternative is that they are
+    not identical. [scipy_2021]_
+
+    Parameters
+    ----------
+    data : array_like
+        1-D array of observations of random variables
+    dist: scipy.stats.rv_continuous
+        Can be any continuous distribution from the
+        scipy.stats library.
+    N : int, optional
+        Sample size, by default 100
+    alpha : float, optional
+        Significance level for testing, default is 0.05
+        which is equal to a a confidence level of 95%;
+        that is, the null hypothesis will be rejected in
+        favor of the alternative if the p-value is
+        less than 0.05.
+
+    Returns
+    -------
+    string, float, bool, array_like
+        distribution name, p-value and fitted parameters
+
+    References
+    -------
+    .. [scipy_2021] Onnen, H.: Intro to Probability
+     Distributions and Distribution Fitting with Pythons
+    SciPy, 2021.
+    """
+    fitted = dist.fit(data, scale=data.std())
+    ks = kstest(data, dist.name, fitted, N=N)[1]
+    rej_h0 = ks < alpha
+    return dist.name, ks, rej_h0, *fitted
+
+
+def dists_test(
+    data: Series,
+    distributions: Optional[List[ContinuousDist]] = None,
+    N: int = 100,
+    alpha: float = 0.05,
+) -> DataFrame:
+    """Fit a list of distribution and perform the
+    two-sided Kolmogorov-Smirnov test for goodness
+    of fit. The null hypothesis is that the data and
+    distributions are identical, the alternative is
+    that they are not identical. [scipy_2021]_
+
+    Parameters
+    ----------
+    data : array_like
+        1-D array of observations of random variables
+    distributions : list of scipy.stats.rv_continuous, optional
+        A list of (can be) any continuous distribution from the scipy.stats
+        library, by default None which makes a custom selection
+    N : int, optional
+        Sample size, by default 100
+    alpha : float, optional
+        Significance level for testing, default is 0.05
+        which is equal to a a confidence level of 95%;
+        that is, the null hypothesis will be rejected in
+        favor of the alternative if the p-value is
+        less than 0.05.
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame with the distribution names,
+        pvalues and parameters
+
+    References
+    -------
+    .. [scipy_2021] Onnen, H.: Intro to Probability
+     Distributions and Distribution Fitting with Pythons
+    SciPy, 2021.
+    """
+    if distributions is None:
+        distributions = [
+            norm,
+            gamma,
+            genextreme,
+            pearson3,
+            fisk,
+            lognorm,
+            logistic,
+            genlogistic,
+        ]
+
+    df = DataFrame([dist_test(data, D, N, alpha) for D in distributions])
+    cols = ["Distribution", "KS p-value", "Reject H0"]
+    cols += [f"Param {i+1}" for i in range(df.columns.stop - len(cols))]
+    df.columns = cols
+    df = df.set_index(cols[0])
+    df["Dist"] = distributions
+
+    return df
