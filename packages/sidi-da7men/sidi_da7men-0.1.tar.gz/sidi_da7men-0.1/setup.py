15d684fb9923e0faffd58a3fe3cd9429a53b474b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:47 +0000

    Add package sidi-da7men/0.1/sidi_da7men-0.1.tar.gz

---
+++ packages/sidi-da7men/sidi_da7men-0.1.tar.gz/sidi_da7men-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name='sidi_da7men',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    author='Belajdel rabek ',
+    author_email='belajdelmedamine@gmail.com',
+    description='A package that generates bills'
+)
+
+
