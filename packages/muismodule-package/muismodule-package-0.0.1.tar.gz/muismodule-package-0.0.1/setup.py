b9efb896b1439d3a0f2801428c1570c6f6905f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:03 +0000

    Add package muismodule-package/0.0.1/muismodule-package-0.0.1.tar.gz

---
+++ packages/muismodule-package/muismodule-package-0.0.1.tar.gz/muismodule-package-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+  name="muismodule-package",
+  version="0.0.1",
+  author="muis",
+  author_email="exlkip@gmail.com",
+  description="Testing!",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/mui",
+  packages=find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',
+)
