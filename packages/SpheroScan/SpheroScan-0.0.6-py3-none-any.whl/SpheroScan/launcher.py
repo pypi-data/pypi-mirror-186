b3f25ab3f79b5fe097f23444a74bfd76bb383eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:00 +0000

    Add package SpheroScan/0.0.6/SpheroScan-0.0.6-py3-none-any.whl

---
+++ packages/SpheroScan/SpheroScan-0.0.6-py3-none-any.whl/SpheroScan/launcher.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 16 14:59:20 2023
+
+@author: akshay
+"""
+
+import subprocess
+import SpheroScan
+a=SpheroScan.__path__
+#a='/Users/akshay/OneDrive - Universitaet Bern/spheroid/webGUI'
+
+def launch(port="localhost:8000",workers="4"):
+    subprocess.run(["gunicorn","--chdir",a,"main:server","-b",port,"-w",workers])
+
+launch()
