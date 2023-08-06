d8d028ba411744a75348d4b4528cea0749e856f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:53 +0000

    Add package protmo/0.0.5/protmo-0.0.5.tar.gz

---
+++ packages/protmo/protmo-0.0.5.tar.gz/protmo-0.0.5/setup.py
@@ -0,0 +1,37 @@
+import pathlib
+from setuptools import setup, find_packages
+from distutils.core import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='protmo',
+    url='',
+    version='0.0.5',
+    author='Thomas Deniffel',
+    author_email='tdeniffel@gmail.com',
+    packages=find_packages(), # find_packages(),
+    license='',
+    install_requires=[
+        'grpcio-tools',
+        'pymongo',
+        'python-keycloak'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    description='',
+    long_description='', # README
+    long_description_content_type="text/markdown",
+    python_requires='>=3',
+    include_package_data=True,
+    entry_points={
+    }
+)
