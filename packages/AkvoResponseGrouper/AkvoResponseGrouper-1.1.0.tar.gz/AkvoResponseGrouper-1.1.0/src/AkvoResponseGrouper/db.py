6f6c484e80851d88343b4d23120b4a46dde5d370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:19 +0000

    Add package AkvoResponseGrouper/1.1.0/AkvoResponseGrouper-1.1.0.tar.gz

---
+++ packages/AkvoResponseGrouper/AkvoResponseGrouper-1.1.0.tar.gz/AkvoResponseGrouper-1.1.0/src/AkvoResponseGrouper/db.py
@@ -0,0 +1,22 @@
+from os import environ
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def get_db_url():
+    TESTING = environ.get("TESTING")
+    DATABASE_URL = environ["DATABASE_URL"]
+    DB_URL = f"{DATABASE_URL}_test" if TESTING else DATABASE_URL
+    return DB_URL
+
+
+engine = create_engine(get_db_url(), pool_size=10, max_overflow=20)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
