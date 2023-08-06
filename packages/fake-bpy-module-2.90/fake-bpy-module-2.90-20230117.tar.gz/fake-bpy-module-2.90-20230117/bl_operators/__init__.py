13ee2d6e379fd54b93f2435962d70e6a8e1d00aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:54 +0000

    Add package fake-bpy-module-2.90/20230117/fake-bpy-module-2.90-20230117.tar.gz

---
+++ packages/fake-bpy-module-2.90/fake-bpy-module-2.90-20230117.tar.gz/fake-bpy-module-2.90-20230117/bl_operators/__init__.py
@@ -0,0 +1,48 @@
+import sys
+import typing
+from . import mesh
+from . import presets
+from . import userpref
+from . import constraint
+from . import view3d
+from . import image
+from . import uvcalc_lightmap
+from . import file
+from . import sequencer
+from . import object_quick_effects
+from . import screen_play_rendered_anim
+from . import simulation
+from . import add_mesh_torus
+from . import uvcalc_smart_project
+from . import clip
+from . import uvcalc_follow_active
+from . import bmesh
+from . import console
+from . import freestyle
+from . import rigidbody
+from . import wm
+from . import object_randomize_transform
+from . import gpencil_mesh_bake
+from . import anim
+from . import object_align
+from . import node
+from . import vertexpaint_dirt
+from . import object
+
+GenericType = typing.TypeVar("GenericType")
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
