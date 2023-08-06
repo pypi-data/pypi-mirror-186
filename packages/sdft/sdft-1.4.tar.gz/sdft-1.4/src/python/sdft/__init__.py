21d6d8f327844cea09dcb09f1a630f8692ce6aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:56 +0000

    Add package sdft/1.4/sdft-1.4.tar.gz

---
+++ packages/sdft/sdft-1.4.tar.gz/sdft-1.4/src/python/sdft/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "1.4"
+
+from .sdft import SDFT
+from .stft import STFT
---
+++ packages/sdft/sdft-1.4.tar.gz/sdft-1.4/test/test.py
@@ -0,0 +1,57 @@
+import os, sys
+src = os.path.join(os.path.dirname(__file__), '..', 'src', 'python')
+sys.path.insert(0, src)
+
+import numpy as np
+
+from sdft import SDFT
+from wav import readwav, writewav
+
+
+def main():
+
+    if len(sys.argv) < 8:
+        exit(1)
+
+    dftsize = int(sys.argv[1])
+    hopsize = int(sys.argv[2])
+    window = sys.argv[3]
+    latency = float(sys.argv[4])
+    srcfile = sys.argv[5]
+    wavfile = sys.argv[6]
+    dftfile = sys.argv[7]
+
+    sdft = SDFT(dftsize, window, latency)
+
+    x, sr = readwav(srcfile)
+    size = x.size
+
+    print(f'PY\t{srcfile} {size} {sr}');
+    size = (x.size // hopsize) * hopsize
+
+    x = x[:size]
+    y = np.ndarray((0), float)
+    hops = np.split(x, size // hopsize)
+    dfts = np.ndarray((len(hops), dftsize), complex)
+
+    progress = 0
+
+    for hop, samples in enumerate(hops):
+
+        percent = (hop + 1) / len(hops)
+
+        if int(percent * 10) != progress:
+            progress = int(percent * 10)
+            print(f'{progress * 10}%')
+
+        tmp = sdft.sdft(samples)
+        dfts[hop] = tmp[0]
+        y = np.concatenate((y, sdft.isdft(tmp)))
+
+    writewav(wavfile, y, sr)
+    dfts.tofile(dftfile)
+
+
+if __name__ == '__main__':
+
+    main()
