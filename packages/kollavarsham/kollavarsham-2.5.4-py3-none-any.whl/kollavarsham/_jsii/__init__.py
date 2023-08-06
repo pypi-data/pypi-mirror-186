f55d05228708c4911e0426f544cef59f5c9b4c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:02 +0000

    Add package kollavarsham/2.5.4/kollavarsham-2.5.4-py3-none-any.whl

---
+++ packages/kollavarsham/kollavarsham-2.5.4-py3-none-any.whl/kollavarsham/_jsii/__init__.py
@@ -0,0 +1,21 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "kollavarsham", "2.5.4", __name__[0:-6], "kollavarsham@2.5.4.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
