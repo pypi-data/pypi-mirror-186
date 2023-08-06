f8e87cbeae2b55d77e6fd46bb6f813fc96a4a9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:49 +0000

    Add package Mensajitos-saidnahum/6.0/Mensajitos_saidnahum-6.0-py3-none-any.whl

---
+++ packages/Mensajitos-saidnahum/Mensajitos_saidnahum-6.0-py3-none-any.whl/paquete_mensajes/subpaquete_adios/modulo_despedidas.py
@@ -0,0 +1,6 @@
+def despedir():
+   print("Adios desde despedidas.despedir")
+
+class Despedida:
+   def __init__(self):
+      print("Adios desde Despedida.__init__()")
---
+++ packages/Mensajitos-saidnahum/Mensajitos_saidnahum-6.0-py3-none-any.whl/paquete_mensajes/subpaquete_hola/modulo_saludos.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def saludar(nombre):
+   print(f"Hola {nombre} desde saludos.saludar()")
+   
+def prueba():
+   print("Esto es una prueba de la nueva version 6.0")
+   
+def generar_array(numeros):
+   return np.arange(numeros)
+
+class Saludo:
+   def __init__(self):
+      print("Hola desde Saludo.__init__")
+
+# Para correr sólo en este script y no desde otro importado
+if __name__ == '__main__':
+   print(generar_array(5))
