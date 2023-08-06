496be655a8ee671639bc9a6e4fcaeeb90db18fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:40 +0000

    Add package math-kavin/0.0.1/math_kavin-0.0.1-py3-none-any.whl

---
+++ packages/math-kavin/math_kavin-0.0.1-py3-none-any.whl/math_kavin/basic_math.py
@@ -0,0 +1,16 @@
+def addition (a,b):
+    add=a+b
+    return add
+
+def subtraction (a,b):
+    sub=a-b
+    return sub
+
+def multiplication (a,b):
+    mult=a*b
+    return mult
+
+def division (a,b):
+    div=a/b
+    return div
+
