4befa80fc0806ba1e754b3667724859ad56fea64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:15 +0000

    Add package SRC-Endpoints/0.0.0.1.9/SRC_Endpoints-0.0.0.1.9.tar.gz

---
+++ packages/SRC-Endpoints/SRC_Endpoints-0.0.0.1.9.tar.gz/SRC_Endpoints-0.0.0.1.9/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.0.0.1.9'
+PACKAGE_NAME = 'SRC_Endpoints'
+AUTHOR = 'Samson Rock Capital'
+AUTHOR_EMAIL = 'david.woroniuk@samsonrock.com'
+
+
+LICENSE = 'MIT'
+DESCRIPTION = 'A higher level of abstraction around API Endpoints.'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'numpy',
+      'pandas',
+      'requests',
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
