0bfc58940af04128685da39a0fa7e0a106011127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:58 +0000

    Add package btclib/2023.1.17/btclib-2023.1.17.tar.gz

---
+++ packages/btclib/btclib-2023.1.17.tar.gz/btclib-2023.1.17/docs/source/btclib.ecc.rst
@@ -0,0 +1,77 @@
+btclib.ecc package
+==================
+
+Submodules
+----------
+
+btclib.ecc.bms module
+---------------------
+
+.. automodule:: btclib.ecc.bms
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+btclib.ecc.borromean module
+---------------------------
+
+.. automodule:: btclib.ecc.borromean
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+btclib.ecc.dh module
+--------------------
+
+.. automodule:: btclib.ecc.dh
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+btclib.ecc.dsa module
+---------------------
+
+.. automodule:: btclib.ecc.dsa
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+btclib.ecc.pedersen module
+--------------------------
+
+.. automodule:: btclib.ecc.pedersen
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+btclib.ecc.rfc6979_nonce module
+-------------------------
+
+.. automodule:: btclib.ecc.rfc6979_nonce
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+btclib.ecc.sign\_to\_contract module
+------------------------------------
+
+.. automodule:: btclib.ecc.sign_to_contract
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+btclib.ecc.ssa module
+---------------------
+
+.. automodule:: btclib.ecc.ssa
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: btclib.ecc
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ packages/btclib/btclib-2023.1.17.tar.gz/btclib-2023.1.17/docs/source/conf.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+# Copyright (C) The btclib developers
+#
+# This file is part of btclib. It is subject to the license terms in the
+# LICENSE file found in the top-level directory of this distribution.
+#
+# No part of btclib including this file, may be copied, modified, propagated,
+# or distributed except according to the terms contained in the LICENSE file.
+
+"""Configuration file for the Sphinx documentation builder.
+
+This file only contains a selection of the most common options. For a
+full list see the documentation: www.sphinx-doc.org/en/master/config
+"""
+
+# -- Path setup --------------------------------------------------------------
+from __future__ import annotations
+
+import sys
+from os import path
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, path.abspath("../../"))
+
+
+# -- Project information -----------------------------------------------------
+
+project = "btclib"
+project_copyright = "2017-2023 The btclib developers"
+author = "The btclib developers"
+
+# The full version, including alpha/beta/rc tags
+release = "2023.1.17"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ["sphinx.ext.autodoc", "m2r2"]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns: list[str] = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "alabaster"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path: list[str] = []
+
+master_doc = "index"
