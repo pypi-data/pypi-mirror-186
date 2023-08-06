c5b3b3ddf4a07c493b24ea789af48f0fdb7499dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:07 +0000

    Add package bennycloth/0.0.7/bennycloth-0.0.7.tar.gz

---
+++ packages/bennycloth/bennycloth-0.0.7.tar.gz/bennycloth-0.0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.7'
+DESCRIPTION = 'A collection of Python functions for structural engineering'
+LONG_DESCRIPTION = 'This package contains random utilitly functions accrued over many years of structural engineering research and teaching'
+
+setup(
+	name = 'bennycloth',
+	version = VERSION,
+	author = 'Michael H. Scott',
+	author_email = 'mhscott@oregonstate.edu',
+	description = DESCRIPTION,
+	long_description = LONG_DESCRIPTION,
+	packages = find_packages(),
+	install_requires = [],
+	keywords = ['python','opensees'],
+	classifiers = ['Programming Language :: Python :: 3',
+		'Operating System :: OS Independent'
+	],
+	python_requires = '>=3.6'
+)
