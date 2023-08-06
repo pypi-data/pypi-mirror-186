c41c78824c942e7d730d8cc83724b6b16516568e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:34 +0000

    Add package zaml/0.0.6/zaml-0.0.6.tar.gz

---
+++ packages/zaml/zaml-0.0.6.tar.gz/zaml-0.0.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension
+from pathlib import Path
+
+from builder import ZigBuilder
+
+zaml = Extension("zaml", sources=["zamlmodule.zig"])
+
+setup(
+    name="zaml",
+    version="0.0.6",
+    description="Fast YAML 1.2 Parser for Python 3.6+",
+    ext_modules=[zaml],
+    cmdclass={"build_ext": ZigBuilder},
+    long_description=(Path(__file__).parent / "README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+)
