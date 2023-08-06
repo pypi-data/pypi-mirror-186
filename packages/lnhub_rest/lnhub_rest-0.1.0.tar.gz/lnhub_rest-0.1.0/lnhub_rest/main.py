6fd5f34d9af06f366e7f2304feba192f068e3db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:23 +0000

    Add package lnhub_rest/0.1.0/lnhub_rest-0.1.0.tar.gz

---
+++ packages/lnhub_rest/lnhub_rest-0.1.0.tar.gz/lnhub_rest-0.1.0/lnhub_rest/main.py
@@ -0,0 +1,28 @@
+import os
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from lnhub_rest.routers import user
+
+root_path = "/" + os.getenv("ROOT_PATH", "")
+port = os.getenv("PORT", 8000)
+
+
+app = FastAPI(openapi_prefix=root_path)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(user.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=port, root_path=root_path)
