a8c48173274263c667aa3f3b0da272e9e95eaa04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:59 +0000

    Add package secenv/0.1.1/secenv-0.1.1-py3-none-any.whl

---
+++ packages/secenv/secenv-0.1.1-py3-none-any.whl/secenv/stores/env.py
@@ -0,0 +1,14 @@
+import hvac
+import os
+from . import EnvPrefix, StoreInterface
+
+
+class Store(StoreInterface):
+    def __init__(self, name, infos):
+        pass
+
+    def gen_parser(self, parser):
+        parser.add_argument("secret")
+
+    def read_secret(self, secret):
+        return os.getenv(secret)
