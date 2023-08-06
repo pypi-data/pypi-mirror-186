d8d964d0b965ac0686cd4ab13a0969208d1604f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:12 +0000

    Add package rmost/1.0/rmost-1.0.tar.gz

---
+++ packages/rmost/rmost-1.0.tar.gz/rmost-1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='rmost',
+	version='1.0',
+	author='YarKiy',
+	author_email='rosya6721@gmail.com',
+	description='NO',
+	packages=['rmost'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
