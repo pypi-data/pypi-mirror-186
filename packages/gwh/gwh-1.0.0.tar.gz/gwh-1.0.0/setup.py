cc023c5400e89277780257f0d073afa35137ddbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:12 +0000

    Add package gwh/1.0.0/gwh-1.0.0.tar.gz

---
+++ packages/gwh/gwh-1.0.0.tar.gz/gwh-1.0.0/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+setup(
+    name="gwh",
+    version="1.0.0",
+    author='Andy Hebrank',
+    author_email='ahebrank@gmail.com',
+    packages=['gwh'],
+    url='https://github.com/ahebrank/gitlab-webhook-handler',
+    description='Webhook Handler for GitLab',
+    license='Apache License, Version 2.0',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'Flask>=1.0',
+        'requests>=2.19.0'
+    ]
+)
