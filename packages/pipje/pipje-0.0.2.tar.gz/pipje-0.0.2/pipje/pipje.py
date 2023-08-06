e56b0d99e61b3e38484f9dadc8418b6c9322e419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:22:34 +0000

    Add package pipje/0.0.2/pipje-0.0.2.tar.gz

---
+++ packages/pipje/pipje-0.0.2.tar.gz/pipje-0.0.2/pipje/pipje.py
@@ -0,0 +1,15 @@
+from PIL import Image, ImageTk
+from urllib.request import urlopen
+import tkinter as tk
+def pipje():
+    root = tk.Tk()
+    root.title("Pip")
+    URL = "https://i.imgur.com/1sfOxTw_d.webp?maxwidth=760&fidelity=grand"
+    u = urlopen(URL)
+    raw_data = u.read()
+    u.close()
+    photo = ImageTk.PhotoImage(data=raw_data) # <-----
+    label = tk.Label(image=photo)
+    label.image = photo
+    label.pack()
+    root.mainloop()
