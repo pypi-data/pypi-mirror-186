7f951197f740393f3464f9d0367dfc9d96b8d217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:32 +0000

    Add package econometrics-tests-statistics/1.3/econometrics_tests_statistics-1.3.tar.gz

---
+++ packages/econometrics-tests-statistics/econometrics_tests_statistics-1.3.tar.gz/econometrics_tests_statistics-1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(name='econometrics_tests_statistics',
+      version='1.3',
+      description='Numerical methods',
+      packages=find_packages(),
+      author_email='vovalagutov1111@yandex.ru',
+      install_requires = ['numpy'],
+      zip_safe=False,
+      )
