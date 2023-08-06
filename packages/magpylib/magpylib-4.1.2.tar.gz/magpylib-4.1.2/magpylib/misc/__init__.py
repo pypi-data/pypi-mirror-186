fec776a4435d466398f8fb7bc2ef6979f741d806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:19 +0000

    Add package magpylib/4.1.2/magpylib-4.1.2.tar.gz

---
+++ packages/magpylib/magpylib-4.1.2.tar.gz/magpylib-4.1.2/magpylib/misc/__init__.py
@@ -0,0 +1,8 @@
+"""
+The `magpylib.misc` sub-package contains miscellaneous source objects.
+"""
+
+__all__ = ["Dipole", "CustomSource"]
+
+from magpylib._src.obj_classes.class_misc_Dipole import Dipole
+from magpylib._src.obj_classes.class_misc_CustomSource import CustomSource
