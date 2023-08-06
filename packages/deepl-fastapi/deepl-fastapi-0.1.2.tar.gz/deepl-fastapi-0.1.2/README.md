5ec80de74967a37034ce09d627b8b9c8270db46a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:00 +0000

    Add package deepl-fastapi/0.1.2/deepl-fastapi-0.1.2.tar.gz

---
+++ packages/deepl-fastapi/deepl-fastapi-0.1.2.tar.gz/deepl-fastapi-0.1.2/README.md
@@ -0,0 +1,105 @@
+# deepl-fastapi
+<!--- repo-name  pypi-name  mod_name func_name --->
+[![tests](https://github.com/ffreemt/deepl-fastapi/actions/workflows/routine-tests.yml/badge.svg)][![python](https://img.shields.io/static/v1?label=python+&message=3.7%2B&color=blue)](https://img.shields.io/static/v1?label=python+&message=3.7%2B&color=blue)[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)[![PyPI version](https://badge.fury.io/py/deepl-fastapi.svg)](https://badge.fury.io/py/deepl-fastapi)
+
+Your own deepl server via fastapi, cross-platform (Windows/Linux/MacOs) with API for OmegaT
+
+## Installation
+*   Create a virual environment: optional but recommended
+    e.g.,
+    ```bash
+    # Linux and friends
+    python3.7 -m venv .venv
+    source .venv/bin/activate
+
+    # Windows
+    # py -3.7 -m venv .venv
+    # .venv\Scripts\activate
+    ```
+
+```bash
+pip install deepl-fastapi
+```
+or (if your use poetry)
+```bash
+poetry add deepl-fastapi
+```
+or
+```
+ pip install git+https://github.com/ffreemt/deepl-fastapi.git
+```
+or
+*   Clone the repo [https://github.com/ffreemt/deepl-fastapi.git](https://github.com/ffreemt/deepl-fastapi.git)
+    ```bash
+    git clone https://github.com/ffreemt/deepl-fastapi.git
+    ```
+    and `cd deepl-fastapi`
+*   `pip install -r requirements.txt
+    * or ``poetry install``
+
+## Usage
+
+*   Start the server
+
+Use uvicorn directly (note the `deepl_server` module, not `run_uvicorn`)
+```bash
+uvicorn deepl_fastapi.deepl_server:app
+```
+
+or
+```bash
+deepl-fastapi
+# this option is available only if installed via pip install or poetry add
+```
+
+or
+```bash
+python3.7 -m deepl_fastapi.run_uvicorn
+```
+
+or run the server on the external net, for example at port 9888
+```
+uvicorn deepl_fastapi.deepl_server:app --reload --host 0.0.0.0 --port 9888
+```
+
+*   Explore and consume
+
+Point your browser to [http://127.0.0.1:8000/text/?q=test&to_lang=zh](http://127.0.0.1:8000/text/?q=test&to_lang=zh)
+
+Or in python code (`pip install requests` first)
+```python
+import requests
+
+# get
+url =  "http://127.0.0.1:8000/text/?q=test me&to_lang=zh"
+print(requests.get(url).json())
+# {'q': 'test me', 'from_lang': None, 'to_lang': 'zh',
+# 'trtext': '考我 试探我 测试我 试探'}
+
+# post
+text = "test this and that"
+data = {"text": text, "to_lang": "zh"}
+resp = requests.post("http://127.0.0.1:8000/text", json=data)
+print(resp.json())
+# {'q': {'text': 'test this and that', 'from_lang': None, 'to_lang': 'zh', 'description': None},
+# 'result': '试探 左右逢源 检验 审时度势'}
+
+```
+
+## Interactive Docs (Swagger UI)
+
+ [http://127.0.0.1:8000/docs](http://127.0.0.1:8000/docs)
+
+## OmegaT ``Fake MT`` plugin setup
+* Copy ``omegat-plugin-fake-mt-1.0.0.jar`` （available at https://github.com/briacp/omegat-plugin-fake-mt） to OmegaT\plugins （e.g., C:\Program Files\OmegaT\plugins） 
+
+Run``OmegaT``and setup ``omegat-plugin-fake-mt``
+```bash
+OmegaT/Preferences/Machine Translation/Fake MT/Configure
+	Name: Fake Deepl MT
+	URL: http://localhost:8000/text
+	Source Parameter: from_lang
+	Target Parameter: to_lang
+	Text Parameter: q
+```
+![](img/plugin-setup.png)
---
+++ packages/deepl-fastapi/deepl-fastapi-0.1.2.tar.gz/deepl-fastapi-0.1.2/deepl_fastapi/deepl_server.py
@@ -0,0 +1,255 @@
+"""Run fastapi server."""
+# pylint: disable=invalid-name, duplicate-code, no-name-in-module, broad-except
+# import nest_asyncio
+
+# import sys
+import asyncio
+import os
+from signal import SIG_DFL, SIGINT, signal
+from typing import Optional
+
+import nest_asyncio
+import portalocker
+import uvicorn
+from deepl_scraper_pp.deepl_tr import deepl_tr
+from fastapi import FastAPI, Query
+from get_ppbrowser.get_ppbrowser import get_ppbrowser
+
+# import logzero
+from logzero import logger
+from pydantic import BaseModel
+
+from deepl_fastapi import __version__
+
+# lazy loading LOOP, wait for run_uvicorn to start first
+# import lazy_import
+# get_ppbrowser = lazy_import.lazy_module(get_ppbrowser)
+
+
+nest_asyncio.apply()
+port = 8001
+
+
+async def get_page():
+    """Get page."""
+    try:
+        browser = await get_ppbrowser()
+    except Exception as exc:
+        logger.error(exc)
+        raise
+    try:
+        page = await browser.newPage()
+    except Exception as exc:
+        logger.error(exc)
+        raise
+
+    url = r"https://www.deepl.com/translator"
+    try:
+        await page.goto(url, timeout=16 * 1000)
+    except Exception as exc:
+        logger.error(exc)
+        raise
+
+    return page
+
+
+try:
+    LOOP = asyncio.get_event_loop()
+    # LOOP = asyncio.get_running_loop()
+except Exception as exc_:
+    logger.error("weird: %s", exc_)
+    raise SystemExit(1) from exc_
+
+try:
+    PAGE = LOOP.run_until_complete(get_page())
+except Exception as exc_:
+    logger.error(exc_)
+    raise SystemExit("Unable to connect to deepl.com") from exc_
+
+
+class Text(BaseModel):  # pylint: disable=too-few-public-methods
+    """Define Text model."""
+
+    text: str
+    from_lang: Optional[str] = None
+    to_lang: Optional[str] = None
+    description: Optional[str] = None
+
+
+app = FastAPI(
+    title="deepl-fastapi",
+    version=__version__,
+)
+
+
+@app.post("/text/")
+async def post_text(q: Text):
+    """Post q."""
+    text = q.text
+    to_lang = q.to_lang
+    from_lang = q.from_lang
+    logger.debug("text: %s", text)
+
+    # _ = sent_corr(text1, text2)
+    try:
+        _ = await deepl_tr(
+            text,
+            from_lang,
+            to_lang,
+            page=PAGE,
+        )
+    except Exception as exc:
+        logger.error(exc)
+        _ = {"error": True, "message": str(exc)}
+
+    return {"q": q, "result": _}
+
+
+@app.get("/text/")
+async def get_text(
+    q: Optional[str] = Query(
+        None,
+        max_length=1500,
+        min_length=1,
+        title="text to translate",
+        description=(
+            "max. 5000 chars, paragraphs will not be preserved."
+            "multiple translations may be provided for short phrases."
+        ),
+    ),
+    from_lang: Optional[str] = None,
+    to_lang: Optional[str] = "zh",
+):
+    """Get text.
+
+    http://127.0.0.1:8000/text/?q=abc&to_lang=zh
+    """
+    result = {
+        "q": q,
+        "from_lang": from_lang,
+        "to_lang": to_lang,
+    }
+    try:
+        trtext = await deepl_tr(
+            q,
+            from_lang,
+            to_lang,
+            page=PAGE,
+        )
+    except Exception as exc:
+        logger.error(exc)
+        trtext = str(exc)
+
+    result.update({"trtext": trtext})
+    result.update({"translation": trtext})
+
+    logger.debug("result: %s", result)
+
+    return result
+
+
+def run_uvicorn():
+    """Run uvicor.
+
+    Must be run from a different file, e.g., run_uvicorn.py
+    """
+    uvicorn.run(
+        # app="deepl_fastapi.deepl_server:app",
+        app=app,  # this should work with python -m deepl_fastapi.deepl_server
+        # still "attached to a different loop" error
+        host="0.0.0.0",
+        # port=8000,
+        port=port,
+        # debug=True,
+        # reload=True,
+        # workers=2,
+        # loop="asyncio",  # default "auto"
+        # loop="uvloop",  # posix (linux and mac) only
+    )
+
+
+async def main():
+    """Start run_uvicorn in the current our own LOOP.
+
+    LOOP was used in deepl_scraper_pp.deepl_tr
+    """
+    # from get_ppbrowser.get_ppbrowser import LOOP
+
+    # loop = asyncio.new_event_loop()
+    # loop = asyncio.get_event_loop()
+
+    # loop.run_in_executor(None, run_uvicorn)  # current loop
+
+    future = LOOP.run_in_executor(None, run_uvicorn)
+
+    # future = loop.run_in_executor(None, run_uvicorn)  # future from different loop
+
+    # errors, crashed
+    # future = LOOP.run_in_executor(LOOP, run_uvicorn)
+    # future = loop.run_in_executor(LOOP, run_uvicorn)
+
+    await future  # wrong?
+
+    _ = """
+    try:
+        await future
+    except Exception as exc:
+        logger.error(exc)
+        raise SystemExit(1) from exc
+    # """
+
+
+if __name__ == "__main__":
+    logger.info("pid: %s", os.getpid())
+
+    signal(SIGINT, SIG_DFL)
+    print("ctrl-C to interrupt")
+
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
+    # uvicorn.run("app.app:app",host='0.0.0.0', port=4557, reload=True, debug=True, workers=3)
+
+    # uvicorn deepl_fastapi.deepl_server:app --reload
+    # works with nest_asyncio
+
+    # loop = asyncio.new_event_loop()
+    # loop.create_task(main())
+    # loop.run_forever()
+
+    _ = """
+    try:
+        LOOP.run_until_complete(main())
+    except Exception as exc:
+        logger.error(exc)
+    finally:
+        LOOP.close()
+    # """
+
+    _ = """
+    try:
+        LOOP.create_task(main())
+        LOOP.run_forever()
+    except Exception as exc:
+        logger.error(exc)
+    finally:
+        LOOP.close()
+    # """
+    # only run one instance
+    # if not Path(f"{__file__}.portalocker.lock").exists(): sp.Popen("touch
+
+    try:
+        # portalocker.lock(file, portalocker.constants.LOCK_EX)
+        with open(f"{__file__}.portalocker.lock", "r+", encoding="utf8") as fha:
+            portalocker.lock(fha, portalocker.LOCK_EX | portalocker.LOCK_NB)
+    except Exception as exc_:
+        logger.debug(exc_)
+        logger.error("Another copy is running, exiting...")
+        raise SystemExit(1) from exc_
+        # raise
+    finally:
+        # LOOP.close()
+        ...
+
+    logger.debug(
+        "starting uvicorn at port %s, accessible 127.0.0.1:%s/text/q=test", port, port
+    )
+    run_uvicorn()
