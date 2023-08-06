9cb7a10d9557fca1c3a0e7d5b041ae0c4f10297b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:37 +0000

    Add package minerva-cargo/1.0/minerva_cargo-1.0-py3-none-any.whl

---
+++ packages/minerva-cargo/minerva_cargo-1.0-py3-none-any.whl/cargo/__init__.py
@@ -0,0 +1,12 @@
+"""
+CARGO
+
+A data-centric transformation of monoliths into microservices
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Rahul Krishna <imralk+oss@gmail.com> and Vikram Nitin <vikramnitin9@gmail.com>'
+__credits__ = 'IBM Research :: Columbia University'
+
+from .cargo import Cargo
+__all__ = ['Cargo']
