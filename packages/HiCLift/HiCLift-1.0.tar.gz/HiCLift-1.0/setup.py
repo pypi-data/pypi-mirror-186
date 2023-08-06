1d70bc8eea1108ad1371f1cc02c9e699ef2acf77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:47 +0000

    Add package HiCLift/1.0/HiCLift-1.0.tar.gz

---
+++ packages/HiCLift/HiCLift-1.0.tar.gz/HiCLift-1.0/setup.py
@@ -0,0 +1,50 @@
+# Author: Tarik Salameh
+
+"""
+Setup script for HiCLift.
+
+This is a free software under GPLv3. Therefore, you can modify, redistribute
+or even mix it with other GPL-compatible codes. See the file LICENSE
+included with the distribution for more details.
+
+"""
+import os, sys, HiCLift, glob
+import setuptools
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+if (sys.version_info.major!=3) or (sys.version_info.minor<6):
+    print('PYTHON 3.6+ IS REQUIRED. YOU ARE CURRENTLY USING PYTHON {}'.format(sys.version.split()[0]))
+    sys.exit(2)
+
+# Guarantee Unix Format
+for src in glob.glob('scripts/*'):
+    text = open(src, 'r').read().replace('\r\n', '\n')
+    open(src, 'w').write(text)
+
+setuptools.setup(
+    name = 'HiCLift',
+    version = HiCLift.__version__,
+    author = HiCLift.__author__,
+    author_email = 'wangxiaotao686@gmail.com',
+    url = 'https://github.com/XiaoTaoWang/HiCLift',
+    description = 'Convert genomic coordinates of contact pairs from one assembly to another.',
+    keywords = 'liftover pairs Hi-C 4DN',
+    packages = setuptools.find_packages(),
+    package_data = {
+        '': ['data/*']
+    },
+    scripts = glob.glob('scripts/*'),
+    long_description = read('README.rst'),
+    long_description_content_type='text/x-rst',
+    classifiers = [
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        ]
+    )
+
