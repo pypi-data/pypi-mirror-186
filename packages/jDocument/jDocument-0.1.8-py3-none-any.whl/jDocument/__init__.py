705a442a0a16c5bd6f8f5074022aa1cd079b0ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:47 +0000

    Add package jDocument/0.1.8/jDocument-0.1.8-py3-none-any.whl

---
+++ packages/jDocument/jDocument-0.1.8-py3-none-any.whl/jDocument/__init__.py
@@ -0,0 +1,11 @@
+""" jDocument
+
+    20/Ago/2020 - José Carlos Cordeiro
+
+    Modulos:
+        jDocument
+        json2table
+"""
+from .jDocument import jDocument
+from .jsjson import loads, dumps
+
