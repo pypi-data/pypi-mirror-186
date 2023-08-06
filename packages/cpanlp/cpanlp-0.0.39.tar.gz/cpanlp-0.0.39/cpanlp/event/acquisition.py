3b2f6f38eeb5259d661ac825945c8587f217bdb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:23 +0000

    Add package cpanlp/0.0.39/cpanlp-0.0.39.tar.gz

---
+++ packages/cpanlp/cpanlp-0.0.39.tar.gz/cpanlp-0.0.39/cpanlp/event/acquisition.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+class Acquisition:
+    def __init__(self, target_company, acquiring_company, price):
+        self.target_company = target_company
+        self.acquiring_company = acquiring_company
+        self.price = price
+        self.date = datetime.now()
+
+    def execute(self):
+        self.target_company.is_acquired = True
+        self.target_company.acquirer = self.acquiring_company
+        self.target_company.acquisition_price = self.price
+        
