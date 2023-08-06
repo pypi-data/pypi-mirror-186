c237b33f2160ce1b680a0d906bd2d74c8cbe3fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:20 +0000

    Add package lightning/1.9.0/lightning-1.9.0-py3-none-any.whl

---
+++ packages/lightning/lightning-1.9.0-py3-none-any.whl/lightning/app/utilities/log.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+from lightning.app.storage.path import _storage_root_dir
+
+
+def get_logfile(filename: str = "logs.log") -> Path:
+    log_dir = Path(_storage_root_dir(), "frontend")
+    log_dir.mkdir(parents=True, exist_ok=True)
+    log_file = log_dir / filename
+    return log_file
