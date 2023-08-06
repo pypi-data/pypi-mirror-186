49a1f3932b42d5ba3004b1f3f6d393be1eaf2a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:18 +0000

    Add package starlite-saqlalchemy/0.28.1/starlite_saqlalchemy-0.28.1-py3-none-any.whl

---
+++ packages/starlite-saqlalchemy/starlite_saqlalchemy-0.28.1-py3-none-any.whl/starlite_saqlalchemy/__init__.py
@@ -0,0 +1,69 @@
+"""
+starlite-saqlalchemy
+---
+
+An implementation of a `Starlite` application configuration plugin.
+
+Example:
+```python
+from starlite import Starlite, get
+
+from starlite_saqlalchemy import ConfigureApp
+
+
+@get("/example")
+def example_handler() -> dict:
+    return {"hello": "world"}
+
+
+app = Starlite(route_handlers=[example_handler], on_app_init=[ConfigureApp()])
+```
+"""
+# this is because pycharm wigs out when there is a module called `exceptions`:
+# noinspection PyCompatibility
+from . import (
+    cache,
+    compression,
+    db,
+    dependencies,
+    dto,
+    exceptions,
+    health,
+    http,
+    log,
+    openapi,
+    redis,
+    repository,
+    sentry,
+    service,
+    settings,
+    sqlalchemy_plugin,
+    type_encoders,
+    worker,
+)
+from .init_plugin import ConfigureApp, PluginConfig
+
+__all__ = [
+    "ConfigureApp",
+    "PluginConfig",
+    "cache",
+    "compression",
+    "db",
+    "dependencies",
+    "dto",
+    "exceptions",
+    "health",
+    "http",
+    "log",
+    "openapi",
+    "redis",
+    "repository",
+    "sentry",
+    "service",
+    "settings",
+    "sqlalchemy_plugin",
+    "type_encoders",
+    "worker",
+]
+
+__version__ = "0.28.1"
---
+++ packages/starlite-saqlalchemy/starlite_saqlalchemy-0.28.1-py3-none-any.whl/starlite_saqlalchemy/repository/__init__.py
@@ -0,0 +1,11 @@
+"""Abstraction over the data storage for the application."""
+from __future__ import annotations
+
+from . import abc, filters, sqlalchemy, types
+
+__all__ = [
+    "abc",
+    "filters",
+    "sqlalchemy",
+    "types",
+]
