ff46934ab135166a075b513cb2a7442c9967a47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:02 +0000

    Add package xj-enroll/1.0.84/xj_enroll-1.0.84.tar.gz

---
+++ packages/xj-enroll/xj_enroll-1.0.84.tar.gz/xj_enroll-1.0.84/xj_enroll/service_register.py
@@ -0,0 +1,31 @@
+# encoding: utf-8
+"""
+@project: djangoModel->service_register
+@author: 孙楷炎
+@Email: sky4834@163.com
+@synopsis: 对外开放服务调用注册白名单
+@created_time: 2023/1/12 14:29
+"""
+
+import xj_enroll
+from .service import enroll_services
+
+register_list = [
+    {
+        "service_name": "check_can_add",
+        "pointer": enroll_services.EnrollRecordServices.check_can_add
+    },
+    {
+        "service_name": "test",
+        "pointer": enroll_services.EnrollRecordServices.test
+    },
+]
+
+
+def register():
+    for i in register_list:
+        setattr(xj_enroll, i["service_name"], i["pointer"])
+
+
+if __name__ == '__main__':
+    register()
