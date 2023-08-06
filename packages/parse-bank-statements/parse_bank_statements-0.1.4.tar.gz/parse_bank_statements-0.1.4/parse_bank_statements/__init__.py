23c7447fff92534c10eb33633b8fbce3cbaeb071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:20 +0000

    Add package parse-bank-statements/0.1.4/parse_bank_statements-0.1.4.tar.gz

---
+++ packages/parse-bank-statements/parse_bank_statements-0.1.4.tar.gz/parse_bank_statements-0.1.4/parse_bank_statements/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""Parse bank statements
+"""
+
+__author__  = 'Giovanni Tardini'
+__version__ = '0.1.4'
+__date__    = '18.01.2023'
+
+import logging
+
+fmt = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s: %(message)s', '%Y-%m-%d %H:%M:%S')
+hnd = logging.StreamHandler()
+hnd.setFormatter(fmt)
+hnd.setLevel(level=logging.INFO)
+logger = logging.getLogger('parse-bank-init')
+logger.addHandler(hnd)
+logger.setLevel(logging.INFO)
+logger.propagate = False
+
+import encodings.utf_8
