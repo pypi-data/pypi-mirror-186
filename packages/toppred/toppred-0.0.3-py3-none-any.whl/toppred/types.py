ba99675fbcfe6944e49105f039322912ae8043ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:18:49 +0000

    Add package toppred/0.0.3/toppred-0.0.3-py3-none-any.whl

---
+++ packages/toppred/toppred-0.0.3-py3-none-any.whl/toppred/types.py
@@ -0,0 +1,16 @@
+# Imports
+import numpy as np
+from typing import List, Tuple, Union
+
+# Definition of https://numpy.org/doc/stable/reference/generated/numpy.asarray.html
+array_like_1d = Union[List, Tuple, np.ndarray]
+array_like_2d = Union[
+    List[array_like_1d],
+    Tuple[array_like_1d],
+    np.ndarray,
+]
+array_like = Union[
+    array_like_1d,
+    List["array_like"],
+    Tuple["array_like"],
+]
