e86a483db2450c6aaa6b3961601d2844d800e17d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:19:04 +0000

    Add package sunnyday-calcetto/1.0.0/sunnyday-calcetto-1.0.0.tar.gz

---
+++ packages/sunnyday-calcetto/sunnyday-calcetto-1.0.0.tar.gz/sunnyday-calcetto-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+setup(
+    name='sunnyday-calcetto',
+    packages=['sunnyday'],
+    version='1.0.0',
+    author='Denis Vreshtazi(helped by Ardit Sulce)',
+    author_email='vreshtazi21@gmail.com',
+    url='https://github.com/denisvreshtazi/Weather-Package',
+    description='Weather forecast data',
+    license='MIT',
+    keywords=['weather', 'forecast', 'openweather'],
+    install_rquires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development :: Build Tools',
+    ],
+
+)
+
---
+++ packages/sunnyday-calcetto/sunnyday-calcetto-1.0.0.tar.gz/sunnyday-calcetto-1.0.0/sunnyday/sunnyday.py
@@ -0,0 +1,57 @@
+import requests
+class Weather:
+
+    """
+        Create a WEather object, taking as an input an apikey, a city name or lan and let coordinates.
+
+        Package use example:
+        # Create a Weather object using the city name:
+        # The apikey below is not guaranteed to work.
+        # Get your own apikey from openweathermap.org.
+        # And wait a couple of hours for the apikey to get activated
+        # The apikey could be activated even within minutes
+
+        >>> weather = Weather(apikey='c4bcb6957g9e0b1ae584263c62fb5d3b', city='Vlore')
+
+        # Using latitude and longitude coordinates
+        >>> weather = Weather(apikey='c4bcb6957g9e0b1ae584263c62fb5d3b', lat=40, lon= -4)
+
+        # Get complete weather data for the next 12 hours
+        >>> weather.next_12h()
+
+        # Simplified data for the next 12h
+        >>> weather.next_12h_simplified()
+
+    """
+
+    def __init__(self, apikey, city=None, lat=None, lon=None):
+        self.apikey = apikey
+        self.city = city
+        if city:
+            url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={apikey}&units=metrics'
+            r = requests.get(url)
+            self.data = r.json()
+        elif lat and lon:
+            url = f'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apikey}&units=metrics'
+            r = requests.get(url)
+            self.data = r.json()
+        else:
+            raise TypeError("Provide either a city or lat and lon arguments")
+
+        if self.data['cod'] != "200":
+            raise ValueError(self.data["message"])
+
+    def next_12h(self):
+        """
+            Returns 3-hour data for the next 12 hours as a dict.
+        """
+        return self.data['list'][:4]
+
+    def next_12h_simplified(self):
+        """
+            Returns date, temperature, and sky condition every 3-hours for the next 12 hours as a touple of touples
+        """
+        simple_data = []
+        for dicty in self.data['list'][:4]:
+            simple_data.append((dicty['dt_txt'], dicty['main']['temp'], dicty['weather'][0]['description']))
+        return simple_data
