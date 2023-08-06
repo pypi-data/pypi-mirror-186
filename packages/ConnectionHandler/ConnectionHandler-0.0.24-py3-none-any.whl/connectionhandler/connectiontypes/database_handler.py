79d1add6f3273cb61a30a2982fdbb618127c7d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:52 +0000

    Add package ConnectionHandler/0.0.24/ConnectionHandler-0.0.24-py3-none-any.whl

---
+++ packages/ConnectionHandler/ConnectionHandler-0.0.24-py3-none-any.whl/connectionhandler/connectiontypes/database_handler.py
@@ -0,0 +1,21 @@
+from .postgres_handler import Postgres_Handler as pg_handler
+
+class Database_Handler:
+    def __init__(self, db_type, user_name, password, host, port,database):
+        self.db_type = db_type
+        self.__build_connection(user_name, password, host, port,database)
+
+    def __build_connection(self, user_name, password, host, port, database):
+        self.handler = pg_handler(user_name, password, host, port, database)
+    
+    def add_sql_props(self, columns, keywords, table_name):
+        self.handler.define_columns(columns)
+        self.handler.set_table_name(table_name)
+        self.handler.set_keywords(keywords)
+    
+    def search_records(self):
+        return self.handler.search_for_records()
+    
+    def insert_record(self, keywords):
+        return self.handler.insert_row(keywords)
+    
