89382bdb10dc2152dfae56ac00aae307352d4b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:05 +0000

    Add package segytools/0.1.0/segytools-0.1.0.tar.gz

---
+++ packages/segytools/segytools-0.1.0.tar.gz/segytools-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['segytools']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ibm2ieee>=1.2.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'segytools',
+    'version': '0.1.0',
+    'description': 'low level toolkit for manipulating and analyzing segy formatted data',
+    'long_description': '# segytools\n\n`segytools` is a simple python library that provides low level tools for analyzing and parsing segy formatted files. There is no `read_segy()` function per se because that assumes the data is properly formatted in which case there are other tools much better suited, e.g. [segyio](https://github.com/equinor/segyio). The tools here are designed to aid in the analysis of "non-standard" segy files and converting those files to a more standard form easily read by other third party software.\n\nCurrently only segy revision 2 file header and trace header formats are provided. However, custom header containers can be created by inheriting `SegyAbstractHeader`.\n\nOriginally, much of the work had been derived from another open-source segy package [segpy](https://github.com/sixty-north/segpy), but version `0.1.0` has been almost completely rewritten. IBM float conversion is done with [enthought/ibm2ieee](https://github.com/enthought/ibm2ieee).\n',
+    'author': 'anthonytorlucci',
+    'author_email': 'anthonytorlucci@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
