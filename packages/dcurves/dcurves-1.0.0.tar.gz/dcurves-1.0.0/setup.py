b8aff0d61b1232672a3d7465b30dceaf73fcd57a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:32 +0000

    Add package dcurves/1.0.0/dcurves-1.0.0.tar.gz

---
+++ packages/dcurves/dcurves-1.0.0.tar.gz/dcurves-1.0.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Python package for Andrew Vickers\' Decision Curve Analysis method to evaluate prediction models and diagnostic tests'
+
+# Setting up
+setup(
+    name="dcurves",
+    version=VERSION,
+    author="Shaun Porwal",
+    author_email="<shaun.porwal@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'beartype', 'typing',
+                      'pkg_resources', 'statsmodels', 'lifelines',
+                      'matplotlib', 'random', 'typing'],
+    keywords=['python', 'dcurves', 'decision', 'curve', 'analysis', 'MSK'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    include_package_data=True,
+    package_data={'': ['data/*.csv']}
+)
+
