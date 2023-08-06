2747392688491ae0bec8a98a502e3619fbf77699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:35 +0000

    Add package th2-data-services/2.0.0.dev3949340687/th2_data_services-2.0.0.dev3949340687.tar.gz

---
+++ packages/th2-data-services/th2_data_services-2.0.0.dev3949340687.tar.gz/th2_data_services-2.0.0.dev3949340687/th2_data_services/interfaces/command.py
@@ -0,0 +1,53 @@
+#  Copyright 2022 Exactpro (Exactpro Systems Limited)
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Callable
+
+if TYPE_CHECKING:
+    from th2_data_services.interfaces.data_source import IDataSource
+
+
+class ICommand(ABC):
+    """High level interface for Command."""
+
+    @abstractmethod
+    def handle(self, data_source: IDataSource):
+        pass
+
+
+class IAdaptableCommand(ICommand):
+    def __init__(self):
+        """Class to make Command classes adaptable."""
+        self._workflow = []
+
+    def apply_adapter(self, adapter: Callable) -> "IAdaptableCommand":
+        """Adds adapter to the Command workflow.
+        
+        Note, sequence that you will add adapters make sense.
+        
+        Args:
+            adapter: Callable function that will be used as adapter.
+        Returns:
+            self
+        """
+        self._workflow.append(adapter)
+        return self
+
+    def _handle_adapters(self, data):
+        for step in self._workflow:
+            data = step(data)
+        return data
