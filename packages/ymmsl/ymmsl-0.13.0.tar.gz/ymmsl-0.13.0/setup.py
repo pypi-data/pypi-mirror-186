b92a6789bfb3278d71d14147625dd12356642bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:56 +0000

    Add package ymmsl/0.13.0/ymmsl-0.13.0.tar.gz

---
+++ packages/ymmsl/ymmsl-0.13.0.tar.gz/ymmsl-0.13.0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='ymmsl',
+    version='0.13.0',
+    description="Python bindings for the YAML version of the Multiscale Modeling and Simulation Language",
+    long_description=readme + '\n\n',
+    author="Lourens Veen",
+    author_email='l.veen@esciencecenter.nl',
+    url='https://github.com/multiscale/ymmsl-python',
+    packages=[
+        'ymmsl',
+    ],
+    package_dir={'ymmsl':
+                 'ymmsl'},
+    include_package_data=True,
+    license="Apache Software License 2.0",
+    zip_safe=False,
+    keywords='yMMSL multiscale modeling simulation YAML',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.7, <4',
+    test_suite='tests',
+    install_requires=[
+        'ruamel.yaml<=0.17.21,!=0.17.5',
+        'yatiml>=0.10.0,<0.11.0'
+    ],
+)
