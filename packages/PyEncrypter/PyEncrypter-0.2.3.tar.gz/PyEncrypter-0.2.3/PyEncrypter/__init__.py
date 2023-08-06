adcbe42773697d4f301075fc44a106eec7bc1ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:19 +0000

    Add package PyEncrypter/0.2.3/PyEncrypter-0.2.3.tar.gz

---
+++ packages/PyEncrypter/PyEncrypter-0.2.3.tar.gz/PyEncrypter-0.2.3/PyEncrypter/__init__.py
@@ -0,0 +1,1 @@
+from PyEncrypter.script import ObjInit
---
+++ packages/PyEncrypter/PyEncrypter-0.2.3.tar.gz/PyEncrypter-0.2.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyEncrypter",                     
+    version="0.2.3",                        
+    author="G8MR",
+    url = "https://github.com/G8MR/PyEncrypter/",
+    description="A simple yet secure encryption package.",
+    long_description=long_description,      
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      
+    python_requires='>=3.6',                
+    py_modules=["PyEncrypter"],             
+    install_requires=[]                     
+)
