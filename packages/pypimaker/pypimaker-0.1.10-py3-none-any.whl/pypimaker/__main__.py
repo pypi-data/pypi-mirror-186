cd0be9f3606384300df34f8a9a80dfa06a739228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:03 +0000

    Add package pypimaker/0.1.10/pypimaker-0.1.10-py3-none-any.whl

---
+++ packages/pypimaker/pypimaker-0.1.10-py3-none-any.whl/pypimaker/__main__.py
@@ -0,0 +1,66 @@
+import argparse
+import sys
+import pypimaker.files as files
+import pypimaker.reset as reset
+import pypimaker.upload as upload
+from pypimaker.ui import navigation
+from pypimaker.ui.OptionSelector import OptionSelector
+
+INVALID_ARGUMENT_TEXT = "Invalid argument given. Must be 'generate', 'upload', 'reset', \
+'fiximports', 'unfiximports', or 'help'."
+
+ARGS_DESCRIPTION_TEXT = "Enter command 'generate', 'upload', 'reset', 'fiximports', \
+'unfiximports', or 'help'"
+
+HELP_TEXT = "\nPyPI Maker\n----------\n\n\
+To run PyPI Maker, enter 'pypimaker' followed by a valid command.\n\
+Some examples:\n\n\
+   pypimaker generate\n\
+   pypimaker upload\n\
+   pypimaker -r\
+\n\nThe possible commands (or their aliases) are the following:\n\n\
+   generate  (-g)\n\
+   upload    (-u)\n\
+   reset     (-r)\n\
+   help      (-h)\n\n\
+See the documentation on GitHub or PyPI for more information.\n"
+
+def main(argv=sys.argv):
+	parser = argparse.ArgumentParser(description="Example input: pypimaker generate")
+	parser.add_argument(
+		"command", 
+		type=str, 
+		help=ARGS_DESCRIPTION_TEXT, 
+		nargs=1
+	)
+	userOption = "help" if len(sys.argv) == 1 else sys.argv[1]
+	
+	if userOption in ["generate", "-g"]:
+		options = OptionSelector()
+		files.generate(
+			options.filepath,
+			options.projectName,
+			options.authorsArray,
+			options.emailsArray,
+			options.correspondingEmail,
+			options.githubUsername,
+			options.shortDescription,
+			options.classifier
+		)
+	
+	elif userOption in ["upload", "-u"]:
+		filepath = navigation.getDirectory()
+		upload.upload(filepath)
+	
+	elif userOption in ["reset", "-r"]:
+		filepath = navigation.getDirectory()
+		reset.reset(filepath)
+	
+	elif userOption in ["help", "-h"]:
+		print(HELP_TEXT)
+	
+	else:
+		print(HELP_TEXT)
+
+if __name__ == '__main__':
+	main()
