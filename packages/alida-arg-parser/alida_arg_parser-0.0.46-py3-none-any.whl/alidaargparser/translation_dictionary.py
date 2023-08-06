e74a91195c5c9282bc2179b62c8647674c2b579b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:29 +0000

    Add package alida-arg-parser/0.0.46/alida_arg_parser-0.0.46-py3-none-any.whl

---
+++ packages/alida-arg-parser/alida_arg_parser-0.0.46-py3-none-any.whl/alidaargparser/translation_dictionary.py
@@ -0,0 +1,6 @@
+from .type_utils import str2bool
+
+translation = {"type":{str:"STRING", int: "INT", float:"DOUBLE", str2bool:"BOOLEAN", None:"ANY"},
+                "column_types": {str:"STRING", None:"ANY", int:"NUMBER", float: "NUMBER"}
+                }
+
