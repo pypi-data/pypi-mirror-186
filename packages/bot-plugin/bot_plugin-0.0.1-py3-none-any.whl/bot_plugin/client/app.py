be16d0ac5892fcd31bc85674b41497539e049324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:27 +0000

    Add package bot-plugin/0.0.1/bot_plugin-0.0.1-py3-none-any.whl

---
+++ packages/bot-plugin/bot_plugin-0.0.1-py3-none-any.whl/bot_plugin/client/app.py
@@ -0,0 +1,14 @@
+import aiohttp as aiohttp
+
+from bot_plugin.consts.routes import Module
+from bot_plugin.domain.plugin_request import PluginRequest
+
+
+class AppClient:
+    def __init__(self, app_url: str):
+        self._app_url = app_url
+
+    async def plugin(self, request: PluginRequest):
+        async with aiohttp.ClientSession(self._app_url) as session:
+            async with session.post(Module.PLUGIN, json=request.to_json()) as response:
+                response.raise_for_status()
