8d68b1a9da8204a82ff8ff7e8f1685d605fa7eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:21 +0000

    Add package lightning/1.9.0/lightning-1.9.0.tar.gz

---
+++ packages/lightning/lightning-1.9.0.tar.gz/lightning-1.9.0/src/lightning/app/runners/runtime_type.py
@@ -0,0 +1,20 @@
+from enum import Enum
+from typing import Type, TYPE_CHECKING
+
+from lightning.app.runners import CloudRuntime, MultiProcessRuntime
+
+if TYPE_CHECKING:
+    from lightning.app.runners.runtime import Runtime
+
+
+class RuntimeType(Enum):
+    MULTIPROCESS = "multiprocess"
+    CLOUD = "cloud"
+
+    def get_runtime(self) -> Type["Runtime"]:
+        if self == RuntimeType.MULTIPROCESS:
+            return MultiProcessRuntime
+        elif self == RuntimeType.CLOUD:
+            return CloudRuntime
+        else:
+            raise ValueError("Unknown runtime type")
