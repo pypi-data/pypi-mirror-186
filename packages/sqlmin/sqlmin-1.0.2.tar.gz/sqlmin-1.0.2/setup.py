a4686ea93c8e82af977b797e0b3bcede46153def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:41 +0000

    Add package sqlmin/1.0.2/sqlmin-1.0.2.tar.gz

---
+++ packages/sqlmin/sqlmin-1.0.2.tar.gz/sqlmin-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name='sqlmin',
+    version='1.0.2',
+    description='SQL Minifier',
+    long_description="SQL Minifier",
+    author='Enrique Rodriguez',
+    author_email='rodriguez.enrique.pr@gmail.com',
+    url='https://github.com/enrique-rodriguez/sqlmin',
+    keywords = ['sql minifier', 'minify sql', 'sqlmin', 'minsql'],
+    entry_points={
+        'console_scripts': [
+            'sqlmin = sqlmin:main',
+        ]
+    }
+)
