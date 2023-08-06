d30a07d9740ec593b023a11bb0fa4fce21f2b1d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:36 +0000

    Add package stoobly-agent/0.19.2/stoobly_agent-0.19.2-py3-none-any.whl

---
+++ packages/stoobly-agent/stoobly_agent-0.19.2-py3-none-any.whl/stoobly_agent/app/models/response_model.py
@@ -0,0 +1,35 @@
+import pdb
+import requests
+
+from stoobly_agent.app.models.adapters.response_adapter_factory import ResponseAdapterFactory
+from stoobly_agent.app.settings import Settings
+from stoobly_agent.lib.logger import Logger
+
+class ResponseModel():
+
+  def __init__(self, settings: Settings):
+    self.settings = settings
+
+    if not settings.cli.features.remote:
+      self.as_local()
+    else:
+      self.as_remote()
+
+  def as_local(self):
+    self.adapter =  ResponseAdapterFactory(self.settings.remote).local_db()
+
+  def as_remote(self):
+    # raise('Not yet supported.')
+    pass
+
+  def mock(self, request_id: str) -> requests.Response:
+    try:
+      return self.adapter.mock(request_id)
+    except requests.exceptions.RequestException as e:
+      self.__handle_request_error(e)
+      return None
+
+  def __handle_request_error(self, e: requests.exceptions.RequestException):
+      response: requests.Response = e.response
+      if response:
+        Logger.instance().error(f"{response.status_code} {response.content}")
