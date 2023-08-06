2d94f5f6f6b8cd31e74cf8867960e5fd251c5750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:10 +0000

    Add package prospr/0.2a22/prospr-0.2a22.tar.gz

---
+++ packages/prospr/prospr-0.2a22.tar.gz/prospr-0.2a22/prospr/__init__.py
@@ -0,0 +1,16 @@
+from prospr_core import AminoAcid, Protein, depth_first, depth_first_bnb
+from .datasets import load_vanEck250, load_vanEck1000, load_vanEck_hratio
+from .visualize import plot_protein
+
+__version__ = "0.2a22"
+
+__all__ = [
+    "AminoAcid",
+    "Protein",
+    "depth_first",
+    "depth_first_bnb",
+    "load_vanEck250",
+    "load_vanEck1000",
+    "load_vanEck_hratio",
+    "plot_protein",
+]
