1eb91f9fc5ee65080d471e110e6e318eebf20898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:12 +0000

    Add package sketch-converter/0.1.1/sketch_converter-0.1.1-py3-none-any.whl

---
+++ packages/sketch-converter/sketch_converter-0.1.1-py3-none-any.whl/sketch_converter/cli.py
@@ -0,0 +1,41 @@
+import typer
+
+from sketch_converter import __copyright__, __version__
+
+from .sketch import CameraProcess, ImageProcess
+
+app = typer.Typer()
+
+
+@app.command()
+def version():
+    print(f"Sketch Converter - {__version__}")
+    print(__copyright__)
+
+
+@app.command()
+def video_capture(videosrc: int = 0, pgcolor: int = 1, pscolor: int = 0) -> None:
+    if videosrc >= 0:
+        if pgcolor == 1 and pscolor == 1:
+            print("Grayscale and Colorful Pencil Sketch can't use at the same time")
+            return
+        print(f"Video mode: {videosrc}")
+        CameraProcess(pgcolor, pscolor, videosrc)
+    return
+
+
+@app.command()
+def picture_capture(image_file: str, pgcolor: int, pscolor: int) -> None:
+    if image_file is not None or image_file != "":
+        if pgcolor == 1 and pscolor == 1:
+            print("Grayscale and Colorful Pencil Sketch can't use at the same time")
+            return
+        print(f"Picture mode: {image_file}")
+        files: list[str] = []
+        files.append(image_file)
+        ImageProcess(files, pgcolor, pscolor)
+    return
+
+
+if __name__ == "__main__":
+    app()
