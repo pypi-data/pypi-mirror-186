fba45a39718be12b4a6cdd9e8f45c20203a0a066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:08 +0000

    Add package flaim/0.0.3/flaim-0.0.3-py3-none-any.whl

---
+++ packages/flaim/flaim-0.0.3-py3-none-any.whl/flaim/models/__init__.py
@@ -0,0 +1,22 @@
+"""
+State-of-the-art pre-trained vision models.
+"""
+
+
+from . import (
+	cait,
+	convmixer,
+	convnext,
+	efficientnetv2,
+	gcvit,
+	hornet,
+	maxvit,
+	nest,
+	pvtv2,
+	resnet,
+	swin,
+	van,
+	vgg,
+	vit,
+	)
+from .factory import get_model, list_models
