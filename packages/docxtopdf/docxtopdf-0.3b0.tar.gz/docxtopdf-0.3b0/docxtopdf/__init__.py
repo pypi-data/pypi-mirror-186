76030768ec6ee1fe5052954b115182905ee313c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:34 +0000

    Add package docxtopdf/0.3b0/docxtopdf-0.3b0.tar.gz

---
+++ packages/docxtopdf/docxtopdf-0.3b0.tar.gz/docxtopdf-0.3b0/docxtopdf/__init__.py
@@ -0,0 +1,5 @@
+from docxtopdf.docxtopdf import convert
+
+__version__ = '0.3b'
+__author__ = 'MrPowerUp - github.com/MrPowerUp82'
+__license__ = 'MIT'
---
+++ packages/docxtopdf/docxtopdf-0.3b0.tar.gz/docxtopdf-0.3b0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name = 'docxtopdf',
+    version = '0.3b',
+    author = 'MrPowerUp',
+    author_email = 'gustavohenrique8282@hotmail.com',
+    packages = ['docxtopdf'],
+    description = 'CLI e Lib.',
+    long_description = 'CLI e Lib.',
+    url = 'https://github.com/MrPowerUp82/docxtopdf',
+    project_urls = {
+        'Código fonte': 'https://github.com/MrPowerUp82/docxtopdf',
+    },
+    license = 'MIT',
+    keywords = 'CLI Lib docxtopdf',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+    install_requires=[]
+)
