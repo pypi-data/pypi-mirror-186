dfd0a3a44e0f6489865b40090481f8a90eb5ba2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:32 +0000

    Add package testix/10.2.0/testix-10.2.0-py3-none-any.whl

---
+++ packages/testix/testix-10.2.0-py3-none-any.whl/testix/frequentlyused.py
@@ -0,0 +1,6 @@
+from testix.fake import Fake
+from testix.fakeobject import FakeObject
+from testix.argumentexpectations import *
+from testix.scenario import *
+from testix.hook import Hook
+from testix.DSL import *
