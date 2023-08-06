4eee8cba440918963bcefde8a01e89cb8948b132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:50 +0000

    Add package strime/1.0.3/strime-1.0.3-py3-none-any.whl

---
+++ packages/strime/strime-1.0.3-py3-none-any.whl/strime/__init__.py
@@ -0,0 +1,10 @@
+"""
+## Strime
+A simple string (time+unit) to seconds converter.
+
+---
+:copyright: Copyright 2023 (c) woidzero. All rights reserved.
+:license: MIT license.
+"""
+
+from .main import *
