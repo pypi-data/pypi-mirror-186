d17613cafd57d3ea0c67d930a660160e119df268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:17 +0000

    Add package ogr/0.42.0/ogr-0.42.0.tar.gz

---
+++ packages/ogr/ogr-0.42.0.tar.gz/ogr-0.42.0/ogr/__init__.py
@@ -0,0 +1,34 @@
+# Copyright Contributors to the Packit project.
+# SPDX-License-Identifier: MIT
+
+"""
+Module providing one api for multiple git services (github/gitlab/pagure)
+"""
+
+from importlib.metadata import PackageNotFoundError, distribution
+
+from ogr.factory import (
+    get_project,
+    get_service_class,
+    get_service_class_or_none,
+    get_instances_from_dict,
+)
+from ogr.services.github import GithubService
+from ogr.services.gitlab import GitlabService
+from ogr.services.pagure import PagureService
+
+try:
+    __version__ = distribution(__name__).version
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+__all__ = [
+    GithubService.__name__,
+    PagureService.__name__,
+    GitlabService.__name__,
+    get_project.__name__,
+    get_service_class.__name__,
+    get_service_class_or_none.__name__,
+    get_instances_from_dict.__name__,
+]
