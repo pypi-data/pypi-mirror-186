800fa69ccc717bd7893971f3d5daf2993d8f1769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:19 +0000

    Add package coding-chaska-package/0.1/coding_chaska_package-0.1.tar.gz

---
+++ packages/coding-chaska-package/coding_chaska_package-0.1.tar.gz/coding_chaska_package-0.1/coding_chaska_package/module1.py
@@ -0,0 +1,10 @@
+
+def foo():
+    print("foo function called")
+
+def koo():
+    """koo function does nothing. It is written for testing"""
+    print("Koo function called")
+
+def zoo(a):
+    return a**2 
---
+++ packages/coding-chaska-package/coding_chaska_package-0.1.tar.gz/coding_chaska_package-0.1/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='coding_chaska_package',
+    # url='https://github.com/jladan/package_demo',
+    author='Rohit Maurya',
+    author_email='codingchaska.info@gmail.com',
+    # Needed to actually package something
+    # Needed for dependencies
+    install_requires=[],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='An example of a python package from pre-existing code',
+)
