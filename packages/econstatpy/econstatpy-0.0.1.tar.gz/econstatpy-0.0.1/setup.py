635b728ef005ebb8384c8dcc78abdff5a60d298a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:23 +0000

    Add package econstatpy/0.0.1/econstatpy-0.0.1.tar.gz

---
+++ packages/econstatpy/econstatpy-0.0.1.tar.gz/econstatpy-0.0.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 18 18:07:05 2022
+@author:
+Kestler
+"""
+
+from setuptools import setup, find_packages
+long_description= '''Library for the 4 semester'''
+setup(name='econstatpy',
+      version='0.0.1',
+      packages=['econstatpy'],
+      license='MIT',
+      description='',
+      zip_safe=False,
+      include_package_data=True
+      )
