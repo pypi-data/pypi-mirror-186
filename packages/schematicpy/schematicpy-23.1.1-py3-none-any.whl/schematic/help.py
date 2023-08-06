1e13eb764bdf7eb30c591de82232ee2edb1c1e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:50 +0000

    Add package schematicpy/23.1.1/schematicpy-23.1.1-py3-none-any.whl

---
+++ packages/schematicpy/schematicpy-23.1.1-py3-none-any.whl/schematic/help.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+
+# `schematic manifest` related sub-commands description
+manifest_commands = {
+    "manifest": {
+        "config": (
+            "Specify the path to the `config.yml` using this option. This is a required argument."
+        ),
+        "get": {
+            "short_help": (
+                "Specify the path to the `config.yml` using this option. "
+                "This is a required argument."
+            ),
+            "title": (
+                "Specify the title of the manifest (or title prefix of multiple manifests) that "
+                "will be created at the end of the run. You can either explicitly pass the "
+                "title of the manifest here or provide it in the `config.yml` "
+                "file as a value for the `(manifest > title)` key."
+            ),
+            "data_type": (
+                "Specify the component(s) (data type) from the data model that is to be used "
+                "for generating the metadata manifest file. To make all available manifests enter 'all manifests'. "
+                "You can either explicitly pass the data type here or provide "
+                "it in the `config.yml` file as a value for the `(manifest > data_type)` key."
+            ),
+            "jsonld": (
+                "Specify the path to the JSON-LD data model (schema) using this option. You can either explicitly pass the "
+                "schema here or provide a value for the `(model > input > location)` key."
+            ),
+            "dataset_id": (
+                "Specify the synID of a dataset folder on Synapse. If there is an exisiting manifest already present "
+                "in that folder, then it will be pulled with the existing annotations for further annotation/modification. "
+            ),
+            "sheet_url": (
+                "This is a boolean flag. If flag is provided when command line utility is executed, result will be a link/URL "
+                "to the metadata manifest file. If not it will produce a pandas dataframe for the same."
+            ),
+            "output_csv": ("Path to where the CSV manifest template should be stored."),
+            "output_xlsx": ("Path to where the Excel manifest template should be stored."),
+            "use_annotations": (
+                "This is a boolean flag. If flag is provided when command line utility is executed, it will prepopulate template "
+                "with existing annotations from Synapse."
+            ),
+            "json_schema": (
+                "Specify the path to the JSON Validation Schema for this argument. "
+                "You can either explicitly pass the `.json` file here or provide it in the `config.yml` file "
+                "as a value for the `(model > input > validation_schema)` key."
+            ),
+            "alphabetize_valid_values": (
+                "Specify to alphabetize valid attribute values either ascending (a) or descending (d)." 
+                "Optional"
+            ),
+        },
+        "migrate": {
+            "short_help": (
+                "Specify the path to the `config.yml` using this option. "
+                "This is a required argument."
+            ),
+            "project_scope": (
+                "Specify a comma-separated list of projects where manifest entities will be migrated to tables."
+            ),
+            "archive_project": (
+                "Specify a single project where legacy manifest entities will be stored after migration to table."
+            ),
+            "return_entities": (
+                "This is a boolean flag. If flag is provided when command line utility is executed, "
+                "entities that have been transferred to an archive project will be returned to their original folders."
+            ),
+            "dry_run": (
+                "This is a boolean flag. If flag is provided when command line utility is executed, "
+                "a dry run will be performed. No manifests will be re-uploaded and no entities will be migrated, "
+                "but archival folders will still be created. "
+                "Migration information for testing purposes will be logged to the INFO level."
+            ),
+        },
+    }
+}
+
+
+# `schematic model` related sub-commands description
+model_commands = {
+    "model": {
+        "config": (
+            "Specify the path to the `config.yml` using this option. This is a required argument."
+        ),
+        "submit": {
+            "short_help": ("Validation (optional) and submission of manifest files."),
+            "manifest_path": (
+                "Specify the path to the metadata manifest file that you want to submit to a dataset on Synapse. "
+                "This is a required argument."
+            ),
+            "dataset_id": (
+                "Specify the synID of the dataset folder on Synapse to which you intend to submit "
+                "the metadata manifest file. This is a required argument."
+            ),
+            "validate_component": (
+                "The component or data type from the data model which you can use to validate the "
+                "data filled in your manifest template."
+            ),
+            "use_schema_label": (
+                "Store attributes using the schema label (--use_schema_label, default) or store attributes using the display label "
+                "(--use_display_label). Attribute display names in the schema must not only include characters that are "
+                "not accepted by Synapse. Annotation names may only contain: letters, numbers, '_' and '.'"
+            ),
+            "hide_blanks":(
+                "This is a boolean flag. If flag is provided when command line utility is executed, annotations with blank values will be hidden from a dataset's annotation list in Synaspe."
+                "If not, annotations with blank values will be displayed."
+            ),
+            "manifest_record_type":(
+                "Specify the way the manifest should be store as on Synapse. Options are 'entity', 'table' and "
+                "'both'. 'entity' will store the manifest as a csv and create Synapse files for each row in the manifest. "
+                "'table' will store the manifest as a table and a csv on Synapse. 'both' will do both of the options specified above. "
+                "Default value is 'table'."
+            ),      
+        },
+        "validate": {
+            "short_help": ("Validation of manifest files."),
+            "manifest_path": (
+                "Specify the path to the metadata manifest file that you want to submit to a dataset on Synapse. "
+                "This is a required argument."
+            ),
+            "data_type": (
+                "Specify the component (data type) from the data model that is to be used "
+                "for validating the metadata manifest file. You can either explicitly pass the data type here or provide "
+                "it in the `config.yml` file as a value for the `(manifest > data_type)` key."
+            ),
+            "json_schema": (
+                "Specify the path to the JSON Validation Schema for this argument. "
+                "You can either explicitly pass the `.json` file here or provide it in the `config.yml` file "
+                "as a value for the `(model > input > validation_schema)` key."
+            ),
+            "restrict_rules":(
+                "This is a boolean flag. If flag is provided when command line utility is executed, validation suite will only run with in-house validation rules, "
+                "and Great Expectations rules and suite will not be utilized."
+                "If not, the Great Expectations suite will be utilized and all rules will be available."
+            ),
+            "project_scope": (
+                "Specify a comma-separated list of projects to search through for cross manifest validation."
+            ),
+        },
+    }
+}
+
+
+# `schematic schema` related sub-commands description
+schema_commands = {
+    "schema": {
+        "convert": {
+            "short_help": (
+                "Convert specification from CSV data model to JSON-LD data model."
+            ),
+            "base_schema": (
+                "Path to base data model. BioThings data model is loaded by default."
+            ),
+            "output_jsonld": (
+                "Path to where the generated JSON-LD file needs to be outputted."
+            ),
+        }
+    }
+}
+
+
+# `schematic init` command description
+init_command = {
+    "init": {
+        "short_help": ("Initialize authentication for schematic."),
+        "config": (
+            "Specify the path to the `config.yml` using this option. This is a required argument."
+        ),
+    }
+}
+
+viz_commands = {
+    "visualization": {
+        "config": (
+            "Specify the path to the `config.yml` using this option. This is a required argument."
+        ),
+        "tangled_tree": {
+            "figure_type": ("Specify the type of schema visualization to make. Either 'dependency' or 'component'."
+                ),
+            "text_format": ("Specify the type of text to gather for tangled tree visualization, either 'plain' or 'highlighted'."
+                ),
+        }
+    }
+    
+}
---
+++ packages/schematicpy/schematicpy-23.1.1-py3-none-any.whl/schematic/models/commands.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python3
+
+from gc import callbacks
+import logging
+import sys
+
+import click
+import click_log
+
+from jsonschema import ValidationError
+
+from schematic.models.metadata import MetadataModel
+from schematic.utils.cli_utils import get_from_config, fill_in_from_config, query_dict, parse_synIDs, parse_comma_str_to_list
+from schematic.help import model_commands
+from schematic.exceptions import MissingConfigValueError
+from schematic import CONFIG
+
+logger = logging.getLogger(__name__)
+click_log.basic_config(logger)
+
+CONTEXT_SETTINGS = dict(help_option_names=["--help", "-h"])  # help options
+
+# invoke_without_command=True -> forces the application not to show aids before losing them with a --h
+@click.group(context_settings=CONTEXT_SETTINGS, invoke_without_command=True)
+@click_log.simple_verbosity_option(logger)
+@click.option(
+    "-c",
+    "--config",
+    type=click.Path(),
+    envvar="SCHEMATIC_CONFIG",
+    help=query_dict(model_commands, ("model", "config")),
+)
+@click.pass_context
+def model(ctx, config):  # use as `schematic model ...`
+    """
+    Sub-commands for Metadata Model related utilities/methods.
+    """
+    try:
+        logger.debug(f"Loading config file contents in '{config}'")
+        ctx.obj = CONFIG.load_config(config)
+    except ValueError as e:
+        logger.error("'--config' not provided or environment variable not set.")
+        logger.exception(e)
+        sys.exit(1)
+
+
+# prototype based on submit_metadata_manifest()
+@model.command(
+    "submit", short_help=query_dict(model_commands, ("model", "submit", "short_help"))
+)
+@click_log.simple_verbosity_option(logger)
+@click.option(
+    "-mp",
+    "--manifest_path",
+    help=query_dict(model_commands, ("model", "submit", "manifest_path")),
+)
+@click.option(
+    "-d",
+    "--dataset_id",
+    help=query_dict(model_commands, ("model", "submit", "dataset_id")),
+)
+@click.option(
+    "-vc",
+    "--validate_component",
+    help=query_dict(model_commands, ("model", "submit", "validate_component")),
+)
+@click.option(
+    "--use_schema_label/--use_display_label",
+    "-sl/-dl",
+    default=True,
+    help=query_dict(model_commands, ("model", "submit", "use_schema_label")),
+)
+@click.option(
+    "--hide_blanks",
+    "-hb",
+    is_flag=True,
+    help=query_dict(model_commands,("model","submit","hide_blanks")),
+)
+@click.option(
+    "--manifest_record_type",
+    "-mrt",
+    default='both',
+    type=click.Choice(['table', 'entity', 'both'], case_sensitive=True),
+    help=query_dict(model_commands, ("model", "submit", "manifest_record_type")))
+@click.option(
+    "-rr",
+    "--restrict_rules",
+    is_flag=True,
+    help=query_dict(model_commands,("model","validate","restrict_rules")),
+)
+@click.option(
+    "-ps",
+    "--project_scope",
+    default=None,
+    callback=parse_synIDs,
+    help=query_dict(model_commands, ("model", "validate", "project_scope")),
+)
+@click.pass_obj
+def submit_manifest(
+    ctx, manifest_path, dataset_id, validate_component, manifest_record_type, use_schema_label, hide_blanks, restrict_rules, project_scope,
+):
+    """
+    Running CLI with manifest validation (optional) and submission options.
+    """
+
+    jsonld = get_from_config(CONFIG.DATA, ("model", "input", "location"))
+
+    model_file_type = get_from_config(CONFIG.DATA, ("model", "input", "file_type"))
+
+    metadata_model = MetadataModel(
+        inputMModelLocation=jsonld, inputMModelLocationType=model_file_type
+    )
+
+    try:
+        manifest_id = metadata_model.submit_metadata_manifest(
+            path_to_json_ld = jsonld,
+            manifest_path=manifest_path,
+            dataset_id=dataset_id,
+            validate_component=validate_component,
+            manifest_record_type=manifest_record_type,
+            restrict_rules=restrict_rules,
+            use_schema_label=use_schema_label,
+            hide_blanks=hide_blanks,
+            project_scope=project_scope,
+        )
+
+        '''
+        if censored_manifest_id:
+            logger.info(
+                f"File at '{manifest_path}' was censored and successfully associated "
+                f"with dataset '{dataset_id}'. "
+                f"An uncensored version has also been associated with dataset '{dataset_id}' "
+                f"and submitted to the Synapse Access Control Team to begin the process "
+                f"of adding terms of use or review board approval."
+            )
+        '''
+        if manifest_id:
+            logger.info(
+                f"File at '{manifest_path}' was successfully associated "
+                f"with dataset '{dataset_id}'."
+            )
+    except ValueError:
+        logger.error(
+            f"Component '{validate_component}' is not present in '{jsonld}', or is invalid."
+        )
+    except ValidationError:
+        logger.error(
+            f"Validation errors resulted while validating with '{validate_component}'."
+        )
+    except LookupError:
+        logger.error(
+            f"'{dataset_id}' could not be found in the asset view (or file view for Synapse user)"
+        )
+
+
+# prototype based on validateModelManifest()
+@model.command(
+    "validate",
+    short_help=query_dict(model_commands, ("model", "validate", "short_help")),
+)
+@click_log.simple_verbosity_option(logger)
+@click.option(
+    "-mp",
+    "--manifest_path",
+    type=click.Path(exists=True),
+    required=True,
+    help=query_dict(model_commands, ("model", "validate", "manifest_path")),
+)
+@click.option(
+    "-dt",
+    "--data_type",
+    callback=parse_comma_str_to_list,
+    help=query_dict(model_commands, ("model", "validate", "data_type")),
+)
+@click.option(
+    "-js",
+    "--json_schema",
+    help=query_dict(model_commands, ("model", "validate", "json_schema")),
+)
+@click.option(
+    "-rr",
+    "--restrict_rules",
+    is_flag=True,
+    help=query_dict(model_commands,("model","validate","restrict_rules")),
+)
+@click.option(
+    "-ps",
+    "--project_scope",
+    default=None,
+    callback=parse_synIDs,
+    help=query_dict(model_commands, ("model", "validate", "project_scope")),
+)
+@click.pass_obj
+def validate_manifest(ctx, manifest_path, data_type, json_schema, restrict_rules,project_scope):
+    """
+    Running CLI for manifest validation.
+    """
+    data_type = fill_in_from_config("data_type", data_type, ("manifest", "data_type"))
+    
+    try:
+        len(data_type) == 1
+    except:
+        logger.error(
+            f"Can only validate a single data_type at a time. Please provide a single data_type"
+        )
+
+    data_type = data_type[0]
+
+    json_schema = fill_in_from_config(
+        "json_schema",
+        json_schema,
+        ("model", "input", "validation_schema"),
+        allow_none=True,
+    )
+    
+    jsonld = get_from_config(CONFIG.DATA, ("model", "input", "location"))
+
+    model_file_type = get_from_config(CONFIG.DATA, ("model", "input", "file_type"))
+
+    metadata_model = MetadataModel(
+        inputMModelLocation=jsonld, inputMModelLocationType=model_file_type
+    )
+
+    errors, warnings = metadata_model.validateModelManifest(
+        manifestPath=manifest_path, rootNode=data_type, jsonSchema=json_schema, restrict_rules=restrict_rules, project_scope=project_scope,
+    )
+
+    if not errors:
+        click.echo(
+            "Your manifest has been validated successfully. "
+            "There are no errors in your manifest, and it can "
+            "be submitted without any modifications."
+        )
+    else:
+        click.echo(errors)
