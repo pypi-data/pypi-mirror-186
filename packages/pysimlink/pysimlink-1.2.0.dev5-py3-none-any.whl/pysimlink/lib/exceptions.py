f294ab4c31fc8e3f691f0e91e2aeb519359f833d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:43 +0000

    Add package pysimlink/1.2.0.dev5/pysimlink-1.2.0.dev5-py3-none-any.whl

---
+++ packages/pysimlink/pysimlink-1.2.0.dev5-py3-none-any.whl/pysimlink/lib/exceptions.py
@@ -0,0 +1,38 @@
+class GenerationError(Exception):
+    """
+    Exception raised when generating build files fails.
+
+    Attributes:
+        dump (str): stdout and stderr output of the **generation** process
+        cmake (str): contents of the :file:`CMakeLists.txt` file
+    """
+
+    def __init__(self, *args):
+        self.dump = args[0]
+        self.cmake = args[1]
+
+    def __str__(self):
+        return (
+            "Generating the CMakeLists for this model failed. This could be a c/c++/cmake setup issue, bad paths, or a bug! "
+            f"Output from CMake generation is in {self.dump}"
+        )
+
+
+class BuildError(Exception):
+    """
+    Exception raised when generating build files succeeded but compiling the model fails.
+
+    Attributes:
+        dump (str): stdout and stderr output of the **build** process
+        cmake (str): contents of the :file:`CMakeLists.txt` file
+    """
+
+    def __init__(self, *args):
+        self.dump = args[0]
+        self.cmake = args[1]
+
+    def __str(self):
+        return (
+            "Building the model failed. This could be a c/c++/cmake setup issue, bad paths, or a bug! "
+            f"Output from the build process is in {self.dump}"
+        )
