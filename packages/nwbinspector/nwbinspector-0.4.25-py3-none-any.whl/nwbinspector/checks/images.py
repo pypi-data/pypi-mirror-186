a404aba43a123329305cb5062542bfef7d95ab29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:44 +0000

    Add package nwbinspector/0.4.25/nwbinspector-0.4.25-py3-none-any.whl

---
+++ packages/nwbinspector/nwbinspector-0.4.25-py3-none-any.whl/nwbinspector/checks/images.py
@@ -0,0 +1,40 @@
+"""Checks specific to the Images neurodata type."""
+from ..register_checks import register_check, Importance, InspectorMessage
+
+try:  # The Images neurodata type was unavailable prior to PyNWB v.2.1.0
+    from pynwb.base import Images
+
+    HAVE_IMAGES = True
+except ImportError:
+    HAVE_IMAGES = False
+
+from pynwb.image import IndexSeries
+
+if HAVE_IMAGES:
+
+    @register_check(importance=Importance.BEST_PRACTICE_VIOLATION, neurodata_type=Images)
+    def check_order_of_images_unique(images: Images):
+        """Check that all the values in the order_of_images field of an Images object are unique."""
+        if images.order_of_images is None:
+            return
+        if not len(set(images.order_of_images)) == len(images.order_of_images):
+            return InspectorMessage(message="order_of_images should have unique values.")
+
+    @register_check(importance=Importance.BEST_PRACTICE_VIOLATION, neurodata_type=Images)
+    def check_order_of_images_len(images: Images):
+        """Check that all the values in the order_of_images field of an Images object are unique."""
+        if images.order_of_images is None:
+            return
+        if not len(images.order_of_images) == len(images.images):
+            return InspectorMessage(
+                message=f"Length of order_of_images ({len(images.order_of_images)}) does not match the number of "
+                f"images ({len(images.images)})."
+            )
+
+    @register_check(importance=Importance.BEST_PRACTICE_VIOLATION, neurodata_type=IndexSeries)
+    def check_index_series_points_to_image(index_series: IndexSeries):
+        if index_series.indexed_timeseries is not None:
+            return InspectorMessage(
+                message="Pointing an IndexSeries to a TimeSeries will be deprecated. Please point to an Images "
+                "container instead."
+            )
