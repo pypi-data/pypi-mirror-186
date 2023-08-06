edd7864df87bae9cd466174a751faa48563a41a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:08 +0000

    Add package swibots/1.0.2/swibots-1.0.2.tar.gz

---
+++ packages/swibots/swibots-1.0.2.tar.gz/swibots-1.0.2/swibots/config.py
@@ -0,0 +1,32 @@
+import os
+
+
+APP_CONFIG = None
+
+
+def get_config():
+    return APP_CONFIG
+
+
+def reload_config():
+    return {
+        "CHAT_SERVICE": {
+            "BASE_URL": os.getenv("CHAT_SERVICE_BASE_URL") or "http://51.159.11.53:9999",
+            "WS_URL": os.getenv("CHAT_SERVICE_WS_URL")
+            or "ws://51.159.11.53:9999/v1/websocket/message/ws",
+        },
+        "BOT_SERVICE": {
+            "BASE_URL": os.getenv("BOT_SERVICE_BASE_URL") or "http://51.159.11.53:9999",
+        },
+        "AUTH_SERVICE": {
+            "BASE_URL": os.getenv("AUTH_SERVICE_BASE_URL") or "http://51.159.11.53:9999/api",
+        },
+        "COMMUNITY_SERVICE": {
+            "BASE_URL": os.getenv("COMMUNITY_SERVICE_BASE_URL") or "http://51.159.11.53:9999",
+            "WS_URL": os.getenv("COMMUNITY_SERVICE_WS_URL")
+            or "ws://51.159.11.53:9999/v1/websocket/community/ws",
+        },
+    }
+
+
+APP_CONFIG = reload_config()
