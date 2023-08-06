d4e2db914d756c1a358ba8ee2b2169a6803913d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:09 +0000

    Add package testing-guro/1.0/testing_guro-1.0.tar.gz

---
+++ packages/testing-guro/testing_guro-1.0.tar.gz/testing_guro-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testing_guro',
+    version='1.0',
+    description='testing_guro for pocs in our org',
+    url='https://github.com/practiccollie/testing_guro',
+    author='practiccollie',
+    author_email='practiccollie@example.com',
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+)
