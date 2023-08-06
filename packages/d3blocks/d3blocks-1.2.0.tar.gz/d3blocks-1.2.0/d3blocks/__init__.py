39619164e7d5fa5e30770919f20a43abeb90369e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:50 +0000

    Add package d3blocks/1.2.0/d3blocks-1.2.0.tar.gz

---
+++ packages/d3blocks/d3blocks-1.2.0.tar.gz/d3blocks-1.2.0/d3blocks/__init__.py
@@ -0,0 +1,32 @@
+from d3blocks.d3blocks import D3Blocks
+
+# import movingbubbles.Movingbubbles as Movingbubbles
+# import timeseries.Timeseries as Timeseries
+# import sankey.Sankey as Sankey
+# import imageslider.Imageslider as Imageslider
+# import chord.Chord as Chord
+# import scatter.Scatter as Scatter
+# import violin.Violin as Violin
+# import particles.Particles as Particles
+
+__author__ = 'Erdogan Taskesen, Oliver Verver'
+__email__ = 'erdogant@gmail.com, mail@oliver3.nl'
+__version__ = '1.2.0'
+
+# module level doc-string
+__doc__ = """
+d3blocks
+=====================================================================
+
+Description
+-----------
+d3blocks is for the creation of stand-alone and interactive d3 graphs.
+
+References
+----------
+* D3Blocks (blog): https://towardsdatascience.com/d3blocks-the-python-library-to-create-interactive-and-standalone-d3js-charts-3dda98ce97d4
+* D3Graph (blog): https://towardsdatascience.com/creating-beautiful-stand-alone-interactive-d3-charts-with-python-804117cb95a7
+* Github : https://github.com/d3blocks/d3blocks
+* Docs: https://d3blocks.github.io/d3blocks/
+
+"""
