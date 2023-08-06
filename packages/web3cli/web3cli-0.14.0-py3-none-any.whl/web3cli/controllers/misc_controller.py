42e4183227304360cf2dcdedb9a4ffaf0fad71a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:44 +0000

    Add package web3cli/0.14.0/web3cli-0.14.0-py3-none-any.whl

---
+++ packages/web3cli/web3cli-0.14.0-py3-none-any.whl/web3cli/controllers/misc_controller.py
@@ -0,0 +1,98 @@
+import json
+from pprint import pformat
+
+import web3
+from cement import ex
+
+from web3cli.controllers.controller import Controller
+from web3cli.exceptions import Web3CliError
+from web3cli.helpers.chain import chain_ready_or_raise
+from web3cli.helpers.client_factory import (
+    make_client,
+    make_contract_client,
+    make_wallet,
+)
+from web3cli.helpers.signer import signer_ready_or_raise
+from web3core.models.address import Address
+
+
+class MiscController(Controller):
+    """Handler of simple top-level commands"""
+
+    class Meta:
+        label = "misc"
+        help = "simple top-level commands"
+        stacked_type = "embedded"
+        stacked_on = "base"
+
+    @ex(
+        help="Get the balance of the given address in the blockchain coin (ETH, BNB, AVAX, etc)",
+        arguments=[(["address"], {"action": "store"})],
+    )
+    def balance(self) -> None:
+        chain_ready_or_raise(self.app)
+        balance = make_client(self.app).getBalanceInEth(
+            Address.resolve_address(self.app.pargs.address)
+        )
+        self.app.render(
+            {"amount": balance, "ticker": self.app.chain.coin},
+            "balance.jinja2",
+            handler="jinja2",
+        )
+
+    @ex(
+        help="Get the latest block, or the block corresponding to the given identifier",
+        arguments=[
+            (
+                ["block_identifier"],
+                {
+                    "help": "Block identifier. Can be a block number, an hash, or one of the following: latest, earliest, pending, safe, finalized",
+                    "nargs": "?",
+                    "default": "latest",
+                },
+            )
+        ],
+    )
+    def block(self) -> None:
+        chain_ready_or_raise(self.app)
+        block = None
+        client = make_client(self.app)
+        # In case a block number was given
+        try:
+            block = client.w3.eth.get_block(int(self.app.pargs.block_identifier))
+        except ValueError:
+            pass
+        # In case a string
+        if not block:
+            block = client.w3.eth.get_block(self.app.pargs.block_identifier)
+        block_as_dict = json.loads(web3.Web3.toJSON(block))
+        self.app.render(block_as_dict, indent=4, handler="json")
+
+    @ex(
+        help="Sign the given message and show the signed message, as returned by web3.py",
+        arguments=[(["msg"], {"action": "store"})],
+    )
+    def sign(self) -> None:
+        signer_ready_or_raise(self.app)
+        signed_message = make_wallet(self.app).signMessage(self.app.pargs.msg)
+        self.app.print(pformat(signed_message._asdict()))
+
+    @ex(
+        help="Call a function in the given smart contract, using eth_call. To see the list of functions, use `w3 abi functions`.",
+        arguments=[
+            (["contract"], {"action": "store"}),
+            (["function"], {"action": "store"}),
+            (["args"], {"action": "store", "nargs": "*"}),
+        ],
+    )
+    def call(self) -> None:
+        chain_ready_or_raise(self.app)
+        # Try to fetch the function from the ABI
+        functions = make_contract_client(self.app, self.app.pargs.contract).functions
+        try:
+            function = functions[self.app.pargs.function]
+        except web3.exceptions.ABIFunctionNotFound:
+            raise Web3CliError(f"Function must be one of: {', '.join(functions)}")
+        # Call the function
+        result = function(*self.app.pargs.args).call()
+        self.app.print(str(result))
