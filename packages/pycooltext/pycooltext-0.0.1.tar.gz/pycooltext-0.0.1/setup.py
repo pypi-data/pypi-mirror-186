a51fcd51a6310517a458ccd8106f4d85983c30f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:25 +0000

    Add package pycooltext/0.0.1/pycooltext-0.0.1.tar.gz

---
+++ packages/pycooltext/pycooltext-0.0.1.tar.gz/pycooltext-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pycooltext",
+    description="A function to print cool text.",
+    version="0.0.1",
+    author="Hart Traveller",
+    author_email="hart@cephalon.io",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["rich", "pathlib"],
+)
