a0297e9283a57bd73cc6370e3c97603d44bfbb25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:31 +0000

    Add package geezlibs/1.2.2/geezlibs-1.2.2.tar.gz

---
+++ packages/geezlibs/geezlibs-1.2.2.tar.gz/geezlibs-1.2.2/geezlibs/geez/database/gbandb.py
@@ -0,0 +1,116 @@
+from typing import Dict, List, Union
+from geezlibs.geez.database import mongodb
+
+gbansdb = mongodb.gban
+sudoersdb = mongodb.sudoers
+blockeddb = mongodb.blockedusers
+chatsdb = mongodb.chats
+
+async def get_gbanned() -> list:
+    results = []
+    async for user in gbansdb.find({"user_id": {"$gt": 0}}):
+        user_id = user["user_id"]
+        results.append(user_id)
+    return results
+
+
+async def is_gbanned_user(user_id: int) -> bool:
+    user = await gbansdb.find_one({"user_id": user_id})
+    if not user:
+        return False
+    return True
+
+
+async def add_gban_user(user_id: int):
+    is_gbanned = await is_gbanned_user(user_id)
+    if is_gbanned:
+        return
+    return await gbansdb.insert_one({"user_id": user_id})
+
+
+async def remove_gban_user(user_id: int):
+    is_gbanned = await is_gbanned_user(user_id)
+    if not is_gbanned:
+        return
+    return await gbansdb.delete_one({"user_id": user_id})
+
+
+async def get_banned_users() -> list:
+    results = []
+    async for user in blockeddb.find({"user_id": {"$gt": 0}}):
+        user_id = user["user_id"]
+        results.append(user_id)
+    return results
+
+
+async def get_banned_count() -> int:
+    users = blockeddb.find({"user_id": {"$gt": 0}})
+    users = await users.to_list(length=100000)
+    return len(users)
+
+
+async def is_banned_user(user_id: int) -> bool:
+    user = await blockeddb.find_one({"user_id": user_id})
+    if not user:
+        return False
+    return True
+
+
+async def add_banned_user(user_id: int):
+    is_gbanned = await is_banned_user(user_id)
+    if is_gbanned:
+        return
+    return await blockeddb.insert_one({"user_id": user_id})
+
+
+async def remove_banned_user(user_id: int):
+    is_gbanned = await is_banned_user(user_id)
+    if not is_gbanned:
+        return
+    return await blockeddb.delete_one({"user_id": user_id})
+
+
+async def get_served_chats() -> list:
+    chats_list = []
+    async for chat in chatsdb.find({"chat_id": {"$lt": 0}}):
+        chats_list.append(chat)
+    return chats_list
+
+async def is_served_chat(chat_id: int) -> bool:
+    chat = await chatsdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return False
+    return True
+
+
+async def add_served_chat(chat_id: int):
+    is_served = await is_served_chat(chat_id)
+    if is_served:
+        return
+    return await chatsdb.insert_one({"chat_id": chat_id})
+
+"""
+from geezlibs.geez.database import cli
+
+gbun = cli["GBAN"]
+
+
+async def gban_user(user, reason="#GBanned"):
+    await gbun.insert_one({"user": user, "reason": reason})
+
+
+async def ungban_user(user):
+    await gbun.delete_one({"user": user})
+
+
+async def gban_list():
+    return [lo async for lo in gbun.find({})]
+
+
+async def gban_info(user):
+    kk = await gbun.find_one({"user": user})
+    if not kk:
+        return False
+    else:
+        return kk["reason"]
+"""
