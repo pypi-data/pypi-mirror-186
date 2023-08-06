830e9debc84a638af77f7020f771f138becd2557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:33 +0000

    Add package Topsis-Amrita-102017017/0.0.3/Topsis-Amrita-102017017-0.0.3.tar.gz

---
+++ packages/Topsis-Amrita-102017017/Topsis-Amrita-102017017-0.0.3.tar.gz/Topsis-Amrita-102017017-0.0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'A command line tool to perform TOPSIS, a multi-criteria decision making technique'
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# Setting up
+setup(
+    name="Topsis-Amrita-102017017",
+    version=VERSION,
+    author="Amrita Bhatia",
+    author_email="<nonie.bhatia@gmail.com>",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'topsis=src.topsis:main'
+        ]
+    },
+    keywords=['python', 'TOPSIS', 'MCDM', 'MCDA', 'statistics', 'prescriptive analytics', 'cli'],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.5'
+)
