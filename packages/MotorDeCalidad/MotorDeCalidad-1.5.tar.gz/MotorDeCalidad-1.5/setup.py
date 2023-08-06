6a8101f7e260b4674bc1ed9e2e426bd711afe1b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:29 +0000

    Add package MotorDeCalidad/1.5/MotorDeCalidad-1.5.tar.gz

---
+++ packages/MotorDeCalidad/MotorDeCalidad-1.5.tar.gz/MotorDeCalidad-1.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='MotorDeCalidad',
+version='1.5',
+description='Paquete demo de Motor de Calidad',
+author='Enzo Ipanaque',
+author_email='enzo.ipanaque@ms-peru.com',
+license='Management Solutions',
+packages=['motordecalidad'],
+py_modules=['functions','constants','rules','utilities'],
+zip_safe=False)
