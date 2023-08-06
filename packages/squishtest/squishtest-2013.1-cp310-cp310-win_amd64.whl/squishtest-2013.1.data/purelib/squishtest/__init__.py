607a5fa41395b5ff33d2b7ae5a8497cc1ebd9714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:44 +0000

    Add package squishtest/2013.1/squishtest-2013.1-cp310-cp310-win_amd64.whl

---
+++ packages/squishtest/squishtest-2013.1-cp310-cp310-win_amd64.whl/squishtest-2013.1.data/purelib/squishtest/__init__.py
@@ -0,0 +1,14 @@
+from importlib import import_module
+import sys
+
+
+try:
+    sys.modules[__name__] = import_module('squishtest.squishtest%d' % sys.version_info.major)
+except ImportError as e:
+    if 'No module named' in str(e):
+        raise ImportError("Cannot load %s module with this Python version" % __name__)
+    else:
+        raise
+
+
+
