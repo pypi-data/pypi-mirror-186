faa8883ebfb4e1adf43a43bfa867b9a7393f020c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:45 +0000

    Add package netbox-storage/0.0.0.0.0.19/netbox_storage-0.0.0.0.0.19-py3-none-any.whl

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.0.19-py3-none-any.whl/netbox_storage/tables/linux_volume.py
@@ -0,0 +1,67 @@
+import django_tables2 as tables
+
+from netbox.tables import (
+    NetBoxTable,
+    ToggleColumn,
+    ActionsColumn,
+)
+
+from netbox_storage.models import LinuxVolume
+
+
+class LinuxVolumeBaseTable(NetBoxTable):
+    """Base class for tables displaying LinuxVolume"""
+
+    fs = tables.Column(
+        linkify=True,
+    )
+    vg_name = tables.Column(
+        linkify=True,
+    )
+    lv_name = tables.Column(
+        linkify=True,
+    )
+
+
+class LinuxVolumeTable(LinuxVolumeBaseTable):
+    """Table for displaying LinuxVolume objects."""
+
+    pk = ToggleColumn()
+
+    class Meta(NetBoxTable.Meta):
+        model = LinuxVolume
+        fields = (
+            "pk",
+            "vg_name",
+            "lv_name",
+            "path",
+            "fs",
+            "description",
+        )
+        default_columns = (
+            "vg_name",
+            "lv_name",
+            "path",
+            "fs",
+            "description"
+        )
+
+
+class RelatedLinuxVolumeTable(LinuxVolumeBaseTable):
+    actions = ActionsColumn(actions=())
+
+    class Meta(NetBoxTable.Meta):
+        model = LinuxVolume
+        fields = (
+            "pk",
+            "vg_name",
+            "lv_name",
+            "path",
+            "fs",
+            "description",
+        )
+        default_columns = (
+            "fs",
+            "description"
+        )
+
