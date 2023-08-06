53cb05f9e9ea5be6b978d379f09e7f4f44d24998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:16:59 +0000

    Add package padthai/0.3.0/padthai-0.3.0.tar.gz

---
+++ packages/padthai/padthai-0.3.0.tar.gz/padthai-0.3.0/padthai/__init__.py
@@ -0,0 +1,3 @@
+from padthai.flax import FlaxGPT2FewShot
+from padthai.gpt_neo import GPTNeoFewShot
+from padthai.mgpt import mGPTFewShot
---
+++ packages/padthai/padthai-0.3.0.tar.gz/padthai-0.3.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8-sig") as f:
+    readme = f.read()
+
+with open("requirements.txt", "r", encoding="utf-8-sig") as f:
+    requirements = [i.strip() for i in f.readlines()]
+
+setup(
+    name="padthai",
+    version="0.3.0",
+    description="Make Pad Thai From few-shot learning 😉",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Wannaphong Phatthiyaphaibun",
+    author_email="wannaphong@yahoo.com",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    url="https://github.com/PyThaiNLP/padthai",
+    python_requires=">=3.6",
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    project_urls={
+        "Documentation": "https://pythainlp.github.io/padthai/",
+        "Tutorials": "https://github.com/PyThaiNLP/padthai/tree/main/notebooks",
+        "Source Code": "https://github.com/PyThaiNLP/padthai",
+        "Bug Tracker": "https://github.com/PyThaiNLP/padthai/issues",
+    }
+)
