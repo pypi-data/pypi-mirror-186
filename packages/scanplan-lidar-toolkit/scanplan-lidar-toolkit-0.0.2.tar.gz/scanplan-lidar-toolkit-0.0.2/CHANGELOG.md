93cb5232878c7a25b738159cedf10bf2acc94b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:58 +0000

    Add package scanplan-lidar-toolkit/0.0.2/scanplan-lidar-toolkit-0.0.2.tar.gz

---
+++ packages/scanplan-lidar-toolkit/scanplan-lidar-toolkit-0.0.2.tar.gz/scanplan-lidar-toolkit-0.0.2/CHANGELOG.md
@@ -0,0 +1,10 @@
+## Version 0.0.X (D/M/Y)
+
+### Bug fix/Issue/Improvements/Changed/Updates
+- item1
+- item2
+
+## Version 0.0.1 (10/01/2023)
+
+- Create the package
+- Add lidar converters to io
---
+++ packages/scanplan-lidar-toolkit/scanplan-lidar-toolkit-0.0.2.tar.gz/scanplan-lidar-toolkit-0.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("./requirements.txt") as f:
+    requirements = f.readlines()
+
+with open("./LICENSE") as f:
+    LICENSE = f.read()
+
+with open("./VERSION") as f:
+    __version__ = f.readline()
+
+setup(name='scanplan-lidar-toolkit',
+      version=__version__,
+      description='ScanPlan LiDAR Tool Kit',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      license='Licensed to ScanPlan',
+      keywords='lidar gis las',
+      author=LICENSE,
+      author_email='shayan@scan-plan.nl',
+      url='https://github.com/bimbuildings/ltk',
+      python_requires='>=3.8',
+      install_requires=requirements,
+      packages=find_packages(),
+      classifiers=[
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 3",
+      ],
+    )
