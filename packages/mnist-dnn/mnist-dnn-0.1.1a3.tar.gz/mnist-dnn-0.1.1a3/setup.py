2e96953083b73bc3e488ca96f13f2f7bb1de9b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:54 +0000

    Add package mnist-dnn/0.1.1a3/mnist-dnn-0.1.1a3.tar.gz

---
+++ packages/mnist-dnn/mnist-dnn-0.1.1a3.tar.gz/mnist-dnn-0.1.1a3/setup.py
@@ -0,0 +1,42 @@
+import io
+import setuptools
+
+def long_description():
+    with io.open('README.md', 'r', encoding='utf-8') as f:
+        readme = f.read()
+    return readme
+
+def requirements():
+    with io.open('requirements.txt', 'r', encoding='utf-8') as f:
+        requirements = f.read()
+    return requirements
+
+setuptools.setup(
+    name="mnist-dnn",
+    version="0.1.1a3",
+    author="John Yoon",
+    author_email="fedelejohn7008@gmail.com",
+    description="Deep Neural Network from Scratch with MNIST dataset",
+    long_description=long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/fedele7008/MNIST_DNN",
+    project_urls={
+        "Issue Tracker": "https://github.com/fedele7008/MNIST_DNN/issues",
+        "Wiki": "https://github.com/fedele7008/MNIST_DNN/wiki"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License"
+    ],
+    package_data={
+        'mnist_dnn': ['train.csv']
+    },
+    install_requires=requirements(),
+    setup_requires=[],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    zip_safe=False
+)
