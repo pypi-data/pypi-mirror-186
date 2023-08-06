b7bd87f817d183897b229cc80589c543d59dafbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:31 +0000

    Add package whisp/0.0.1.7/whisp-0.0.1.7.tar.gz

---
+++ packages/whisp/whisp-0.0.1.7.tar.gz/whisp-0.0.1.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1.7'
+DESCRIPTION = 'Whisper package'
+LONG_DESCRIPTION = 'A package that allows easily to use the Whisper library'
+
+# Setting up
+setup(
+    name="whisp",
+    version=VERSION,
+    author="Peteris N.",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['setuptools-rust', 'whisper'],
+    keywords=[],
+    classifiers=[]
+)
---
+++ packages/whisp/whisp-0.0.1.7.tar.gz/whisp-0.0.1.7/whisp/whisp_main.py
@@ -0,0 +1,38 @@
+import whisper
+from enum import Enum
+from typing import Union
+from pathlib import Path
+
+LANGUAGE = "english"
+
+class MODEL(str, Enum):
+    tiny = "tiny",
+    base = "base",
+    small = "small",
+    medium = "medium",
+    large = "large"
+
+    def __str__(self) -> str:
+        return self.value
+
+class MODEL_EN(str, Enum):
+    tiny = "tiny.en",
+    base = "base.en",
+    small = "small.en",
+    medium = "medium.en"
+
+    def __str__(self) -> str:
+        return self.value
+
+print(MODEL.base)
+
+def recognize(model: Union[MODEL, MODEL_EN], audio: Path, language=LANGUAGE):
+    mod = whisper.load_model(model)
+
+    ## audio = whisper.load_audio("audio.mp3")
+
+    options = whisper.DecodingOptions()
+    result = mod.transcribe(audio)
+
+    print(result.text)
+# print the recognized text
