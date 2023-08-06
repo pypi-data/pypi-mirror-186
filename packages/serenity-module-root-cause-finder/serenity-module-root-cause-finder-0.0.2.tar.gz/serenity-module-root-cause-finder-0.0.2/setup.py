d4547c6c236c6d84b4c92c0b0d6372af73426447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:38 +0000

    Add package serenity-module-root-cause-finder/0.0.2/serenity-module-root-cause-finder-0.0.2.tar.gz

---
+++ packages/serenity-module-root-cause-finder/serenity-module-root-cause-finder-0.0.2.tar.gz/serenity-module-root-cause-finder-0.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+readme = "Some README"
+license = "Some License"
+version = "0.0.2"
+
+setup(
+    name='serenity-module-root-cause-finder',
+    version=version,
+    url='https://soemthing.hehe',
+    author='Noone',
+    author_email='noone@nonaaaae.com',
+    description='A schema and validator for YAML.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ]
+)
---
+++ packages/serenity-module-root-cause-finder/serenity-module-root-cause-finder-0.0.2.tar.gz/serenity-module-root-cause-finder-0.0.2/yamale/__init__.py
@@ -0,0 +1,2 @@
+"""Internally-developed tooling that provides management/orchestration/automation/compliance of AzDO"""
+import os
