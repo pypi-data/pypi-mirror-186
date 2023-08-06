84c6c0185263e96d81b7a853da4f31afc5291a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:56 +0000

    Add package ddtrace/0.61.5/ddtrace-0.61.5-cp310-cp310-macosx_10_9_universal2.whl

---
+++ packages/ddtrace/ddtrace-0.61.5-cp310-cp310-macosx_10_9_universal2.whl/ddtrace/utils/cache.py
@@ -0,0 +1,17 @@
+from ddtrace.internal.utils.deprecations import DDTraceDeprecationWarning
+
+from ..internal.utils.cache import CachedMethodDescriptor  # noqa
+from ..internal.utils.cache import F  # noqa
+from ..internal.utils.cache import M  # noqa
+from ..internal.utils.cache import T  # noqa
+from ..internal.utils.cache import cached  # noqa
+from ..internal.utils.cache import cachedmethod  # noqa
+from ..internal.utils.cache import miss  # noqa
+from ..vendor.debtcollector.removals import removed_module
+
+
+removed_module(
+    module="ddtrace.utils.cache",
+    category=DDTraceDeprecationWarning,
+    removal_version="1.0.0",
+)
