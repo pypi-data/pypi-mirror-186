a059e36dba8d558459bda2507efe8b51db4a2bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:36 +0000

    Add package package-template-chanky/0.0.1/package_template_chanky-0.0.1.tar.gz

---
+++ packages/package-template-chanky/package_template_chanky-0.0.1.tar.gz/package_template_chanky-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='package_template_chanky',
+    version='0.0.1',
+    packages=[''],
+    url='https://www.github.com/chankyswami/package_template_chanky)',
+    license='hcbhjds',
+    author='Chanky Swami',
+    author_email='c.innovator@gmail.com',
+    description='cbskjbcasbhc'
+)
