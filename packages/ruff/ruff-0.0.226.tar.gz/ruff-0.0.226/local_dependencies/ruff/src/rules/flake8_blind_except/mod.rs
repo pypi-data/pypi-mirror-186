8bc8a50ff8b9c098f2c65ed1c589e06911c693c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:37 +0000

    Add package ruff/0.0.226/ruff-0.0.226.tar.gz

---
+++ packages/ruff/ruff-0.0.226.tar.gz/ruff-0.0.226/local_dependencies/ruff/src/rules/flake8_blind_except/mod.rs
@@ -0,0 +1,28 @@
+//! Rules from [flake8-blind-except](https://pypi.org/project/flake8-blind-except/0.2.1/).
+pub(crate) mod rules;
+
+#[cfg(test)]
+mod tests {
+    use std::convert::AsRef;
+    use std::path::Path;
+
+    use anyhow::Result;
+    use test_case::test_case;
+
+    use crate::linter::test_path;
+    use crate::registry::RuleCode;
+    use crate::settings;
+
+    #[test_case(RuleCode::BLE001, Path::new("BLE.py"); "BLE001")]
+    fn rules(rule_code: RuleCode, path: &Path) -> Result<()> {
+        let snapshot = format!("{}_{}", rule_code.as_ref(), path.to_string_lossy());
+        let diagnostics = test_path(
+            Path::new("./resources/test/fixtures/flake8_blind_except")
+                .join(path)
+                .as_path(),
+            &settings::Settings::for_rule(rule_code),
+        )?;
+        insta::assert_yaml_snapshot!(snapshot, diagnostics);
+        Ok(())
+    }
+}
---
+++ packages/ruff/ruff-0.0.226.tar.gz/ruff-0.0.226/local_dependencies/ruff/src/rules/flake8_debugger/mod.rs
@@ -0,0 +1,29 @@
+//! Rules from [flake8-debugger](https://pypi.org/project/flake8-debugger/4.1.2/).
+pub(crate) mod rules;
+pub(crate) mod types;
+
+#[cfg(test)]
+mod tests {
+    use std::convert::AsRef;
+    use std::path::Path;
+
+    use anyhow::Result;
+    use test_case::test_case;
+
+    use crate::linter::test_path;
+    use crate::registry::RuleCode;
+    use crate::settings;
+
+    #[test_case(RuleCode::T100, Path::new("T100.py"); "T100")]
+    fn rules(rule_code: RuleCode, path: &Path) -> Result<()> {
+        let snapshot = format!("{}_{}", rule_code.as_ref(), path.to_string_lossy());
+        let diagnostics = test_path(
+            Path::new("./resources/test/fixtures/flake8_debugger")
+                .join(path)
+                .as_path(),
+            &settings::Settings::for_rule(rule_code),
+        )?;
+        insta::assert_yaml_snapshot!(snapshot, diagnostics);
+        Ok(())
+    }
+}
