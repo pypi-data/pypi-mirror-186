d1a828b6d909d1677d19d6a8c66f4fc4ad5acd3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:56 +0000

    Add package nonebot-plugin-biliav/1.0.11/nonebot_plugin_biliav-1.0.11-py3-none-any.whl

---
+++ packages/nonebot-plugin-biliav/nonebot_plugin_biliav-1.0.11-py3-none-any.whl/nonebot_plugin_biliav/__init__.py
@@ -0,0 +1,38 @@
+# import nonebot
+import asyncio
+
+from nonebot import get_driver, on_regex
+from .config import Config
+from nonebot.typing import T_State
+from nonebot.adapters.onebot.v11 import Bot, Event
+from nonebot.params import T_State
+
+global_config = get_driver().config
+config = global_config.dict()
+b_sleep_time = config.get('b_sleep_time', 2)
+b_sleep_time = int(b_sleep_time)
+
+from .data_source import get_av_data
+import re
+
+# Export something for other plugin
+# export = nonebot.export()
+# export.foo = "bar"
+
+# @export.xxx
+# def some_function():
+#     pass
+biliav = on_regex("[Aa][Vv]\d{1,12}|[Bb][Vv]1[A-Za-z0-9]{2}4.1.7[A-Za-z0-9]{2}|[Bb]23\.[Tt][Vv]/[A-Za-z0-9]{7}")
+
+
+@biliav.handle()
+async def handle(bot: Bot, event: Event, state: T_State):
+    avcode_list: list[str] = re.compile(
+        "[Aa][Vv]\d{1,12}|[Bb][Vv]1[A-Za-z0-9]{2}4.1.7[A-Za-z0-9]{2}|[Bb]23\.[Tt][Vv]/[A-Za-z0-9]{7}").findall(
+        str(event.get_message()))
+    if not avcode_list:
+        return
+    rj_list: list[str] = await get_av_data(avcode_list)
+    for rj in rj_list:
+        await bot.send(event=event, message=rj)
+        await asyncio.sleep(b_sleep_time)
