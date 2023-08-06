d66c8f00672ed88800ee96369e3c73c87f1b9057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:36 +0000

    Add package chankypractice/0.0.5/chankypractice-0.0.5.tar.gz

---
+++ packages/chankypractice/chankypractice-0.0.5.tar.gz/chankypractice-0.0.5/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+from setuptools import setup, find_packages
+
+setup(
+    name='chankypractice',
+    version='0.0.5',
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url='',
+    license='',
+    author='Chanky Swami',
+    author_email='c.innovator@gmail.com',
+    description='Using find_packages()'
+)
