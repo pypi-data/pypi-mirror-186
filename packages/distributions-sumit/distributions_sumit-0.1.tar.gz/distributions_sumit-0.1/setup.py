5a65f33be554298c7f0fa55eaf35382121aa5a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:50 +0000

    Add package distributions-sumit/0.1/distributions_sumit-0.1.tar.gz

---
+++ packages/distributions-sumit/distributions_sumit-0.1.tar.gz/distributions_sumit-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_sumit',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['distributions_sumit'],
+      author="Sumit Kumar",
+      author_email="sumitkrkanti@gmail.com",
+      zip_safe=False)
