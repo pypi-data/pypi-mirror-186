14677d4d6eb68aee1c5444654f8f05571e588584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:30 +0000

    Add package whisp/0.0.1.27/whisp-0.0.1.27-py3-none-any.whl

---
+++ packages/whisp/whisp-0.0.1.27-py3-none-any.whl/whisp/whisp_main.py
@@ -0,0 +1,36 @@
+from enum import Enum
+from typing import Union
+from pathlib import Path
+from whisp import Whisper
+
+LANGUAGE = "english"
+
+class MODEL(str, Enum):
+    tiny = "tiny",
+    base = "base",
+    small = "small",
+    medium = "medium",
+    large = "large"
+
+    def __str__(self) -> str:
+        return self.value
+
+class MODEL_EN(str, Enum):
+    tiny = "tiny.en",
+    base = "base.en",
+    small = "small.en",
+    medium = "medium.en"
+
+    def __str__(self) -> str:
+        return self.value
+
+def recognize(model: Union[MODEL, MODEL_EN], audio: Path, language=LANGUAGE):
+    mod = Whisper.load_model(model)
+
+    ## audio = Whisper.load_audio("audio.mp3")
+
+    options = Whisper.DecodingOptions()
+    result = mod.transcribe(audio, fp16=False)
+
+    print(result.text)
+# print the recognized text
