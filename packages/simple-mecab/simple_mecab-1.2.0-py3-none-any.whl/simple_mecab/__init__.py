e91a5ae49c45796f2e2b1140b45c252d236655bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:47 +0000

    Add package simple-mecab/1.2.0/simple_mecab-1.2.0-py3-none-any.whl

---
+++ packages/simple-mecab/simple_mecab-1.2.0-py3-none-any.whl/simple_mecab/__init__.py
@@ -0,0 +1,7 @@
+import simple_mecab._version as __version
+from simple_mecab.exceptions import *
+from simple_mecab.mecab_wrapper import MeCabWrapper
+from simple_mecab.morpheme import Morpheme
+
+__version__ = __version.version
+__version_tuple__ = __version.version_tuple
