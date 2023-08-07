db5801b940e37d391607c3489f4852650d68d5d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:05:01 +0000

    Add package episteme/0.0.1/episteme-0.0.1-py2.py3-none-any.whl

---
+++ packages/episteme/episteme-0.0.1-py2.py3-none-any.whl/episteme/_main.py
@@ -0,0 +1,25 @@
+
+workspace = {
+    "app": [
+        {
+            "name": "todo app",
+            "version": "0.0.1",
+            "description": "a todo app",
+            "web_capabilities": True,
+        }
+    ],
+    "web-layouts": [],
+    "web-pages": [],
+    "web-components": [],
+    "web-styles": [],
+    "web-scripts": [],
+    "models": [],
+    "model-validations": [],
+    "model-relationships": [],
+    "events": [],
+    "actions": [],
+}
+
+def main():
+    print("Hello, world!")
+
