4e3abc1a057fbc4e0717771abb1f6a2dbe51b530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:05:10 +0000

    Add package shellous/0.21.0/shellous-0.21.0-py3-none-any.whl

---
+++ packages/shellous/shellous-0.21.0-py3-none-any.whl/shellous/__init__.py
@@ -0,0 +1,47 @@
+"""
+.. include:: ../README.md
+"""
+
+# pylint: disable=cyclic-import
+# pyright: reportUnusedImport=false
+
+__version__ = "0.21.0"
+
+import sys
+
+from .command import CmdContext, Command, Options  # noqa: F401
+from .pipeline import Pipeline  # noqa: F401
+from .pty_util import cbreak, cooked, raw  # noqa: F401
+from .result import PipeResult, Result, ResultError  # noqa: F401
+from .runner import AUDIT_EVENT_SUBPROCESS_SPAWN, PipeRunner, Runner  # noqa: F401
+
+if sys.platform != "win32":
+    from .watcher import DefaultChildWatcher  # noqa: F401
+
+
+sh = CmdContext()
+"""`sh` is the default command context (`CmdContext`).
+
+Use `sh` to create commands or new command contexts.
+
+```python
+from shellous import sh
+result = await sh("echo", "hello")
+```
+"""
+
+__all__ = [
+    "sh",
+    "CmdContext",
+    "Command",
+    "Pipeline",
+    "cbreak",
+    "cooked",
+    "raw",
+    "Result",
+    "ResultError",
+    "Runner",
+    "PipeRunner",
+    "AUDIT_EVENT_SUBPROCESS_SPAWN",
+    "DefaultChildWatcher",
+]
