a53821548f866cb82e72181c5cb5ff846d02b17a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:53 +0000

    Add package pyvaloapi/1.1.8/pyvaloapi-1.1.8.tar.gz

---
+++ packages/pyvaloapi/pyvaloapi-1.1.8.tar.gz/pyvaloapi-1.1.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+from pathlib import Path
+this_directory = Path(__file__).parent
+LONG_DESCRIPTION = (this_directory / "README.md").read_text()
+
+VERSION = '1.1.8'
+
+# Setting up
+setup(
+    name="pyvaloapi",
+    version=VERSION,
+    author="Mohamed Yassine Ahmed Ali",
+    author_email="<yassineahmedali02@gmail.com>",
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python', 'api', 'valorant', 'games', 'gaming', 'http'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
