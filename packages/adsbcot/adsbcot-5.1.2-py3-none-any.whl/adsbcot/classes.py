86c572845c3754002733f43582afbce78efe8fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:46:11 +0000

    Add package adsbcot/5.1.2/adsbcot-5.1.2-py3-none-any.whl

---
+++ packages/adsbcot/adsbcot-5.1.2-py3-none-any.whl/adsbcot/classes.py
@@ -0,0 +1,301 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2023 Greg Albrecht <oss@undef.net>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author:: Greg Albrecht W2GMD <oss@undef.net>
+#
+
+"""ADSBCOT Class Definitions."""
+
+import asyncio
+
+from typing import Union
+from urllib.parse import ParseResult, urlparse
+
+import aiohttp
+import pytak
+import aircot
+import adsbcot
+
+# We won't use pyModeS if it isn't installed:
+try:
+    import pyModeS.streamer.source
+    import pyModeS.streamer.decode
+    import pyModeS as pms
+except ImportError:
+    pass
+
+
+__author__ = "Greg Albrecht W2GMD <oss@undef.net>"
+__copyright__ = "Copyright 2023 Greg Albrecht"
+__license__ = "Apache License, Version 2.0"
+
+
+class ADSBWorker(pytak.QueueWorker):
+    """Read ADS-B Data from inputs, serialize to CoT, and put on TX queue."""
+
+    def __init__(self, queue, config):
+        """Initialize this class."""
+        super().__init__(queue, config)
+        self.known_craft_db = None
+        self.session = None
+        self.uid_key: str = self.config.get("UID_KEY", "ICAO")
+
+        known_craft = self.config.get("KNOWN_CRAFT")
+        if known_craft:
+            self._logger.info("Using KNOWN_CRAFT: %s", known_craft)
+            self.known_craft_db = aircot.read_known_craft(known_craft)
+
+    async def handle_data(self, data: list) -> None:
+        """Handle Data from ADS-B receiver: Render to CoT, put on TX queue.
+
+        Parameters
+        ----------
+        data : `list[dict, ]`
+            List of craft data as key/value arrays.
+        """
+        if not isinstance(data, list):
+            self._logger.warning("Invalid aircraft data, should be a Python `list`.")
+            return
+
+        if not data:
+            self._logger.warning("Empty aircraft list")
+            return
+
+        lod = len(data)
+        i = 1
+        for craft in data:
+            i += 1
+            if not isinstance(craft, dict):
+                self._logger.warning("Aircraft list item was not a Python `dict`.")
+                continue
+
+            icao: str = craft.get("hex", "")
+            if icao:
+                icao = icao.strip().upper()
+            else:
+                continue
+
+            if "~" in icao and not self.config.getboolean("INCLUDE_TISB"):
+                continue
+
+            known_craft: dict = aircot.get_known_craft(self.known_craft_db, icao, "HEX")
+
+            # Skip if we're using known_craft CSV and this Craft isn't found:
+            if (
+                self.known_craft_db
+                and not known_craft
+                and not self.config.getboolean("INCLUDE_ALL_CRAFT")
+            ):
+                continue
+
+            event: Union[str, None] = adsbcot.adsb_to_cot(
+                craft, self.config, known_craft
+            )
+
+            if not event:
+                self._logger.debug("Empty COT Event for craft=%s", craft)
+                continue
+
+            self._logger.debug("Handling %s/%s ICAO: %s", i, lod, icao)
+            await self.put_queue(event)
+
+    async def get_dump1090_feed(self, url: str):
+        """Poll the dump1090 JSON API and passes data to data handler."""
+        async with self.session.get(url) as resp:
+            if resp.status != 200:
+                response_content = await resp.text()
+                self._logger.error("Received HTTP Status %s for %s", resp.status, url)
+                self._logger.error(response_content)
+                return
+
+            json_resp = await resp.json()
+            if json_resp is None:
+                return
+
+            data = json_resp.get("aircraft")
+            if data is None:
+                return
+
+            self._logger.info("Retrieved %s aircraft messages.", str(len(data) or "No"))
+            await self.handle_data(data)
+
+    async def run(self, number_of_iterations=-1):
+        """Run this Thread, Reads from Pollers."""
+        url: str = self.config.get("DUMP1090_URL", "")
+        if not url:
+            raise Exception("Please specify a DUMP1090_URL.")
+
+        self._logger.info("Running %s", self.__class__)
+
+        known_craft: str = self.config.get("KNOWN_CRAFT", "")
+        poll_interval: str = self.config.get(
+            "POLL_INTERVAL", adsbcot.DEFAULT_POLL_INTERVAL
+        )
+
+        if known_craft:
+            self._logger.info("Using KNOWN_CRAFT: %s", known_craft)
+            self.known_craft_db = aircot.read_known_craft(known_craft)
+
+        async with aiohttp.ClientSession() as self.session:
+            while 1:
+                self._logger.info(
+                    "%s polling every %ss: %s", self.__class__, poll_interval, url
+                )
+                await self.get_dump1090_feed(url)
+                await asyncio.sleep(int(poll_interval))
+
+
+class ADSBNetWorker(ADSBWorker):
+    """Read ADS-B Data from network, renders to COT, and puts on queue."""
+
+    def __init__(
+        self, queue, net_queue, config, data_type
+    ):  # NOQA pylint: disable=too-many-arguments
+        """Initialize this class."""
+        super().__init__(queue, config)
+        self.net_queue = net_queue
+        self.config = config
+        self.data_type = data_type
+
+        self.local_buffer_adsb_msg = []
+        self.local_buffer_adsb_ts = []
+        self.local_buffer_commb_msg = []
+        self.local_buffer_commb_ts = []
+
+    def _reset_local_buffer(self):
+        """Reset Socket Buffers."""
+        self.local_buffer_adsb_msg = []
+        self.local_buffer_adsb_ts = []
+        self.local_buffer_commb_msg = []
+        self.local_buffer_commb_ts = []
+
+    async def run(
+        self, number_of_iterations=-1
+    ):  # NOQA pylint: disable=too-many-locals, too-many-branches
+        """Run the main process loop."""
+        self._logger.info(
+            "Running %s for data_type: %s", self.__class__, self.data_type
+        )
+
+        self._reset_local_buffer()
+
+        decoder = pyModeS.streamer.decode.Decode()
+        net_client = pyModeS.streamer.source.NetSource("x", 1, self.data_type)
+
+        while 1:
+            messages = []
+            received = await self.net_queue.get()
+            if not received:
+                continue
+
+            net_client.buffer.extend(received)
+            if "beast" in self.data_type:
+                messages = net_client.read_beast_buffer()
+            elif "raw" in self.data_type:
+                messages = net_client.read_raw_buffer()
+            elif "skysense" in self.data_type:
+                messages = net_client.read_skysense_buffer()
+
+            self._logger.debug("Received %s messages", len(messages))
+
+            if not messages:
+                continue
+
+            for msg, t_msg in messages:
+                if len(msg) != 28:  # wrong data length
+                    continue
+
+                dl_fmt = pms.df(msg)
+
+                if dl_fmt != 17:  # not ADSB
+                    continue
+
+                if pms.crc(msg) != 0:  # CRC fail
+                    continue
+
+                # icao = pms.adsb.icao(msg)
+                # typecode = pms.adsb.typecode(msg)
+
+                if dl_fmt in (17, 18):
+                    self.local_buffer_adsb_msg.append(msg)
+                    self.local_buffer_adsb_ts.append(t_msg)
+                elif dl_fmt in (20, 21):
+                    self.local_buffer_commb_msg.append(msg)
+                    self.local_buffer_commb_ts.append(t_msg)
+                else:
+                    continue
+
+            if len(self.local_buffer_adsb_msg) > 1:
+                decoder.process_raw(
+                    self.local_buffer_adsb_ts,
+                    self.local_buffer_adsb_msg,
+                    self.local_buffer_commb_ts,
+                    self.local_buffer_commb_msg,
+                )
+                self._reset_local_buffer()
+
+            acs = decoder.get_aircraft()
+            for key, val in acs.items():
+                _data: dict = {
+                    "hex": key,
+                    "lat": val.get("lat"),
+                    "lon": val.get("lon"),
+                    "flight": val.get("call", key).replace("_", ""),
+                    "alt_geom": val.get("alt"),
+                    "gs": val.get("gs"),
+                    "reg": val.get("r"),
+                    "trk": val.get("track", val.get("trk")),
+                }
+                if all(_data):
+                    await self.handle_data([_data])
+
+
+class ADSBNetReceiver(pytak.QueueWorker):  # pylint: disable=too-few-public-methods
+    """Read ADS-B Data from network and puts on queue."""
+
+    def __init__(self, queue, config, data_type):
+        """Initialize this class."""
+        super().__init__(queue, config)
+        self.data_type: str = data_type
+
+    async def run(self, number_of_iterations=-1):
+        """Run the main process loop."""
+        url: ParseResult = urlparse(self.config.get("DUMP1090_URL"))
+
+        self._logger.info("Running %s for %s", self.__class__, url.geturl())
+
+        if ":" in url.netloc:
+            host, port = url.netloc.split(":")
+        else:
+            host = url.netloc
+            if self.data_type == "raw":
+                port = adsbcot.DEFAULT_DUMP1090_TCP_RAW_PORT
+            elif self.data_type == "beast":
+                port = adsbcot.DEFAULT_DUMP1090_TCP_BEAST_PORT
+            else:
+                raise Exception("Invalid data_type='%s'" % self.data_type)
+
+        self._logger.debug("host=%s port=%s", host, port)
+
+        reader, _ = await asyncio.open_connection(host, port)
+
+        if self.data_type == "raw":
+            while 1:
+                received = await reader.readline()
+                self.queue.put_nowait(received)
+        elif self.data_type == "beast":
+            while 1:
+                received = await reader.read(4096)
+                self.queue.put_nowait(received)
---
+++ packages/adsbcot/adsbcot-5.1.2-py3-none-any.whl/adsbcot/functions.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2023 Greg Albrecht <oss@undef.net>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author:: Greg Albrecht W2GMD <oss@undef.net>
+#
+
+"""ADSBCOT Functions."""
+
+import asyncio
+import warnings
+import xml.etree.ElementTree as ET
+
+from configparser import SectionProxy
+from typing import Union, Set
+from urllib.parse import ParseResult, urlparse
+
+import aircot
+import pytak
+import adsbcot
+
+
+__author__ = "Greg Albrecht W2GMD <oss@undef.net>"
+__copyright__ = "Copyright 2023 Greg Albrecht"
+__license__ = "Apache License, Version 2.0"
+
+
+APP_NAME = "adsbcot"
+
+# We won't use pyModeS if it isn't installed:
+WITH_PYMODES = False
+try:
+    import pyModeS  # NOQA pylint: disable=unused-import
+
+    WITH_PYMODES = True
+except ImportError:
+    pass
+
+
+def create_tasks(
+    config: SectionProxy, clitool: pytak.CLITool
+) -> Set[pytak.Worker,]:
+    """Create specific coroutine task set for this application.
+
+    Parameters
+    ----------
+    config : `SectionProxy`
+        Configuration options & values.
+    clitool : `pytak.CLITool`
+        A PyTAK Worker class instance.
+
+    Returns
+    -------
+    `set`
+        Set of PyTAK Worker classes for this application.
+    """
+    tasks = set()
+
+    _dump1090_url: str = config.get("DUMP1090_URL", adsbcot.DEFAULT_DUMP1090_URL)
+    config.setdefault("DUMP1090_URL", _dump1090_url)
+    _dump1090_url = config.get("DUMP1090_URL", "")
+
+    if "://" not in _dump1090_url:
+        warnings.warn(f"Invalid DUMP1090_URL: '{_dump1090_url}'", SyntaxWarning)
+        raise Exception(
+            "Please specify DUMP1090_URL with full URL, including '://', for "
+            "example: tcp+beast://example.com:30005"
+        )
+
+    # Gateway code:
+    dump1090_url: ParseResult = urlparse(config.get("DUMP1090_URL"))
+
+    # ADS-B Workers (receivers):
+    if "http" in dump1090_url.scheme:
+        tasks.add(adsbcot.ADSBWorker(clitool.tx_queue, config))
+    elif "tcp" in dump1090_url.scheme:
+        if not WITH_PYMODES:
+            warnings.warn(
+                (f"Please reinstall {APP_NAME} with pyModeS support:"
+                 f"$ python3 -m pip install {APP_NAME}[with_pymodes]"), 
+                 ImportWarning)
+            raise Exception
+
+        net_queue: asyncio.Queue = asyncio.Queue()
+
+        if "+" in dump1090_url.scheme:
+            _, data_type = dump1090_url.scheme.split("+")
+        else:
+            data_type = "raw"
+
+        tasks.add(adsbcot.ADSBNetReceiver(net_queue, config, data_type))
+
+        tasks.add(adsbcot.ADSBNetWorker(clitool.tx_queue, net_queue, config, data_type))
+
+    return tasks
+
+
+def adsb_to_cot_xml(  # NOQA pylint: disable=too-many-locals,too-many-branches,too-many-statements
+    craft: dict,
+    config: Union[SectionProxy, None] = None,
+    known_craft: Union[dict, None] = None,
+) -> Union[ET.Element, None]:
+    """Serialize a Dump1090 ADS-B aircraft object as Cursor on Target XML.
+
+    Parameters
+    ----------
+    craft : `dict`
+        Key/Value data struct of decoded ADS-B aircraft data.
+    config : `configparser.SectionProxy`
+        Configuration options and values.
+        Uses config options: UID_KEY, COT_STALE, COT_HOST_ID
+
+    Returns
+    -------
+    `xml.etree.ElementTree.Element`
+        Cursor-On-Target XML ElementTree object.
+    """
+    lat = craft.get("lat")
+    lon = craft.get("lon")
+
+    if lat is None or lon is None:
+        return None
+
+    known_craft: dict = known_craft or {}
+    config: dict = config or {}
+    remarks_fields = []
+
+    uid_key = config.get("UID_KEY", "ICAO")
+    cot_stale = int(config.get("COT_STALE", pytak.DEFAULT_COT_STALE))
+    cot_host_id = config.get("COT_HOST_ID", pytak.DEFAULT_HOST_ID)
+
+    aircotx = ET.Element("_aircot_")
+    aircotx.set("cot_host_id", cot_host_id)
+
+    icao_hex = craft.get("hex", "")
+    reg = craft.get("reg", "")
+    flight = craft.get("flight", "")
+    cat = craft.get("category")
+    squawk = craft.get("squawk")
+
+    if flight:
+        flight = flight.strip().upper()
+        remarks_fields.append(flight)
+        aircotx.set("flight", flight)
+
+    if reg:
+        reg = reg.strip().upper()
+        remarks_fields.append(reg)
+        aircotx.set("reg", reg)
+
+    if squawk:
+        squawk = squawk.strip().upper()
+        remarks_fields.append(f"Squawk: {squawk}")
+        aircotx.set("squawk", squawk)
+
+    if icao_hex:
+        icao_hex = icao_hex.strip().upper()
+        remarks_fields.append(icao_hex)
+        aircotx.set("icao", icao_hex)
+
+    if cat:
+        cat = cat.strip().upper()
+        remarks_fields.append(f"Cat.: {cat}")
+        aircotx.set("cat", cat)
+
+    if "REG" in uid_key and reg:
+        cot_uid = f"REG-{reg}"
+    elif "ICAO" in uid_key and icao_hex:
+        cot_uid = f"ICAO-{icao_hex}"
+    if "FLIGHT" in uid_key and flight:
+        cot_uid = f"FLIGHT-{flight}"
+    elif icao_hex:
+        cot_uid = f"ICAO-{icao_hex}"
+    elif flight:
+        cot_uid = f"FLIGHT-{flight}"
+    else:
+        return None
+
+    if flight:
+        callsign = flight
+    elif reg:
+        callsign = reg
+    else:
+        callsign = icao_hex
+
+    _, callsign = aircot.set_name_callsign(icao_hex, reg, None, flight, known_craft)
+    cat = aircot.set_category(cat, known_craft)
+    cot_type = aircot.set_cot_type(icao_hex, cat, flight, known_craft)
+
+    point: ET.Element = ET.Element("point")
+    point.set("lat", str(lat))
+    point.set("lon", str(lon))
+    point.set("ce", str(craft.get("nac_p", "9999999.0")))
+    point.set("le", str(craft.get("nac_v", "9999999.0")))
+
+    # alt_geom: geometric (GNSS / INS) altitude in feet referenced to the
+    #           WGS84 ellipsoid
+    alt_geom = int(craft.get("alt_geom", 0))
+    if alt_geom:
+        point.set("hae", str(alt_geom * 0.3048))
+    else:
+        point.set("hae", "9999999.0")
+
+    uid: ET.Element = ET.Element("UID")
+    uid.set("Droid", cot_uid)
+
+    contact: ET.Element = ET.Element("contact")
+    contact.set("callsign", callsign)
+
+    track: ET.Element = ET.Element("track")
+    track.set("course", str(craft.get("trk", craft.get("track", "9999999.0"))))
+
+    # gs: ground speed in knots
+    gnds = int(craft.get("gs", 0))
+    if gnds:
+        track.set("speed", str(gnds * 0.514444))
+    else:
+        track.set("speed", "9999999.0")
+
+    detail = ET.Element("detail")
+    detail.set("uid", cot_uid)
+    detail.append(uid)
+    detail.append(contact)
+    detail.append(track)
+
+    remarks = ET.Element("remarks")
+
+    remarks_fields.append(f"{cot_host_id}")
+
+    _remarks = " ".join(list(filter(None, remarks_fields)))
+
+    remarks.text = _remarks
+    detail.append(remarks)
+
+    root = ET.Element("event")
+    root.set("version", "2.0")
+    root.set("type", cot_type)
+    root.set("uid", cot_uid)
+    root.set("how", "m-g")
+    root.set("time", pytak.cot_time())
+    root.set("start", pytak.cot_time())
+    root.set("stale", pytak.cot_time(cot_stale))
+
+    root.append(point)
+    root.append(detail)
+    root.append(aircotx)
+
+    return root
+
+
+def adsb_to_cot(
+    craft: dict,
+    config: Union[SectionProxy, None] = None,
+    known_craft: Union[dict, None] = None,
+) -> Union[bytes, None]:
+    """Return CoT XML object as an XML string."""
+    cot: Union[ET.Element, None] = adsb_to_cot_xml(craft, config, known_craft)
+    return (
+        b"\n".join([pytak.DEFAULT_XML_DECLARATION, ET.tostring(cot)]) if cot else None
+    )
