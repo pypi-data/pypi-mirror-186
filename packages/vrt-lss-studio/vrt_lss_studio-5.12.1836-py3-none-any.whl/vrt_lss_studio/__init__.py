7fd3e74a52790f7e25debbc5a38ad6466eb7a94b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:06 +0000

    Add package vrt-lss-studio/5.12.1836/vrt_lss_studio-5.12.1836-py3-none-any.whl

---
+++ packages/vrt-lss-studio/vrt_lss_studio-5.12.1836-py3-none-any.whl/vrt_lss_studio/__init__.py
@@ -0,0 +1,26 @@
+# flake8: noqa
+
+"""
+    VRt.Studio [ST]
+
+    The version of the OpenAPI document: 5.12.1836
+    Contact: servicedesk@veeroute.com
+    Auto Generated
+"""
+
+
+__version__ = "5.12.1836"
+
+# import ApiClient
+from vrt_lss_studio.api_client import ApiClient
+
+# import Configuration
+from vrt_lss_studio.configuration import Configuration
+
+# import exceptions
+from vrt_lss_studio.exceptions import OpenApiException
+from vrt_lss_studio.exceptions import ApiAttributeError
+from vrt_lss_studio.exceptions import ApiTypeError
+from vrt_lss_studio.exceptions import ApiValueError
+from vrt_lss_studio.exceptions import ApiKeyError
+from vrt_lss_studio.exceptions import ApiException
