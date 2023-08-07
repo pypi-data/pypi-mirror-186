dd9e5e27e31436a8c9f1eac10efc8689b6636950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:45:21 +0000

    Add package DonatelloPyzza/1.4.13/donatellopyzza-1.4.13.tar.gz

---
+++ packages/DonatelloPyzza/donatellopyzza-1.4.13.tar.gz/donatellopyzza-1.4.13/donatellopyzza/agent.py
@@ -0,0 +1,58 @@
+import os, sys, inspect, pygame
+from .originator import *
+from .careTaker import *
+from .orientation import *
+
+
+cmd_subfolder_grid = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"grid")))
+if cmd_subfolder_grid not in sys.path:
+    sys.path.insert(0, cmd_subfolder_grid)
+
+from .test import *
+
+from random import randint
+import pkg_resources
+
+class Agent:
+    def __init__(self, name, begin, end, color):
+        self.test = Test()
+        self._name = name
+        self.orientation = Orientation.NORTH
+        self.num = id(self)
+        self.begin = begin
+        self.end = end
+        self.color = color
+        self.originator = Originator()
+        self.caretaker = CareTaker()
+        self.position = None
+        path = pkg_resources.resource_filename(__name__, "data/images/turtle_small.png")
+        self.turtle = pygame.image.load(path)
+        self.angle = 0
+
+    def setCurrentPosition(self, position):
+        self.position = position
+
+    def savePosition(self, square):
+        self.originator.setState(square)
+        self.caretaker.add(self.originator.saveStateToMemento())
+
+    def turn(self, orientation):
+        diff = abs(self.orientation.value - orientation)
+        if diff < 2 or diff == 3:
+            self.orientation = Orientation(orientation)
+
+    def goBack(self):
+        self.originator.getStateFromMemento(self.caretaker.getLast())
+
+    def getLastPosition(self):
+        return self.caretaker.getLast().getState()
+
+    def getCurrentPosition(self):
+        return self.position
+
+    def draw(self, screen, xy):
+        cste_angle = 90
+        new_angle = cste_angle * self.orientation.value
+        self.turtle = pygame.transform.rotate(self.turtle, new_angle - self.angle)
+        self.angle = new_angle
+        screen.blit(self.turtle, xy)
