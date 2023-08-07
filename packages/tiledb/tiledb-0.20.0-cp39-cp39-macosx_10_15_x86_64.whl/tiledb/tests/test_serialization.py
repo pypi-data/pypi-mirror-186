ae4f9e1d1bc60ca90bf4d8063907fda9deb87533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:04:07 +0000

    Add package tiledb/0.20.0/tiledb-0.20.0-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/tiledb/tiledb-0.20.0-cp39-cp39-macosx_10_15_x86_64.whl/tiledb/tests/test_serialization.py
@@ -0,0 +1,30 @@
+import pytest
+
+ser_test = pytest.importorskip(
+    "tiledb.main.test_serialization", reason="Serialization not enabled."
+)
+
+import numpy as np
+from numpy.testing import assert_array_equal
+
+import tiledb
+from tiledb.tests.common import DiskTestCase
+
+
+class SerializationTest(DiskTestCase):
+    def test_query_deserialization(self):
+        path = self.path("test_query_deserialization")
+        dom = tiledb.Domain(tiledb.Dim(domain=(1, 10), dtype=np.uint32))
+        attrs = [tiledb.Attr(dtype=np.int64)]
+        schema = tiledb.ArraySchema(domain=dom, attrs=attrs, sparse=True)
+        tiledb.Array.create(path, schema)
+
+        data = np.random.randint(-5, 5, 10)
+
+        with tiledb.open(path, "w") as A:
+            A[np.arange(1, 11)] = data
+
+        with tiledb.open(path, "r") as A:
+            ctx = tiledb.default_ctx()
+            ser_qry = ser_test.create_serialized_test_query(ctx, A)
+            assert_array_equal(A.query()[3:8][""], A.set_query(ser_qry)[""])
