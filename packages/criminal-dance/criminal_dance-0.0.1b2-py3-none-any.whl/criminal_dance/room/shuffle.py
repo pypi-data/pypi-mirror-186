addfd29fa003b0ee449b1fa2596238e1adadbf57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:04:58 +0000

    Add package criminal-dance/0.0.1b2/criminal_dance-0.0.1b2-py3-none-any.whl

---
+++ packages/criminal-dance/criminal_dance-0.0.1b2-py3-none-any.whl/criminal_dance/room/shuffle.py
@@ -0,0 +1,18 @@
+from random import randint
+
+
+def _shuffle(cards: list[str]):
+    n = len(cards)
+    for i in range(n):
+        j = randint(i, n-1)
+        cards[i], cards[j] = cards[j], cards[i]
+    return cards
+
+
+def shuffle(cards: list[str]):
+    # ---- 临时调试
+    return cards
+    
+    for i in range(3):
+        _shuffle(cards)
+    return cards
