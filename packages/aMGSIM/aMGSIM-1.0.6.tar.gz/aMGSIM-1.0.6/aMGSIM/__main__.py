4fe42ed810b2dbc3264ee39c52242d2fa2c1f4e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:05:01 +0000

    Add package aMGSIM/1.0.6/aMGSIM-1.0.6.tar.gz

---
+++ packages/aMGSIM/aMGSIM-1.0.6.tar.gz/aMGSIM-1.0.6/aMGSIM/__main__.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+# import
+import sys
+import logging
+
+from docopt import docopt
+
+# import Utils
+from MGSIM.Commands import Communities
+
+from aMGSIM.Commands.Estimate import estimate
+from aMGSIM.Commands.AncientGenomes import get_ancient_genomes
+from aMGSIM.Commands.AncientReads import get_ancient_reads
+from aMGSIM.Commands.ProteinAnalysis import do_proteins_analysis
+from aMGSIM.Commands.MultiCov import generate_multi_cov
+
+from aMGSIM.library.cli import get_arguments
+
+
+def main():
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(levelname)s ::: %(asctime)s ::: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    if len(sys.argv) > 1 and sys.argv[1] == "communities":
+        # args = sys.argv[2:]
+        # print(args)
+        docs = Communities.__doc__
+        # print(docs)
+        args = docopt(docs, argv=sys.argv[2:], options_first=True)
+        Communities.main(args)
+    else:
+        args = get_arguments()
+        logging.getLogger("my_logger").setLevel(
+            logging.DEBUG if args.debug else logging.INFO
+        )
+        if args.action == "estimate":
+            estimate(args)
+        elif args.action == "multicov":
+            generate_multi_cov(args)
+        elif args.action == "ancient-genomes":
+            get_ancient_genomes(args)
+        elif args.action == "ancient-reads":
+            get_ancient_reads(args)
+        elif args.action == "protein-analysis":
+            do_proteins_analysis(args)
+
+
+if __name__ == "__main__":
+    main()
