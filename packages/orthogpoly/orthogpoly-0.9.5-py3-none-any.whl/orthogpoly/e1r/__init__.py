b0eb9d6e065768531471e08960bae51bf57723aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:45:43 +0000

    Add package orthogpoly/0.9.5/orthogpoly-0.9.5-py3-none-any.whl

---
+++ packages/orthogpoly/orthogpoly-0.9.5-py3-none-any.whl/orthogpoly/e1r/__init__.py
@@ -0,0 +1,5 @@
+from .main import Eval, RecurrenceCoefficients
+# from .tools import plot, savefig, show
+
+__all__ = ["Eval", "RecurrenceCoefficients", "show", "plot", "savefig"]
+__all__ = ["Eval", "RecurrenceCoefficients"]
