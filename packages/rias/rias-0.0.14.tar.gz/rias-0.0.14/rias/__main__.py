f9d9d5180fc894fd6372985678baadf0b45f67d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:11 +0000

    Add package rias/0.0.14/rias-0.0.14.tar.gz

---
+++ packages/rias/rias-0.0.14.tar.gz/rias-0.0.14/rias/__main__.py
@@ -0,0 +1,17 @@
+"""
+ Start Code
+"""
+
+import sys
+from rias.helper import loader
+
+def main() -> None:
+    """
+    Simple Test Function
+    """
+    # Simple Test
+    loader(sys.argv[1])
+
+
+if __name__ == "__main__":
+    main()
