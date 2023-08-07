fd583be516f956e553eec86564a753238e0f01e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:45:15 +0000

    Add package libPy2023/1.2023.1.19.1826/libPy2023-1.2023.1.19.1826-py3-none-any.whl

---
+++ packages/libPy2023/libPy2023-1.2023.1.19.1826-py3-none-any.whl/libPy2023/__init__.py
@@ -0,0 +1,11 @@
+__version__="1.2023.1.19.1826"
+__author__ ="Ludwig Corales M. dexsys@gmail.com"
+__all__=['ClearCrt','CleanText','Color','logs','ProgressBar','Table',
+         '_PRUEBA','_TIPOEJECUCION','PrintError','Cronometro','ImportarHojasXLSX',
+         'EnviarCorreoAdjunto','emailSender','Box','toVars','GenerateQuery']
+
+#,'','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','']from .Funciones import leer_pdf
+
+from .libPy2023 import ClearCrt,CleanText,Color,logs,ProgressBar,Table,\
+         _PRUEBA,_TIPOEJECUCION,PrintError,Cronometro,ImportarHojasXLSX,\
+         EnviarCorreoAdjunto,emailSender,Box,toVars,GenerateQuery
