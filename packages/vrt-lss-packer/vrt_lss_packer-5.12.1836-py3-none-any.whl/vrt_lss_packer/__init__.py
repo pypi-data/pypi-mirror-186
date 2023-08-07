9950dc3b41661905cda87daac939d4b6b678dad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:05 +0000

    Add package vrt-lss-packer/5.12.1836/vrt_lss_packer-5.12.1836-py3-none-any.whl

---
+++ packages/vrt-lss-packer/vrt_lss_packer-5.12.1836-py3-none-any.whl/vrt_lss_packer/__init__.py
@@ -0,0 +1,26 @@
+# flake8: noqa
+
+"""
+    VRt.Packer [PC]
+
+    The version of the OpenAPI document: 5.12.1836
+    Contact: servicedesk@veeroute.com
+    Auto Generated
+"""
+
+
+__version__ = "5.12.1836"
+
+# import ApiClient
+from vrt_lss_packer.api_client import ApiClient
+
+# import Configuration
+from vrt_lss_packer.configuration import Configuration
+
+# import exceptions
+from vrt_lss_packer.exceptions import OpenApiException
+from vrt_lss_packer.exceptions import ApiAttributeError
+from vrt_lss_packer.exceptions import ApiTypeError
+from vrt_lss_packer.exceptions import ApiValueError
+from vrt_lss_packer.exceptions import ApiKeyError
+from vrt_lss_packer.exceptions import ApiException
