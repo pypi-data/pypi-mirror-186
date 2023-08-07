ccb9599da6939e9ed0f6b87a97253b25fd6b58ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:02 +0000

    Add package pydbk/0.1.1/pydbk-0.1.1.tar.gz

---
+++ packages/pydbk/pydbk-0.1.1.tar.gz/pydbk-0.1.1/README.md
@@ -0,0 +1,13 @@
+![](https://img.shields.io/github/actions/workflow/status/BenPortner/pydbk/install-lint-test.yml?label=tests) ![](https://img.shields.io/github/actions/workflow/status/BenPortner/pydbk/release-pypi.yml?label=release) ![](https://img.shields.io/github/license/BenPortner/pydbk) ![](https://img.shields.io/pypi/pyversions/pydbk)
+
+# Pydbk: A Python tool to extract .dbk archives
+
+Pydbk consists of a python module `pydbk.py` and a command line tool `pydbk_cli.py`.
+
+## Usage
+
+For help type:
+
+```
+python pydbk_cli.py -h
+```
---
+++ packages/pydbk/pydbk-0.1.1.tar.gz/pydbk-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name="pydbk",
+    version="0.1.1",
+    description="A Python tool to extract .dbk archives.",
+    long_description=open('README.md', encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Benjamin W. Portner",
+    author_email="benwportner@gmail.com",
+    url="https://github.com/BenPortner/pydbk",
+    packages=["pydbk"],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers"
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    license="License :: OSI Approved :: GNU Affero General Public License v3",
+)
