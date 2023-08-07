46a366bcd967775ff73d59e5a9596859b6cce5e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:05:02 +0000

    Add package finary/0.1.1/finary-0.1.1.tar.gz

---
+++ packages/finary/finary-0.1.1.tar.gz/finary-0.1.1/README.md
@@ -0,0 +1,18 @@
+# Finary
+
+## À propos
+
+`Finary` se compose de différents sous-modules ayant tous pour objectif d'augmenter la productivité d'un actuaire dans ses tâches du quotidien.
+
+
+
+## Les projets
+
+Voici une liste non-exhaustive des idées à dates :
+
+* `math` : calculs basiques d'actuariat (tx équivalent, valeur actuelle...)
+* `excel` : intéractions avec un fichier Excel améliorés (à partir des noms)
+* `model` : modèles financiers (black & Scholes, pricer option...)
+* `alm` : modèle ALM (gros projet)
+* `provisions` : méthodes basiques de provisionnement (chain ladder...)
+
---
+++ packages/finary/finary-0.1.1.tar.gz/finary-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['finary']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.24.1,<2.0.0', 'pandas>=1.5.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 'finary',
+    'version': '0.1.1',
+    'description': "Finary se compose de différents sous-modules ayant tous pour objectif d'augmenter la productivité d'un actuaire dans ses tâches du quotidien.",
+    'long_description': "# Finary\n\n## À propos\n\n`Finary` se compose de différents sous-modules ayant tous pour objectif d'augmenter la productivité d'un actuaire dans ses tâches du quotidien.\n\n\n\n## Les projets\n\nVoici une liste non-exhaustive des idées à dates :\n\n* `math` : calculs basiques d'actuariat (tx équivalent, valeur actuelle...)\n* `excel` : intéractions avec un fichier Excel améliorés (à partir des noms)\n* `model` : modèles financiers (black & Scholes, pricer option...)\n* `alm` : modèle ALM (gros projet)\n* `provisions` : méthodes basiques de provisionnement (chain ladder...)\n\n",
+    'author': 'Mathieu Clavier',
+    'author_email': 'mathieu.clavier@outlook.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
