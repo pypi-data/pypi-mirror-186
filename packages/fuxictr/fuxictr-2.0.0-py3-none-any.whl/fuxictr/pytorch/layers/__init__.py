993c9b4490dca41b4328762594c759ce2b0bba5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:05:04 +0000

    Add package fuxictr/2.0.0/fuxictr-2.0.0-py3-none-any.whl

---
+++ packages/fuxictr/fuxictr-2.0.0-py3-none-any.whl/fuxictr/pytorch/layers/__init__.py
@@ -0,0 +1,10 @@
+from .pooling import *
+from .embeddings import *
+from .activations import *
+from .shallow import *
+from .mlp_block import *
+from .interactions import *
+from .attentions import *
+
+
+
